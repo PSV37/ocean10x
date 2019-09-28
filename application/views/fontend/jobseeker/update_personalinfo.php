0070e1d223c1770396d9f4f0760e64e410e098d7
unknown <santu@edunetsol.com>
2019-09-28 12:22:30 +0530

    Personal information validation

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -106,7 +106,8 @@
               <div class="col-md-8 col-md-offset-2">
               <div class="input-group">
                   <span class="input-group-addon"><label class="control-label col-sm-3" for="email">Father Name:</label></span>
-                  <input type="text" name="father_name" class="form-control" id="father_name" pattern="[A-Za-z]" placeholder="Enter Father Name"
+                  <input type="text" name="father_name" class="form-control" id="father_name" onkeypress="return (event.charCode > 64 && 
+	event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" placeholder="Enter Father Name"
                    value="<?php
                          if (!empty($js_personal_info->father_name)) {
                            echo $js_personal_info->father_name;
@@ -116,7 +117,8 @@
               
               <div class="input-group">
                   <span class="input-group-addon"><label class="control-label col-sm-3" for="email">Mother Name:</label></span>
-                  <input type="text" name="mother_name" class="form-control" id="mother_name" placeholder="Enter Mother Title"
+                  <input type="text" name="mother_name" class="form-control" id="mother_name" onkeypress="return (event.charCode > 64 && 
+	event.charCode < 91) || (event.charCode > 96 && event.charCode < 123)" placeholder="Enter Mother Title"
                    value="<?php
                          if (!empty($js_personal_info->mother_name)) {
                            echo $js_personal_info->mother_name;
