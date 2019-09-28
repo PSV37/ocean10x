fcd7e39a71bdd296983fb33dce4d7be390fe761c
unknown <santu@edunetsol.com>
2019-09-28 15:42:44 +0530

    (Job Post Edit Section)Job deadline day - job deadline month

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -113,6 +113,8 @@ class Employer extends MY_Employer_Controller
                         "job_deadline"       => (empty($job_deadline)) ? null : date('Y-m-d', strtotime(str_replace('/', '-', $job_deadline))),
                         'preferred_age'      => $this->input->post('preferred_age_from'),
 						'preferred_age_to'      => $this->input->post('preferred_age_to'),
+						'job_deadline_day'      => $this->input->post('job_deadline_day'),
+						'job_deadline_month'      => $this->input->post('job_deadline_month'),
                         'working_hours'      => $this->input->post('working_hours'),
                     );
                     if (empty($job_post_id)) {
