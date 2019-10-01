1d2866cbd66063f3e90fc3296c3330e5d3f79358
unknown <santu@edunetsol.com>
2019-10-01 15:44:10 +0530

    country state city joiningffgggh

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -24,7 +24,7 @@ class Job_seeker extends MY_Seeker_Controller
 						'city' => 'city.id = js_personal_info.city_id|INNER'
 	);
 	        $data["result"] = $this->Master_model->get_master_row("js_personal_info", $select = false, $where=false, $join);
-            $this->load->view('fontend/jobseeker/seeker_info', compact('data'));
+            $this->load->view('fontend/jobseeker/seeker_info',$data);
     }
 	
 
