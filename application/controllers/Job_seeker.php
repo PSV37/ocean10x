0deb67c96930ab212040b5649ad6ef7b689fa417
Santu Chatterjee <santu@edunetsol.com>
2019-10-02 15:50:05 +0530

    country code add Reference section

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -406,6 +406,7 @@ exit;*/
                 'org_name'      => $this->input->post('org_name'),
                 'designation'   => $this->input->post('designation'),
                 'email'         => $this->input->post('email'),
+				'country_code'        => $this->input->post('country_code'),
                 'mobile'        => $this->input->post('mobile'),
                 'relation'      => $this->input->post('relation'),
             );
