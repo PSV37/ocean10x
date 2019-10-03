626fa43ed9a35e073a284732824d89c3b48fc781
unknown <santu@edunetsol.com>
2019-10-03 14:30:38 +0530

    state city country

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -480,14 +480,14 @@ exit;*/
 		$city = $this->Master_model->getMaster('city',$where=false);
 		$country = $this->Master_model->getMaster('country',$where=false);
 		$state = $this->Master_model->getMaster('state',$where=false);
-		//$c_id = $this->input->get('id');
-		//$where = "job_apply_id='$c_id'";
+		$c_id = $this->input->get('id');
+		$where = "js_training_id='$c_id'";
 		$join = array(
 						'country' => 'country.country_id = js_training.country_id|INNER',
 						'state' => 'state.state_id = js_training.state_id|INNER',
 						'city' => 'city.id = js_training.city_id|INNER'
 			);
-		$result = $this->Master_model->get_master_row("js_training", $select = false, $where=false, $join);
+		$result = $this->Master_model->get_master_row("js_training", $select = false, $where, $join);
         $this->load->view('fontend/jobseeker/view_resume', compact('resume', 'edcuaiton_list', 'experinece_list', 'training_list', 'reference_list', 'country', 'state', 'city', 'result'));
     }
 
