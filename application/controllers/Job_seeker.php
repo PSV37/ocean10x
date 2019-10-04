a90ead7d12656a844cbf14ee9cec1ed7f3945356
unknown <santu@edunetsol.com>
2019-10-04 17:03:56 +0530

    attach photo resume

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -482,11 +482,12 @@ exit;*/
         $training_list   = $this->Job_training_model->training_list_by_id($jobseeker_id);
         $reference_list  = $this->Job_reference_model->reference_list_by_id($jobseeker_id);
 		$js_personal_info = $this->job_seeker_personal_model->personalinfo_list_by_id($jobseeker_id);
+		$job_seeker_photo = $this->Job_seeker_photo_model->photo_by_seeker($jobseeker_id);
 		$city = $this->Master_model->getMaster('city',$where=false);
 		$country = $this->Master_model->getMaster('country',$where=false);
 		$state = $this->Master_model->getMaster('state',$where=false);
         //echo $this->db->last_query();
-		$this->load->view('fontend/jobseeker/view_resume', compact('resume', 'edcuaiton_list', 'experinece_list', 'training_list', 'reference_list', 'js_personal_info', 'country', 'state', 'city'));
+		$this->load->view('fontend/jobseeker/view_resume', compact('resume', 'edcuaiton_list', 'experinece_list', 'training_list', 'reference_list', 'js_personal_info', 'job_seeker_photo', 'country', 'state', 'city'));
     
 	}
 
