bfdc44f7c584c069bb782d78929b1b70c78f2ee0
unknown <santu@edunetsol.com>
2019-10-04 15:55:28 +0530

    country state city joining

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -83,15 +83,15 @@
                                     </tr>
 									<tr>
                                       <td>City:</td>
-                                    <td><?php echo $js_personal_info->city_name; ?></td>
+                                    <td><?php echo $results['city_name']; ?></td>
                                     </tr>
 									<tr>
                                       <td>State:</td>
-                                    <td><?php echo $js_personal_info->state_name; ?></td>
+                                    <td><?php echo $results['state_name']; ?></td>
                                     </tr>
 									<tr>
-                                      <td>country:</td>
-                                    <td><?php echo $js_personal_info->country_name; ?></td>
+                                      <td>Country:</td>
+                                    <td><?php echo $results['country_name']; ?></td>
                                     </tr>
                                   </tbody>
                                 </table>
