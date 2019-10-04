ab9366564f09ad63b26a41552e05ac0f11a9430f
shyam <shyam@itdivine.in>
2019-10-04 11:37:43 +0530

    Skills Master

---
+++ application/controllers/admin/Skill_master.php
@@ -0,0 +1,82 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+
+
+class Skill_master extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /*** Dashboard ***/
+    public function index()
+    {   
+
+        $data['title'] = 'Add Skill';
+
+        $where_cn= "status=1";
+        $select = "skill_name, id";
+        $data['skills_data'] = $this->Master_model->getMaster('skill_master',$where_cn,$join = FALSE, $order = false, $field = false, $select,$limit=false,$start=false, $search=false);
+        
+        $this->load->view('admin/jobsetting/skill_master', $data);
+    }
+
+        public function save_industry($id = null){
+          
+            $user_id = $this->session->userdata('admin_user_id');
+            
+            $state_dt=array(
+                'skill_name' => $this->input->post('skill_name'),
+            );
+
+            if(empty($id)){
+                $state_dt['created_date']=date('Y-m-d H:i:s');
+                $state_dt['created_by']=$user_id;
+
+                $this->Master_model->master_insert($state_dt,'skill_master');
+               
+                redirect('admin/skill_master');
+            }
+            else {
+                $state_dt['updated_date']=date('Y-m-d H:i:s');
+                $state_dt['updated_by']=$user_id;
+
+                $where['id']=$id;
+                $this->Master_model->master_update($state_dt,'skill_master',$where);
+               
+                redirect('admin/skill_master');
+            }
+        }
+
+    public function delete_industry($id) {
+        
+        $state_status = array(
+            'status'=>0,
+        );
+        $where_del['id']=$id;
+        $this->Master_model->master_update($state_status,'skill_master',$where_del);
+        redirect('admin/skill_master');
+    }
+
+    public function edit_industry($id){
+        $data['title']="Skills Master Edit";
+
+        $where_st = "id='$id'";
+        $selectedit = "skill_name, id";
+        $data['edit_skill_info'] = $this->Master_model->getMaster('skill_master',$where_st,$join = FALSE, $order = false, $field = false, $selectedit,$limit=false,$start=false, $search=false);
+        
+        $where_cn= "status=1";
+        $select = "skill_name, id";
+        $data['skills_data'] = $this->Master_model->getMaster('skill_master',$where_cn,$join = FALSE, $order = false, $field = false, $select,$limit=false,$start=false, $search=false);
+        
+        $this->load->view('admin/jobsetting/skill_master',$data);
+    }
+
+
+
+}
---
+++ application/views/admin/jobsetting/skill_master.php
@@ -0,0 +1,119 @@
+<?php $this->load->view('admin/components/header'); ?>
+<body class="skin-blue" data-baseurl="<?php echo base_url(); ?>">
+    <div class="wrapper">
+        
+    <?php $this->load->view('admin/components/user_profile'); ?>
+       
+        <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+            <?php $this->load->view('admin/components/navigation'); ?>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
+
+        <div class="right-side">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+            </section>
+
+            <br/>
+            <div class="container-fluid">
+               <section class="content">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="box box-primary">
+                <div class="box-header box-header-background with-border">
+                    <div class="col-md-offset-3">
+                        <h3 class="box-title ">Skills Master</h3>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-background">
+                <!-- form start -->
+                <form role="form" enctype="multipart/form-data" action="<?php echo base_url(); ?>admin/skill_master/save_skill/<?php  if (!empty($edit_skill_info)) { foreach($edit_skill_info as $row)
+                        echo $row['id'];
+                      }
+                     ?>" method="post">
+
+                    <div class="row">
+
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+
+                            <div class="box-body">
+                            
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Skill Name <span class="required">*</span></label>
+                                      <input type="text" name="skill_name" class="form-control" value="<?php if (!empty($edit_skill_info)) echo $row['skill_name'];?>" placeholder='Skill Name' required>
+                                    </div>
+                                </div>
+                                <div class="panel-body"></div>
+                                <button type="submit" class="btn bg-navy" type="submit">Save Skill
+                                </button><br/><br/>
+                            </div>
+                            <!-- /.box-body -->
+
+                        </div>
+                    </div>
+
+                </form>
+                    </div>
+                <div class="box-footer">
+
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                <table class="table table-bordered table-striped" id="dataTables-example">
+                    <thead>
+                    <tr>
+                        <th class="active">SL</th>
+                        <th class="active">Skill Name</th>
+                        <th class="active col-sm-2">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $key = 1 ;?>
+                    <?php if (!empty($skills_data)): foreach ($skills_data as $st_row) : ?>
+                        <tr>
+                            <td><?php echo $key ?></td>
+                            <td><?php echo $st_row['skill_name'] ?></td>
+                            <td>
+                                <?php echo btn_edit('admin/skill_master/edit_skill/' . $st_row['id']); ?>
+                                <?php echo btn_delete('admin/skill_master/delete_skill/' . $st_row['id']); ?>
+                            </td>
+                        </tr>
+                    <?php
+                    $key++;
+                    endforeach;
+                    ?>
+                    <?php else : ?> 
+                        <td colspan="3">
+                            <strong>There is no record for display</strong>
+                        </td>
+                    <?php
+                    endif; ?>
+                    </tbody>
+                </table>
+
+                    </div>
+                </div>
+            </div>
+            <!-- /.box -->
+        </div>
+        <!--/.col end -->
+    </div>
+    <!-- /.row -->
+</section>
+</div>
+
+<br />
+
+
+</div><!-- /.right-side -->
+
+       
+<?php $this->load->view('admin/components/footer'); ?>
