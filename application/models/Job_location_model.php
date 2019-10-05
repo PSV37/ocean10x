d2648d59415b9b56bd2f2fab82564483ba4303f6
unknown <santu@edunetsol.com>
2019-10-05 17:25:14 +0530

    updated post job section

--- application/models/Job_location_model.php
+++ application/models/Job_location_model.php
@@ -8,9 +8,9 @@ if (!defined('BASEPATH')) {
 
 class Job_Location_Model extends MY_Model{
 
-    protected $_table_name = 'job_location';
-    protected $_primary_key = 'job_location_id';
-    protected $_order_by = "job_location_id desc";
+    protected $_table_name = 'country';
+    protected $_primary_key = 'country_id';
+    protected $_order_by = "country_id desc";
 
     public function __construct()
     {
@@ -23,18 +23,18 @@ class Job_Location_Model extends MY_Model{
         $output='';
         foreach($joblocationlist as $location)
         {
-            $selected=$location->job_location_id==$id?'selected="selected"':'';
-            $output.=sprintf('<option %s value="%s">%s</option> ',$selected,$location->job_location_id,$location->job_location_name);
+            $selected=$location->country_id==$id?'selected="selected"':'';
+            $output.=sprintf('<option %s value="%s">%s</option> ',$selected,$location->country_id,$location->country_name);
         }
         return $output;
     }
 
             public function get_location_name_by_id($id)
             {
-                $this->db->select("job_location_name");
+                $this->db->select("country_name");
                 $this->db->where($this->_primary_key,$id);
                 $result=$this->db->get($this->_table_name)->result();
-                return $result[0]->job_location_name;
+                return $result[0]->country_name;
             }
 
 
