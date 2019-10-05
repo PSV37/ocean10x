efb98e80f6411ea961894fadd2c0542d8ca26aa6
unknown <santu@edunetsol.com>
2019-10-05 14:43:27 +0530

    updated post job section

--- application/models/Job_posting_model.php
+++ application/models/Job_posting_model.php
@@ -77,10 +77,10 @@ order by job_post_id desc limit 10");
     
     public function get_random_jobs()
     {
-        $query = $this->db->query("select t1.*, t2.company_name, t2.company_logo, t2.company_slug,t3.job_location_name, t4.job_nature_name
+        $query = $this->db->query("select t1.*, t2.company_name, t2.company_logo, t2.company_slug,t3.country_name, t4.job_nature_name
 from job_posting t1
 inner join company_profile t2 on t1.company_profile_id=t2.company_profile_id
-inner join job_location t3 on t3.job_location_id=t1.job_location
+left join country t3 on t3.country_id=t1.job_location
 inner join job_nature t4 on t1.job_nature=t4.job_nature_id 
 where job_status=1
 order by RAND() limit 3");
@@ -109,10 +109,10 @@ AND t1.job_post_id='$jobid'");
 
  public function get_job_details_employer($jobid)
     {
-       $query = $this->db->query("select t1.*, t2.company_name, t2.company_logo, t2.company_slug,t3.job_location_name, t4.job_nature_name, t5.job_category_name, t6.job_level_name, t7.job_types_name, t8.education_level_name
+       $query = $this->db->query("select t1.*, t2.company_name, t2.company_logo, t2.company_slug,t3.country_name, t4.job_nature_name, t5.job_category_name, t6.job_level_name, t7.job_types_name, t8.education_level_name
 from job_posting t1
 left join company_profile t2 on t1.company_profile_id=t2.company_profile_id
-left join job_location t3 on t3.job_location_id=t1.job_location
+left join country t3 on t3.country_id=t1.job_location
 left join job_nature t4 on t1.job_nature=t4.job_nature_id 
 left join job_category t5 on t1.job_category=t5.job_category_id
 left join job_level t6 on t1.job_level=t6.job_level_id
