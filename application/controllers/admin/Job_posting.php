6041840b1929716aaeff56adcbd1a5f30a74cf7f
shyam <shyam@itdivine.in>
2019-10-07 16:26:46 +0530

    Create Job Changes

--- application/controllers/admin/Job_posting.php
+++ application/controllers/admin/Job_posting.php
@@ -144,7 +144,7 @@ function getSkillsByRole() {
 
     $sk = $data['skill_set'];
    // explode(',', string)
-    $where_sk= "id IN ($sk) AND status=1";
+    $where_sk= "id IN (".$data['skill_set'].") AND status=1";
     $select_sk = "skill_name ,id";
     $skills = $this->Master_model->getMaster('skill_master',$where_sk,$join = FALSE, $order = false, $field = false, $select_sk,$limit=false,$start=false, $search=false);
        echo $this->db->last_query();
