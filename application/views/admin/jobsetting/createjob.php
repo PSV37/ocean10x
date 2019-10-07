6906b0e74ddc6a779dc64d86a87e104d2db0f683
shyam <shyam@itdivine.in>
2019-10-07 17:53:06 +0530

    Create Job Changes

--- application/views/admin/jobsetting/createjob.php
+++ application/views/admin/jobsetting/createjob.php
@@ -337,6 +337,7 @@
                                       </div>
                                     </div>
                                   </div>
+                                  <div class="panel-body"></div>
                                   <div class="row">
                                     <div class="col-md-12 col-sm-12"> 
                                       <div class="formrow">   
@@ -345,6 +346,7 @@
                                       </div>
                                     </div>
                                   </div>
+                                  <div class="panel-body"></div>
                                   <div class="row">
                                     <div class="col-md-12 col-sm-12"> 
                                        	<div class="formrow">   
