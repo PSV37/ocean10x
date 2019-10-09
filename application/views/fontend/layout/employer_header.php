dacd5d330dea34ba78fd56cf79241198964e6eb5
shyam <shyam@itdivine.in>
2019-10-09 12:39:16 +0530

    Company Job Post changes labels and fields addition

--- application/views/fontend/layout/employer_header.php
+++ application/views/fontend/layout/employer_header.php
@@ -25,14 +25,7 @@
     <script type="text/javascript" src="<?php echo base_url(); ?>asset/ckeditor/ckeditor.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>asset/ckfinder/ckfinder.js"></script>
         
-    <style>
-      label {
-        display: inline-block;
-        max-width: 100%;
-        margin-bottom: 5px;
-        font-weight: bold;
-      }
-    </style>
+      
     <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> <![endif]-->
 </head>
 
