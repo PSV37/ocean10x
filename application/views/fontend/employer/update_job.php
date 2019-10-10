9f9e2a65445fb768f44858aa03b0d7bc8017bb18
unknown <santu@edunetsol.com>
2019-10-10 17:39:32 +0530

    updated postjob

--- application/views/fontend/employer/update_job.php
+++ application/views/fontend/employer/update_job.php
@@ -101,12 +101,8 @@
               <div class="row">
                 <div class="formrow">
                   <div class="col-md-12 col-sm-12"> 
-				  <div id="skills_result">
                     <label><b>Skill Set</b> <span class="required">*</span></label><br>
-					<?php foreach($skill_master as $skill_row) { ?>
-					  <input type='checkbox' name='skill_set[]' style='height:15px; width:20px;' id='skill_set' value="<?php echo $skill_row['id']; ?>"<?php if(!empty($job_info)) if($job_info->skills_required==$skill_row['id']) echo 'checked'; ?> > <?php echo $skill_row['skill_name'];?>
-					<?php } ?>
-					</div>
+                    <div id="skills_result">Please Select Job Role.</div>
                   </div>
                 </div>
               </div>
