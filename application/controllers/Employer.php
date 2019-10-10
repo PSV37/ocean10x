f91c260864aafc951bf2bc89a56e68b89338ded9
shyam <shyam@itdivine.in>
2019-10-10 15:39:20 +0530

    Forword tab

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -547,26 +547,6 @@ function getstate(){
     
     public function forword_job($job_id = null)
         {
-<<<<<<< HEAD
-            // if (!empty($job_id)) {
-            //     $company_id = $this->session->userdata('company_profile_id');
-            //     if ($this->job_posting_model->check_jobid_and_post_id($job_id, $company_id) == true) {
-            //         $data['job_info'] = $this->job_posting_model->get($job_id);
-            //         $data['city'] = $this->Master_model->getMaster('city',$where=false);
-            //         $data['country'] = $this->Master_model->getMaster('country',$where=false);
-            //         $data['state'] = $this->Master_model->getMaster('state',$where=false);
-                    $this->load->view('fontend/employer/forword_job');
-            //     } else {
-            //         echo "error";
-            //     }
-            // } else {
-            //     echo "Not Found";
-            // }
-
-        }
-
-}
-=======
             if (!empty($job_id)) {
                  $job_id; 
                  $company_id = $this->session->userdata('company_profile_id');
@@ -592,14 +572,13 @@ function getstate(){
              $employer_id = $this->session->userdata('company_profile_id');
                 if ($_POST) {
                     $employer_id  = $this->session->userdata('company_profile_id');
-                    // $job_deadline = strtolower($this->input->post('job_deadline'));
-                    // $job_post_id  = $this->input->post('job_post_id');
+                    $job_deadline = strtolower($this->input->post('job_deadline'));
+                    $job_post_id  = $this->input->post('job_post_id');
 
                     $candiate_email  = $this->input->post('candiate_email');
                     $where_can = "email IN ('".$candiate_email."')";
-                    $data['can_data'] = $this->Master_model->getMaster('js_info',$where_can);
-                    //echo $this->db->last_query();
-                    //print_r($data['can_data']);
+                    $can_data = $this->Master_model->getMaster('js_info',$where_can);
+                    //print_r($can_data);
                     // foreach($can_data as $cand_row)
                     // {
                     //   echo 'Emails: - '.$email = $cand_row['email']; echo "<br><br>";
@@ -638,4 +617,3 @@ function getstate(){
 
 } // end class
 
->>>>>>> 1659d42ca0b39692e32c29f3d073b0ee290f35d0
