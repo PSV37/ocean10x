48f251846a904f3ab22e4e66f8e9640c1e47f290
shyam <shyam@itdivine.in>
2019-10-10 17:51:55 +0530

    Forword tab

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -628,73 +628,15 @@ function getstate(){
 
                         // echo $seeker_id; echo "<br><br>";
 
-                        $apply_array = array(
-                            'job_seeker_id' => $seeker_id,
-                            'company_id'    => $employer_id,
-                            'job_post_id'   => $
-                        );
+                        // $apply_array = array(
+                        //     'job_seeker_id' => $seeker_id,
+                        //     'company_id'    => $employer_id,
+                        //     'job_post_id'   => $
+                        // );
 
                          //send job requirement to user's email id
 
-                        $ci = get_instance();
-                        $ci->load->library('email');
-                        /*$config['protocol'] = "smtp";
-                        $config['smtp_host'] = "ssl://smtp.gmail.com";
-                        $config['smtp_port'] = "465";
-                        $config['smtp_user'] = "notification@yourdomain.com"; 
-                        $config['smtp_pass'] = "Romesh-shil1995";*/
-                        
-                        $config['protocol'] = "mail";
-                        $config['charset'] = "utf-8";
-                        $config['mailtype'] = "html";
-                        $config['newline'] = "\r\n";
-
-                        $message='<div style="max-width:600px!important;padding:4px">
-              <table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0">
-                <tbody>
-                  <tr>
-                    <td align="center"><table width="100%" cellspacing="0" border="0">
-                        <tbody>
-                          <tr>
-                            <td style="font-size:0px;text-align:left" valign="top"><?php echo get_logo();?></td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <table width="100%" cellspacing="0" cellpadding="0" border="0">
-                        <tbody>
-                          <tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left">
-                            <td><br>
-                              <br>
-                              Hello Dear,<br>
-                              Please click on link below to reset your password: <br>
-                              
-                           <a style="border-radius:4px;font-size:15px;color:white;text-decoration:none;padding:14px 7px 14px 7px;width:210px;max-width:210px;font-family:&quot;Open Sans&quot;,&quot;Helvetica Neue&quot;,Arial;margin:0;display:block;background-color:#6caa4d;text-align:center" href="'.base_url().'employer_login/reset_password/' . md5($to_email) . '">Reset Password</a>
-                              <br>
-                              <br>
-                              © 2017 ConsultnHire All Rights Reserved.</td>
-                          </tr>
-                          <tr>
-                            <td height="40"></td>
-                          </tr>
-                        </tbody>
-                      </table></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            ';
-
-            $ci->email->initialize($config);
-            $ci->email->from('info@consultnhire.com', 'ConsultnHire');
-            $ci->email->to($to_email);
-            $ci->email->reply_to('info@consultnhire.com', 'Vorerpata Mail');
-            $ci->email->subject('Account Recovery');
-            $ci->email->message($message);
-            $ci->email->send(FALSE);
-          //  $ci->email->print_debugger(array('headers'));
-          //  exit;
-            return true;
-    
+                     
     
                       
                     }
