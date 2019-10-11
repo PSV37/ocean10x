f8f63659097c0528d68a568e92e6905b8cb84f87
unknown <santu@edunetsol.com>
2019-10-11 15:28:39 +0530

    updated postjob

--- application/views/fontend/downloadcv.php
+++ application/views/fontend/downloadcv.php
@@ -683,21 +683,21 @@ PERSONAL DETAILS:
          <td style="padding-left:10px;" align="left">city</td>
          <td align="center">:</td>
          <td align="left">       
-         <?php echo $resume->city_id; ?>   
+         <?php echo $js_personal_info->city_name; ?>   
          </td>
          </tr>
 		 <tr class="resume-textthree">
          <td style="padding-left:10px;" align="left">State</td>
          <td align="center">:</td>
          <td align="left">       
-         <?php echo $resume->state_id; ?>   
+         <?php echo $js_personal_info->state_name; ?>   
          </td>
          </tr>
 		 <tr class="resume-textthree">
          <td style="padding-left:10px;" align="left">Country</td>
          <td align="center">:</td>
          <td align="left">       
-         <?php echo $resume->country_id; ?>   
+         <?php echo $js_personal_info->country_name; ?>   
          </td>
          </tr>
       <?php endif; ?>
