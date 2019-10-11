1496dc67f282cbfd11f5f8575ed4531260cb23bf
unknown <santu@edunetsol.com>
2019-10-11 15:55:44 +0530

    updated postjob

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -433,7 +433,7 @@ class Employer extends MY_Employer_Controller
                         $data['reference_list']  = $this->Job_reference_model->reference_list_by_id($jobseeker_id);
 						//$data['js_personal_info'] = $this->job_seeker_personal_model->personalinfo_list_by_id($jobseeker_id);
 						//echo $this->db->last_query();
-						$data['js_photo'] = $this->Job_seeker_photo_model->js_photo_id($jobseeker_id);
+						//$data['js_photo'] = $this->Job_seeker_photo_model->js_photo_id($jobseeker_id);
 						//$data['city'] = $this->Master_model->getMaster('city',$where=false);
 						//$data['country'] = $this->Master_model->getMaster('country',$where=false);
 						//$data['state'] = $this->Master_model->getMaster('state',$where=false);
