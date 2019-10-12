240f7fb4ca41eea10aa0e774414d65fca08c683a
unknown <santu@edunetsol.com>
2019-10-12 18:20:39 +0530

    add topic

--- application/config/routes.php
+++ application/config/routes.php
@@ -167,7 +167,7 @@ $route['admin/designation-master']="admin/designation_master";
 // $route['admin/industry-master']="admin/industry_master";
 $route['admin/skill-master']="admin/skill_master";
 $route['admin/job-role']="admin/job_role";
-$route['admin/topic/(:num)']="admin/topic/$1";
+$route['admin/topic/(:any)']="admin/topic/index/(:any)/$1";
 
 
 
