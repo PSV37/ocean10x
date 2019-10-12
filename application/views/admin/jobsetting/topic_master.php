df76b44e62d3afa086958313db0e10bba96a8c99
shyam <shyam@itdivine.in>
2019-10-12 17:23:35 +0530

    Topic changes

--- application/views/admin/jobsetting/topic_master.php
+++ application/views/admin/jobsetting/topic_master.php
@@ -28,7 +28,7 @@
             <div class="box box-primary">
                 <div class="box-header box-header-background with-border">
                     <div class="col-md-offset-3">
-                        <h3 class="box-title ">Topic Master</h3>
+                        <h3 class="box-title ">Topic Masters</h3>
                     </div>
                 </div>
                 <!-- /.box-header -->
