40b69b4db770adcfb2be039f0282d3d9b621a97a
unknown <santu@edunetsol.com>
2019-10-14 10:30:36 +0530

    form

--- application/config/routes.php
+++ application/config/routes.php
@@ -167,7 +167,8 @@ $route['admin/designation-master']="admin/designation_master";
 // $route['admin/industry-master']="admin/industry_master";
 $route['admin/skill-master']="admin/skill_master";
 $route['admin/job-role']="admin/job_role";
-
+//$route['admin/topic/(:any)']="admin/topic/save_topic/$1";
+//$route['adm']="admin/topic/";in/topic/save_topic
 
 
 
