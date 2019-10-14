a56bdeb0619a7f7d3a11ad91ab5cc93228521744
unknown <santu@edunetsol.com>
2019-10-14 16:27:35 +0530

    topic controller setting

---
+++ application/controllers/admin/Subtopic.php
@@ -0,0 +1,121 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+
+
+class Subtopic extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /*** Dashboard ***/
+    public function index()
+    {   
+
+        $data['title'] = 'Add Subtopic';
+
+        $where_cn= "status=1";
+        $data['skill_master'] = $this->Master_model->getMaster('skill_master',$where=false);
+
+        $where_state= "topic_status=1";
+        $data['topic'] = $this->Master_model->getMaster('topic',$where_state);
+        
+        $where_all = "subtopic.subtopic_status='1'";
+        $join_emp = array(
+                'skill_master' => 'skill_master.id=topic.technical_id |INNER',
+                'topic' => 'topic.topic_id=subtopic.topic_id |INNER',
+            );
+        $data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_all,$join_emp);
+
+        $this->load->view('admin/jobsetting/subtopic_master', $data);
+    }
+
+        public function save_subtopic($id = null){
+          
+            $user_id = $this->session->userdata('admin_user_id');
+            
+            $state_dt=array(
+                'technical_id' => $this->input->post('technical_id'),
+                'topic_id' => $this->input->post('topic_id'),
+                'subtopic_name' => $this->input->post('subtopic_name'),
+				'subtopic_desc' => $this->input->post('subtopic_desc'),
+            );
+
+            if(empty($id)){
+                $state_dt['subtopic_created_date']=date('Y-m-d H:i:s');
+                $state_dt['subtopic_created_by']=$user_id;
+
+                $this->Master_model->master_insert($state_dt,'subtopic');
+               
+                redirect('admin/subtopic_master');
+            }
+            else {
+                $state_dt['subtopic_updated_date']=date('Y-m-d H:i:s');
+                $state_dt['subtopic_updated_by']=$user_id;
+
+                $where['subtopic_id']=$id;
+                $this->Master_model->master_update($state_dt,'subtopic',$where);
+               
+                redirect('admin/subtopic_master');
+            }
+        }
+
+    public function delete_subtopic($id) {
+        
+        $state_status = array(
+            'subtopic_status'=>0,
+        );
+        $where_del['subtopic_id']=$id;
+        $this->Master_model->master_update($state_status,'subtopic',$where_del);
+        redirect('admin/subtopic_master');
+    }
+
+    public function edit_subtopic($id){
+        $data['title']="Edit subtopic";
+
+        $where_all = "subtopic.subtopic_status='1'";
+        $join_emp = array(
+               'skill_master' => 'skill_master.id=topic.technical_id |INNER',
+                'topic' => 'topic.topic_id=subtopic.topic_id |INNER',
+
+            );
+        $data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_all,$join_emp);
+
+        $where_ct = "subtopic_id='$id'";
+        $data['edit_subtopic_info'] = $this->Master_model->getMaster('subtopic',$where_ct);
+        
+        $where_cn= "status=1";
+         $data['skill_master'] = $this->Master_model->getMaster('skill_master',$where=false);
+
+        $where_state= "topic_status=1";
+         $data['topic'] = $this->Master_model->getMaster('topic',$where=false);
+        
+        $this->load->view('admin/jobsetting/subtopic_master',$data);
+    }
+
+
+
+function gettopic(){
+    $topic_id = $this->input->post('id');
+    $where['id'] = $skill_id;
+    $topics = $this->Master_model->getMaster('topic',$where);
+    $result = '';
+    if(!empty($topics)){ 
+        $result .='<option value="">Select Topic</option>';
+        foreach($topics as $key){
+          $result .='<option value="'.$key['topic_id'].'">'.$key['topic_name'].'</option>';
+        }
+    }else{
+    
+        $result .='<option value="">Topic not available</option>';
+    }
+     echo $result;
+}
+
+
+}
---
+++ application/views/admin/jobsetting/subtopic_master.php
@@ -0,0 +1,194 @@
+<?php $this->load->view('admin/components/header'); ?>
+<body class="skin-blue" data-baseurl="<?php echo base_url(); ?>">
+    <div class="wrapper">
+        
+    <?php $this->load->view('admin/components/user_profile'); ?>
+       
+        <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+            <?php $this->load->view('admin/components/navigation'); ?>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
+
+        <div class="right-side">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+            </section>
+
+            <br/>
+            <div class="container-fluid">
+               <section class="content">
+    <div class="row">
+        <div class="col-md-12">
+
+            <div class="box box-primary">
+                <div class="box-header box-header-background with-border">
+                    <div class="col-md-offset-3">
+                        <h3 class="box-title ">Subtopic Master</h3>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-background">
+                <!-- form start -->
+                <form role="form" enctype="multipart/form-data" action="<?php echo base_url(); ?>admin/subtopic/save_subtopic/<?php  if (!empty($edit_subtopic_info)) { foreach($edit_subtopic_info as $row)
+                        echo $row['subtopic_id'];
+                      }
+                     ?>" method="post">
+
+                    <div class="row">
+
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+
+                            <div class="box-body">
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Subject <span class="required">*</span></label>
+                                         <select id="technical_id"  name="technical_id" class="form-control" required onchange="getTopic(this.value)">
+                                           <option value="">Select Subject</option> 
+                                        <?php if (!empty($skill_master))
+                                           foreach($skill_master as $skill) 
+                                           {
+                                        ?>   
+                                            <option value="<?php echo $skill['id']; ?>"<?php if (!empty($edit_spectial_info)) if($row['technical_id']==$skill['id'])echo "selected";?>><?php echo $skill['skill_name']; ?></option> 
+                                        <?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Topic <span class="required">*</span></label>
+                                        <select id="topic_id"  name="topic_id" class="form-control" required>
+                                           <option value="">Select Topic</option> 
+                                        <?php if (!empty($state_data))
+                                           foreach($state_data as $st_row) 
+                                           {
+                                        ?>   
+                                            <option value="<?php echo $st_row['state_id']; ?>"<?php if (!empty($edit_city_info)) if($row['state_id']==$st_row['state_id'])echo "selected";?>><?php echo $st_row['state_name']; ?></option> 
+                                        <?php } ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                 <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Subtopic Name <span class="required">*</span></label>
+                                      <input type="text" name="city_name" class="form-control" value="<?php if (!empty($edit_city_info)) echo $row['city_name'];?>" required>
+                                    </div>
+                                </div>
+                                <div class="panel-body"></div>
+                                <button type="submit" class="btn bg-navy" type="submit">Save Subtopic
+                                </button><br/><br/>
+                            </div>
+                            <!-- /.box-body -->
+
+                        </div>
+                    </div>
+
+                </form>
+                    </div>
+                <div class="box-footer">
+
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                <table class="table table-bordered table-striped" id="dataTables-example">
+                    <thead>
+                    <tr>
+                        <th class="active">SL</th>
+                        <th class="active">Country Name</th>
+                        <th class="active">State Name</th>
+                        <th class="active">City Name</th>
+                        <th class="active col-sm-2">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $key = 1 ;?>
+                    <?php if (!empty($city_info)): foreach ($city_info as $ct_row) : ?>
+                        <tr>
+                            <td><?php echo $key ?></td>
+                            <td><?php echo $ct_row['country_name'] ?></td>
+                            <td><?php echo $ct_row['state_name'] ?></td>
+                            <td><?php echo $ct_row['city_name'] ?></td>
+                            <td>
+                                <?php echo btn_edit('admin/city_master/edit_city/' . $ct_row['id']); ?>
+                                <?php echo btn_delete('admin/city_master/delete_city/' . $ct_row['id']); ?>
+                            </td>
+                        </tr>
+                    <?php
+                    $key++;
+                    endforeach;
+                    ?>
+                    <?php else : ?> 
+                        <td colspan="3">
+                            <strong>There is no record for display</strong>
+                        </td>
+                    <?php
+                    endif; ?>
+                    </tbody>
+                </table>
+
+                    </div>
+                </div>
+            </div>
+            <!-- /.box -->
+        </div>
+        <!--/.col end -->
+    </div>
+    <!-- /.row -->
+</section>
+</div>
+
+<br />
+
+
+</div><!-- /.right-side -->
+
+       
+<?php $this->load->view('admin/components/footer'); ?>
+<script>
+    function getTopic(id){
+        if(id){
+            $.ajax({
+                type:'POST',
+                url:'<?php echo base_url();?>admin/subtopic/gettopic',
+                data:{id:id},
+                success:function(res){
+                    $('#topic_id').html(res);
+                }
+                
+            }); 
+          }
+   
+       }
+
+    $(document).ready(function(){
+
+
+
+    function getTopic_load(){
+        var id = $('#technical_id').val();
+
+        if(id){
+            $.ajax({
+                type:'POST',
+                url:'<?php echo base_url();?>admin/subtopic/gettopic',
+                data:{id:id},
+                success:function(res){
+                    $('#topic_id').html(res);
+                    $('#topic_id').val(<?php echo $row['topic_id']; ?>);
+                }
+                
+            }); 
+          }
+   
+       }
+       getTopic_load();
+    });
+       
+</script>
