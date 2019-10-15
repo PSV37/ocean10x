333c6984194b42453d6e19f465a056f50eef6d8b
shyam <shyam@itdivine.in>
2019-10-15 11:53:10 +0530

    Comapany profile  edit chages

--- application/views/fontend/employer/dashboard.php
+++ application/views/fontend/employer/dashboard.php
@@ -328,7 +328,7 @@
                                         <div class="col-md-6 col-sm-12">
                                         <div class="formrow">
                                         	<label class="control-label">Company Industry:</label>
-                                            <select name="company_category" class="form-control selectpicker" data-style="btn-default" data-live-search="true">
+                                            <select name="company_category" class="form-control" data-style="btn-default" data-live-search="true">
                                                 <?php if(!empty($company_info->company_category)) {
                                                 echo $this->job_category_model->selected($company_info->company_category);
                                                 } else {
@@ -355,7 +355,7 @@
 										<div class="col-md-6 col-sm-6">
 											<div class="formrow">
 												<label class="control-label">Company Type:</label>
-												<select name="hot_jobs" required id="hot_jobs" class="form-control selectpicker">
+												<select name="hot_jobs" required id="hot_jobs" class="form-control">
 													<option value="">Select Type</option> 
 													<option value="1" <?php echo ( ($company_info->hot_jobs =='1')?'selected':''); ?>>Selected Resume</option> 
 													<option value="2" <?php echo ( ($company_info->hot_jobs=='2')?'selected':''); ?>>University</option> 
