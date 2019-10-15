cc874f0629d204672a31b5ad569d22c09f431608
unknown <santu@edunetsol.com>
2019-10-15 15:40:17 +0530

    topic controller

--- application/views/admin/jobsetting/questionbank_master.php
+++ application/views/admin/jobsetting/questionbank_master.php
@@ -62,8 +62,8 @@
 
                                 <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="exampleInputEmail1">Topic <span class="required" onchange="getSubtopic(this.value)">*</span></label>
-                                        <select id="topic_id"  name="topic_id" class="form-control" required>
+                                        <label for="exampleInputEmail1">Topic <span class="required">*</span></label>
+                                        <select id="topic_id"  name="topic_id" class="form-control" required onchange="getSubtopic(this.value)">
                                            <option value="">Select Topic</option> 
                                         <?php if (!empty($topic))
                                            foreach($topic as $st_row) 
