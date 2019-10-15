4bac9736ade7356ae3e77928170853b9dbd56c71
unknown <santu@edunetsol.com>
2019-10-15 13:02:10 +0530

    topic controller

--- application/views/admin/jobsetting/subtopic_master.php
+++ application/views/admin/jobsetting/subtopic_master.php
@@ -181,7 +181,8 @@
 	   </script>
        
 </script>
-<script src="<?php echo base_url(); ?>assets/js/select2.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/js/select2.min.js"></script>
 <script>
 $("#technical_id").select2( {
 	placeholder: "Select Subject",
