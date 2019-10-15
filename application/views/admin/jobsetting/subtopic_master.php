8eb9656940dbbf64cf116de392ae566cf568663c
unknown <santu@edunetsol.com>
2019-10-15 13:40:59 +0530

    topic controller

--- application/views/admin/jobsetting/subtopic_master.php
+++ application/views/admin/jobsetting/subtopic_master.php
@@ -182,7 +182,7 @@
        
 </script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/js/select2.min.js"></script>
+<script src="https://www.consultnhire.com/assets/js/select2.min.js"></script>
 <script>
 $("#subject").select2( {
 	placeholder: "Select Subject",
