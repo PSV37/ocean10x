01df016a24bf527049ed395a3d2aeb1d7feee666
unknown <santu@edunetsol.com>
2019-10-15 13:49:05 +0530

    topic controller

--- application/views/admin/jobsetting/subtopic_master.php
+++ application/views/admin/jobsetting/subtopic_master.php
@@ -194,7 +194,6 @@ function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
