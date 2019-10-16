f1646ac5fa9d8c2ae2ed3f532d90e221a0c17756
unknown <santu@edunetsol.com>
2019-10-16 15:13:30 +0530

    questionbank

--- application/views/admin/jobsetting/questionbank_master.php
+++ application/views/admin/jobsetting/questionbank_master.php
@@ -135,7 +135,7 @@
 								 <div class="col-md-4">
                                     <div class="form-group">
 									<label for="exampleInputEmail1">Question Type<span class="required">*</span></label>
-									<select  name="ques_type" class="form-control" onchange='hideshowfun()' id="category">
+									<select  name="ques_type" class="form-control" onload='hideshowfun()' id="category">
 									                                      
                                         <option value="MCQ"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='MCQ')echo "selected";?>>MCQ</option>
 										<option value="Subjective"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='Subjective')echo "selected";?>>Subjective</option>
