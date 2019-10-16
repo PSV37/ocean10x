12f54cf807a01d69a46e119d2eb7f9dfc6364c48
unknown <santu@edunetsol.com>
2019-10-16 16:57:48 +0530

    questionbank

--- application/views/admin/jobsetting/questionbank_master.php
+++ application/views/admin/jobsetting/questionbank_master.php
@@ -59,7 +59,7 @@
             <div class="box box-primary">
                 <div class="box-header box-header-background with-border">
                     <div class="col-md-offset-3">
-                        <h3 class="box-title ">Question Bank</h3>
+                        <h3 class="box-title ">QuestionBank Master</h3>
                     </div>
                 </div>
                 <!-- /.box-header -->
