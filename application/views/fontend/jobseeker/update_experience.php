e75b3944113519d62cafd06e1fdd2a6b1b95bd87
shyam <shyam@itdivine.in>
2019-10-17 11:57:54 +0530

    user experiance change dada

--- application/views/fontend/jobseeker/update_experience.php
+++ application/views/fontend/jobseeker/update_experience.php
@@ -287,8 +287,9 @@ if (!empty($experinece->start_date)) {
 
             <div class="form-group">
                 <label class="control-label col-sm-3" for="email">Joining Date</label>
-                <div class="col-sm-9"><input class="datepicker form-control"  required id="start_date" name="start_date" value="" class="form-control" >
-                <label><input type="checkbox" id="chkDisable" onclick="disableAddDP()">  Continue</label>
+                <div class="col-sm-9">
+                  <input type="text" class="datepicker form-control"  required id="start_date" name="start_date" value="">
+                  <label><input type="checkbox" id="chkDisable" onclick="disableAddDP()">  Continue</label>
                 </div>
               </div>
 
