7949efcf3ba4460f21c08c646d015c21341ee83a
Santu Chatterjee <santu@edunetsol.com>
2019-10-17 22:48:47 +0530

    lineitem hidden

--- application/controllers/admin/Lineitem.php
+++ application/controllers/admin/Lineitem.php
@@ -56,8 +56,8 @@ class Lineitem extends MY_Controller
 
             //print_r($req_skill_details); die;
             $state_dt=array(
-				'technical_id' => $skill_master,
-				'topic_id' => $topic,
+				'technical_id' => $subtopic,
+				'topic_id' => $subtopic,
 				'subtopic_id' => $subtopic,
 				'title' => $this->input->post('title'),
 				'lineitem_desc' => $this->input->post('lineitem_desc'),
