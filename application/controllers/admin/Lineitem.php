6428e81116ffb2b100644494452de5cf94afbb88
unknown <santu@edunetsol.com>
2019-10-17 18:50:52 +0530

    questionbank

--- application/controllers/admin/Lineitem.php
+++ application/controllers/admin/Lineitem.php
@@ -48,13 +48,13 @@ class Lineitem extends MY_Controller
           
             $user_id = $this->session->userdata('admin_user_id');
 			
-			$where_cnn= "status=1";
+			$where_cnn= "skill_master.id";
             $skills= $this->Master_model->getMaster('skill_master',$where_cnn);
 			
-			$where_topics= "topic.topic_status=1";
+			$where_topics= "topic.topic_id";
             $topics = $this->Master_model->getMaster('topic',$where_topic);
 			
-			 $where_subtopics= "subtopic.subtopic_status=1";
+			 $where_subtopics= "subtopic.subtopic_id";
              $subtopics = $this->Master_model->getMaster('subtopic',$where_subtopics);
             $state_dt=array(
 				'technical_id'=$skills;
