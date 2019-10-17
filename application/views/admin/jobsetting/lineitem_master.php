cc3f187d0ab5222d1dc76e1c16a2b2afead64f53
Santu Chatterjee <santu@edunetsol.com>
2019-10-17 23:13:48 +0530

    lineitem hidden

--- application/views/admin/jobsetting/lineitem_master.php
+++ application/views/admin/jobsetting/lineitem_master.php
@@ -76,9 +76,7 @@
 
                 </form>
                     </div>
-                <div class="box-footer">
 
-                </div>
                <!-- <div class="row">
                     <div class="col-md-10 col-md-offset-1">
                 <table class="table table-bordered table-striped" id="dataTables-example">
