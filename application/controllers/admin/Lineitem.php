4c6d5258f8305e215185564f073afc1db36956e4
unknown <santu@edunetsol.com>
2019-10-17 18:38:03 +0530

    questionbank

--- application/controllers/admin/Lineitem.php
+++ application/controllers/admin/Lineitem.php
@@ -47,8 +47,8 @@ class Lineitem extends MY_Controller
         public function save_lineitem($id = null){
           
             $user_id = $this->session->userdata('admin_user_id');
-			
-             $subtopic = $this->Master_model->getMaster('subtopic');
+			 $where_subtopics= "subtopic.subtopic_status=1";
+             $subtopic = $this->Master_model->getMaster('subtopic',$where_subtopics);
             $state_dt=array(
 				
 				'title' => $this->input->post('title'),
