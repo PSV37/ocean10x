aa58a5ec206b3ca21be8172309efbabde58e7573
unknown <santu@edunetsol.com>
2019-10-17 18:09:10 +0530

    questionbank

--- application/views/admin/jobsetting/subtopic_master.php
+++ application/views/admin/jobsetting/subtopic_master.php
@@ -128,7 +128,7 @@
                             <td>
                                 <?php echo btn_edit('admin/subtopic/edit_subtopic/' . $ct_row['subtopic_id']); ?>
                                 <?php echo btn_delete('admin/subtopic/delete_subtopic/' . $ct_row['subtopic_id']); ?>
-								<?php echo btn_add('admin/lineitem/index'. $ct_row['subtopic_id']); ?>
+								<?php echo btn_add('admin/lineitem/index/'. $ct_row['subtopic_id']); ?>
                             </td>
                         </tr>
                     <?php
