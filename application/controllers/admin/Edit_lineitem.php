c8d23f9b5c4f31e6c440d9525a6d6147ef532dfb
unknown <santu@edunetsol.com>
2019-10-18 14:17:10 +0530

    lineitem adding

---
+++ application/controllers/admin/Edit_lineitem.php
@@ -0,0 +1,116 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+
+
+class Edit_lineitem extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /*** Dashboard ***/
+    public function index($id = null)
+    {   
+
+        if (!empty($id)) {
+                 
+            $data['title'] = 'Edit Lineitems';
+
+            $where_cn= "status=1";
+            $data['skill_master'] = $this->Master_model->getMaster('skill_master',$where_cn);
+
+            $where_topic= "topic.topic_status=1";
+            $data['topic'] = $this->Master_model->getMaster('topic',$where_topic);
+            
+            $where_subtopic= "subtopic.subtopic_status=1 AND subtopic.subtopic_id ='$id'";
+            $data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_subtopic);
+            
+            $where_all = "lineitem.lineitem_status='1'";
+			$join_emp = array(
+                'skill_master' => 'skill_master.id=lineitem.technical_id |INNER',
+                'topic' => 'topic.topic_id=lineitem.topic_id |INNER',
+				'subtopic' => 'subtopic.subtopic_id=lineitem.subtopic_id |INNER',
+            );
+        $data['lineitem'] = $this->Master_model->getMaster('lineitem',$where_all,$join_emp);
+            
+
+            $this->load->view('admin/jobsetting/edit_lineitem_master', $data);
+                
+            } else {
+                redirect('admin/lineitem/index/'.$id);
+            }
+
+       
+    }
+
+
+
+        public function save_lineitem($id = null){
+          
+            $user_id = $this->session->userdata('admin_user_id');
+
+            $state_dt=array(
+				'technical_id' => $this->input->post('technical_id'),
+				'topic_id' => $this->input->post('topic_id'),
+				'subtopic_id' => $this->input->post('subtopic_id'),
+				'title' => $this->input->post('title'),
+				'lineitem_desc' => $this->input->post('lineitem_desc'),
+				//'subtopic_id'=$subtopic;
+            );
+
+            if(empty($id)){
+                $state_dt['lineitem_created_date']=date('Y-m-d H:i:s');
+                $state_dt['lineitem_created_by']=$user_id;
+
+                $this->Master_model->master_insert($state_dt,'lineitem');
+               
+                redirect('admin/lineitem/index/'.$id);
+            }
+            else {
+                $state_dt['lineitem_updated_date']=date('Y-m-d H:i:s');
+                $state_dt['lineitem_updated_by']=$user_id;
+
+                $where['lineitem_id']=$id;
+                $this->Master_model->master_update($state_dt,'lineitem',$where);
+               
+                redirect('admin/lineitem/index/'.$id);
+            }
+        }
+
+    
+  public function edit_lineitem($id){
+   $data['title']="Edit Lineitem";
+		
+        $where_all = "lineitem.lineitem_status='1'";
+        $join_emp = array(
+                'skill_master' => 'skill_master.id=lineitem.technical_id |INNER',
+                'topic' => 'topic.topic_id=lineitem.topic_id |INNER',
+				'subtopic' => 'subtopic.subtopic_id=lineitem.subtopic_id |INNER',
+            );
+        $data['lineitem'] = $this->Master_model->getMaster('lineitem',$where_all,$join_emp);
+        	
+        $where_ct = "lineitem_id='$id'";
+        $data['edit_lineitems_info'] = $this->Master_model->getMaster('lineitem',$where_ct);
+        
+		//$where_sub = "subtopic_id='$id'";
+		//$data['edit_subtopic_info'] = $this->Master_model->getMaster('subtopic',$where_sub);
+		
+        $where_cn= "status=1";
+        $data['skill_master'] = $this->Master_model->getMaster('skill_master',$where_cn);
+
+        $where_state= "topic_status=1";
+        $data['topic'] = $this->Master_model->getMaster('topic',$where_state);
+        
+		$where_subtopic = "subtopic.subtopic_status='1' AND subtopic.subtopic_id ='$id'";
+		$data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_subtopic);
+		
+		
+        $this->load->view('admin/jobsetting/edit_lineitem_master',$data);
+    }
+
+}
---
+++ application/views/admin/jobsetting/edit_lineitem_master.php
@@ -0,0 +1,104 @@
+<?php $this->load->view('admin/components/header'); ?>
+<body class="skin-blue" data-baseurl="<?php echo base_url(); ?>">
+    <div class="wrapper">
+        
+    <?php $this->load->view('admin/components/user_profile'); ?>
+       
+        <!-- Left side column. contains the logo and sidebar -->
+      <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- Sidebar user panel -->
+            <?php $this->load->view('admin/components/navigation'); ?>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
+
+        <div class="right-side">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+            </section>
+
+            <br/>
+            <div class="container-fluid">
+               <section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            
+             <?php /* print_r($subtopic); */ ?> 
+
+            <div class="box box-primary">
+                <div class="box-header box-header-background with-border">
+                    <div class="col-md-offset-3">
+                        <h3 class="box-title ">Add Line Item</h3>
+                    </div>
+                </div>
+               <div class="box-background">
+              
+                <form role="form" enctype="multipart/form-data" action="<?php echo base_url(); ?>admin/edit_lineitem/save_lineitem/<?php  if (!empty($subtopic)) { foreach($subtopic as $row)
+                        echo $row['lineitem_id'];
+                      }
+                     ?>" method="post">
+
+                    <div class="row">
+
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+									 <input type="hidden" name="technical_id" id="technical_id"  class="form-control" value="<?php if (!empty($subtopic)) echo $row['technical_id'];?>" required/>
+                                    <input type="hidden" name="topic_id" id="topic_id"  class="form-control" value="<?php if (!empty($subtopic)) echo $row['topic_id'];?>" required/>
+                                    <input type="hidden" name="subtopic_id" id="subtopic_id"  class="form-control" value="<?php if (!empty($subtopic)) echo $row['subtopic_id'];?>" required/>
+                                   
+									 <div class="box-body">
+
+                                    <div class="form-group">
+									<div class="col-md-12">
+                                        <label for="exampleInputEmail1">Title<span class="required">*</span></label>
+                                      <input type="text" name="title" id="title"  class="form-control" value="<?php if (!empty($subtopic)) echo $row['title'];?>" required/>
+                                     </div>
+									</div>
+                                </div>
+								 <div class="box-body">
+
+                                    <div class="form-group">
+									<div class="col-md-12">
+                                        <label for="exampleInputEmail1">Description<span class="required">*</span></label>
+                                      <textarea name="lineitem_desc" id="lineitem_desc" class="form-control ckeditor" required><?php if (!empty($subtopic)) echo $row['lineitem_desc'];?></textarea>
+                                    </div>
+									</div>
+                                </div>
+                                <div class="panel-body"></div>
+                                <button type="submit" class="btn bg-navy" type="submit">Save Lineitem
+                                </button><br/><br/>
+                            
+                          
+
+                        </div>
+                    </div>
+
+                </form>
+                    </div>
+         
+        
+            </div>
+            <!-- /.box -->
+        </div>
+        <!--/.col end -->
+    </div>
+    <!-- /.row -->
+</section>
+</div>
+
+<br />
+
+
+</div><!-- /.right-side -->
+<br/><br/><br/><br/>
+       
+<?php $this->load->view('admin/components/footer'); ?>
+
+<script src="<?php echo base_url() ?>asset/js/select2.min.js"></script>
+<script>
+$("#subject").select2( {
+	placeholder: "Select Subject",
+	allowClear: true
+	} );
+</script>
