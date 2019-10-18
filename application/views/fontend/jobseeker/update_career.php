0d59a6aedeefdc32763771c8d987b10f9754c45e
shyam <shyam@itdivine.in>
2019-10-18 12:14:18 +0530

    Career option work in candidate section

--- application/views/fontend/jobseeker/update_career.php
+++ application/views/fontend/jobseeker/update_career.php
@@ -265,12 +265,12 @@ function delete_Career(id) {
                 autocomplete: {
                   source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
                   delay: 100
-                  z-index: 9999
                 },
                 showAutocompleteOnFocus: true,
 
               })
-           
+           $(".tokenfield").css("z-index","9999")
+
 
         });
             </script>
