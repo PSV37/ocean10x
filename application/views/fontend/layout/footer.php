cc5db7f4717b4a029af6a9eebd27956057d0ccb9
shyam <shyam@itdivine.in>
2019-10-18 10:53:25 +0530

    Career option work in candidate section

--- application/views/fontend/layout/footer.php
+++ application/views/fontend/layout/footer.php
@@ -178,6 +178,8 @@ function readURL2(input) {
 <script src="<?php echo base_url(); ?>asset/js/plugins/dataTables/jquery.dataTables.js" type="text/javascript"></script> 
 <script src="<?php echo base_url(); ?>asset/js/plugins/dataTables/dataTables.bootstrap.js" type="text/javascript"></script> 
 <script src="<?php echo base_url() ?>asset/js/jquery-ui.js"></script> 
+<script src="<?php echo base_url() ?>asset/js/typehead.js"></script> 
+
 <script>
 
       $(document).ready(function() {
