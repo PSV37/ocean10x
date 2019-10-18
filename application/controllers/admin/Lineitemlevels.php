13be8eb4486238868a03e48db3bde6edf26f1ad1
unknown <santu@edunetsol.com>
2019-10-18 17:39:16 +0530

    lineitem adding

--- application/controllers/admin/Lineitemlevels.php
+++ application/controllers/admin/Lineitemlevels.php
@@ -117,7 +117,7 @@ class Lineitemlevels extends MY_Controller
 		$where_lineitem= "lineitem.lineitem_status=1 AND lineitem.lineitem_id ='$id'";
         $data['lineitem'] = $this->Master_model->getMaster('lineitem',$where_subtopic);
 		
-        $this->load->view('admin/jobsetting/edit_lineitemlevel',$data);
+        $this->load->view('admin/jobsetting/edit_lineitemlevels',$data);
     }
 
 	
