f29ded19c077d131adc571fab45b0d1792fba415
unknown <santu@edunetsol.com>
2019-10-19 15:11:07 +0530

    lineitem adding

--- application/views/admin/jobsetting/questionbank_master.php
+++ application/views/admin/jobsetting/questionbank_master.php
@@ -164,18 +164,7 @@
 									 </div>
                                 </div>
 								
-								 <div class="col-md-4">
-                                    <div class="form-group">
-									<label for="exampleInputEmail1">Question Type<span class="required">*</span></label>
-									<select  name="ques_type" class="form-control" onchange="hideshowfun()" id="category">
-									                                      
-                                        <option value="MCQ"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='MCQ')echo "selected";?>>MCQ</option>
-										<option value="Subjective"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='Subjective')echo "selected";?>>Subjective</option>
-										<option value="Practical"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='Practical')echo "selected";?>>Practical</option>
 								
-									</select>
-									 </div>
-                                </div>
 								</div>
 								<div class="container-fluid">
 								<div class="col-md-4">
