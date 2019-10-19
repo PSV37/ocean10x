bf36459834ccd304f068080fe4769bcf33fb23e3
unknown <santu@edunetsol.com>
2019-10-19 09:44:53 +0530

    lineitem adding

--- application/views/admin/jobsetting/lineitem_master.php
+++ application/views/admin/jobsetting/lineitem_master.php
@@ -39,7 +39,7 @@
 					<div class="col-md-10"></div>
 					<div class="col-md-2">
 				
-					<br/><?php echo btn_add('admin/lineitem/save_lineitem/index/'. $subtopic[0]['subtopic_id']); ?>
+					<br/><?php echo btn_add('admin/lineitem/index/'. $subtopic[0]['subtopic_id']); ?>
 					
 					</div>
 					</div>
