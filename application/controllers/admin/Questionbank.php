3d6692a59e8d1ec771e007950a728a43a5305f8d
unknown <santu@edunetsol.com>
2019-10-19 18:47:13 +0530

    lineitem adding

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -138,21 +138,12 @@ class Questionbank extends MY_Controller
 		
         $where_ct = "ques_id='$id'";
         $data['edit_questionbank_info'] = $this->Master_model->getMaster('questionbank',$where_ct);
+		
+		$where_answer = "question_id='$id'";
+        $data['questionbank_answer'] = $this->Master_model->getMaster('questionbank_answer',$where_answer);
         
         $where_cn= "status=1";
         $data['skill_master'] = $this->Master_model->getMaster('skill_master');
-
-        $where_state= "topic_status=1";
-        $data['topic'] = $this->Master_model->getMaster('topic',$where_state);
-        
-		$where_subtopic = "subtopic.subtopic_status='1'";
-		$data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_subtopic);
-		
-		$where_lineitem = "lineitem.lineitem_status='1'";
-		$data['lineitem'] = $this->Master_model->getMaster('lineitem',$where_lineitem);
-		
-		$where_lineitemlevel = "lineitemlevel.lineitemlevel_status='1'";
-		$data['lineitemlevel'] = $this->Master_model->getMaster('lineitemlevel',$where_lineitemlevel);
 		
         $this->load->view('admin/jobsetting/questionbank_master',$data);
     }
