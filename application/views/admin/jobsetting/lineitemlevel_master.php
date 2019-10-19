af34cf4a13712e8fb3fc182631c59c0ca6cd4984
unknown <santu@edunetsol.com>
2019-10-19 14:25:51 +0530

    lineitem adding

--- application/views/admin/jobsetting/lineitemlevel_master.php
+++ application/views/admin/jobsetting/lineitemlevel_master.php
@@ -24,36 +24,31 @@
                <section class="content">
     <div class="row">
         <div class="col-md-12">
-            
-             <?php /* print_r($subtopic); */ ?> 
-
+           
             <div class="box box-primary">
                 <div class="box-header box-header-background with-border">
                     <div class="col-md-offset-3">
-                        <h3 class="box-title ">Add Line Item</h3>
+                        <h3 class="box-title ">Add LineItem Level</h3>
                     </div>
                 </div>
                <div class="box-background">
               
-                <form role="form" enctype="multipart/form-data" action="<?php echo base_url(); ?>admin/lineitem/save_lineitemlevel/<?php  if (!empty($lineitem)) { foreach($lineitem as $row)
-                        echo $row['lineitemlevel_id'];
-                      }
-                     ?>" method="post">
+                <form role="form" enctype="multipart/form-data" action="" method="post">
 
                     <div class="row">
 
                         <div class="col-md-12 col-sm-12 col-xs-12">
-									 <input type="hidden" name="technical_id" id="technical_id"  class="form-control" value="<?php if (!empty($lineitem)) echo $row['technical_id'];?>" required/>
-                                    <input type="hidden" name="topic_id" id="topic_id"  class="form-control" value="<?php if (!empty($lineitem)) echo $row['topic_id'];?>" required/>
-                                    <input type="hidden" name="subtopic_id" id="subtopic_id"  class="form-control" value="<?php if (!empty($lineitem)) echo $row['subtopic_id'];?>" required/>
-									<input type="hidden" name="lineitem_id" id="lineitem_id"  class="form-control" value="<?php if (!empty($lineitem)) echo $row['lineitem_id'];?>" required/>
+									 <input type="hidden" name="technical_id" id="technical_id"  class="form-control" value="<?php if (!empty($lineitem_data)) echo $lineitem_data[0]['technical_id'];?>" required/>
+                                    <input type="hidden" name="topic_id" id="topic_id"  class="form-control" value="<?php if (!empty($lineitem_data)) echo $lineitem_data[0]['topic_id'];?>" required/>
+                                    <input type="hidden" name="subtopic_id" id="subtopic_id"  class="form-control" value="<?php if (!empty($lineitem_data)) echo $lineitem_data[0]['subtopic_id'];?>" required/>
+									 <input type="hidden" name="subtopic_id" id="subtopic_id"  class="form-control" value="<?php if (!empty($lineitem_data)) echo $lineitem_data[0]['lineitem_id'];?>" required/>
                                    
 									 <div class="box-body">
 
                                     <div class="form-group">
 									<div class="col-md-12">
                                         <label for="exampleInputEmail1">Title<span class="required">*</span></label>
-                                      <input type="text" name="titles" id="titles"  class="form-control" value="<?php if (!empty($lineitem)) echo $row['titles'];?>" required/>
+                                      <input type="text" name="titles" id="titles"  class="form-control" value="<?php  if(isset($line_itemlevel_data)){ echo $line_itemlevel_data[0]['titles'];  } ?>" required/>
                                      </div>
 									</div>
                                 </div>
@@ -62,12 +57,12 @@
                                     <div class="form-group">
 									<div class="col-md-12">
                                         <label for="exampleInputEmail1">Description<span class="required">*</span></label>
-                                      <textarea name="lineitemlevel_desc" id="lineitemlevel_desc" class="form-control ckeditor" required><?php if (!empty($lineitem)) echo $row['lineitemlevel_desc'];?></textarea>
+                                      <textarea name="lineitemlevel_desc" id="lineitemlevel_desc" class="form-control ckeditor" required><?php  if(isset($line_itemlevel_data)){ echo $line_itemlevel_data[0]['lineitemlevel_desc'];  } ?></textarea>
                                     </div>
 									</div>
                                 </div>
                                 <div class="panel-body"></div>
-                                <button type="submit" class="btn bg-navy" type="submit">Save Lineitem
+                                <button type="submit" class="btn bg-navy" name="save_lineitemlevel" type="submit">Save Lineitem
                                 </button><br/><br/>
                             
                           
@@ -88,9 +83,6 @@
 </section>
 </div>
 
-
-
-
 <br />
 
 
@@ -98,11 +90,3 @@
 <br/><br/><br/><br/>
        
 <?php $this->load->view('admin/components/footer'); ?>
-
-<script src="<?php echo base_url() ?>asset/js/select2.min.js"></script>
-<script>
-$("#subject").select2( {
-	placeholder: "Select Subject",
-	allowClear: true
-	} );
-</script>
