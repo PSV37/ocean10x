2aafd5678285a2b0a123241a6e6ac466fe48efe7
unknown <santu@edunetsol.com>
2019-10-22 17:29:35 +0530

    Employee(Employee Section)

--- application/views/fontend/employee/edit_employee.php
+++ application/views/fontend/employee/edit_employee.php
@@ -197,82 +197,77 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
 	   
 	   
 <script>
-	function getStates(id){
-		if(id){
-            $.ajax({
-                type:'POST',
-                url:'<?php echo base_url();?>Employer/getstate',
-                data:{id:id},
-                success:function(res){
-                    $('#state_id').html(res);
-                }
-				
-            }); 
-        }
-   
-	}
-	   
-	  
-	  function getCitys(id){
-		if(id){
-            $.ajax({
-                type:'POST',
-                url:'<?php echo base_url();?>Employer/getcity',
-                data:{id:id},
-                success:function(res){
-                    $('#city_id').html(res);
-                }
-				
-            }); 
-          }
-   
-	   }
-	   
-	  $(document).ready(function(){
+			function getStates(id){
+				if(id){
+					$.ajax({
+						type:'POST',
+						url:'<?php echo base_url();?>employer/getstate',
+						data:{id:id},
+						success:function(res){
+							$('#state_id').html(res);
+						}
+						
+					}); 
+				  }
+		   }
 
-    function getStates_load(){
-        var id = $('#country_id').val();
+    $(document).ready(function(){
+		
+		
+		function getCitys_load(){
+        var id = $('#state_id').val();
 
         if(id){
             $.ajax({
                 type:'POST',
-                url:'<?php echo base_url();?>Employer/getstate',
+                url:'<?php echo base_url();?>employer/getcity',
                 data:{id:id},
                 success:function(res){
-                    $('#state_id').html(res);
-                    $('#state_id').val(<?php echo $state->state_id; ?>);
-                     getCitys_load(<?php echo $city->state_id; ?>);
+                    $('#city_id').html(res);
+                    $('#city_id').val(<?php echo $row['city_id']; ?>);
+					getStates_load();
                 }
                 
             }); 
           }
    
        }
-    
-    function getCitys_load(){
-      //var id = $('#state_id').val();
-      // alert(id);
+		
+		function getStates_load(){
+			var id = $('#country_id').val();
+			if(id){
+				$.ajax({
+					type:'POST',
+					url:'<?php echo base_url();?>employer/getstate',
+					data:{id:id},
+					success:function(res){
+						$('#state_id').html(res);
+						$('#state_id').val(<?php echo $row['state_id']; ?>);
+					}
+					
+				}); 
+			}
+       }
+       getCitys_load();
+    });
+       
+</script>
+	   <script>
+    function getCitys(id){
         if(id){
             $.ajax({
                 type:'POST',
-                url:'<?php echo base_url();?>Employer/getcity',
+                url:'<?php echo base_url();?>employer/getcity',
                 data:{id:id},
                 success:function(res){
                     $('#city_id').html(res);
-                    $('#city_id').val(<?php echo $city->city_id; ?>);
                 }
                 
             }); 
           }
    
-       }
-
-  getCitys_load();
-  getStates_load();
- 
-});
-
-</script>  
+    }
+</script>
 
 <!-- <script src="<?php echo base_url() ?>asset/js/select2.min.js"></script> -->
 <!-- <script>
