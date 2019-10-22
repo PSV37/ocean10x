082c434b9b6603abb8a8e1aa938ced35451b49d1
unknown <santu@edunetsol.com>
2019-10-22 14:55:35 +0530

    Employee(Employee Section)

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1067,8 +1067,8 @@ $data['state'] = $this->Master_model->getMaster('state',$where=false);
 
   public function allemployee(){
 	$employer = $this->session->userdata('company_profile_id');
-	$company=$employer['company_profile_id'];
-	$where='employee.org_id="'.$company.'" and employee.emp_status!= 0';
+	//$company=$employer['company_profile_id'];
+	$where='employee.org_id="'.$employer.'" and employee.emp_status!= 0';
 	//$data['result'] = $this->Master_model->getMaster('industry',$where=FALSE);
 	$join = array(
 		'department' => 'department.dept_id = employee.dept_id|INNER'
