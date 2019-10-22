3584d29b42b30456dea94ef6fd0785502e987835
shyam <shyam@itdivine.in>
2019-10-22 16:06:28 +0530

    online test builing form

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -48,15 +48,15 @@ class Exam extends MY_Seeker_Controller
                 $skill_row['skills'];
                 
                 $wherechk = "skill_name='".$skill_row['skills']."'";
-                $select ="skill_master.id";
-                $can_skill = $this->Master_model->getMaster('skill_master',$wherechk,$join = FALSE, $order = false, $field = false, $select,$limit=false,$start=false, $search=false);
+               // $select ="skill_master.id";
+                $can_skill = $this->Master_model->getMaster('skill_master',$wherechk,$join = FALSE, $order = false, $field = false, $select=false,$limit=false,$start=false, $search=false);
                //echo $this->db->last_query(); echo "<br><br>";
                foreach($can_skill as $skill_id)
           
                {
                     $wherechks = "technical_id='".$skill_id['id']."'";
                     $data['questions'] = $this->Master_model->getMaster('questionbank',$wherechks);
-                     echo $this->db->last_query();
+                     //echo $this->db->last_query();
                }
                 
 
