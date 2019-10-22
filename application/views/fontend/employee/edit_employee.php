9bba37b41df2b9ddc7940e104e17fbe1424cf635
unknown <santu@edunetsol.com>
2019-10-22 18:17:23 +0530

    Employee(Employee Section)

--- application/views/fontend/employee/edit_employee.php
+++ application/views/fontend/employee/edit_employee.php
@@ -242,7 +242,7 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
                 success:function(res){
                     $('#state_id').html(res);
                     $('#state_id').val(<?php echo $result->state_id; ?>);
-                    
+                    getCitys_load();
                 }
                 
             }); 
