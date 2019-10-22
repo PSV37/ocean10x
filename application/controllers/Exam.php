25557d213ecc2fca545adae44389a1f9f7a471dc
shyam <shyam@itdivine.in>
2019-10-22 16:43:18 +0530

    online test builing form

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -45,23 +45,13 @@ class Exam extends MY_Seeker_Controller
             $data['skills'] = $this->Master_model->getMaster('job_posting`',$wherechk);
 
             foreach($data['skills'] as $skill_row)
-            {
-               //  $skill_row['skills'];
-                
-               //  $wherechk = "skill_name='".$skill_row['skills']."'";
-               // // $select ="skill_master.id";
-               //  $data['can_skill'] = $this->Master_model->getMaster('skill_master',$wherechk,$join = FALSE, $order = false, $field = false, $select=false,$limit=false,$start=false, $search=false);
-               // echo $this->db->last_query(); echo "<br><br>";
-               // foreach($data['can_skill'] as $skill_id)
-          
-               // {
-                    $wherechks = "technical_id IN (".$skill_row['skills_required'].")";
-                    $data['questions'] = $this->Master_model->getMaster('questionbank',$wherechks);
+            {}
+                $skill_id = $skill_row['skills_required'];
+                $where_req_skill="technical_id IN (".$skill_id.")";
+              
+                $data['questions'] = $this->Master_model->getMaster('questionbank',$where_req_skill);
                      echo $this->db->last_query();
-               // }
-                
 
-            }
             // $wherechk = "job_id='$job_id'";
             // $data['topics'] = $this->Master_model->getMaster('job_test_topics',$wherechk);
 
