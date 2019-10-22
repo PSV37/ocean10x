04adeb20c9b23528e4fb579fd9def57dbd091560
shyam <shyam@itdivine.in>
2019-10-22 18:49:12 +0530

    online test builing form

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -50,7 +50,7 @@ class Exam extends MY_Seeker_Controller
                 
                 $data['questions'] = $this->Master_model->getMaster('questionbank',$where_req_skill,$join = FALSE, $order = false, $field = false, $select = false,$limit='1',$start=false, $search=false);
                 foreach($data['questions'] as $qrow){}
-                $question_id = $qrow['question_id'];
+                $question_id = $qrow['ques_id'];
 
                 $wherechks = "question_id='$question_id'";
                 $data['ans'] = $this->Master_model->getMaster('questionbank_answer',$wherechks);
