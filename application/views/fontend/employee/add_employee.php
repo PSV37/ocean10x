8d8b76b2c7cacd8ee588b3770577e63edda4eb3a
unknown <santu@edunetsol.com>
2019-10-22 11:15:41 +0530

    Add Employee(Employee Section)

--- application/views/fontend/employee/add_employee.php
+++ application/views/fontend/employee/add_employee.php
@@ -10,7 +10,7 @@
         <h1 class="page-heading">All Question's </h1>
       </div>
       <div class="col-md-6 col-sm-6">
-        <div class="breadCrumb"><a href="#.">Home</a> / <span>All Question's</span></div>
+        <div class="breadCrumb"><a href="#.">Home</a> / <span>Add Employee</span></div>
       </div>
     </div>
   </div>
@@ -25,215 +25,76 @@
         <div class="userccount empdash">
           <div class="formpanel"> <?php echo $this->session->flashdata('success'); ?>
            
-    		<form role="form" enctype="multipart/form-data" action="<?php echo base_url(); ?>employer/save_questionbank/<?php  if (!empty($edit_questionbank_info)) { foreach($edit_questionbank_info as $row)
-                    echo $row['ques_id'];
-            	}
-            ?>" method="post">
-
-            	<div class="row">
-
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="box-body">
-					<div class="container-fluid">
-                        <div class="col-md-4">
-                            <div class="form-group">                                       
-							   <label for="exampleInputEmail1">Subject <span class="required">*</span></label>
-                                
-								<select id="subject"  name="technical_id" class="form-control" required onchange="getTopic(this.value)">
-                                   <option value="">Select Subject</option> 
-                                <?php if (!empty($skill_master))
-                                   foreach($skill_master as $skill) 
-                                   {
-                                ?>   
-                                    <option value="<?php echo $skill['id']; ?>"<?php if (!empty($edit_questionbank_info)) if($row['technical_id']==$skill['id'])echo "selected";?>><?php echo $skill['skill_name']; ?></option> 
-                                <?php } ?>
-                                </select>
-								</div>
-                        </div>
-
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Main Topic <span class="required">*</span></label>
-                                        <select id="topic_id"  name="topic_id" class="form-control"  onchange="getSubtopic(this.value)">
-                                          
-                                        </select>
-                                    </div>
-                                </div>
-									 <div class="col-md-4">
-								  <div class="form-group">
-                                        <label for="exampleInputEmail1">Subtopic<span class="required">*</span></label>
-                                     <select id="subtopic_id"  name="subtopic_id" class="form-control"  onchange="getLineitem(this.value)">
-                                          
-                                        </select>
-										</div>
-									</div>
-                                </div>
-									 <div class="container-fluid">
-									 <div class="col-md-4">
-								  <div class="form-group">
-                                        <label for="exampleInputEmail1">Line Item(Level 1)<span class="required">*</span></label>
-                                     <select id="lineitem_id"  name="lineitem_id" class="form-control"  onchange="getLineitemlevel(this.value)">
-                                          
-                                        </select> 
-										</div>
-									</div>
-									
-										 <div class="col-md-4">
-								  <div class="form-group">
-                                        <label for="exampleInputEmail1">Line Item(Level 2)<span class="required">*</span></label>
-                                     <select id="lineitemlevel_id"  name="lineitemlevel_id" class="form-control">
-                                           
-                                        </select> 
-										</div>
-									</div>
-								
+    		 <form method="post" action="<?php echo base_url();?>emp/addemployee" enctype="multipart/form-data">
+		    <?php  $org = $this->session->userdata('OrgLogged_In');
 									 
-									 <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Level<span class="required">*</span></label>
-                                      <select  name="level" class="form-control">
-									                                          
-										<option value="Expert"<?php if (!empty($edit_questionbank_info)) if($row['level']=='Expert')echo "selected";?>>Expert</option>
-										<option value="Medium"<?php if (!empty($edit_questionbank_info)) if($row['level']=='Medium')echo "selected";?>>Medium</option>
-										<option value="Beginner"<?php if (!empty($edit_questionbank_info)) if($row['level']=='Beginner')echo "selected";?>>Beginner</option>
-									 </select>
-									 </div>
-                                </div>
-								
-								
-								</div>
-								<div class="container-fluid">
-								<div class="col-md-4">
-                                    <div class="form-group">
-									<label for="exampleInputEmail1">Question Type<span class="required">*</span></label>
-									<select  name="ques_type" class="form-control" onchange="hideshowfun()" id="category">
-									                                      
-                                        <option value="MCQ"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='MCQ')echo "selected";?>>MCQ</option>
-										<option value="Subjective"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='Subjective')echo "selected";?>>Subjective</option>
-										<option value="Practical"<?php if (!empty($edit_questionbank_info)) if($row['ques_type']=='Practical')echo "selected";?>>Practical</option>
-								
-									</select>
-									 </div>
-                                </div>
-								</div>
-								<div class="container-fluid">
-									<div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Question<span class="required">*</span></label>
-									 <textarea name="question" id="question" class="form-control ckeditor"  required><?php if (!empty($edit_questionbank_info)) echo $row['question'];?></textarea>
-								   </div>
-								   </div>
-                                </div>
-								<div id="name">
-								<div class="container-fluid">
-								
-					<div class="form-group">
-			  
+			  ?>
+              <div class="panel-body">
+			 <div class="form-group">
+                 <input type="hidden" name="org_id" id="org_id" class="form-control" value="<?php echo $org['org_id']; ?>">
+                  <div class="col-sm-4">
+				   <label>Employee No.:</label>
+                    <input type="text" name="emp_no" id="emp_no" class="form-control">
+                  </div>
+				  <div class="col-sm-4">
+				  <label>Photo:</label>
+				 <input type="file" name="photo" id="photo" class="form-control">
+                  </div>
 				  
-				 <div class="col-sm-12">
-				   <label>Option 1:</label>
-                    <textarea name="option1" id="option1" class="form-control ckeditor"><?php if (!empty($edit_questionbank_info)) echo $row['option1'];?></textarea>
+				  <label>Name:</label>
+                  <div class="col-sm-4">
+                    <input type="text" name="emp_name" id="emp_name" class="form-control">
                   </div>
+				  
+                </div>
+				 <div class="form-group">
+				  
 				  </div>
-				  </div>
-				  <div class="container-fluid">
-				  <div class="col-sm-12">
-				  <div class="form-group">
-                  
-				   <label>Option 2:</label>
-                    <textarea name="option2" id="option2" class="form-control ckeditor"><?php if (!empty($edit_questionbank_info)) echo $row['option2'];?></textarea>
+			  <div class="form-group">
+                  <div class="col-sm-4">
+				  <label>Salary:</label>
+                    <input type="text" name="emp_salary" id="emp_salary" class="form-control">
                   </div>
-				  </div>
-				  </div>
-				  <div class="container-fluid">
-				   <div class="col-sm-12">
-				  <div class="form-group">
-                 
-				   <label>Option 3:</label>
-                    <textarea name="option3" id="option3" class="form-control ckeditor"><?php if (!empty($edit_questionbank_info)) echo $row['option3'];?></textarea>
+				 
+                  <div class="col-sm-4">
+				   <label>Email-Id:</label>
+                    <input type="text" name="email" id="email" class="form-control">
+                  </div>
+				 
+                  <div class="col-sm-4">
+				   <label>Password:</label>
+                   <input type="password" name="password" id="password" class="form-control">
                   </div>
                 </div>
-				</div>
-				<div class="container-fluid">
-				<div class="form-group">
-				
-				  <div class="col-sm-12">
-				   <label>Option 4:</label>
-                    <textarea name="option4" id="option4" class="form-control ckeditor"><?php if (!empty($edit_questionbank_info)) echo $row['option4'];?></textarea>
+				 <div class="form-group">
+                  
+                  <div class="col-sm-4">
+				   <label>Contact No.:</label>
+                    <input type="tel" name="mobile" id="mobile" class="form-control">
                   </div>
-				  </div>
-				  </div>
-				  <div class="container-fluid">
-				  <div class="form-group">
-                  <div class="col-sm-12">
-				   <label>Option 5:</label>
-                    <textarea name="option5" id="option5" class="form-control ckeditor"><?php if (!empty($edit_questionbank_info)) echo $row['option5'];?></textarea>
-                  </div>
-				  </div>
-				  </div>
-				  <div class="container-fluid">
-				  <div class="form-group">
-				  <div class="col-sm-8">
-				  <div class="multiselect">
-				  <div class="selectBox" onclick="showCheckboxes()">
-				  
-				   <label>Correct Answer: <span class="required">*</span></label>
-				   <select  class="form-control" style="height:100px;">	
-					<option>Select Answer</option>
-					</select>
-					<div class="overSelect"></div>
-				   </div>
-				  <div id="checkboxes">
-     
-				<?php 
-					  if(!empty($options))
-						foreach($options as $key){
-							$checked="";
-							for($i=0;$i<sizeof($questionbank_answer);$i++){
-								if($key['options_id']==$questionbank_answer[$i]['answer_id']){
-									$checked ="checked";
-									break;
-								}
-							}
-							 
-						?>
-	                    <input type="checkbox" <?php echo $checked; ?> name="correct_answer[]" id="correct_answer[]" value="<?php echo $key['options_id'];?>">&nbsp;&nbsp;<?php echo $key['options_type']; ?>&nbsp;&nbsp;
-					<?php
-						}
-					?>
-			
-		 		</div>
-				   </div>
-				  </div>
-				  </div>
-				  </div>
-				  </div>
 				 
-		 
-								
-								<div class="box-body">
-								<input type="hidden" name="is_admin" value="1" class="form-control"> 
-								</div>
-				  <div class="container-fluid">
-				   <div class="col-sm-12">
-				  <div class="form-group" id="comp_name" style="display:none;">
-                 
-				  <label>Answer: <span class="required">*</span></label>
-                    <textarea name="sub_answer" id="sub_answer" class="form-control ckeditor" style="height:100px;"><?php if (!empty($edit_questionbank_info)) echo $row['sub_answer'];?></textarea>
+                  <div class="col-sm-4">
+				    <label>Department.:</label>
+                    <select  name="dept_id" id="dept_id" class="form-control">
+					<option value="">Select Department</option>
+					<?php foreach($result as $key){?>
+					<option value="<?php echo $key['dept_id']; ?>"><?php echo $key['department_name']; ?></option>
+					<?php } ?>
+				  </select>
                   </div>
-				  
-                </div>	
-				</div>
-                                <div class="panel-body"></div>
-                                <button type="submit" class="btn bg-navy" type="submit">Save Question
-                                </button><br/><br/>
-                            
-                        
-
-                        
-                    </div>
-
-                </form>
+				 
+                  <div class="col-sm-4">
+				   <label>Address:</label>
+				 <textarea class="form-control" name="address"></textarea>
+                  </div>
+                </div>		  				 
+              </div><!-- panel-body -->
+			  <span class="text-danger"><?php echo validation_errors(); ?></span>
+              <div class="panel-footer">
+                <button class="btn btn-primary" name="submit_employee" id="submit_employee">Submit</button>
+                <button type="reset" class="btn btn-default">Reset</button>
+              </div><!-- panel-footer -->
+			  </form>
 
 
 
