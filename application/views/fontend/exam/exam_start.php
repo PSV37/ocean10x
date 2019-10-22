c2b92f59441db8586aa76adb19d92c20dad13b06
shyam <shyam@itdivine.in>
2019-10-22 15:20:02 +0530

    online test builing form

--- application/views/fontend/exam/exam_start.php
+++ application/views/fontend/exam/exam_start.php
@@ -24,14 +24,16 @@
             <div class="job-header">
               <div class="contentbox">
                 <div class="formpanel">
-                  <!-- <?php echo $this->session->flashdata('change_password'); ?> -->
-                  <span id='timer'></span>
+
+                  <h3 style="color:#FF0000" align="center">
+                    Time Left : <span id='timer'></span>
+                  </h3>
 
                   <form id="submit" class="submit-form" action="<?php echo base_url(); ?>exam/exam_start" method="post">
                     <div class="row">
                       <div class="col-md-12 col-sm-12">
 
-                        <!-- <label>Instruction:</label> 
+                        <label>Instruction:</label> 
                           <ul>
                             <li>This is a FREE online test. DO NOT pay money to anyone to attend this test.</li>
                             <li>All Questions are compulsory.</li>
@@ -40,8 +42,8 @@
                             <li>Each question carry 1 mark, no negative marks.</li>
                             <li>DO NOT refresh the page.</li>
                             <li>All the best.</li>
-                          </ul> -->
-                          <!-- <button type="submit" class="btn btn-primary pull-right">Start Test</button> -->
+                          </ul>
+                          <button type="submit" class="btn btn-primary pull-right">Start Test</button>
                       </div>
                     </div>
                   </form>
@@ -58,32 +60,32 @@
  <?php $this->load->view("fontend/layout/footer.php"); ?>
  <script>
   //define your time in second
-    var c=60;
-        var t;
-        timedCount();
+    var c=1800;
+    var t;
+    timedCount();
 
-        function timedCount()
+    function timedCount()
     {
 
-          var hours = parseInt( c / 3600 ) % 24;
-          var minutes = parseInt( c / 60 ) % 60;
-          var seconds = c % 60;
+        var hours = parseInt( c / 3600 ) % 24;
+        var minutes = parseInt( c / 60 ) % 60;
+        var seconds = c % 60;
 
-          var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+        var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
 
             
-          $('#timer').html(result);
-            if(c == 0 )
-      {
-              //setConfirmUnload(false);
-                //$("#quiz_form").submit();
-        window.location="logout.html";
-      }
-            c = c - 1;
-            t = setTimeout(function()
+      $('#timer').html(result);
+      if(c == 0 )
+        {
+                //setConfirmUnload(false);
+                  //$("#quiz_form").submit();
+          // window.location="logout.html";
+        }
+          c = c - 1;
+          t = setTimeout(function()
       {
        timedCount()
       },
       1000);
-        }
+    }
   </script>
