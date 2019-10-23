3fe40ba3621e9033bf3cf0319528d5c056afaaa2
unknown <santu@edunetsol.com>
2019-10-23 18:25:12 +0530

    Employer register section

--- application/views/fontend/employer/dashboard.php
+++ application/views/fontend/employer/dashboard.php
@@ -328,7 +328,7 @@
                                         <div class="col-md-6 col-sm-12">
                                         <div class="formrow">
                                         	<label class="control-label">Company Industry: <span class="required">*</span></label>
-                                            <select name="company_category" class="form-control" data-style="btn-default" data-live-search="true">
+                                            <select name="company_category" id="company_category" class="form-control" data-style="btn-default" data-live-search="true">
                                             	<option value="">Select Industry</option> 
                                                 <?php if(!empty($company_info->company_category)) {
                                                 echo $this->job_category_model->selected($company_info->company_category);
