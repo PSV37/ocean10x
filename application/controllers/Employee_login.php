a7551f999acc311c45e89540d205e5ecb055a809
unknown <santu@edunetsol.com>
2019-10-23 12:20:49 +0530

    Employee(Employee login)

--- application/controllers/Employee_login.php
+++ application/controllers/Employee_login.php
@@ -50,45 +50,7 @@ class Employee_Login extends CI_Controller
     }
     
     
-    public function forgot_pass()
-    {
-    	if($this->input->post('email')==''){
-    	 $this->load->view('fontend/employer/forgot');
-    	 return;
-	    }
-	 
-        $jobseeker_email    = $this->input->post('email');
-        $result             = $this->employer_login_model->check_forgot_user_info($jobseeker_email);
-        if (!empty($result)) {
-            $data['job_seeker_id'] = $result->job_seeker_id;
-            $data['user_name']     = $result->user_name;
-            $this->session->set_userdata($data);
-            $this->session->set_flashdata('invalid', '<div class="alert alert-success text-center">An email has been sent to you to reset password.</div>');
-            redirect('employer_login');
-        } else {
-            $this->session->set_flashdata('invalid', '<div class="alert alert-danger text-center">Sorry! Invalid email address provided!</div>');
-            redirect('employer_login/forgot_pass');
-        }
-    }
     
-     public function reset_password($hash = '')
-    {
-    
-    	if(!$this->input->post('password')){
-    	 $this->load->view('fontend/employer/reset_password');
-    	 return;
-    	}
-	
-	    $pass    = md5($this->input->post('password'));
    
-        if ($this->employer_login_model->reset_account($hash,$pass)) {
-            $this->session->set_flashdata('verify_msg', '<div class="alert alert-success text-center">Your password is successfully reset! Please login to access your account!</div>');
-            redirect('employer_login');
-        } else {
-            $this->session->set_flashdata('verify_msg', '<div class="alert alert-danger text-center">Sorry! There is error in verifying your account!</div>');
-            redirect('employer_login');
-        }
-
-    }
     
 }
