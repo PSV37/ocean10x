fb9bf0b2bf7bb0f0af58412fb31bc1a2a8699ac9
unknown <santu@edunetsol.com>
2019-10-23 11:19:22 +0530

    Employee(Employee Section)

--- application/views/fontend/employee/employee_master.php
+++ application/views/fontend/employee/employee_master.php
@@ -60,7 +60,7 @@
 				<td><?php echo $key['emp_updated_date']; ?></td>
 				<!--<td><?php if($key['emp_status']=='1'){ echo "Active"; }else{ echo "Inactive"; } ?></td>-->
                 <td>
-                 &nbsp;&nbsp; <a href="<?php echo base_url();?>employer/edit_city?id=<?php echo $key['emp_id']; ?>"><i class="fa fa-pencil"></i></a>
+                 &nbsp;&nbsp; <a href="<?php echo base_url();?>employer/editemployee?id=<?php echo $key['emp_id']; ?>"><i class="fa fa-pencil"></i></a>
                  &nbsp;&nbsp; <!--<a href="<?php echo base_url();?>emp/deletestatus?id=<?php echo $key['emp_id']; ?>"> <i class="fa fa-toggle-on"></i></a>-->
                 &nbsp;&nbsp; <a href='#' "title='Delete Record' data-toggle="modal" data-target="#deleteModal"  onclick="$('#del_id').val('<?php echo $key['emp_id'];?>');"><i class="fa fa-trash-o"></i></a>
 				</td>
