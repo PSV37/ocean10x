2d272ef2b2ed384cb7bd64ebf3ca055e99076b58
unknown <santu@edunetsol.com>
2019-10-24 18:13:48 +0530

    add pincode master

--- application/models/Pincode_model.php
+++ application/models/Pincode_model.php
@@ -1,7 +1,7 @@
 <?php
 class Pincode_model extends MY_Model{
  
-    function search_blog($title){
+    function search_pincode($pincode){
         $this->db->like('pincode', $pincode , 'both');
         $this->db->order_by('pincode', 'ASC');
         $this->db->limit(10);
