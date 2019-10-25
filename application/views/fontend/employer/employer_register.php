85929f611cdc30e0c27dd433ef8eb87640ff891f
unknown <santu@edunetsol.com>
2019-10-25 16:06:48 +0530

    username employer register section

--- application/views/fontend/employer/employer_register.php
+++ application/views/fontend/employer/employer_register.php
@@ -381,6 +381,19 @@ $("#company_category").select2( {
   });
  });
 </script>-->
+<script>
+$(function() {
+  // choose target dropdown
+  var select = $('select');
+  select.html(select.find('option').sort(function(x, y) {
+    // to change to descending order switch "<" for ">"
+    return $(x).text() > $(y).text() ? 1 : -1;
+  }));
+
+  // select default item after sorting (first item)
+  // $('select').get(0).selectedIndex = 0;
+});
+</script>
   
   
  <?php $this->load->view("fontend/layout/footer.php"); ?>
