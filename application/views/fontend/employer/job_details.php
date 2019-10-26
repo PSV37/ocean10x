b97a1ab3bd2190832dbde5279d577e2f4e9d5c79
unknown <santu@edunetsol.com>
2019-10-26 13:06:36 +0530

    Add Job post employer section

--- application/views/fontend/employer/job_details.php
+++ application/views/fontend/employer/job_details.php
@@ -137,7 +137,7 @@
                     </div>
 
                     <div class="col-md-4">
-                      <?php =$this->Job_seeker_experience_model->experience_list_by_id($v_applicant->job_seeker_id)[0]->company_name;;?>
+                      <?= $this->Job_seeker_experience_model->experience_list_by_id($v_applicant->job_seeker_id)[0]->company_name;?>
 
                     	<div class="exp">
 						            <label>Company Name</label>
