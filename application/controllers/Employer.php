933fb495461797d4785d600c2d738d93920c5956
unknown <santu@edunetsol.com>
2019-10-26 18:12:15 +0530

    validation employee

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1158,7 +1158,6 @@ public function postEditData(){
 		$this->form_validation->set_rules('emp_name', 'Name', 'required');
 		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[employee.email]');
 		$this->form_validation->set_rules('mobile', ' Contact No.', 'required|regex_match[/^[0-9]{10}$/]');
-		$this->form_validation->set_rules('password', 'password', 'required|max_length[15]|min_length[6]|alpha_numeric');
 		$this->form_validation->set_rules('dept_id', 'Department', 'required');
 		$this->form_validation->set_rules('address', 'Address', 'required');
 		$this->form_validation->set_rules('pincode', 'Pincode', 'required|numeric');
