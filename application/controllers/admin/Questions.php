84b79c7f7492e70bd49012231eee12ae9e8ff248
unknown <santu@edunetsol.com>
2019-10-30 16:47:45 +0530

    autocomplete off admin topic section

--- application/controllers/admin/Questions.php
+++ application/controllers/admin/Questions.php
@@ -36,11 +36,11 @@ class Questions extends MY_Controller
 		
 		$where_all = "questionbank.ques_status!='0' and questionbank.ques_id='".$id."'";
         $join_emp = array(
-                'skill_master' => 'skill_master.id=questionbank.technical_id |INNER',
-                'topic' => 'topic.topic_id=questionbank.topic_id |INNER',
-				'subtopic' => 'subtopic.subtopic_id=questionbank.subtopic_id |INNER',
-				'lineitem' => 'lineitem.lineitem_id=questionbank.lineitem_id |INNER',
-				'lineitemlevel' => 'lineitemlevel.lineitemlevel_id=questionbank.lineitemlevel_id |INNER',
+                'skill_master' => 'skill_master.id=questionbank.technical_id |OUTER',
+                'topic' => 'topic.topic_id=questionbank.topic_id |OUTER',
+				'subtopic' => 'subtopic.subtopic_id=questionbank.subtopic_id |OUTER',
+				'lineitem' => 'lineitem.lineitem_id=questionbank.lineitem_id |OUTER',
+				'lineitemlevel' => 'lineitemlevel.lineitemlevel_id=questionbank.lineitemlevel_id |OUTER',
             );
 			
         $data['questionbank'] = $this->Master_model->getMaster('questionbank',$where_all,$join_emp,$where_all);
