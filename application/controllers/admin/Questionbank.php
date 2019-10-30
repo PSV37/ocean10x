a0da1435751bf5a3db67cd1fc8045ccbbba42038
unknown <santu@edunetsol.com>
2019-10-30 17:26:30 +0530

    autocomplete off admin topic section

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -17,10 +17,31 @@ class Questionbank extends MY_Controller
     public function index()
     {   
 
-        $data['title'] = 'Edit Questionbank';
+        $data['title'] = 'Add Questionbank';
 
+        $where_skill= "status=1";
+		
+        $data['skill_master'] = $this->Master_model->getMaster('skill_master',$where_skill);
+		
+		$where_opt= "options.status=1";
+        $data['options'] = $this->Master_model->getMaster('options',$where_opt);
+		
+        $where_topic= "topic.topic_status=1";
+        $data['topic'] = $this->Master_model->getMaster('topic',$where_topic);
+        
+        $where_subtopic = "subtopic.subtopic_status='1'";
+		$data['subtopic'] = $this->Master_model->getMaster('subtopic',$where_subtopic);
+		
+		$where_lineitem = "lineitem.lineitem_status='1'";
+		$data['lineitem'] = $this->Master_model->getMaster('lineitem',$where_lineitem);
+		
+		$where_lineitemlevel = "lineitemlevel.lineitemlevel_status='1'";
+		$data['lineitemlevel'] = $this->Master_model->getMaster('lineitemlevel',$where_lineitemlevel);
+		
+		
+        $data['questionbank'] = $this->Master_model->getMaster('questionbank');
 
-        $this->load->view('admin/jobsetting/questionbank_master');
+        $this->load->view('admin/jobsetting/questionbank_master', $data);
     }
 
         public function save_questionbank($id = null){
@@ -110,8 +131,6 @@ class Questionbank extends MY_Controller
 
     public function edit_questionbank($id){
         $data['title']="Edit Questionbank";
-		//$where_opt= "options.status=1";
-        $data['options'] = $this->Master_model->getMaster('options');
         		
         $where_questionbank = "ques_id='$id'";
         $data['edit_questionbank_info'] = $this->Master_model->getMaster('questionbank',$where_questionbank);
