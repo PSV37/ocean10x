bf8ed6026343760829c25b5dc5055453ad1966f4
unknown <santu@edunetsol.com>
2019-10-30 14:21:24 +0530

    pin code add jobseeker personal information section

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -7,7 +7,10 @@
 }
 </style>  
 
-
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
           <div class="section lb">
                 <div class="container">
