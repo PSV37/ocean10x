22e3bb4ae910380818e8a2c65cc380a0bae3637a
unknown <santu@edunetsol.com>
2019-10-30 10:18:31 +0530

    employer phone validation

--- application/views/fontend/employer/dashboard.php
+++ application/views/fontend/employer/dashboard.php
@@ -799,7 +799,7 @@ $(function() {
                 var a = [];
                 var k = e.which;
 
-                for (i = 51; i < 58; i++)
+                for (i = 38; i < 48; i++)
                     a.push(i);
 
                 if (!(a.indexOf(k)>=0))
