5f48143f5d4e19e2f875bf5570ef9984d8595a1b
unknown <santu@edunetsol.com>
2019-10-30 12:10:18 +0530

    pin code add jobseeker personal information section

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -660,17 +660,3 @@ function get_skills_autocomplete(){
 
 
 } //end function
-
-
-
-
-public function search(){
- 
-        $term = $this->input->get('term');
- 
-        $this->db->like('pincode', $term);
- 
-        $data = $this->db->get("pincode")->result();
- 
-        echo json_encode( $data);
-    }
