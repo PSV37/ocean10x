20ccf417b3fb4dcc8c8641fc39a1710c815cd8a4
unknown <santu@edunetsol.com>
2019-10-30 16:42:00 +0530

    autocomplete off admin topic section

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -38,8 +38,8 @@ class Questionbank extends MY_Controller
 		$where_lineitemlevel = "lineitemlevel.lineitemlevel_status='1'";
 		$data['lineitemlevel'] = $this->Master_model->getMaster('lineitemlevel',$where_lineitemlevel);
 		
-		$where_all = "questionbank.ques_status!='0' and questionbank.ques_id='".$id."'";
-        $data['questionbank'] = $this->Master_model->getMaster('questionbank',$where_all);
+		
+        $data['questionbank'] = $this->Master_model->getMaster('questionbank');
 
         $this->load->view('admin/jobsetting/questionbank_master', $data);
     }
@@ -133,9 +133,10 @@ class Questionbank extends MY_Controller
         $data['title']="Edit Questionbank";
 		//$where_opt= "options.status=1";
         $data['options'] = $this->Master_model->getMaster('options');
-       
+        
+        $data['questionbank'] = $this->Master_model->getMaster('questionbank',$where_all,$join_emp);
 		
-        $where_questionbank = "questionbank.ques_status!='0' and questionbank.ques_id='".$id."'";
+        $where_questionbank = "ques_id='$id'";
         $data['edit_questionbank_info'] = $this->Master_model->getMaster('questionbank',$where_questionbank);
 		
 		$where_answer = "question_id='$id'";
