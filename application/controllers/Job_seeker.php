73288f74e17c6cecc61592fa3f91b1b9d4dd73d4
unknown <santu@edunetsol.com>
2019-10-30 14:07:47 +0530

    pin code add jobseeker personal information section

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -648,17 +648,17 @@ exit;*/
     }
 
 // to get skill master for autocomplete
-function get_skills_autocomplete(){
-        if (isset($_GET['term'])) {
+//function get_skills_autocomplete(){
+       // if (isset($_GET['term'])) {
 
-            $result = $this->Job_seeker_experience_model->search_skills($_GET['term']);
-            if (count($result) > 0) {
-            foreach ($result as $row)
-                $arr_result[] = $row->skill_name;
+           // $result = $this->Job_seeker_experience_model->search_skills($_GET['term']);
+           // if (count($result) > 0) {
+           // foreach ($result as $row)
+             //   $arr_result[] = $row->skill_name;
                 echo json_encode($arr_result);
-            }
-        }
-    }
+            //}
+       // }
+  //  }
 
 
 public function search(){
