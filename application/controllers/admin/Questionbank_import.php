cfe36480d662d24f781618121c6faef9d50db2cd
unknown <santu@edunetsol.com>
2019-10-31 16:05:13 +0530

    import questionbank

--- application/controllers/admin/Questionbank_import.php
+++ application/controllers/admin/Questionbank_import.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Questionbank_import extends MY_Controller {
+class Questionbank_import extends CI_Controller {
 
 	public function __construct(){
 
--- application/models/Questionbank_model.php
+++ application/models/Questionbank_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Questionbank_model extends MY_Model {
+class Questionbank_model extends CI_Model {
 
     function insertRecord($record){
         
