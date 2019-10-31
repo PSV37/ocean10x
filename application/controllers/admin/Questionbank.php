b0c1fbb352c3867566035ab5baba33e6091bc343
unknown <santu@edunetsol.com>
2019-10-31 16:51:45 +0530

    import questionbank

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -237,4 +237,69 @@ function getLineitemlevel(){
 	 echo $result;
 }
 
+
+public function importquestion(){
+		$this->load->model('Questionbank_model');
+		// Check form submit or not 
+ 		if($this->input->post('upload') != NULL ){ 
+   			$data = array(); 
+   			if(!empty($_FILES['file']['name'])){ 
+    			// Set preference 
+    			$config['upload_path'] = 'assets/files/'; 
+    			$config['allowed_types'] = 'csv'; 
+    			$config['max_size'] = '1000'; // max_size in kb 
+    			$config['file_name'] = $_FILES['file']['name']; 
+
+    			// Load upload library 
+    			$this->load->library('upload',$config); 
+   
+    			// File upload
+    			if($this->upload->do_upload('file')){ 
+     				// Get data about the file
+     				$uploadData = $this->upload->data(); 
+     				$filename = $uploadData['file_name']; 
+
+     				// Reading file
+                    $file = fopen("assets/files/".$filename,"r");
+                    $i = 0;
+
+                    $importData_arr = array();
+                       
+                    while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) {
+                        $num = count($filedata);
+
+                        for ($c=0; $c < $num; $c++) {
+                            $importData_arr[$i][] = $filedata[$c];
+                        }
+                        $i++;
+                    }
+                    fclose($file);
+
+                    $skip = 0;
+
+                    // insert import data
+                    foreach($importData_arr as $userdata){
+                        if($skip != 0){
+                            $this->Questionbank_model->insertRecord($userdata);
+                        }
+                        $skip ++;
+                    }
+     				$data['response'] = 'successfully uploaded '.$filename; 
+    			}else{ 
+     				$data['response'] = 'failed'; 
+    			} 
+   			}else{ 
+    			$data['response'] = 'failed'; 
+   			} 
+   			// load view 
+   			$this->load->view('admin/jobsetting/questionbank_view',$data); 
+  		}else{
+   			// load view 
+   			$this->load->view('admin/jobsetting/questionbank_view'); 
+  		} 
+
+	}
+
+
+
 }
