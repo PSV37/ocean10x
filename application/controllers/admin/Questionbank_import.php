c696663bde2b87763b03df6e2e32ce813dca8090
unknown <santu@edunetsol.com>
2019-10-31 16:06:11 +0530

    import questionbank

--- application/controllers/admin/Questionbank_import.php
+++ application/controllers/admin/Questionbank_import.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Questionbank_import extends CI_Controller {
+class Questionbank_import extends MY_Controller {
 
 	public function __construct(){
 
