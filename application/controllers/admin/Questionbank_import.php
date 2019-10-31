0ab64cef80d246f5949f13b01ec363d6f91179e2
unknown <santu@edunetsol.com>
2019-10-31 15:06:54 +0530

    import questionbank

--- application/controllers/admin/Questionbank_import.php
+++ application/controllers/admin/Questionbank_import.php
@@ -7,9 +7,6 @@ class Questionbank_import extends MY_Controller {
 
 		parent::__construct();
 
-		// load base_url
-		$this->load->helper('url');
-
 		// Load Model
 		$this->load->model('Questionbank_model');
 	}
