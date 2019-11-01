1b1d3b625f23fdb866416b9e2c810130c44b2661
unknown <santu@edunetsol.com>
2019-11-01 18:19:29 +0530

    import questionbank

--- application/models/Questionbank_model.php
+++ application/models/Questionbank_model.php
@@ -25,7 +25,7 @@ class Questionbank_model extends MY_Model {
                 $this->db->insert('questionbank', $newquestionsbank);
 				
 				$newquestionsbanks = array(
-					"answer_id" => trim($record[0])
+					"answer_id" => trim($record[13])
                 );
 				
                 $this->db->insert('questionbank_answer', $newquestionsbanks);
