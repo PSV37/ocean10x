d388113a89048dbd7e9a0a1a6f4f4c631875a085
shyam <shyam@itdivine.in>
2019-11-01 10:53:32 +0530

    exam result show in company

--- application/views/fontend/exam/result_details.php
+++ application/views/fontend/exam/result_details.php
@@ -46,11 +46,12 @@
               </thead>
               <tbody>
                 <?php $key = 1; if (!empty($exam_attended_candidates)): foreach ($exam_attended_candidates as $ct_row) :
+
                   $js_id = $ct_row['js_id'];
                   $exam_res = getExamResultByID($js_id,$job_id); 
-                  print_r($exam_res);
-                  // $marks = $ct_row['total_marks']; 
-                  // $per = ($marks * 100)/NUMBER_QUESTIONS;
+                  if (!empty($exam_res)): foreach ($exam_res as $res_row) :
+                  $marks = $res_row['total_marks']; 
+                  $percentage = ($marks * 100)/NUMBER_QUESTIONS;
                 ?>
                     <tr>
                       <td><?php echo $key ?></td>
@@ -58,13 +59,15 @@
                         <h3><a target="_blank" href="<?php echo base_url() ?>employer/view_resume/<?php echo $ct_row['js_id'] ."/".$job_id; ?>"><?php echo $ct_row['full_name'] ?></a></h3>
                       </td>
                       <td><?php echo NUMBER_QUESTIONS; ?></td>
-                      <td><?php echo '12'; ?></td>
-                      <td><?php echo '1'; ?></td>
-                      <td><?php echo '5%'; ?></td>
+                      <td><?php echo $res_row['total_questions'] ?></td>
+                      <td><?php echo $res_row['total_marks'] ?></td>
+                      <td><?php echo round($percentage, 2).'%'; ?></td>
                      
                   </tr>
                   <?php
                       $key++;
+                      endforeach;
+                      
                       endforeach;
                   ?>
                   <?php else : ?> 
