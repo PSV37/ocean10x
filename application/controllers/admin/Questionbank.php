3aa0909867a09c1d23e7ab10477ab0df629236b6
unknown <santu@edunetsol.com>
2019-11-01 15:33:08 +0530

    import questionbank navigation

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -217,7 +217,7 @@ class Questionbank extends MY_Controller
 							$topic=$userdata[1];
 							$where_topic="topic_name='".$topic."'";
 							$topic_data = $this->Master_model->getMaster('topic', $where_topic);
-							print_r($topic_data); die;
+							print_r($topic_data); 
 							$userdata[1]=$topic_data[1]['topic_id'];
 							
 							$subtopic=$userdata[2];
