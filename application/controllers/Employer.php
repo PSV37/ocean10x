8cc4946c0385f948653d29dda726576f0d9e7853
shyam <shyam@itdivine.in>
2019-11-01 10:18:18 +0530

    exam result show in company

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1304,7 +1304,7 @@ function getLineitemlevel(){
                 'js_info' => 'js_info.job_seeker_id=js_test_info.js_id |INNER',
             );
             $select_result = "js_test_info.marks,js_test_info.test_id,js_test_info.js_id,js_info.full_name";
-            $data['exam_attended_candidates'] = $this->Master_model->getMaster('js_test_info', $where_test,$join_arr=false , $order = false, $field = false, $select_result,$limit=false,$start=false, $search=false);
+            $data['exam_attended_candidates'] = $this->Master_model->getMaster('js_test_info', $where_test,$join_arr , $order = false, $field = false, $select_result,$limit=false,$start=false, $search=false);
 
 
             // $where_test = "js_test_info.job_id='$job_id'";
