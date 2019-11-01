009224daebbbb5bc48e07f6f325aa1292682ba8f
unknown <santu@edunetsol.com>
2019-11-01 16:59:51 +0530

    import questionbank

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -246,7 +246,7 @@ class Questionbank extends MY_Controller
                         $skip ++;
                     }
      				$data['response'] = 'successfully uploaded '.$filename; 
-					 redirect('admin/questionbank-import',$data);
+					// redirect('admin/questionbank-import',$data);
     			}else{ 
      				$data['response'] = 'failed'; 
     			} 
