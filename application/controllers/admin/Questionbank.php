e433c3c9e20b53c324ae2faa25451ff3f6434901
unknown <santu@edunetsol.com>
2019-11-02 10:58:32 +0530

    import questionbank navigation

--- application/controllers/admin/Questionbank.php
+++ application/controllers/admin/Questionbank.php
@@ -247,7 +247,7 @@ class Questionbank extends MY_Controller
 							$userdata[13]=$options_data[0]['options_id'];
 							
 							$question='ques_id';
-							$where_question="ques_id";
+							$where_question="ques_id='.$question.'";
 							$userdata = $this->Master_model->getMaster('questionbank', $where_question);
 							print_r($userdata);die(); 
 							
