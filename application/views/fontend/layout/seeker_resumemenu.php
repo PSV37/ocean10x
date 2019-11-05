e89c69cdb6a62110a3713e65af51fcfb95552b1c
unknown <santu@edunetsol.com>
2019-11-05 14:30:05 +0530

    jobseeker dashboard menus

--- application/views/fontend/layout/seeker_resumemenu.php
+++ application/views/fontend/layout/seeker_resumemenu.php
@@ -24,9 +24,9 @@ echo '<ul class="nav nav-tabs">
 		<ul class="nav nav-tabs">
 			<li class="active"><a data-toggle="tab" href="#vspinfo" onClick="load_data('update_personalinfo','vspinfo');">My Profile</a></li>
 			<li><a data-toggle="tab" onClick="load_data('update_education','vsedu');" href="#vsedu" >Education</a></li>
-			<li><a data-toggle="tab" href="#vscareer" onClick="load_data('update_experience','vscareer');">Experience </a></li>
+			<!--<li><a data-toggle="tab" href="#vscareer" onClick="load_data('update_experience','vscareer');">Experience </a></li>-->
             <li><a data-toggle="tab" href="#vscareers" onClick="load_data('update_career','vscareers');">Work Experience</a></li>
 			<li><a data-toggle="tab" href="#vstrain" onClick="load_data('update_training','vstrain');">Certs & Trainings</a></li>
 			<li><a data-toggle="tab" href="#vsref" onClick="load_data('update_reference','vsref');">Reference</a></li>
-			<li><a data-toggle="tab" href="#vsphoto" onClick="load_data('update_photo','vsphoto');">Photo</a></li>
+			<!--<li><a data-toggle="tab" href="#vsphoto" onClick="load_data('update_photo','vsphoto');">Photo</a></li>-->
 		</ul>
