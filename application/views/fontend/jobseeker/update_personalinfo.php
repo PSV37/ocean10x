6f6fa19105f26dc6786340194d77c288b921c304
unknown <santu@edunetsol.com>
2019-11-05 16:25:31 +0530

    jobseeker dashboard menus

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -23,7 +23,7 @@
                                  <!-- Your Personal Information -->
                                  <div id="vspinfo" class="tab-pane fade in active">
                                 <h5>My Profile
-                                <a href="#" data-toggle="modal" data-target="#PersonalinfoUpdate" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>                
+                                              
                                 </h5>
 								 <div id="vsphoto" class="tab-pane fade in">
                                  
@@ -116,7 +116,7 @@
 								 
                                   <div class="table-responsive">          
                                 <table class="table">
-    
+     <a href="#" data-toggle="modal" data-target="#PersonalinfoUpdate" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a> 
                                   <tbody>
                                   <tr>
                                       <td width="30%">Father Name:</td>
