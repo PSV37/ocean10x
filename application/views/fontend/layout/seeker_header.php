8f51736de2ed1367126a5afdb3c7602d520be01b
shyam <shyam@itdivine.in>
2019-11-05 14:11:13 +0530

    Login logs of user

--- application/views/fontend/layout/seeker_header.php
+++ application/views/fontend/layout/seeker_header.php
@@ -78,10 +78,10 @@
   <!-- Header container end --> 
 </div>
 
-<?php echo $this->session->flashdata('message');; ?>
-
-
-
+<div id="smsg" class="alert alert-<?php echo $this->session->flashdata('type');?> alert-dismissible fade in">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  <strong><?php echo $this->session->flashdata('Message');?> </strong>
+</div>
 
 <div class="modal fade" id="lookingjob" tabindex="-1" role="dialog">
   <div class="modal-dialog modal-sm">
