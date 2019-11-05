4256ece2986c1b637ea70d9419812c66b99e359e
shyam <shyam@itdivine.in>
2019-11-05 15:06:29 +0530

    Login logs of user

--- application/views/fontend/jobseeker/seeker_info.php
+++ application/views/fontend/jobseeker/seeker_info.php
@@ -1,12 +1,12 @@
 <?php 
     $this->load->view('fontend/layout/seeker_header.php');
-   // print_r($results);
-?>                
-<div id="smsg" class="alert alert-<?php echo $this->session->flashdata('type');?> alert-dismissible fade in">
+?>        
+<div class="col-md-4"></div>        
+<div id="smsg" class="alert alert-<?php echo $this->session->flashdata('type');?> alert-dismissible fade in col-md-4">
   <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
   <strong><?php echo $this->session->flashdata('Message');?> 	</strong>
 </div>
-
+<div class="col-md-4"></div>
 <div class="tabdata"></div>
 
  <?php $this->load->view("fontend/layout/footer.php"); ?>
