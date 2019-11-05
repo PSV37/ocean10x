a08f353e78ce8305e756dab01f9cc6dcf806d591
unknown <santu@edunetsol.com>
2019-11-05 13:59:40 +0530

    jobseeker dashboard menus

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -72,7 +72,7 @@ class Job_seeker extends MY_Seeker_Controller
             $jobseeker_id     = $this->session->userdata('job_seeker_id');
             $js_personal_info = $this->job_seeker_personal_model->personalinfo_list_by_id($jobseeker_id);
 			//$jobseeker_id     = $this->session->userdata('job_seeker_id');
-			$job_seeker_photo = $this->Job_seeker_photo_model->photo_by_seeker($jobseeker_id);
+			//$job_seeker_photo = $this->Job_seeker_photo_model->photo_by_seeker($jobseeker_id);
 			$city = $this->Master_model->getMaster('city',$where=false);
 			$country = $this->Master_model->getMaster('country',$where=false);
 			$state = $this->Master_model->getMaster('state',$where=false);
@@ -83,7 +83,7 @@ class Job_seeker extends MY_Seeker_Controller
 				'city' => 'city.id = js_personal_info.city1_id|INNER'
 			);
 			
-			$results = $this->Master_model->get_master_row("js_personal_info","job_seeker_photo", $select = false, $where_sek, $join1);
+			$results = $this->Master_model->get_master_row("js_personal_info", $select = false, $where_sek, $join1);
 			//echo $this->db->last_query();
             echo $this->load->view('fontend/jobseeker/update_personalinfo', compact('jobseeker_id', 'js_personal_info', 'city', 'country', 'state', 'results'),true);
         }
