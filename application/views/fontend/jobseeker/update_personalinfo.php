ddd6d136d3c23970541191c385089c9da296d854
unknown <santu@edunetsol.com>
2019-11-05 16:21:24 +0530

    jobseeker dashboard menus

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -25,6 +25,95 @@
                                 <h5>My Profile
                                 <a href="#" data-toggle="modal" data-target="#PersonalinfoUpdate" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>                
                                 </h5>
+								 <div id="vsphoto" class="tab-pane fade in">
+                                 	<h5>Your Profile Picture</h5>
+                                    <?php echo $this->session->flashdata('msg'); ?>
+                                    <div class="row">
+                                          <div class="col-md-4">
+                                          
+                                          <div class="containe1r" id="crop-avatar">
+
+    <!-- Current avatar -->
+    <div class="avatar-view" title="Change the Photo">
+     <br/> <img src="<?php echo base_url() ?>upload/<?php if(!empty($job_seeker_photo->photo_path)) { echo $job_seeker_photo->photo_path;} else { echo "image-notfound.png";} ?>" alt="Photo">
+    </div>
+
+    <!-- Cropping modal -->
+    <div class="modal fade" id="avatar-modal" aria-hidden="true" aria-labelledby="avatar-modal-label" role="dialog" tabindex="-1">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <form class="avatar-form" action="<?php echo base_url('Job_seeker/save_photo');?>/<?php if(!empty($job_seeker_photo->js_photo_id)){echo $job_seeker_photo->js_photo_id;} ?>" enctype="multipart/form-data" method="post">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title" id="avatar-modal-label">Change Photo</h4>
+            </div>
+            <div class="modal-body">
+              <div class="avatar-body">
+
+                <!-- Upload image and data -->
+                <div class="avatar-upload">
+                  <input type="hidden" class="avatar-src" name="avatar_src">
+                  <input type="hidden" class="avatar-data" name="avatar_data">
+                  <label for="avatarInput">Upload Photo</label>
+                  <input type="file" class="avatar-input" id="avatarInput" name="avatar_file">
+                </div>
+
+                <!-- Crop and preview -->
+                <div class="row">
+                  <div class="col-md-9">
+                    <div class="avatar-wrapper"></div>
+                   <!-- <div class="dragtxt">Drag frame to adjust portrait.</div>-->
+                  </div>
+                  <div class="col-md-3">
+                    <div class="avatar-preview preview-lg"></div>
+                   <!-- <p>Must be an actual picture of you! Logos, clip-art, group pictures, and digitally-altered images not allowed.</p>-->
+                  </div>
+                </div>
+
+                <div class="row avatar-btns">
+                  <div class="col-md-9">
+                  </div>
+                  <div class="col-md-3">
+                    <button type="submit" class="btn btn-primary btn-block avatar-save2">Save Photo</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div> -->
+          </form>
+        </div>
+      </div>
+    </div><!-- /.modal -->
+
+    <!-- Loading state -->
+    <div class="loading" aria-label="Loading" role="img" tabindex="-1"></div>
+  </div>
+                                          
+                                        </div>
+                                        <div class="col-md-8">
+                          
+                          <?php if(!empty($error)){
+                            echo $error;
+                            };?>
+                          <?php if($job_seeker_photo->photo_path==""){
+                                echo "Please Upload Your Photo ";
+                            } ?>
+                            <div class="formpanel">
+                            <h6>Upload Max Size 300*300 pixel</h6><br>
+                         	<a href="#." onClick="runit();" class="btn">Click here to Upload Photo</a>
+                            
+                            
+                         
+                        
+                        
+                        </div>
+                        </div>
+                                        </div>
+                                 </div>
+								 
+								 
                                   <div class="table-responsive">          
                                 <table class="table">
     
@@ -105,93 +194,7 @@
                                 </table>
                                 </div>
                                 
-                                       <div id="vsphoto" class="tab-pane fade in">
-                                 	<h5>Your Profile Picture</h5>
-                                    <?php echo $this->session->flashdata('msg'); ?>
-                                    <div class="row">
-                                          <div class="col-md-4">
-                                          
-                                          <div class="containe1r" id="crop-avatar">
-
-    <!-- Current avatar -->
-    <div class="avatar-view" title="Change the Photo">
-     <br/> <img src="<?php echo base_url() ?>upload/<?php if(!empty($job_seeker_photo->photo_path)) { echo $job_seeker_photo->photo_path;} else { echo "image-notfound.png";} ?>" alt="Photo">
-    </div>
-
-    <!-- Cropping modal -->
-    <div class="modal fade" id="avatar-modal" aria-hidden="true" aria-labelledby="avatar-modal-label" role="dialog" tabindex="-1">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <form class="avatar-form" action="<?php echo base_url('Job_seeker/save_photo');?>/<?php if(!empty($job_seeker_photo->js_photo_id)){echo $job_seeker_photo->js_photo_id;} ?>" enctype="multipart/form-data" method="post">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title" id="avatar-modal-label">Change Photo</h4>
-            </div>
-            <div class="modal-body">
-              <div class="avatar-body">
-
-                <!-- Upload image and data -->
-                <div class="avatar-upload">
-                  <input type="hidden" class="avatar-src" name="avatar_src">
-                  <input type="hidden" class="avatar-data" name="avatar_data">
-                  <label for="avatarInput">Upload Photo</label>
-                  <input type="file" class="avatar-input" id="avatarInput" name="avatar_file">
-                </div>
-
-                <!-- Crop and preview -->
-                <div class="row">
-                  <div class="col-md-9">
-                    <div class="avatar-wrapper"></div>
-                    <div class="dragtxt">Drag frame to adjust portrait.</div>
-                  </div>
-                  <div class="col-md-3">
-                    <div class="avatar-preview preview-lg"></div>
-                    <p>Must be an actual picture of you! Logos, clip-art, group pictures, and digitally-altered images not allowed.</p>
-                  </div>
-                </div>
-
-                <div class="row avatar-btns">
-                  <div class="col-md-9">
-                  </div>
-                  <div class="col-md-3">
-                    <button type="submit" class="btn btn-primary btn-block avatar-save2">Save Photo</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div> -->
-          </form>
-        </div>
-      </div>
-    </div><!-- /.modal -->
-
-    <!-- Loading state -->
-    <div class="loading" aria-label="Loading" role="img" tabindex="-1"></div>
-  </div>
-                                          
-                                        </div>
-                                        <div class="col-md-8">
-                          
-                          <?php if(!empty($error)){
-                            echo $error;
-                            };?>
-                          <?php if($job_seeker_photo->photo_path==""){
-                                echo "Please Upload Your Photo ";
-                            } ?>
-                            <div class="formpanel">
-                            <h6>Upload Max Size 300*300 pixel</h6><br>
-                         	<a href="#." onClick="runit();" class="btn">Click here to Upload Photo</a>
-                            
-                            
-                         
-                        
-                        
-                        </div>
-                        </div>
-                                        </div>
-                                 </div>
+                                      
                             
                             
                             
