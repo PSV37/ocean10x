27a354859be15fb6a78ad0d82a49f52ee6da7a8d
unknown <santu@edunetsol.com>
2019-11-05 18:56:05 +0530

    jobseeker dashboard menus

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -1262,7 +1262,7 @@ function runit(){
       </div>
 	   <div class="panel-body"></div>   
 	  <div class="row">
-	  <div class="colmd-12">
+	  <div class="col-md-12">
 	   <div class="col-md-6 col-sm-12">
 				<div class="input-group">
 	                <label class="control-label" for="pwd">City</label>
