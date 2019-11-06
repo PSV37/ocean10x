6bd3ab27790a8dbb5e3a7615227c4338d002a46f
unknown <santu@edunetsol.com>
2019-11-06 13:23:34 +0530

    jobseeker dashboard menus

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -693,7 +693,7 @@ exit;*/
 
 function getspecilization(){
 	$education_id = $this->input->post('id');
-	$where['education_level_id'] = $education_id;
+	$where['edu_level_id'] = $education_id;
 	$specialization = $this->Master_model->getMaster('education_specialization',$where);
 	
 	
