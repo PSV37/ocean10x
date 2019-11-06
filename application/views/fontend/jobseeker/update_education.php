9546793b6fb06944c2f76c98abf54883c36546a2
unknown <santu@edunetsol.com>
2019-11-06 11:02:54 +0530

    jobseeker dashboard menus

--- application/views/fontend/jobseeker/update_education.php
+++ application/views/fontend/jobseeker/update_education.php
@@ -125,7 +125,7 @@
                 <div class="col-sm-9">
                   <select  name="course_id" id="course_id" class="form-control">
 				 <?php foreach($course as $courses){?>
-					<option value="<?php echo $courses['course_id']; ?>"<?php if($edcuaiton_list->course_id==$courses['course_id']){ echo "selected"; }?>><?php echo $courses['course_name']; ?></option>
+					<option value="<?php echo $courses['education_type_id']; ?>"<?php if($edcuaiton_list->course_id==$courses['education_type_id']){ echo "selected"; }?>><?php echo $courses['education_type']; ?></option>
 					<?php } ?>
 				 </select>
                 </div>
@@ -136,7 +136,7 @@
                 <div class="col-sm-9">
                   <select  name="board_id" id="board_id" class="form-control">
 				 <?php foreach($schoolboard as $boards){?>
-					<option value="<?php echo $boards['education_type_id']; ?>"<?php if($edcuaiton_list->board_id==$boards['education_type_id']){ echo "selected"; }?>><?php echo $boards['education_type']; ?></option>
+					<option value="<?php echo $boards['schoolboard_id']; ?>"<?php if($edcuaiton_list->board_id==$boards['schoolboard_id']){ echo "selected"; }?>><?php echo $boards['schoolboard_name']; ?></option>
 					<?php } ?>
 				 </select>
                 </div>
@@ -232,7 +232,7 @@
                 <div class="col-sm-9">
                   <select  name="course_id" id="course_id" class="form-control">
 				 <?php foreach($course as $courses){?>
-					<option value="<?php echo $courses['course_id']; ?>"><?php echo $courses['course_name']; ?></option>
+					<option value="<?php echo $courses['education_type_id']; ?>"><?php echo $courses['education_type']; ?></option>
 					<?php } ?>
 				 </select>
                 </div>
