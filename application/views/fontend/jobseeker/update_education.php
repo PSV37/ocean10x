93e56a9c63cab2206aa2abfaf55501c44b6efc1e
unknown <santu@edunetsol.com>
2019-11-06 13:47:47 +0530

    jobseeker dashboard menus

--- application/views/fontend/jobseeker/update_education.php
+++ application/views/fontend/jobseeker/update_education.php
@@ -404,7 +404,7 @@
 					success:function(res){
 						$('#specializations_id').html(res);
 						$('#specializations_id').val(<?php echo $row['id']; ?>);
-						getSubtopic_load();
+						getSpecial_load();
 					}
 					
 				}); 
