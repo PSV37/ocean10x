c10c9c41a5a80b3dbd8965013d81be33504a89a2
shyam <shyam@itdivine.in>
2019-11-07 12:49:11 +0530

    Exam work

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -44,44 +44,46 @@ class Exam extends MY_Seeker_Controller
             //To get topics fir
             $where_topic="job_id='$job_id'";
             $job_test_topics = $this->Master_model->getMaster('job_test_topics',$where_topic,$join = FALSE, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
-            
-           foreach($job_test_topics as $topic_row)
-           {
-            
-               $tid = $topic_row['test_question_id']; //test topic primary key
-               $topic_id = $topic_row['topic_id'];
-               $level = $topic_row['test_level'];
-               $no_ques = $topic_row['no_questions'];
+            if($job_test_topics)
+            {
+               foreach($job_test_topics as $topic_row)
+               {
                 
-            $where_topic="topic_id='$topic_id' AND level='$level' LIMIT $no_ques";
-            $data['questions'] = $this->Master_model->getMaster('questionbank',$where_topic,$join = FALSE, $order = false, $field = false, $select = false,$limit =false ,$start=false, $search=false);
-                
-            $question_id = $data['questions'][0]['ques_id'];
+                   $tid = $topic_row['test_question_id']; //test topic primary key
+                   $topic_id = $topic_row['topic_id'];
+                   $level = $topic_row['test_level'];
+                   $no_ques = $topic_row['no_questions'];
+                    
+                    $where_topic="topic_id='$topic_id' AND level='$level' LIMIT $no_ques";
+                    $data['questions'] = $this->Master_model->getMaster('questionbank',$where_topic,$join = FALSE, $order = false, $field = false, $select = false,$limit =false ,$start=false, $search=false);
+                        
+                    $question_id = $data['questions'][0]['ques_id'];
 
-            $wherechks = "question_id='$question_id'";
-            $data['ans'] = $this->Master_model->getMaster('questionbank_answer',$wherechks);
+                    $wherechks = "question_id='$question_id'";
+                    $data['ans'] = $this->Master_model->getMaster('questionbank_answer',$wherechks);
 
-                echo $this->db->last_query(); echo "<br><br>";
-                echo "<pre>";
-                print_r($data['ans']);
-           }
+                    // echo $this->db->last_query(); echo "<br><br>";
+                    // echo "<pre>";
+                    // print_r($data['ans']);
+                    $this->load->view('fontend/exam/exam_start',$data);
+               }
+               
+            }else{
+                // get all requried skills for this job post
+                $whereskill = "job_post_id='$job_id'";
+                $data['skills'] = $this->Master_model->get_master_row('job_posting', $select ='skills_required' , $whereskill, $join = FALSE);
+                $skill_id = $data['skills']['skills_required'];
 
+                $where_req_skill="technical_id IN (".$skill_id.")";
+                $data['questions'] = $this->Master_model->getMaster('questionbank',$where_req_skill,$join = FALSE, $order = false, $field = false, $select = false,$limit='1',$start=false, $search=false);
+                foreach($data['questions'] as $qrow){}
+                $question_id = $qrow['ques_id'];
+
+                $wherechks = "question_id='$question_id'";
+                $data['ans'] = $this->Master_model->getMaster('questionbank_answer',$wherechks);
+                $this->load->view('fontend/exam/exam_start',$data);
+            }
             
-            die;
-          // get all requried skills for this job post
-            $whereskill = "job_post_id='$job_id'";
-            $data['skills'] = $this->Master_model->get_master_row('job_posting', $select ='skills_required' , $whereskill, $join = FALSE);
-            $skill_id = $data['skills']['skills_required'];
-
-            $where_req_skill="technical_id IN (".$skill_id.")";
-            $data['questions'] = $this->Master_model->getMaster('questionbank',$where_req_skill,$join = FALSE, $order = false, $field = false, $select = false,$limit='1',$start=false, $search=false);
-            foreach($data['questions'] as $qrow){}
-            $question_id = $qrow['ques_id'];
-
-            $wherechks = "question_id='$question_id'";
-            $data['ans'] = $this->Master_model->getMaster('questionbank_answer',$wherechks);
-            //echo $this->db->last_query(); echo "<br><br>";
-            $this->load->view('fontend/exam/exam_start',$data);
         } else {
             redirect('exam');
         }
