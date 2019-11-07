7ac2aef1f6af19fbc54e3ad144f4c1d35dcb9a71
shyam <shyam@itdivine.in>
2019-11-07 12:37:44 +0530

    Exam work

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -54,7 +54,7 @@ class Exam extends MY_Seeker_Controller
                $no_ques = $topic_row['no_questions'];
                 
             $where_topic="topic_id='$topic_id' AND level='$level'";
-            $topics_ques = $this->Master_model->getMaster('questionbank',$where_topic,$join = FALSE, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
+            $topics_ques = $this->Master_model->getMaster('questionbank',$where_topic,$join = FALSE, $order = false, $field = false, $select = false,$limit='$no_ques',$start=false, $search=false);
                 echo $this->db->last_query(); echo "<br><br>";
                 echo "<pre>";
                 print_r($topics_ques);
