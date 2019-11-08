1864142494d4c4f21146357e716c1636199dd9c7
shyam <shyam@itdivine.in>
2019-11-08 16:36:37 +0530

    Result answers json

--- application/views/fontend/exam/exam_next_question.php
+++ application/views/fontend/exam/exam_next_question.php
@@ -3,46 +3,39 @@
   <div class="row">
     <div class="col-md-12 col-sm-12">
       <?php 
-        $sr_no=1;
-         // print_r($questions);
-        if(!empty($last_count))  $ques_cnt = count($last_count); 
-          $const = NUMBER_QUESTIONS -1;
-         if(!empty($ans))  $anss = count($ans);
-          if(!empty($questions)) foreach($questions as $row){ 
-            $sr_no++;
-           $opt5 = $row['option5'];
+        $new_str = file_get_contents('./exam_questions/'.$job_post_id.'_'.$jobseeker_id.'.json');
+        $new_json = json_decode($new_str, true);
+        echo sizeof($new_json);
+        $ans_cnt = count($questions['answer']);
+          if($ans_cnt > 1){
+            $input_type='checkbox';
+          }else{
+            $input_type='radio';
+          }
       ?>
-      
       <input type="hidden" name="job_id" id="job_id" value="<?php if(!empty($job_id))echo base64_encode($job_id); ?>">
-      <input type="hidden" name="question_id" id="question_id" value="<?php echo $row['ques_id']; ?>">
-      <label><?php echo "Question:-".$row['question']; ?></label> 
-        <?php if($anss > 1){ ?>
+      <input type="hidden" name="question_id" id="question_id" value="<?php echo $questions['ques_id']; ?>">
+      <label><?php echo "Question:- ".$questions['question']; ?></label> 
         <ul>
-          <li><input type="checkbox" name="option[]" id="option1" value="1"><?php echo $row['option1'];?></li>
-          <li><input type="checkbox" name="option[]" id="option2" value="2"><?php echo $row['option2'];?></li>
-          <li><input type="checkbox" name="option[]" id="option3" value="3"><?php echo $row['option3'];?></li>
-          <li><input type="checkbox" name="option[]" id="option4" value="4"><?php echo $row['option4'];?></li>
-          <?php if (!empty($row['option5'])) { ?>
-          <li><input type="checkbox" name="option[]" id="option5" value="5"><?php echo $row['option5'];?></li>
-          <?php } ?>
+
+          <li><input type="<?php echo $input_type; ?>" name="option[]" id="option1" value="1"> <?php echo $questions['option1'];?></li>
+
+          <li><input type="<?php echo $input_type; ?>" name="option[]" id="option2" value="2"> <?php echo $questions['option2'];?></li>
+
+        <?php if(!empty($questions['option3'])){ ?>
+          <li><input type="<?php echo $input_type; ?>" name="option[]" id="option3" value="3"> <?php echo $questions['option3'];?></li>
+        <?php }else{} ?>
+
+        <?php if(!empty($questions['option4'])){ ?>
+          <li><input type="<?php echo $input_type; ?>" name="option[]" id="option4" value="4"> <?php echo $questions['option4'];?></li>
+        <?php }else{} ?>
+
+        <?php if(!empty($questions['option5'])){ ?>
+          <li><input type="<?php echo $input_type; ?>" name="option[]" id="option5" value="5"> <?php echo $questions['option5'];?></li>
+        <?php }else{} ?>
+
         </ul>
-      <?php }else{ ?>
-        <ul>
-          <li><input type="radio" name="option[]" id="option1" value="1"><?php echo $row['option1'];?></li>
-          <li><input type="radio" name="option[]" id="option2" value="2"><?php echo $row['option2'];?></li>
-          <li><input type="radio" name="option[]" id="option3" value="3"><?php echo $row['option3'];?></li>
-          <li><input type="radio" name="option[]" id="option4" value="4"><?php echo $row['option4'];?></li>
-          <?php if (!empty($row['option5'])) { ?>
-          <li><input type="radio" name="option[]" id="option5" value="5"><?php echo $row['option5'];?></li>
-          <?php } ?>
-        </ul>
-      <?php } ?>
-      <?php if($ques_cnt < $const)  {?>
          <button id="next" type="submit" class="btn btn-primary pull-right">Next</button>
-       <?php }else{ ?>
-        <button id="next" type="submit" class="btn btn-primary pull-right">Submit</button>
-      <?php } }?>
-       
     </div>
   </div>
 </form>
