dfa7116a6dc1a9e0024c9e8a7100c8b6726d4dc3
shyam <shyam@itdivine.in>
2019-11-08 14:23:02 +0530

    Result answers json

--- application/views/fontend/exam/exam_start.php
+++ application/views/fontend/exam/exam_start.php
@@ -36,30 +36,19 @@
 
                           // print_r($questions);
                           $sr_no=0;
-                           if(!empty($ans))  $anss = count($ans);
-                            if(!empty($questions)) foreach($questions as $row){ 
-                              $sr_no++;
+                          if(!empty($questions)) foreach($questions as $row){ 
+                            $sr_no++;
                         ?>
                         
                         <input type="hidden" name="job_id" id="job_id" value="<?php if(!empty($job_id))echo base64_encode($job_id); ?>">
                         <input type="hidden" name="question_id" id="question_id" value="<?php echo $row['ques_id']; ?>">
-                        <label><?php echo "Question:-".$row['question']; ?></label> 
-                          <?php if($anss > 1){ ?>
-                          <ul>
-                            <li><input type="checkbox" name="option[]" id="option1" value="1"><?php echo $row['option1'];?></li>
-                            <li><input type="checkbox" name="option[]" id="option2" value="2"><?php echo $row['option2'];?></li>
-                            <li><input type="checkbox" name="option[]" id="option3" value="3"><?php echo $row['option3'];?></li>
-                            <li><input type="checkbox" name="option[]" id="option4" value="4"><?php echo $row['option4'];?></li>
-                            <li><input type="checkbox" name="option[]" id="option5" value="5"><?php echo $row['option5'];?></li>
-                          </ul>
-                        <?php }else{ ?>
+                        <label><?php echo "Question:- ".$row['question']; ?></label> 
                           <ul>
                             <li><input type="radio" name="option[]" id="option1" value="1"><?php echo $row['option1'];?></li>
                             <li><input type="radio" name="option[]" id="option2" value="2"><?php echo $row['option2'];?></li>
                             <li><input type="radio" name="option[]" id="option3" value="3"><?php echo $row['option3'];?></li>
                             <li><input type="radio" name="option[]" id="option4" value="4"><?php echo $row['option4'];?></li>
                             <li><input type="radio" name="option[]" id="option5" value="5"><?php echo $row['option5'];?></li>
-                        <?php } ?>
                            <button id="next" type="submit" class="btn btn-primary pull-right">Next</button>
                         <?php } ?>
                          
