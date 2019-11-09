1a2ad04dabf61f33ce39bb67e2aab8980839b86d
unknown <santu@edunetsol.com>
2019-11-09 15:12:11 +0530

    jobseeker carrer section

--- application/views/admin/jobsetting/questionbank_view.php
+++ application/views/admin/jobsetting/questionbank_view.php
@@ -35,7 +35,7 @@
 				<div class="row">
 				<div class="col-md-9"></div>
 				<div class="col-md-3">
-				<a download="questionimportpattarn.zip">Download Question Import Pattarn</a>
+				<a href="questionimportpattarn.zip">Download Question Import Pattarn</a>
 				</div>
 				</div>
                 <!-- /.box-header -->
