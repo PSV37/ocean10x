e905ce2c4f28865157f9053d060fa3e072c2b945
shyam <shyam@itdivine.in>
2019-11-13 16:05:12 +0530

    career work

--- application/views/fontend/jobseeker/update_education.php
+++ application/views/fontend/jobseeker/update_education.php
@@ -12,12 +12,12 @@
                       <h5>
                         <a href="#">Add Ph.D/Doctorate</a> 
                       </h5>
-                      <h5>>
-                      <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                     
-                     <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
-                            </h5>
-                              <div class="table-responsive">          
+                      <h5>
+                        <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                       
+                        <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
+                      </h5>
+                          <div class="table-responsive">          
                             <table class="table">
 
                               <tbody>
@@ -48,22 +48,167 @@
      
                               </tbody>
                             </table>
-                            </div>
+                          </div>
                       <h5>
                         <a href="#">Add Masters/Post-Graduation</a> 
                       </h5>
+                      <h5>
+                        <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                       
+                        <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
+                      </h5>
+                          <div class="table-responsive">          
+                            <table class="table">
 
+                              <tbody>
+                              <tr>
+                                  <td width="30%">Degree:</td>
+                                  <td><?php echo $v_education->education_level_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Specialization:</td>
+                                    <td><?php echo $v_education->education_specialization; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Institute Name:</td>
+                              <td><?php echo $v_education->js_institute_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Result:</td>
+                                <td><?php echo $v_education->js_resut; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Passing Year:</td>
+                                <td><?php echo $v_education->js_year_of_passing; ?></td>
+                                </tr>
+     
+                              </tbody>
+                            </table>
+                          </div>
                       <h5>
                         <a href="#">Add Graduation/Diploma</a> 
                       </h5>
                       <h5>
+                        <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                       
+                        <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
+                      </h5>
+                          <div class="table-responsive">          
+                            <table class="table">
+
+                              <tbody>
+                              <tr>
+                                  <td width="30%">Degree:</td>
+                                  <td><?php echo $v_education->education_level_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Specialization:</td>
+                                    <td><?php echo $v_education->education_specialization; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Institute Name:</td>
+                              <td><?php echo $v_education->js_institute_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Result:</td>
+                                <td><?php echo $v_education->js_resut; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Passing Year:</td>
+                                <td><?php echo $v_education->js_year_of_passing; ?></td>
+                                </tr>
+     
+                              </tbody>
+                            </table>
+                          </div>
+                      <h5>
                          <a href="#">Add 12th</a> 
                       </h5>
+                      <h5>
+                        <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                       
+                        <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
+                      </h5>
+                          <div class="table-responsive">          
+                            <table class="table">
 
+                              <tbody>
+                              <tr>
+                                  <td width="30%">Degree:</td>
+                                  <td><?php echo $v_education->education_level_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Specialization:</td>
+                                    <td><?php echo $v_education->education_specialization; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Institute Name:</td>
+                              <td><?php echo $v_education->js_institute_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Result:</td>
+                                <td><?php echo $v_education->js_resut; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Passing Year:</td>
+                                <td><?php echo $v_education->js_year_of_passing; ?></td>
+                                </tr>
+     
+                              </tbody>
+                            </table>
+                          </div>
                       <h5>
                         <a href="#">Add 10th</a> 
                       </h5>
-                  
+                    <h5>
+                        <a href="#" data-toggle="modal" data-target="#EditEducation" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                       
+                        <a href="<?php echo base_url() ?>/job_seeker/delete_education/" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
+                      </h5>
+                          <div class="table-responsive">          
+                            <table class="table">
+
+                              <tbody>
+                              <tr>
+                                  <td width="30%">Degree:</td>
+                                  <td><?php echo $v_education->education_level_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Specialization:</td>
+                                    <td><?php echo $v_education->education_specialization; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Institute Name:</td>
+                              <td><?php echo $v_education->js_institute_name; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Result:</td>
+                                <td><?php echo $v_education->js_resut; ?></td>
+                                </tr>
+
+                                <tr>
+                                  <td>Passing Year:</td>
+                                <td><?php echo $v_education->js_year_of_passing; ?></td>
+                                </tr>
+     
+                              </tbody>
+                            </table>
+                          </div>
                    <hr class="invis">
 
                <!--  <?php if (!empty($edcuaiton_list)) : ?> 
