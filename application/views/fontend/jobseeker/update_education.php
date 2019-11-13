3e0a3439e2a810a51e0977537fd54d05147ffcbe
shyam <shyam@itdivine.in>
2019-11-13 15:51:00 +0530

    career work

--- application/views/fontend/jobseeker/update_education.php
+++ application/views/fontend/jobseeker/update_education.php
@@ -1,68 +1,22 @@
-
-
-               
+           
   <div class="section lb">
         <div class="container">
             <div class="row">
              <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
 
-
                 <div class="content col-md-9">
-                    <div class="userccount">
+                  <div class="userccount">
                   <?php $this->load->view('fontend/layout/seeker_resumemenu.php'); ?>
-                                    <hr>
-                   <?php $key = 1 ?>
-                    <?php if (!empty($edcuaiton_list)): foreach ($edcuaiton_list as $v_education) : ?>
-                            <h5>
-                      Your Education No: <?php echo $key; ?>  
+                    <h5>Education</h5>
+                    <hr>  
+                    
+                    <?php if (!empty($education_level)): foreach ($education_level as $edu_level) : ?>
 
-                     <a href="#" data-toggle="modal" data-target="#EditEducation<?php  echo $v_education->js_education_id; ?>" class="btn pull-right bg-navy btn-xs" title="Edit" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                     
-                     <a href="<?php echo base_url() ?>/job_seeker/delete_education/<?php echo $v_education->js_education_id ?>" class="pull-right btn  btn-xs" title="Delete" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this record ?');"><i class="fa fa-trash-o"></i></a>                   
-                            </h5>
-                              <div class="table-responsive">          
-                            <table class="table">
-
-                              <tbody>
-                              <tr>
-                                  <td width="30%">Degree:</td>
-                                  <td><?php echo $v_education->education_level_name; ?></td>
-                                </tr>
-
-                                <tr>
-                                  <td>Specialization:</td>
-                                    <td><?php echo $v_education->education_specialization; ?></td>
-                                </tr>
-
-                                <tr>
-                                  <td>Institute Name:</td>
-                              <td><?php echo $v_education->js_institute_name; ?></td>
-                                </tr>
-
-                                <tr>
-                                  <td>Result:</td>
-                                <td><?php echo $v_education->js_resut; ?></td>
-                                </tr>
-
-                                <tr>
-                                  <td>Passing Year:</td>
-                                <td><?php echo $v_education->js_year_of_passing; ?></td>
-                                </tr>
-     
-                              </tbody>
-                            </table>
-                            </div>
-                     <?php
-                    $key++;
-                    endforeach;
-                    ?><!--get all category if not this empty-->
-                    <?php else : ?> <!--get error message if this empty-->
-                        <td colspan="3">
-                            <strong>There is no Educational Information to show!</strong>
-                        </td><!--/ get error message if this empty-->
+                      <h6><?php echo $edu_level['education_level_name']; ?></h6>
+                  
                     <?php
-                    endif; ?>
-
+                      endforeach;
+                     endif; ?>
                    <hr class="invis">
 
                 <?php if (!empty($edcuaiton_list)) : ?> 
