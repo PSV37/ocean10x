a88306731a7b6bede270057f3047b8b78f6e8d8a
shyam <shyam@itdivine.in>
2019-11-13 11:24:20 +0530

    career work

--- application/views/fontend/jobseeker/update_career.php
+++ application/views/fontend/jobseeker/update_career.php
@@ -188,7 +188,7 @@
                 <?php if (!empty($job_career_info[0]->immediate_join)) {
                           if($job_career_info[0]->immediate_join=='Yes'){
                   ?>
-                  <input type="text" class="form-control" name="avail_to_join" id="avail" value="" disabled>
+                  <input type="text" class="form-control" name="avail_to_join" id="avail_to_join" value="" disabled>
                   <?php
                     }else{?>
                   <input type="text" class="form-control datepicker" name="avail_to_join" id="avail_to_join" value="<?php echo date('d-m-Y', strtotime($job_career_info[0]->availability_date)); ?>">
@@ -376,6 +376,9 @@
                 //$('#join_month').prop('disabled',false);
              }
             
+         }else{
+           $('#avail_to_join').prop('disabled',false);
+            $('#avail_to_join').val('');
          }
          
      });
