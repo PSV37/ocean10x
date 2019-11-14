ffbd288bbbe19d2a1b7ab8d5bfed1115c6e8f536
shyam <shyam@itdivine.in>
2019-11-14 12:43:48 +0530

    education tab working

--- application/views/fontend/jobseeker/education_form.php
+++ application/views/fontend/jobseeker/education_form.php
@@ -12,7 +12,7 @@
         </div>
         <div class="col-sm-1"></div>
       </div>
-      <?php if(!empty($education_level)) if($education_level['education_level_name']!='10th' || $education_level['education_level_name']!='12th'){?>
+      <?php if(!empty($education_level)) if($education_level['education_level_name']=='Ph.D / Doctorate' || $education_level['education_level_name']!='Masters/Post-Graduation' || $education_level['education_level_name']!='Gradution'){?>
       <div class="form-group">
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
