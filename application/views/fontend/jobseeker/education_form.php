f7de137c47433bb34285bf2db9e6f30f2cf3480e
shyam <shyam@itdivine.in>
2019-11-14 13:02:19 +0530

    education tab working

--- application/views/fontend/jobseeker/education_form.php
+++ application/views/fontend/jobseeker/education_form.php
@@ -56,28 +56,26 @@
         <div class="col-sm-10">
           <label class="control-label" for="pwd">Passing Year</label>
           <select  name="js_year_of_passing" id="ddlYear" class="form-control">
-           <option value="">Select Passing Year</option>s
+           <option value="">Select Passing Year</option>
+            <?php
+              $currently_selected = date('Y'); 
+              $earliest_year = 1940; 
+              $latest_year = date('Y'); 
+              foreach ( range( $latest_year, $earliest_year ) as $i ) {
+              ?>
+              <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+            <?php } ?>
           </select>
         </div>
         <div class="col-sm-1"></div>
 
       </div>
-        <?php
-        // Sets the top option to be the current year. (IE. the option that is chosen by default).
-        $currently_selected = date('Y'); 
-        // Year to start available options at
-        $earliest_year = 1950; 
-        // Set your latest year you want in the range, in this case we use PHP to just set it to the current year.
-        $latest_year = date('Y'); 
-
-        print '<select>';
-        // Loops over each int[year] from current year, back to the $earliest_year [1950]
-        foreach ( range( $latest_year, $earliest_year ) as $i ) {
+       <!-- 
           // Prints the option with the next year in range.
           print '<option value="'.$i.'"'.($i === $currently_selected ? ' selected="selected"' : '').'>'.$i.'</option>';
-        }
+        
         print '</select>';
-  ?>
+  ?> -->
 <?php if(!empty($education_level)) if($education_level['education_level_name']=='Ph.D / Doctorate' || $education_level['education_level_name']=='Masters/Post-Graduation' || $education_level['education_level_name']=='Gradution'){?>
       <div class="form-group">
          <div class="col-sm-1"></div>
