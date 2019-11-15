b97beb80e35a3808d45432bbaa74375218894da4
shyam <shyam@itdivine.in>
2019-11-15 11:56:44 +0530

    education

--- application/views/fontend/jobseeker/seeker_dashboard.php
+++ application/views/fontend/jobseeker/seeker_dashboard.php
@@ -1,12 +1,37 @@
 <?php 
     $this->load->view('fontend/layout/seeker_header.php');
 ?>        
-<div id="smsg" class="alert alert-<?php echo $this->session->flashdata('type');?> alert-dismissible fade in">
-  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  <strong><?php echo $this->session->flashdata('Message');?></strong>
+ <!-- Page Title start -->
+<div class="pageTitle">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <h1 class="page-heading">Active Jobs</h1>
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <div class="breadCrumb"><a href="#.">Home</a> / <span>Active Jobs</span></div>
+      </div>
+    </div>
+  </div>
 </div>
+<!-- Page Title End -->             
 
-<div class="tabdata"></div>
+      	<div class="section lb">
+            <div class="container">
+               <div class="row">
+                <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
+                
+                <div class="content col-md-9">
+	                <div class="x_panel">
+		                <div class="x_content">
+		                 
+		                </div>
+	           		</div>
+        		</div>
+
+  		</div> <!--end row -->
+	</div><!-- end container -->
+</div><!-- end section -->
 
 <?php $this->load->view("fontend/layout/footer.php"); ?>
  
