042228adafee50110d5afe963e5e0392277b2a08
shyam <shyam@itdivine.in>
2019-11-15 12:06:25 +0530

    education

--- application/views/fontend/jobseeker/seeker_dashboard.php
+++ application/views/fontend/jobseeker/seeker_dashboard.php
@@ -16,17 +16,41 @@
 </div>
 <!-- Page Title End -->             
 
-      	<div class="section lb">
-            <div class="container">
-               <div class="row">
-                <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
-                
-                <div class="content col-md-9">
-	                <div class="x_panel">
-		                <div class="x_content">
-		                 
-		                </div>
-	           		</div>
+<div class="section lb">
+	<div class="container">
+	   <div class="row">
+	    <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
+
+            <div class="content col-md-9">
+	            <div class="row">
+                    <div class="content col-md-8 col-md-offset-2">
+                        <div class="userccount">
+                            <h5>Your Dashboard</h5>
+
+                            <div class="formpanel">
+                                <div class="formrow">
+                                    <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+	                                        <input type="Password" id="password" name="password" class="form-control" placeholder="Password">
+	                                    </div>
+                                    </div><!-- end row -->
+                                </div>
+                                 
+                               <div class="formrow">
+	                               <div class="row">
+	                                       <div class="col-md-6 col-sm-12">
+	                                            <input type="Password" id="password" name="password" class="form-control" placeholder="Password">
+	                                        </div>
+
+	                                         <div class="col-md-6 col-sm-12">
+	                                            <input type="Password" name="confirm_password" class="form-control" placeholder="Confirm Password">
+	                                        </div>
+	                                </div>
+                                </div>
+
+                            </div><!-- end post-padding -->
+                        </div>
+                    </div><!-- end col -->
         		</div>
 
   		</div> <!--end row -->
