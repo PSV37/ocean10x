9726884c3ae69da9cfe444c0906acf00def321c3
shyam <shyam@itdivine.in>
2019-11-15 16:04:52 +0530

    Dashboard Creatino

--- application/views/fontend/jobseeker/seeker_dashboard.php
+++ application/views/fontend/jobseeker/seeker_dashboard.php
@@ -30,6 +30,10 @@ h6{
 }
 	    
 </style>       
+<div id="smsg" class="alert alert-<?php echo $this->session->flashdata('type');?> alert-dismissible fade in">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  <strong><?php echo $this->session->flashdata('Message');?></strong>
+</div>
 
  <!-- Page Title start -->
 <div class="pageTitle">
@@ -122,4 +126,10 @@ h6{
 
 <?php $this->load->view("fontend/layout/footer.php"); ?>
  
- 
+ <script>
+	$(document).ready (function(){
+		$("#smsg").fadeTo(2000, 500).slideUp(500, function(){
+		$("#smsg").slideUp(500);
+		});   
+	});
+ </script>
