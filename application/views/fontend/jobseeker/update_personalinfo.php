f37494f23dd9821fa58c7036701cdc8046d203e4
shyam <shyam@itdivine.in>
2019-11-25 16:34:37 +0530

    jobseeker forntend changes

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -341,7 +341,7 @@
           <div class="col-md-12 col-sm-12">
             <div class="input-group">
               <label class="control-label" for="email">Name:<span class="required">*</span></label>
-              <input type="text" class="form-control" name="full_name" value="<?php if(!empty($intro_data)) $intro_data['full_name']; ?>">
+              <input type="text" class="form-control" name="full_name" value="<?php if(!empty($intro_data)) echo $intro_data['full_name']; ?>">
             </div>
           </div>
         </div>
