f8398b1fe0184182b9cbefd1e1799293684f6c9a
shyam <shyam@itdivine.in>
2019-11-25 12:46:33 +0530

    jobseeker fonrt end changes

--- application/views/fontend/jobseeker/register.php
+++ application/views/fontend/jobseeker/register.php
@@ -36,8 +36,9 @@
 
                 <div class="formrow">
                   <div class="row">
-                    <div class="col-md-2 col-sm-12">I am a</div>
-                    <div class="col-md-10 col-sm-12">
+                    <!-- <div class="col-md-2 col-sm-12">I am a</div> -->
+                    <div class="col-md-6 col-sm-12">
+                     <span>I am a</span> 
                       <input type="radio" name="profession" value="Fresher"> Fresher
                       <input type="radio" name="profession" value="Professional"> Professional
                     </div>
