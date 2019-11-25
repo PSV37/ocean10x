f82f75eb4f28f3c4623f3f47c9e69d468f5296c3
shyam <shyam@itdivine.in>
2019-11-25 12:04:54 +0530

    jobseeker forgot password change

--- application/views/fontend/jobseeker/login.php
+++ application/views/fontend/jobseeker/login.php
@@ -16,31 +16,19 @@
     </div>
   </div>
 </div> -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6 col-sm-6">
-        <h1 class="page-heading">Hello Talented Professional !<br>
-      Welcome Back !<br>
-  Login into the Ocean know about the latest Professional Opportunities !</h1>
-      </div>
-      <div class="col-md-6 col-sm-6">
-        <div class="breadCrumb"><a href="#.">Home</a> / <span>Seeker Login</span></div>
-      </div>
-    </div>
-  </div>
 <!-- Page Title End --> 
-<!-- Company Logo on Top Here
 
-  <h5>Hello Talented Professional !<br>
-      Welcome Back !<br>
-  Login into the Ocean know about the latest Professional Opportunities !</h5> -->
+  Company Logo on Top Here
+
   
 <div class="listpgWraper">
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <div class="userccount">
-          	
+          	<p>Hello Talented Professional !
+                  Welcome Back !
+              Login into the Ocean know about the latest Professional Opportunities !</p>
          <!--  <h5 align="center">Seeker Login</h5>
           <hr> -->
           <!-- login form -->
