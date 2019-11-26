491600ee2120204dfaa83c98cc71bcee16d1469b
shyam <shyam@itdivine.in>
2019-11-26 18:00:22 +0530

    job seeker changes

--- application/views/fontend/job/job_details.php
+++ application/views/fontend/job/job_details.php
@@ -124,7 +124,7 @@ $jobseeker_id = $this->session->userdata('job_seeker_id');
             <?php  $jobseeker_id;
                 if($jobseeker_id==null): ?>
               <a href="<?php echo base_url(); ?>job-apply/<?php echo $singlejob->job_slugs; ?>" class="btn apply">Apply For This Vacancy</a>
-
+              
               <a href="<?php echo base_url(); ?>save-job/<?php echo $singlejob->job_slugs; ?>" class="btn apply">Save This Jobs</a>
 
               <?php else: 
