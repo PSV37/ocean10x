18d993e75fd05d8d1c77901a9c271b8d9e1ade5e
shyam <shyam@itdivine.in>
2019-11-26 17:39:44 +0530

    job seeker changes

--- application/views/fontend/jobseeker/saved_jobs.php
+++ application/views/fontend/jobseeker/saved_jobs.php
@@ -20,23 +20,21 @@
           <tbody>
           <?php
              $sr_no=0;
-             print_r($saved_job_data);
+            
            if (!empty($saved_job_data)): foreach ($saved_job_data as $applicaiton) : $sr_no++;
-              echo 'dasd'.$applicaiton['job_post_id'];
-                echo $applicaiton->job_post_id;
             ?>
             <tr>
               <td><?php echo $sr_no; ?></td>
               <td>
                 <h4>
-                  <a href="<?php  echo base_url();?>job/show/<?php echo $this->job_posting_model->get_slug_nameby_id($applicaiton->job_post_id) ?>"><?php echo $this->job_posting_model->job_title_by_name($applicaiton->job_post_id); ?></a><br>
+                  <a href="<?php  echo base_url();?>job/show/<?php echo $this->job_posting_model->get_slug_nameby_id($applicaiton['job_post_id']) ?>"><?php echo $this->job_posting_model->job_title_by_name($applicaiton['job_post_id']); ?></a><br>
                 </h4>
               </td>
-              <td><?php echo $this->company_profile_model->company_name($applicaiton->company_profile_id); ?></td>
-              <td><?php echo date('M j, Y',strtotime($applicaiton->created_on));  ?></td>
+              <td><?php echo $this->company_profile_model->company_name($applicaiton['company_profile_id']); ?></td>
+              <td><?php echo date('M j, Y',strtotime($applicaiton['created_on']));  ?></td>
               
               <td>
-                <a href="<?php echo base_url(); ?>job/all-scheduled-interviews/<?php echo $applicaiton->job_post_id; ?>" class="btn btn-success btn-xs">View Interviews</a>
+                <a href="<?php echo base_url(); ?>job/all-scheduled-interviews/<?php echo $applicaiton['job_post_id']; ?>" class="btn btn-success btn-xs">View Interviews</a>
               </td>
             
               
