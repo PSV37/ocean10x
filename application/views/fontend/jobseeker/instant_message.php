55083746ad7b87f47582c9eaf308b40187758d66
shyam <shyam@itdivine.in>
2019-11-27 15:15:51 +0530

    message work

--- application/views/fontend/jobseeker/instant_message.php
+++ application/views/fontend/jobseeker/instant_message.php
@@ -6,29 +6,7 @@
   <div class="container">
     <div class="row">
       <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
-      <div class="content col-md-5">
-        <div class="job-header">
-          <div class="contentbox">
-            <div class="formpanel">
-             <?php echo $this->session->flashdata('change_password'); ?>
-              <form id="submit" class="submit-form" action="<?php echo base_url(); ?>job_seeker/change_password" method="post">
-                <div class="row">
-                  <div class="col-md-6 col-sm-12">
-                    <label class="control-label">Current Password </label>
-                    <input type="password" name="oldpassword" class="form-control" placeholder="Type your current password">
-                      <br>
-                    <label class="control-label">New Password</label>
-                    <input type="password" name="newpassword" class="form-control" placeholder="Type your new password">
-                      <br>
-                    <button type="submit" class="btn btn-primary">Update Password</button>
-                  </div>
-                </div>
-              </form>
-            </div><!-- end post-padding -->
-          </div>
-        </div>
-      </div>
-      <div class="content col-md-4">
+      <div class="content col-md-9">
         <div class="job-header">
           <div class="contentbox">
             <div class="formpanel">
