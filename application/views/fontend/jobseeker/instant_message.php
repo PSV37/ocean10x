369b816bc2ef9ee18290b849b9fd9f5f8f1094b9
shyam <shyam@itdivine.in>
2019-11-27 16:13:21 +0530

    message work

--- application/views/fontend/jobseeker/instant_message.php
+++ application/views/fontend/jobseeker/instant_message.php
@@ -1,193 +1,60 @@
 <?php 
     $this->load->view('fontend/layout/seeker_header.php');
 ?>                
- <style>
-   body {font-family: Arial, Helvetica, sans-serif;}
-* {box-sizing: border-box;}
-
-/* Button used to open the chat form - fixed at the bottom of the page */
-.open-button {
-  background-color: #555;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  opacity: 0.8;
-  position: fixed;
-  bottom: 23px;
-  right: 28px;
-  width: 280px;
-}
-
-/* The popup chat - hidden by default */
-.chat-popup {
-  display: none;
-  position: fixed;
-  bottom: 0;
-  right: 15px;
-  border: 3px solid #f1f1f1;
-  z-index: 9;
-}
-
-/* Add styles to the form container */
-.form-container {
-  max-width: 300px;
-  padding: 10px;
-  background-color: white;
-}
-
-/* Full-width textarea */
-.form-container textarea {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  border: none;
-  background: #f1f1f1;
-  resize: none;
-  min-height: 200px;
-}
-
-/* When the textarea gets focus, do something */
-.form-container textarea:focus {
-  background-color: #ddd;
-  outline: none;
-}
-
-/* Set a style for the submit/send button */
-.form-container .btn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 16px 20px;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  margin-bottom:10px;
-  opacity: 0.8;
-}
-
-/* Add a red background color to the cancel button */
-.form-container .cancel {
-  background-color: red;
-}
-
-/* Add some hover effects to buttons */
-.form-container .btn:hover, .open-button:hover {
-  opacity: 1;
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
-
-.title {
-  color: grey;
-  font-size: 18px;
-}
-
-button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-a {
-  text-decoration: none;
-  font-size: 22px;
-  color: black;
-}
-
-button:hover, a:hover {
-  opacity: 0.7;
-}
-</style>
+            
 <div class="section lb">
   <div class="container">
     <div class="row">
       <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
       <div class="content col-md-9">
-        <div class="job-header">
-          <div class="contentbox">
-            <div class="formpanel">
-            <div class="col-md-6 col-sm-12">
-              <input type="text" name="search_people" id="search_people" placeholder="Search People..">
-            </div>
-             <?php echo $this->session->flashdata('change_password'); ?>
-              <form id="submit" class="submit-form" action="<?php echo base_url(); ?>job_seeker/change_password" method="post">
-                <div class="row">
-                  <!-- <div class="col-md-4 col-sm-12">
-                    
-                  </div>
-                  <div class="col-md-4 col-sm-12">
-                    
-                  </div> -->
-                  <div class="card">
-                    <img src="/w3images/team2.jpg" alt="John" style="width:100%">
-                    <h1>John Doe</h1>
-                    <p class="title">CEO & Founder, Example</p>
-                    <p>Harvard University</p>
-                    <div style="margin: 24px 0;">
-                      <a href="#"><i class="fa fa-dribbble"></i></a> 
-                      <a href="#"><i class="fa fa-twitter"></i></a>  
-                      <a href="#"><i class="fa fa-linkedin"></i></a>  
-                      <a href="#"><i class="fa fa-facebook"></i></a> 
-                    </div>
-                    <p><button>Contact</button></p>
-                  </div>
-
-                </div>
-              </form>
-            </div><!-- end post-padding -->
-          </div>
-        </div>
-
-        <button class="open-button" onclick="openForm()">Message</button>
-
-        <div class="chat-popup" id="myForm">
-          <form action="/action_page.php" class="form-container">
-            <h1>Message</h1>
-
-            <label for="msg"><b>Message</b></label>
-            <textarea placeholder="Type message.." name="msg" required></textarea>
-
-            <button type="submit" class="btn">Send</button>
-            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-          </form>
-        </div>
-
-       
-      </div><!-- end col -->
-    </div><!-- end row -->  
+        <h5>Inbound Job Invitations</h5>
+        <table id="example" class="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Sr.No</th>
+              <th>Name</th>
+              <th>Profession</th>
+              <th>Email Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          <?php
+            $sr_no=0;
+           if (!empty($seeker_data)): foreach ($seeker_data as $js_row) : $sr_no++; ?>
+            <tr>
+              <td><?php echo $sr_no; ?></td>
+              <td>
+               <?php echo $js_row['full_name']; ?>
+              </td>
+              <td>
+               <?php echo $js_row['profession']; ?>
+              </td>
+               <td>
+               <?php echo $js_row['email']; ?>
+              </td>
+            
+              <td>
+                <a href="<?php echo base_url(); ?>" class="btn btn-success btn-xs">View Profile</a>
+              </td>
+              
+             
+            </tr>
+            <?php
+              endforeach;
+            ?>
+            <?php else : ?> 
+              <td colspan="7">
+                  <strong>There is no data to display</strong>
+              </td>
+            <?php endif; ?>
+          </tbody>
+        </table>
+      </div>
+    </div> <!--end row -->
   </div><!-- end container -->
 </div><!-- end section -->
 
 
+  
  <?php $this->load->view("fontend/layout/footer.php"); ?>
-<script>
-  function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-  $(function() {
-      $("#search_people").autocomplete({
-          source: "<?php echo base_url('job_seeker/get_autocomplete'); ?>",
-          select: function(a,b)
-            {
-              $(this).val(b.item.value); //grabed the selected value
-            }
-        });
-    });
-</script>
