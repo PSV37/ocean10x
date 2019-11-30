08e267476abcaeeac8a454c397bb2e21454e1031
shyam <shyam@itdivine.in>
2019-11-30 18:31:26 +0530

    message history

--- application/views/fontend/jobseeker/instant_message.php
+++ application/views/fontend/jobseeker/instant_message.php
@@ -43,6 +43,7 @@
               </td>
             
               <td>
+                <a href="#" class="btn btn-primary" onclick="openForm(<?php echo $js_row['chat_js_id']; ?>)">Message</a>
                 <a href="<?php echo base_url(); ?>seeker/message-history/<?php echo $js_row['chat_js_id']; ?>" class="btn btn-success btn-xs">View History</a>
               </td>
               
@@ -63,6 +64,27 @@
   </div><!-- end container -->
 </div><!-- end section -->
 
+ <div class="chat-popup" id="myForm">
+  
+    <form id="Career-info" class="form-container" action="<?php echo base_url('job_seeker/instant_message');?>" method="post">
+    <!-- <h1>Message</h1> -->
+    <input type="text" name="send_to" id="send_to">
+    <label for="msg"><b>Message</b></label>
+    <textarea placeholder="Type message.." name="user_msg" required></textarea>
 
+    <button type="submit" class="btn">Send</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
   
  <?php $this->load->view("fontend/layout/footer.php"); ?>
+ <script>
+  function openForm(id) {
+    $('#send_to').val(id);
+    document.getElementById("myForm").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+</script>
