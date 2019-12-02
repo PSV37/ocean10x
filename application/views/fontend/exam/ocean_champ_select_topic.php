17cf5a3ac308ecfadcd100be9e7806fd41522637
shyam <shyam@itdivine.in>
2019-12-02 17:29:31 +0530

    ocena champ work

--- application/views/fontend/exam/ocean_champ_select_topic.php
+++ application/views/fontend/exam/ocean_champ_select_topic.php
@@ -5,32 +5,44 @@
 
 <div class="section lb">
   <div class="container">
-     <div class="row">
-       <?php $this->load->view('fontend/layout/employer_left.php'); ?>
-       <?php print_r($skill_data); ?>
-          <div class="content col-md-9">
-          <div class="userccount">
-              <div class="formpanel">
-             <?php echo $this->session->flashdata('change_password'); ?>
-                 <form id="submit" class="submit-form" action="<?php echo base_url(); ?>employer/change_password" method="post">
-                      <div class="row">
-                          <div class="col-md-6 col-sm-12">
-                              <label class="control-label">Current Password </label>
-                              <input type="password" name="oldpassword" class="form-control" placeholder="Type your current password">
-                              <br>
-                              <label class="control-label">New Password</label>
-                              <input type="password" name="newpassword" class="form-control" placeholder="Type your new password">
-                              <br>
-                                 <button type="submit" class="btn btn-primary">Update Password</button>
-                          </div>
+    <div class="row">
+      <?php $this->load->view('fontend/layout/employer_left.php'); ?>
+      <?php print_r($skill_data); ?>
+      <div class="content col-md-9">
+        <div class="userccount">
+          <div class="formpanel">
+            <?php echo $this->session->flashdata('change_password'); ?>
+            <form id="submit" class="submit-form" action="<?php echo base_url(); ?>employer/change_password" method="post">
+              <div class="row">
+                <div class="col-md-6 col-sm-12">
+                  <label class="control-label ">Skill Name<span class="required">*</span> </label>
+                  <select name="skill_name" id="skill_name" required class="form-control industry" data-style="btn-default" data-live-search="true">
+                    <option value="">Select Skill</option>
+                    <?php if(!empty($skill_data)) foreach ($skill_data as $svalue) { ?>
+                      <option value="<?php echo $svalue['id']; ?>"><?php echo $svalue['skill_name']; ?></option>
+                    <?php  }
+                    ?>
+                  </select>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label class="control-label ">Topics<span class="required">*</span> </label>
+                  <select name="topics" id="skill_name" required class="form-control industry" data-style="btn-default" data-live-search="true">
+                    <option value="">Select Topics</option>
+                    <?php if(!empty($skill_data)) foreach ($skill_data as $svalue) { ?>
+                      <option value="<?php echo $svalue['id']; ?>"><?php echo $svalue['skill_name']; ?></option>
+                    <?php  }
+                    ?>
+                  </select>
+                </div>
+                  <button type="submit" class="btn btn-primary">Update Password</button>
+                </div>
+              </div>
+            </form>
+          </div><!-- end post-padding -->
+        </div>
+      </div>
 
-                      </div>
-                  </form>
-              </div><!-- end post-padding -->
-          </div>
-          </div>
-
-  </div> <!--end row -->
+    </div> <!--end row -->
   </div><!-- end container -->
 </div><!-- end section -->
 
