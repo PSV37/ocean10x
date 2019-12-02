72b7eeaa00b86cd787ba1c2584ab0c08c90044ae
shyam <shyam@itdivine.in>
2019-12-02 17:31:44 +0530

    ocena champ work

--- application/views/fontend/exam/ocean_champ_select_topic.php
+++ application/views/fontend/exam/ocean_champ_select_topic.php
@@ -7,7 +7,6 @@
   <div class="container">
     <div class="row">
       <?php $this->load->view('fontend/layout/employer_left.php'); ?>
-      <?php print_r($skill_data); ?>
       <div class="content col-md-9">
         <div class="userccount">
           <div class="formpanel">
@@ -16,7 +15,7 @@
               <div class="row">
                 <div class="col-md-6 col-sm-12">
                   <label class="control-label ">Skill Name<span class="required">*</span> </label>
-                  <select name="skill_name" id="skill_name" required class="form-control industry" data-style="btn-default" data-live-search="true">
+                  <select name="skill_name" id="skill_name" required class="form-control skill" data-style="btn-default" data-live-search="true">
                     <option value="">Select Skill</option>
                     <?php if(!empty($skill_data)) foreach ($skill_data as $svalue) { ?>
                       <option value="<?php echo $svalue['id']; ?>"><?php echo $svalue['skill_name']; ?></option>
@@ -26,7 +25,7 @@
                 </div>
                 <div class="col-md-6 col-sm-12">
                   <label class="control-label ">Topics<span class="required">*</span> </label>
-                  <select name="topics" id="skill_name" required class="form-control industry" data-style="btn-default" data-live-search="true">
+                  <select name="topics" id="skill_name" required class="form-control topic" data-style="btn-default" data-live-search="true">
                     <option value="">Select Topics</option>
                     <?php if(!empty($skill_data)) foreach ($skill_data as $svalue) { ?>
                       <option value="<?php echo $svalue['id']; ?>"><?php echo $svalue['skill_name']; ?></option>
@@ -49,3 +48,27 @@
 
   
  <?php $this->load->view("fontend/layout/footer.php"); ?>
+<script>
+$(function() {
+  // choose target dropdown
+  var select = $('.skill');
+  select.html(select.find('option').sort(function(x, y) {
+    // to change to descending order switch "<" for ">"
+    return $(x).text() > $(y).text() ? 1 : -1;
+  }));
+
+  // select default item after sorting (first item)
+  //$('select').get(0).selectedIndex = 0;
+});
+$(function() {
+  // choose target dropdown
+  var select = $('.topic');
+  select.html(select.find('option').sort(function(x, y) {
+    // to change to descending order switch "<" for ">"
+    return $(x).text() > $(y).text() ? 1 : -1;
+  }));
+
+  // select default item after sorting (first item)
+  //$('select').get(0).selectedIndex = 0;
+});
+</script>
