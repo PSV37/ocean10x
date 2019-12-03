e9abfe4fa600c66caff82e8a83912b002e8e7385
shyam <shyam@itdivine.in>
2019-12-03 12:46:58 +0530

    OceanChamp test

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -277,7 +277,7 @@ class Exam extends MY_Seeker_Controller
             $where_req_skill="skill_name ='".$can_skills[$i]['skills']."'";
             $skill_data = $this->Master_model->getMaster('skill_master',$where_req_skill,$join = FALSE, $order = false, $field = false, $select = false, $limit=false, $start=false, $search=false);
 
-               array_push($temp_array, $skill_data[$i]);
+               array_push($temp_array, $skill_data);
             // echo "<pre>";
             // print_r($skls);
 
