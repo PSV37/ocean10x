a544e4fa58463645386b394530970d5c1c4ecfca
shyam <shyam@itdivine.in>
2019-12-03 15:03:10 +0530

    OceanChamp test

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -281,7 +281,7 @@ class Exam extends MY_Seeker_Controller
             $can_skills[$i]['skill_name']= $skill_data[0]['skill_name'];
             $can_skills[$i]['id']= $skill_data[0]['id'];
             
-           array_push($temp_array, $can_skills);
+           array_push($temp_array, $can_skills[$i]);
             //$data[] = $can_skills;
         }
        // print_r($data);
