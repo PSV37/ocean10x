be653c71f1723b0882adecc6f3cafc6fad0783d8
shyam <shyam@itdivine.in>
2019-12-04 17:40:14 +0530

    ocean exam work

--- application/views/fontend/exam/ocean_champ_result.php
+++ application/views/fontend/exam/ocean_champ_result.php
@@ -8,7 +8,7 @@
     <div class="row">
       <?php $this->load->view('fontend/layout/seeker_left.php'); ?>
       <div class="content col-md-9">
-        <h5>Inbound Job Invitations</h5>
+        <h5>Ocean Champ Exam Result's </h5>
         <table id="example" class="table table-striped table-bordered">
           <thead>
             <tr>
