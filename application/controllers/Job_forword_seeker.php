1342de853db5fc76c9b37101634c3f485c2f95f6
unknown <santu@edunetsol.com>
2019-12-04 17:33:17 +0530

    consultant

--- application/controllers/Job_forword_seeker.php
+++ application/controllers/Job_forword_seeker.php
@@ -165,7 +165,7 @@ class Job_forword_seeker extends CI_Controller {
                         $status = $this->Master_model->master_update($data_status, 'consultants_jobs', $where_update1);
                         if($status==true)
                         {
-                            // $data['consultant_id'] = $job_seeker_id;
+                             $data['comp_profile_id'] = $comp_profile_id;
                             $data['email_id'] = $comp_email;
                             $this->load->view('fontend/employer/consultant_registration',$data);
                         }
@@ -269,7 +269,78 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
 
     }
 
+public function create_consultant_account()
+    {
 
+        if ($_POST) {
+            $consultant_info = array(
+                'company_name' => $this->input->post('company_name'),
+                'company_username' => $this->input->post('company_username'),
+                // 'user_name' => $this->input->post('user_name'),
+                'company_password'  => md5($this->input->post('company_password')),
+                'company_category' => $this->input->post('company_category'),
+                // 'mobile_no'    => $this->input->post('mobile'),
+                'js_status' => 1,
+                'cv_type'   => 1,
+            );
+           
+            $email_to = base64_decode($this->input->get('comp_mail'));
+            $comp_profile_id =$this->input->post('comp_profile_id');
+
+          
+                $inputCaptcha = $this->input->post('captcha');
+                $sessCaptcha  = $this->session->userdata('captchaCode');
+                if ($inputCaptcha === $sessCaptcha) {
+                    
+                $where_up['company_profile_id'] = $comp_profile_id;
+                $this->Master_model->master_update($consultant_info, 'company_profile', $where_up); 
+            
+                // Last Id Add Personal Info Table
+                
+                
+                $subject = "Resisteration done successfully";
+                $message = '<div style="max-width:600px!important;padding:4px"><table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td align="center">
+<table width="100%" cellspacing="0" border="0"><tbody><tr><td style="font-size:0px;text-align:left" valign="top"><?php echo get_logo();?></td></tr></tbody></table><table width="100%" cellspacing="0" cellpadding="0" border="0"><tbody><tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left"><td>
+<br><br>Hi Dear,<br>Your account has been created successfully.<br><br>Good luck for Job search! 
+Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Question? Check out how works and our support team are ready to help.<br><br>© 2017 ConsultnHire All Rights Reserved.<br><br>You have received this mail because your e-mail ID is registered with Consultnhire.com. This is a system-generated e-mail regarding your Consultnhire account preferences, please do not reply to this message. The jobs sent in this mail have been posted by the clients of Consultnhire.com. And we have enabled auto-login for your convenience, you are strongly advised not to forward this email to protect your account from unauthorized access. IEIL has taken all reasonable steps to ensure that the information in this mailer is authentic. Users are advised to research bonafides of advertisers independently. Please do not pay any money to anyone who promises to find you a job. IEIL shall not have any responsibility in this regard. We recommend that you visit our Terms & Conditions and the Security Advice for more comprehensive information.</td></tr><tr><td height="40"></td></tr></tbody></table></td></tr></tbody></table></div>';
+
+                    // successfully sent mail
+                  // $this->job_seeker_model->sendEmail($email_to);
+
+                   $send = sendEmail_JobRequest($email_to,$message,$subject);
+                   // echo $message;);
+
+                    $this->session->unset_userdata('reg_jobseeker');
+  
+
+                    $this->load->view('fontend/jobseeker/forward_register_success');
+                } else {
+                    $this->session->set_flashdata('captcha_msg', '<div class="alert alert-warning text-center">Captcha Code Does not Match Please Try Again</div>');
+                    redirect_back();
+                }
+            
+
+        } else {
+
+            $config = array(
+                'img_path'    => 'captcha_images/',
+                'img_url'     => base_url() . 'captcha_images/',
+                'img_width'   => '150',
+                'img_height'  => 50,
+                'word'        => strtoupper(substr(md5(time()), 0, 4)),
+                'font_path' => FCPATH . 'captcha_images/font/captcha4.ttf',
+
+            );
+            $captcha = create_captcha($config);
+            // Unset previous captcha and store new captcha word
+            $this->session->unset_userdata('captchaCode');
+            $this->session->set_userdata('captchaCode', $captcha['word']);
+            // Send captcha image to view
+            $captcha_images = $captcha['image'];
+            $this->load->view('fontend/jobseeker/jobseeker_set_password', compact('captcha_images'));
+        }
+
+    }
 
 
 }// end class
