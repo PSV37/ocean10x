808ccd67481bb4804d36bc4e9bf13ac7e0211f93
unknown <santu@edunetsol.com>
2019-12-04 14:25:35 +0530

    consultanat

--- application/views/fontend/employer/forword_job.php
+++ application/views/fontend/employer/forword_job.php
@@ -38,8 +38,8 @@
                 <div class="col-md-12 col-sm-12">
                   <div class="formrow">
                     <label class="control-label">Send To:</label>
-                     <input type="radio" name="consultanat" value="consultanat" >Consultanat &nbsp;
-                      <input type="radio" name="consultanat" value="JobSeeker">JobSeeker(candidate)                
+                     <input type="radio" name="consultant" value="consultant" >Consultanat &nbsp;
+                      <input type="radio" name="consultant" value="JobSeeker">JobSeeker(candidate)                
                   </div>
                 </div>
                 <div class="col-md-12 col-sm-12">
