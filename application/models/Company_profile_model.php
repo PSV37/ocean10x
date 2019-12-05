377687ff5b6c25d88dc560a687b5c7aef64754a5
shyam <shyam@itdivine.in>
2019-12-05 17:10:52 +0530

    Exam result section work

--- application/models/Company_profile_model.php
+++ application/models/Company_profile_model.php
@@ -46,9 +46,9 @@ class Company_Profile_Model extends MY_Model
     //send verification email to user's email id
     public function sendEmail($to_email)
     {
-  //      $ci = get_instance();
-  //       $ci->load->library('email');
-  //       $config['protocol']    = 'smtp';
+        //  $ci = get_instance();
+        // $ci->load->library('email');
+        // $config['protocol']    = 'smtp';
 		// $config['smtp_host']    = 'mail.consultnhire.com';
 		// $config['smtp_port']    = '465';
 		// $config['smtp_timeout'] = '7'; 	
@@ -58,18 +58,6 @@ class Company_Profile_Model extends MY_Model
 		// $config['newline']    = "\r\n";
 		// $config['mailtype'] = 'html'; // or html
 	   
-
-
-        // $ci->email->initialize($config);
-
-        // $ci->email->from('info@consultnhire.com', 'ConsultnHire');
-        // $ci->email->to($to_email);
-        // $this->email->reply_to('info@consultnhire.com', 'ConsultnHire');
-        // $ci->email->subject('verify email account');
-        // $ci->email->message($message);
-        // $ci->email->send();
-
-
           $ci = get_instance();
           $ci->load->library('email');
           
@@ -77,7 +65,7 @@ class Company_Profile_Model extends MY_Model
             $config['charset'] = "utf-8";
             $config['mailtype'] = "html";
             $config['newline'] = "\r\n";
-            
+
         $message = '<div style="max-width:600px!important;padding:4px"><table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td align="center">
 <table width="100%" cellspacing="0" border="0"><tbody><tr><td style="font-size:0px;text-align:left" valign="top"></td></tr></tbody></table><table width="100%" cellspacing="0" cellpadding="0" border="0"><tbody><tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left"><td>
 <br><br>Hi Dear,<br>Please click on the below activation link to verify your email address <br><br><a style="border-radius:4px;font-size:15px;color:white;text-decoration:none;padding:14px 7px 14px 7px;width:210px;max-width:210px;font-family:&quot;Open Sans&quot;,&quot;Helvetica Neue&quot;,Arial;margin:0;display:block;background-color:#6caa4d;text-align:center" href="' . base_url() . 'employer_register/verify/' . md5($to_email) . '">Active Account</a><br>Goa a Question? Check out how works and our support team are ready to help.<br><br>© 2017 ConsultnHire. All Rights Reserved.</td></tr><tr><td height="40"></td></tr></tbody></table></td></tr></tbody></table></div>';
@@ -90,7 +78,7 @@ class Company_Profile_Model extends MY_Model
             $ci->email->subject('verify email account');
             $ci->email->message($message);
             $ci->email->send(FALSE);
-            
+
         return true;
     }
 
