85aa993025ccf805123938ee113ce7ded4319691
shyam <shyam@itdivine.in>
2019-12-06 12:37:57 +0530

    corporate  cv bank

--- application/views/fontend/layout/employer_left.php
+++ application/views/fontend/layout/employer_left.php
@@ -1,32 +1,25 @@
-                     <div class="col-md-3">
-                     
-                     	<nav class="side-menu hidden-sm hidden-xs">
-  <ul>
-    <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li>
-    <li class="title">Employer</li>
-    <li> <a href="<?php echo base_url(); ?>employer" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
-    <li> <a href="<?php echo base_url(); ?>employer/profile_setting" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> Edit Profile </a> </li>
-    <li> <a href="<?php echo base_url(); ?>employer/job-post" class=""> <i class="fa fa-pencil" aria-hidden="true"></i> Post New Job </a> </li>
-    <li> <a href="<?php echo base_url() ?>employer/active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
-    <li> <a href="<?php echo base_url() ?>employer/pending-job" class=""><i class="fa fa-clock-o" aria-hidden="true"></i> Pending Job </a> </li>
-    <li> <a href="<?php echo base_url() ?>employer/all-questions" class=""><i class="fa fa-check-square-o" aria-hidden="true"></i> Question Bank</a> </li>
-    <li> <a href="<?php echo base_url() ?>employer/add-question" class=""><i class="fa fa-pencil" aria-hidden="true"></i> Add New Question</a></li>
-	<li> <a href="<?php echo base_url() ?>employer/questionbank-import" class=""><i class="fa fa-upload" aria-hidden="true"></i>Import Question</a></li> 
-	<li> <a href="<?php echo base_url() ?>employer/addemployee" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Employe</a></li> 
-	<li> <a href="<?php echo base_url() ?>employer/allemployee" class=""><i class="fa fa-user" aria-hidden="true"></i> Employee</a></li>
-  <li> <a href="<?php echo base_url() ?>employer/addconsultant" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Consultant</a></li> 
-  <li> <a href="<?php echo base_url() ?>employer/allconsultants" class=""><i class="fa fa-user" aria-hidden="true"></i>All Consultants</a></li> 	
-	<!-- <li> <a href="<?php echo base_url() ?>employer/change-password" class=""><i class="fa fa-lock" aria-hidden="true"></i> Change Password </a> </li>     -->
-    <!-- <li> <a href="<?php echo base_url() ?>employer/logout" class=""> <i class="fa fa-sign-out" aria-hidden="true"></i> Sign Out </a> </li> -->
-    
+<div class="col-md-3">
+  <nav class="side-menu hidden-sm hidden-xs">
+    <ul>
+      <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li>
+      <li class="title">Employer</li>
+      <li> <a href="<?php echo base_url(); ?>employer" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
+      <li> <a href="<?php echo base_url(); ?>employer/profile_setting" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> Edit Profile </a> </li>
+      <li> <a href="<?php echo base_url(); ?>employer/job-post" class=""> <i class="fa fa-pencil" aria-hidden="true"></i> Post New Job </a> </li>
+      <li> <a href="<?php echo base_url() ?>employer/active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
+      <li> <a href="<?php echo base_url() ?>employer/pending-job" class=""><i class="fa fa-clock-o" aria-hidden="true"></i> Pending Job </a> </li>
+      <li> <a href="<?php echo base_url() ?>employer/all-questions" class=""><i class="fa fa-check-square-o" aria-hidden="true"></i> Question Bank</a> </li>
+      <li> <a href="<?php echo base_url() ?>employer/add-question" class=""><i class="fa fa-pencil" aria-hidden="true"></i> Add New Question</a></li>
+  	 <li> <a href="<?php echo base_url() ?>employer/questionbank-import" class=""><i class="fa fa-upload" aria-hidden="true"></i>Import Question</a></li> 
+  	 <li> <a href="<?php echo base_url() ?>employer/addemployee" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Employe</a></li> 
+  	 <li> <a href="<?php echo base_url() ?>employer/allemployee" class=""><i class="fa fa-user" aria-hidden="true"></i> Employee</a></li>
+     <li> <a href="<?php echo base_url() ?>employer/addconsultant" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Consultant</a></li> 
+     <li> <a href="<?php echo base_url() ?>employer/allconsultants" class=""><i class="fa fa-user" aria-hidden="true"></i>All Consultants</a></li> 	
 
-    
-    
-  </ul>
-</nav>
+     <li> <a href="<?php echo base_url() ?>employer/corporate-cv-bank" class=""><i class="fa fa-file-text" aria-hidden="true"></i> CV Bank</a></li> 
+  	
+    </ul>
+  </nav>
 
-                     
-                     
-                           <!-- end widget -->
-                             
-                        </div><!-- end col -->
+ <!-- end widget -->
+</div><!-- end col -->
