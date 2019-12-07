e8e236f0fd7ad14b672f1085955e63b971f60839
unknown <santu@edunetsol.com>
2019-12-07 14:11:38 +0530

    consultant

--- application/controllers/Employer_login.php
+++ application/controllers/Employer_login.php
@@ -80,18 +80,17 @@ class Employer_Login extends CI_Controller
     	 return;
     	}
         
-        $whereres = "token='$hash'";
-        $employer_data= $this->Master_model->get_master_row('company_profile',$select = FALSE,$whereres);
-
-        $to_email = $employer_data['company_email'];
+        
 	
 	    $pass    = md5($this->input->post('password'));
    
         if ($this->employer_login_model->reset_account($hash,$pass)) {
             $this->session->set_flashdata('verify_msg', '<div class="alert alert-success text-center">Your password is successfully reset! Please login to access your account!</div>');
-            $subject = "Password changed Succcessfully";
+            $whereres = "token='$hash'";
+        $employer_data= $this->Master_model->get_master_row('company_profile',$select = FALSE,$whereres);
 
-            
+        $to_email = $employer_data['company_email'];
+           echo $to_email;
             redirect('employer_login');
         } else {
             $this->session->set_flashdata('verify_msg', '<div class="alert alert-danger text-center">Sorry! There is error in verifying your account!</div>');
