5e9952010e40ac3d02664c62f3a281438a25eab1
unknown <santu@edunetsol.com>
2019-12-07 10:29:57 +0530

    consultant

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1261,7 +1261,7 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
             // echo $consultant_id;
             if (isset($company_profile_id)) {
                 $where['company_profile_id']=$consultant_id;
-                
+                print_r($company_profile);
             $this->Master_model->master_update($company_profile,'company_profile',$where);
             $consultant_id=$this->input->post('consultant_id');
             $whr['con_comp_map_id']=$consultant_id;
@@ -1269,7 +1269,7 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
             $this->Master_model->master_update($data,'consultant_company_mapping',$whr);
 
 
-            redirect(base_url().'employer/allconsultants');
+            // redirect(base_url().'employer/allconsultants');
             }
                 # code...
             }
