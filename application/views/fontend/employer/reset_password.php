e623a984a71c6532aca8b9fa2fbca765f3802d38
unknown <santu@edunetsol.com>
2019-12-07 12:41:18 +0530

    consultant

--- application/views/fontend/employer/reset_password.php
+++ application/views/fontend/employer/reset_password.php
@@ -53,7 +53,16 @@
   </div>
 </div>
 
-
+<script>
+   function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+ </script>
 
 
                
