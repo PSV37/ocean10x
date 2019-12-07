60478e5517d71eb3f8c41303b0905f3cf2695439
unknown <santu@edunetsol.com>
2019-12-07 16:16:21 +0530

    consultant

--- application/views/fontend/employer/dashboard.php
+++ application/views/fontend/employer/dashboard.php
@@ -531,20 +531,43 @@
                 <?php }else{ ?>
                   <div class="col-md-12">
                     <div class="col-md-6"> 
-                       <label>Language</label>
+                       <label>Branch Address</label>
                       <input type="text" name="language[]" id="language1" class="form-control" >
                     </div>
-                    <div class="col-md-6">  
-                      <label>Proficiency</label>
-                      <select class="form-control" name="proficiency[]" id="proficiency">
-                        <option value="">Select Proficiency</option>
-                        <option value="Beginner">Beginner</option>
-                        <option value="Proficient">Proficient</option>
-                        <option value="Expert">Expert</option>
-                        
-                      </select>
-                    </div>
-                   
+                    <div class="col-md-4 col-sm-4">
+	                                        	<div class="formrow">
+	                                        <label class="control-label">Company Country: <span class="required">*</span></label>
+										  <select  name="country_id" id="country_id" class="form-control country" onchange="getStates(this.value)">
+											<option value="">Select Country</option>
+											<?php foreach($country as $key){?>
+											<option value="<?php echo $key['country_id']; ?>"<?php if($company_info->country_id==$key['country_id']){ echo "selected"; }?>><?php echo $key['country_name']; ?></option>
+											<?php } ?>
+										  </select>
+	                                        </div>
+	                                    </div>
+
+										
+										<div class="col-md-4 col-sm-4">
+											<div class="formrow">
+											<label class="control-label">Company State: <span class="required">*</span></label>
+											<select  name="state_id" id="state_id" class="form-control" onchange="getCitys(this.value)">
+											<option value="">Select State</option>
+										     
+											</select>
+										</div>
+	                                    </div>
+										
+										<div class="col-md-4 col-sm-4">
+											<div class="formrow">
+											<label class="control-label">Company City: <span class="required">*</span></label>
+											<select  name="city_id" id="city_id" class="form-control">
+											<option value="">Select City</option>
+											 
+											</select>
+	                                    </div>
+										
+
+	                                </div>
                   </div>
                 <?php } ?>
                 </div>
