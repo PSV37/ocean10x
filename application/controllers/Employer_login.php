2687902e2814cf5479fe577d3a89176c29f3a94e
unknown <santu@edunetsol.com>
2019-12-07 14:45:50 +0530

    consultant

--- application/controllers/Employer_login.php
+++ application/controllers/Employer_login.php
@@ -82,16 +82,29 @@ class Employer_Login extends CI_Controller
 	    $pass    = md5($this->input->post('password'));
         print_r($hash);
 
-        print_r($this->employer_login_model->reset_account($hash,$pass));
-   
-        // if ($this->employer_login_model->reset_account($hash,$pass)) {
-        //     $this->session->set_flashdata('verify_msg', '<div class="alert alert-success text-center">Your password is successfully reset! Please login to access your account!</div>');
+        $whereres = "token='$hash'";
+        $employer_data= $this->Master_model->get_master_row('company_profile',$select = FALSE,$whereres);
 
-        //     redirect('employer_login');
-        // } else {
-        //     $this->session->set_flashdata('verify_msg', '<div class="alert alert-danger text-center">Sorry! There is error in verifying your account!</div>');
-        //     redirect('employer_login');
-        // }
+        $company_email = $employer_data['company_email'];
+        $company_password = $employer_data['company_password'];
+       
+        // print_r($this->employer_login_model->reset_account($hash,$pass));
+   
+        if ($this->employer_login_model->reset_account($hash,$pass) || ($company_password==$pass)) {
+            $this->session->set_flashdata('verify_msg', '<div class="alert alert-success text-center">Your password is successfully reset! Please login to access your account!</div>');
+            $subject = "Password changed Succcessfully";
+
+            $message = '<div style="max-width:600px!important;padding:4px"><table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td align="center">
+<table width="100%" cellspacing="0" border="0"><tbody><tr><td style="font-size:0px;text-align:left" valign="top"></td></tr></tbody></table><table width="100%" cellspacing="0" cellpadding="0" border="0"><tbody><tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left"><td>
+<br><br>Hi Dear,<br>Your password changed successfully<br>
+
+Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Question? Check out how works and our support team are ready to help.<br><br>© 2017 ConsultnHire All Rights Reserved.<br><br>You have received this mail because your e-mail ID is registered with Consultnhire.com. This is a system-generated e-mail regarding your Consultnhire account preferences, please do not reply to this message. The jobs sent in this mail have been posted by the clients of Consultnhire.com. And we have enabled auto-login for your convenience, you are strongly advised not to forward this email to protect your account from unauthorized access. IEIL has taken all reasonable steps to ensure that the information in this mailer is authentic. Users are advised to research bonafides of advertisers independently. Please do not pay any money to anyone who promises to find you a job. IEIL shall not have any responsibility in this regard. We recommend that you visit our Terms & Conditions and the Security Advice for more comprehensive information.</td></tr><tr><td height="40"></td></tr></tbody></table></td></tr></tbody></table></div>';
+                            $send = sendEmail_JobRequest($company_email,$message,$subject);
+            redirect('employer_login');
+        } else {
+            $this->session->set_flashdata('verify_msg', '<div class="alert alert-danger text-center">Sorry! There is error in verifying your account!</div>');
+            redirect('employer_login');
+        }
 
     }
     
