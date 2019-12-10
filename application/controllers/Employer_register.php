b5ca3c5dd3aa50463b0f465d771d91451903a8e3
unknown <santu@edunetsol.com>
2019-12-10 14:15:38 +0530

    corporate docs

--- application/controllers/Employer_register.php
+++ application/controllers/Employer_register.php
@@ -96,7 +96,7 @@ class Employer_register extends CI_Controller
 			'country_id'       => $this->input->post('country_id'),
 			'state_id'         => $this->input->post('state_id'),
 			'city_id'          => $this->input->post('city_id'),
-			'company_pincode'          => $this->input->post('company_pincode'),
+			// 'company_pincode'          => $this->input->post('company_pincode'),
             'company_password' => md5($this->input->post('company_password')),
             'token'            => md5($this->input->post('company_email')),
         );
