596c65324fa43c05ddefea9641b5c4a70ff5472c
shyam <shyam@itdivine.in>
2019-12-11 18:27:05 +0530

    seeker photo

--- application/views/fontend/employer/bulk_cv_upload_view.php
+++ application/views/fontend/employer/bulk_cv_upload_view.php
@@ -25,16 +25,21 @@
         <div class="userccount empdash">
           <div class="formpanel"> <?php echo $this->session->flashdata('success'); ?>
            
-    		<form method='post' action="<?php echo base_url();?>employer/bulk_upload_cvs" enctype="multipart/form-data">
-      		<div class="panel-body">
-      		  <input type='file' name='file' required class="form-control">
-      		</div>
-			
-			    <input type="hidden" name="org_id" value="1" class="form-control">
-		      <input type='submit' value='Import' name='upload' class="btn btn-info btn-sm" style="width:60px; margin-left:15px;">
-	</form>
-
-
+        		<form method='post' action="<?php echo base_url();?>employer/bulk_upload_cvs" enctype="multipart/form-data">
+          		<div class="panel-body">
+                <div class="col-md-6">
+                   <input type='file' name='file' required class="form-control">
+                </div>
+                 <div class="col-md-6">
+                  <small>To Import Leads Download Excel Format <a href="<?php echo base_url(); ?>cv_bank_excel/bulk_upload_cv_format.csv" download><strong>Click here To Download</strong></a></small>
+                </div>
+          		 
+          		</div>
+             
+    			
+    			    <input type="hidden" name="org_id" value="1" class="form-control">
+    		      <input type='submit' value='Import' name='upload' class="btn btn-info btn-sm" style="width:60px; margin-left:15px;">
+    	      </form>
 
           </div>
         </div>
