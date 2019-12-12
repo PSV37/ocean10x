b10fd86a0dfa6f4c23bfc51a57b3b155a242f9dd
unknown <santu@edunetsol.com>
2019-12-12 18:24:18 +0530

    corporate docs

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -94,8 +94,7 @@ class Employee extends CI_controller
                     <div style="max-width:600px!important;padding:4px"><table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td align="center">
                     <table width="100%" cellspacing="0" border="0"><tbody><tr><td style="font-size:0px;text-align:left" valign="top"></td></tr></tbody></table><table width="100%" cellspacing="0" cellpadding="0" border="0"><tbody><tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left"><td>
                     <br><br>Hi '.$company_name.',<br>Your Employee '.$name.' Updated Ocean Profile.<br/>
-                    Thank You<br>Ocean Team..
-                        ';
+                    Thank You<br>Ocean Team';
                          
                    $send = sendEmail_JobRequest($to_mail,$message,$subject);
                    // print_r($message);
