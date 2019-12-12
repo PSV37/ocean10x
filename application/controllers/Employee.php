1d5838817a6f79faa0851fe9ededfe328efcb1c1
unknown <santu@edunetsol.com>
2019-12-12 17:46:58 +0530

    corporate docs

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -87,6 +87,7 @@ class Employee extends CI_controller
                     Thank You
                     Ocean Team..
                         ';
+                         $this->session->set_flashdata('success_msg', '<div class="alert alert-success text-center">Company Profile details have been successfully updated !</div>');
                    $send = sendEmail_JobRequest($to_mail,$message,$subject);
 
 
