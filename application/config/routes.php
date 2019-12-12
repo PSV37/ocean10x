63dd33fa0c660e8d0f35be97715701ff921c868f
unknown <santu@edunetsol.com>
2019-12-12 15:51:20 +0530

    corporate docs

--- application/config/routes.php
+++ application/config/routes.php
@@ -162,7 +162,7 @@ $route['save-job/(:any)'] = "job/save_this_job/$1";
 
 $route['employee-login']="employee_login";
 // $route['employee/edit-profile']="employee/edit_profile";
-$route['Edit-profile']="employee/edit_profile"
+$route['Edit-profile']="employee/edit_profile";
 
 // Category and Level Search Jobs
 $route['jobs/type/(:any)'] = "job/get_job_types_job/$1";
