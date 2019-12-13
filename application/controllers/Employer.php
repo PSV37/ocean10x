2d69a92bd122e4a24ab7c7ea1270d40612686d47
unknown <santu@edunetsol.com>
2019-12-13 15:03:47 +0530

    corporate docs

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1217,7 +1217,6 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             if(isset($_POST['add_consultant'])) 
             {
                 $company_id=$this->input->post('company_profile_id');
-                
                 if (isset($company_id) && !empty($company_id)) {
                     
                     $exist_companyid = $this->company_profile_model->companyid_check($company_id,$user_id);
@@ -1238,6 +1237,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
                     }
                 }else{
                         $to_email=$this->input->post('cont_person_email');
+                        echo $to_email;
                         $exist_companyname = $this->company_profile_model->companyname_check($this->input->post('company_name'));
                          $exist_email    = $this->company_profile_model->email_check($this->input->post('company_email'));
                         $exist_username = $this->company_profile_model->username_check($this->input->post('company_username'));
@@ -1296,13 +1296,23 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
 
                     }
             }
-             
+             $subject = "Registration done successfully";
+
+            $message = '<div style="max-width:600px!important;padding:4px"><table style="padding:0 45px;width:100%!important;padding-top:45px;border:1px solid #f0f0f0;background-color:#ffffff" align="center" cellspacing="0" cellpadding="0" border="0"><tbody><tr><td align="center">
+<table width="100%" cellspacing="0" border="0"><tbody><tr><td style="font-size:0px;text-align:left" valign="top"></td></tr></tbody></table><table width="100%" cellspacing="0" cellpadding="0" border="0"><tbody><tr style="font-size:16px;font-weight:300;color:#404040;line-height:26px;text-align:left"><td>
+<br><br>Hi Dear,<br>Your account has been created successfully by '.$comp_name.' <br><br>You can login to our portal using following credentials<br>
+username: '.$to_email.'<br>
+Password: '.$pass.'<br>
+<a href="https://www.consultnhire.com/employer_login" class="btn btn-primary" value="Login" align="center" target="_blank">Login Now</a>
+
+Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Question? Check out how works and our support team are ready to help.<br><br>© 2017 ConsultnHire All Rights Reserved.<br><br>You have received this mail because your e-mail ID is registered with Consultnhire.com. This is a system-generated e-mail regarding your Consultnhire account preferences, please do not reply to this message. The jobs sent in this mail have been posted by the clients of Consultnhire.com. And we have enabled auto-login for your convenience, you are strongly advised not to forward this email to protect your account from unauthorized access. IEIL has taken all reasonable steps to ensure that the information in this mailer is authentic. Users are advised to research bonafides of advertisers independently. Please do not pay any money to anyone who promises to find you a job. IEIL shall not have any responsibility in this regard. We recommend that you visit our Terms & Conditions and the Security Advice for more comprehensive information.</td></tr><tr><td height="40"></td></tr></tbody></table></td></tr></tbody></table></div>';
+                           $send = sendEmail_JobRequest($to_email,$message,$subject);
                                 
 
         }
             elseif (isset($_POST['update_consultant'])) {
             $company_profile_id=$this->input->post('company_profile_id');
-             echo $company_profile_id;
+            // echo $consultant_id;
             if (isset($company_profile_id)) {
                 $where['company_profile_id']=$company_profile_id;
                 // print_r($company_profile);
