ec9da2bdd4e2323e7064ca66a1a36b4cffb6309a
unknown <santu@edunetsol.com>
2019-12-14 14:25:01 +0530

    employee

--- application/views/fontend/employer/forword_job.php
+++ application/views/fontend/employer/forword_job.php
@@ -88,11 +88,15 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
      var employer_id=document.getElementById('company_profile_id').value;
      alert(employer_id);
       $.ajax({
-                type:'POST',
-                url:'<?php echo site_url('employee/get_fav_consultants');?>',
-                data:{id:employer_id},
-                success:function(res){
-                  console.log(res);
+                 url:'<?php echo site_url('employee/get_fav_consultants') ?>',
+              type:'POST',
+              data:{
+                    id:employer_id
+              },
+               dataType: "JSON",  
+               success: function(data)
+               {
+                  console.log(data);
                  // $.each(res, function(index, value) 
                  //  {
                  //    // console.log(value);
