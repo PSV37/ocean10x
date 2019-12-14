9d17a38f316c03f243c34e3df50cecbcb315b180
unknown <santu@edunetsol.com>
2019-12-14 11:38:28 +0530

    employee

--- application/views/fontend/employer/forword_job.php
+++ application/views/fontend/employer/forword_job.php
@@ -92,7 +92,7 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
                 url:'<?php echo base_url();?>consultants',
                 data:{id:employer_id},
                 success:function(res){
-                  console.log(result);
+                  console.log(res);
                     // $('#city_id').html(res);
                     // $('#city_id').val(<?php echo $company_info->city_id; ?>);
                 }
