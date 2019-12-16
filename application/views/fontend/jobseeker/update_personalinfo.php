beff7eb68e358f5f6ab64829ed7b3b21d77cc456
unknown <santu@edunetsol.com>
2019-12-16 15:32:13 +0530

    employee

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -292,12 +292,11 @@
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">add Language</h4>
+        
       </div>
        
   <div class="modal-body">
-    <form id="Personal-info" class="form-horizontal" action="<?php echo base_url('job_seeker/update_personalinfo');?>"  method="post" autocomplete="off">
+    <form id="Personal-info" class="form-horizontal" action="<?php echo base_url('job_seeker/update_language');?>"  method="post" autocomplete="off">
     <input type="hidden" value="<?php echo $js_personal_info->job_personal_info_id; ?>" name="js_personal_info_id">
             
    
@@ -315,7 +314,7 @@
                   <div class="col-md-12">
                     <div class="col-md-6"> 
                        <label>Language</label>
-                      <input type="text" name="language[]" id="language1" class="form-control" value="">
+                      <input type="text" name="language" id="language" class="form-control" value="">
                     </div>
                     <div class="col-md-6">  
                       <label>Proficiency</label>
@@ -1183,116 +1182,8 @@
 
 
     <div class="panel-body"></div>   
-    <div class="row">
-      <div class="col-md-12">
-        <div class="col-md-12 col-sm-12">
-          <div class="input-group">
-              <h6>Languages</h6><br>
-              <button class="btn btn-success btn-xs pull-right add-more" type="button"><i class="fa fa-plus"></i> Add Language</button> <br>
-              <div class="input-group control-group after-add-more">
-                <div>
-                  <?php if(!empty($languages)) foreach($languages as $lrow){?>
-                  <div class="col-md-12">
-                    <div class="col-md-6"> 
-                       <label>Language</label>
-                      <input type="text" name="language[]" id="language1" class="form-control" value="<?php echo $lrow['language']; ?>">
-                    </div>
-                    <div class="col-md-6">  
-                      <label>Proficiency</label>
-                      <select class="form-control" name="proficiency[]" id="proficiency">
-                        <option value="">Select Proficiency</option>
-                        <option value="Beginner"<?php if($lrow['proficiency']=='Beginner'){echo 'selected';} ?>>Beginner</option>
-                        <option value="Proficient"<?php if($lrow['proficiency']=='Proficient'){echo 'selected';} ?>>Proficient</option>
-                        <option value="Expert"<?php if($lrow['proficiency']=='Expert'){echo 'selected';} ?>>Expert</option>
-                        
-                      </select>
-                    </div>
-                    <div class="col-md-12" style="margin-top:10px;">  
-                      <input type="checkbox" name="lang_read[]" id="lang_read" value="Yes"<?php if($lrow['lang_read']=='Yes'){echo 'checked';} ?> style="margin: 0 15px;"> Read
-
-                      <!-- <input type="hidden" name="lang_read[]" id="txtlang_read" value="No" style="margin: 0 15px;">  -->
-
-                      <input type="checkbox" name="lang_write[]" id="lang_write" value="Yes"<?php if($lrow['lang_write']=='Yes'){echo 'checked';} ?> style="margin: 0 15px;"> Write
-                     <!--  <input type="hidden" name="lang_write[]" id="txtlang_write" value="No" style="margin: 0 15px;">  -->
-                      
-                      <input type="checkbox" name="lang_speak[]" id="lang_speak" value="Yes"<?php if($lrow['lang_speak']=='Yes'){echo 'checked';} ?> style="margin: 0 15px;"> Speak
-                      <!-- <input type="hidden" name="lang_speak[]" id="txtlang_speak" value="No" style="margin: 0 15px;"> 
-                     -->
-                      <a href="<?php echo base_url() ?>/job_seeker/delete_mylanguage/<?php echo $lrow['id']; ?>" class="btn btn-danger btn-xs pull-right" title="Remove" data-toggle="tooltip" data-placement="top" onclick="return confirm('Are you sure want to delete this language ?');"><i class="fa fa-trash"></i> Remove</a>
-                    </div>
-                  </div>
-                <?php }else{ ?>
-                  <div class="col-md-12">
-                    <div class="col-md-6"> 
-                       <label>Language</label>
-                      <input type="text" name="language[]" id="language1" class="form-control" >
-                    </div>
-                    <div class="col-md-6">  
-                      <label>Proficiency</label>
-                      <select class="form-control" name="proficiency[]" id="proficiency">
-                        <option value="">Select Proficiency</option>
-                        <option value="Beginner">Beginner</option>
-                        <option value="Proficient">Proficient</option>
-                        <option value="Expert">Expert</option>
-                        
-                      </select>
-                    </div>
-                    <div class="col-md-12" style="margin-top:10px;">  
-                      <input type="checkbox" name="lang_read[]" id="lang_read" value="Yes" style="margin: 0 15px;"> Read
-                      <!-- <input type="hidden" name="lang_read[]" id="txtlang_read" value="No" style="margin: 0 15px;">  -->
-                      
-                      <input type="checkbox" name="lang_write[]" id="lang_write" value="Yes" style="margin: 0 15px;"> Write
-                      <!-- <input type="hidden" name="lang_write[]" id="txtlang_write" value="No" style="margin: 0 15px;">  -->
-                      
-                      <input type="checkbox" name="lang_speak[]" id="lang_speak" value="Yes" style="margin: 0 15px;"> Speak
-                      <!-- <input type="hidden" name="lang_speak[]" id="txtlang_speak" value="No" style="margin: 0 15px;">  -->
-
-                    </div>
-                  </div>
-                <?php } ?>
-                </div>
-              </div>
-          </div>     
-        </div>
-      </div>
-    </div>
-       <!-- Copy Fields -->
-      <div class="copy hide">
-        <div class="control-group input-group" style="margin-top:10px">
-          <div>
-            <div class="col-md-12">
-              <div class="col-md-6"> 
-                 <label>Language</label>
-                <input type="text" name="language[]" id="language2" class="form-control">
-              </div>
-              <div class="col-md-6">  
-                <label>Proficiency</label>
-                <select class="form-control" name="proficiency[]" id="proficiency">
-                  <option value="">Select Proficiency</option>
-                  <option value="Beginner">Beginner</option>
-                  <option value="Proficient">Proficient</option>
-                  <option value="Expert">Expert</option>
-                </select>
-              </div>
-              <div class="col-md-12" style="margin-top:10px;">  
-                <input type="checkbox" name="lang_read[]" id="lang_read" value="Yes" style="margin: 0 15px;"> Read
-                <!-- <input type="hidden" name="lang_read[]" id="txtlang_read" value="No" style="margin: 0 15px;">  -->
-                
-                <input type="checkbox" name="lang_write[]" id="lang_write" value="Yes" style="margin: 0 15px;"> Write
-                <!-- <input type="hidden" name="lang_write[]" id="txtlang_write" value="No" style="margin: 0 15px;">  -->
-                
-                <input type="checkbox" name="lang_speak[]" id="lang_speak" value="Yes" style="margin: 0 15px;"> Speak
-                <!-- <input type="hidden" name="lang_speak[]" id="txtlang_speak" value="No" style="margin: 0 15px;">  -->
-
-                <button class="btn btn-danger btn-xs pull-right remove" type="button"><i class="fa fa-trash"></i> Remove</button><br/>
-              </div>
-
-            </div>
-          </div>
-        
-          <br/>
-        </div>
-      </div>
+           <!-- Copy Fields -->
+      
       <!--<div class="row">
        <div class="col-md-12">
       <div class="col-md-6 col-sm-12">         
@@ -1310,11 +1201,7 @@
       </div>
       </div>-->
       
-               <div class="modal-footer">
-                 
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            <button type="submit" class="btn btn-primary">Save</button>
-          </div>
+              
             </form>
      
         </div>
