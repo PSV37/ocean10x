170d254cf60c7ddcbdfeba05cc93c0da66efada0
unknown <santu@edunetsol.com>
2019-12-16 14:47:18 +0530

    employee

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -1279,23 +1279,23 @@
 
 </script>
 <script type="text/javascript">
-$("form").submit(function () {
+// $("form").submit(function () {
 
-    var this_master = $(this);
+//     var this_master = $(this);
 
-    this_master.find('input[type="checkbox"]').each( function () {
-        var checkbox_this = $(this);
+//     this_master.find('input[type="checkbox"]').each( function () {
+//         var checkbox_this = $(this);
 
 
-        if( checkbox_this.is(":checked") == true ) {
-            checkbox_this.attr('value','no');
-        } else {
-            checkbox_this.prop('checked',true);
-            //DONT' ITS JUST CHECK THE CHECKBOX TO SUBMIT FORM DATA    
-            checkbox_this.attr('value','yes');
-        }
-    })
-})
+//         if( checkbox_this.is(":checked") == true ) {
+//             checkbox_this.attr('value','No');
+//         } else {
+//             checkbox_this.prop('checked',true);
+//             //DONT' ITS JUST CHECK THE CHECKBOX TO SUBMIT FORM DATA    
+//             checkbox_this.attr('value','yes');
+//         }
+//     })
+// })
 
     $('#tokenfield').tokenfield({
       autocomplete: {
