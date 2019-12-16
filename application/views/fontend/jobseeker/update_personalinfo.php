7c331534ed7c6870d95f33f28aa026d036ea6e6b
unknown <santu@edunetsol.com>
2019-12-16 17:28:37 +0530

    employee

--- application/views/fontend/jobseeker/update_personalinfo.php
+++ application/views/fontend/jobseeker/update_personalinfo.php
@@ -195,7 +195,7 @@
                 <td><?php if($lrow['lang_read']=='Yes'){echo "<i class='fa fa-check' style='color:green;'></i>";}else{echo "<i class='fa fa-remove' style='color:red;'></i>";} ?></td>
                 <td><?php if($lrow['lang_write']=='Yes'){echo "<i class='fa fa-check' style='color:green;'></i>";}else{echo "<i class='fa fa-remove' style='color:red;'></i>";}  ?></td>
                 <td><?php if($lrow['lang_speak']=='Yes'){echo "<i class='fa fa-check' style='color:green;'></i>";}else{echo "<i class='fa fa-remove' style='color:red;'></i>";}  ?></td>
-                <td> <?php echo btn_edit('job_seeker/edit_language/' . base64_encode($languages['id'])); ?></td>
+                <td> <?php echo btn_edit('job_seeker/edit_language/' . base64_encode($lrow['id'])); ?></td>
               </tr>
             <?php } ?>
             </tbody>
