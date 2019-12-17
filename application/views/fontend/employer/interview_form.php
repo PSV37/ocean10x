e0a9e8fde4004ccccf8699196e632d7a17d7fc0c
unknown <santu@edunetsol.com>
2019-12-17 18:15:48 +0530

    employee

--- application/views/fontend/employer/interview_form.php
+++ application/views/fontend/employer/interview_form.php
@@ -59,7 +59,7 @@
           <div class="col-sm-3">  
             <label class="control-label" for="email">End Time<span class="required">*</span></label>
             <!-- <input type="time" name="end_time[]" id="end_time" class="form-control" value="<?php echo date('H:i'); ?>"> -->
-            <select id="select1" class="control-label"> 
+            <select id="select1" class="form-control"> 
             <!-- <option value="free">Free</option> 
             <option value="basic">Basic</option> --> 
             </select> 
