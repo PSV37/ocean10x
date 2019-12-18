c408640d2a02117ffa2ecd3364fa4674091953b4
shyam <shyam@itdivine.in>
2019-12-18 11:48:24 +0530

    cv bank data

--- application/views/fontend/employer/corporate_cv_bank.php
+++ application/views/fontend/employer/corporate_cv_bank.php
@@ -53,6 +53,7 @@
                 <?php $key = 1; if (!empty($cv_bank_data)): foreach ($cv_bank_data as $cv_row) : 
                   $resume = getUploadedResume($cv_row['js_email']);
                   $photo = getSeekerPhoto($cv_row['js_email']);
+                  $updates = getSeekerlastUpdates($cv_row['js_email']);
 
                 ?>
                   <tr>
@@ -66,7 +67,7 @@
                       <td><?php echo $cv_row['js_current_designation']; ?></td>
                       <td><?php echo $cv_row['js_current_notice_period']; ?></td>
                       <td><a href="<?php echo  base_url(); ?>upload/Resumes/<?php if(!empty($resume[0]['resume'])){echo $resume[0]['resume'];} ?>" title='Download Attached Resume' download><i class="fa fa-download"></i> </a></td>
-                      <td><?php if(!is_null($cv_row['update_at'] || $cv_row['update_at']!='0000-00-00 00:00:00')) { $mtime = time_ago_in_php($cv_row['update_at']);
+                      <td><?php if(!is_null($updates[0]['update_at'] || $updates[0]['update_at']!='0000-00-00 00:00:00')) { $mtime = time_ago_in_php($updates[0]['update_at']);
                             echo $mtime;} ?></td>
                   </tr>
                   <?php
