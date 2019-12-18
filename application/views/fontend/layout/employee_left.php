331d304c00441eaaddd8f1e18d10d6600a7b102c
unknown <santu@edunetsol.com>
2019-12-18 16:32:08 +0530

    employee

--- application/views/fontend/layout/employee_left.php
+++ application/views/fontend/layout/employee_left.php
@@ -12,10 +12,7 @@
       <!-- <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li> -->
       <li> <a href="<?php echo base_url(); ?>employee/index" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
       <li class="title">Employee</li>
-     <!--  <?php if (in_array("edit_profile", $))
-  {?>
-  <li> <a href="<?php echo base_url() ?>employee/edit-profile" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Profile </a> </li>
-  <?php }?> -->
+     <!--  -->
       
       
       <li> <a href="<?php echo base_url() ?>active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
