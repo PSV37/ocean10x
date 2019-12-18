c5435031643f6e659526d877a8fbbb3aef4e7e21
unknown <santu@edunetsol.com>
2019-12-18 16:34:45 +0530

    employee

--- application/views/fontend/layout/employee_left.php
+++ application/views/fontend/layout/employee_left.php
@@ -4,7 +4,7 @@
         $data = $this->Master_model->get_master_row("employee", $select_edu, $where_apply, $join = FALSE);
         $access=explode(",", $data['access_to_employee']);
         print_r($access);
-       
+
  ?>
 <div class="col-md-3">
   <nav class="side-menu hidden-sm hidden-xs">
@@ -12,7 +12,10 @@
       <!-- <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li> -->
       <li> <a href="<?php echo base_url(); ?>employee/index" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
       <li class="title">Employee</li>
-     <!--  -->
+      <?php if (in_array("edit_profile ", $access))
+  {?>
+  <li> <a href="<?php echo base_url() ?>employee/edit-profile" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Profile </a> </li>
+  <?php }?> 
       
       
       <li> <a href="<?php echo base_url() ?>active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
