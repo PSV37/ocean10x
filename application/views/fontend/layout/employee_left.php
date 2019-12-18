917aa7c1bfc7bab2df24b1c9073614aa026301a0
unknown <santu@edunetsol.com>
2019-12-18 16:25:13 +0530

    employee

--- application/views/fontend/layout/employee_left.php
+++ application/views/fontend/layout/employee_left.php
@@ -2,7 +2,7 @@
  $where_apply="emp_id='$employee_id'";
         $select_edu = "access_to_employee";
         $data = $this->Master_model->get_master_row("employee", $select_edu, $where_apply, $join = FALSE);
-        $access=$data['access_to_employee']);
+        $access=$data['access_to_employee'];
         print_r($access['0']);
         $HiddenProducts = explode(',',$access);
 if (in_array('edit_profile', $HiddenProducts)) {
@@ -17,10 +17,10 @@ if (in_array('edit_profile', $HiddenProducts)) {
       <!-- <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li> -->
       <li> <a href="<?php echo base_url(); ?>employee/index" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
       <li class="title">Employee</li>
-     <!--  <?php if (find_in_set('edit_profile',$access) >0)
+      <?php if (find_in_set('edit_profile',$access) >0)
   {?>
   <li> <a href="<?php echo base_url() ?>employee/edit-profile" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Profile </a> </li>
-  <?php }?> -->
+  <?php }?>
       
       
       <li> <a href="<?php echo base_url() ?>active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
