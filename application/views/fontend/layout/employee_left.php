c6b5bfa06c43c185e64b10bcdbbe8c0933d77c98
unknown <santu@edunetsol.com>
2019-12-18 16:21:55 +0530

    employee

--- application/views/fontend/layout/employee_left.php
+++ application/views/fontend/layout/employee_left.php
@@ -4,6 +4,12 @@
         $data = $this->Master_model->get_master_row("employee", $select_edu, $where_apply, $join = FALSE);
         $access=$data['access_to_employee']);
         print_r($access['0']);
+        $HiddenProducts = explode(',',$access);
+if (in_array('edit_profile', $HiddenProducts)) {
+  echo "Available";
+} else {
+  echo "Not available";
+}
  ?>
 <div class="col-md-3">
   <nav class="side-menu hidden-sm hidden-xs">
@@ -11,7 +17,7 @@
       <!-- <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li> -->
       <li> <a href="<?php echo base_url(); ?>employee/index" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
       <li class="title">Employee</li>
-      <?php if (find_in_set('edit_profile',$access)>0)
+      <?php if (find_in_set('edit_profile',$access) >0)
   {?>
   <li> <a href="<?php echo base_url() ?>employee/edit-profile" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Profile </a> </li>
   <?php }?>
