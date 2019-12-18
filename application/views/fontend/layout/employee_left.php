02351b8763cd598c711e659265aeed79a676c5e3
unknown <santu@edunetsol.com>
2019-12-18 16:44:06 +0530

    employee

--- application/views/fontend/layout/employee_left.php
+++ application/views/fontend/layout/employee_left.php
@@ -2,7 +2,7 @@
  $where_apply="emp_id='$employee_id'";
         $select_edu = "access_to_employee";
         $data = $this->Master_model->get_master_row("employee", $select_edu, $where_apply, $join = FALSE);
-        $access[]= $data['access_to_employee'];
+        $access=explode(",", $data['access_to_employee']);
         print_r($access);
 
  ?>
@@ -12,7 +12,9 @@
       <!-- <li> <a href="<?php echo base_url(); ?>" class=""> <i class="fa fa-home" aria-hidden="true"></i>Home </a> </li> -->
       <li> <a href="<?php echo base_url(); ?>employee/index" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
       <li class="title">Employee</li>
-      <?php if (in_array("edit_profile ", $a))
+      <?php $result=array_search('edit_profile', $access); 
+      echo $result;
+      if($result>0)
   {?>
   <li> <a href="<?php echo base_url() ?>employee/edit-profile" class=""> <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Profile </a> </li>
   <?php }?> 
