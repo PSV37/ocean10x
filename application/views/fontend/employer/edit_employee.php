f5f0a8d7bf23e1a32521c10104382a89eb42b389
unknown <santu@edunetsol.com>
2019-12-20 16:44:34 +0530

    employee

--- application/views/fontend/employer/edit_employee.php
+++ application/views/fontend/employer/edit_employee.php
@@ -95,7 +95,7 @@
                       <div class="form-group">
                           <label for="exampleInputEmail1">Give Access To User<span class="required">*</span></label>
                           <!-- <select class="selectpicker form-control" multiple data-live-search="true" id="user_acc" name="Access[]"> -->
-                            <select class=form-control" multiple data-live-search="true" name="user_acc" id="user_accc">
+                            <select class="form-control" multiple data-live-search="true" name="user_acc" id="user_accc">
                               <!-- <option value="post_job">Post A job</option>
                               <option value="editprofile">Edit Company Profile</option>
                               <option value="Addquestionbank">Add Question Bank</option>
