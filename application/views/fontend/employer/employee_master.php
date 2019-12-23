bcafd102f808812416b0520a1aa087c54e8a2915
unknown <santu@edunetsol.com>
2019-12-23 09:23:26 +0530

    employee

--- application/views/fontend/employer/employee_master.php
+++ application/views/fontend/employer/employee_master.php
@@ -76,10 +76,10 @@
                   &nbsp;&nbsp; <a href='#' title='Delete Record' data-toggle="modal" data-target="#deleteModal"  onclick="$('#del_id').val('<?php echo $key['emp_id'];?>');"><i class="fa fa-trash-o"></i></a>
           				</td>
                  <?php if($key['emp_status']=='1')
-                 {?> <td><button class="btn btn-success"  name="status" id="status" onclick="chnagestatus(this.value);" value="Active" ?></button>Active</td> <?}
+                 {?> <td><button class="btn btn-success"  name="status" id="status" onclick="chnagestatus(this.value);" value="Active" ?>Active</button></td> <?}
                  else
                   { ?> 
-                  <td style=""><button class="btn btn-danger"  name="status" id="status" onclick="chnagestatus(this.value);" value="Inactive" ?></button>Inactive</td>
+                  <td style=""><button class="btn btn-danger"  name="status" id="status" onclick="chnagestatus(this.value);" value="Inactive" ?>Inactive</button></td>
                   <?php } ?>
                 </tr>
 			        <?php } ?>
