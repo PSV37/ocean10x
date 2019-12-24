e36707a723618e727419d0481b6d43f84efbc6e0
unknown <santu@edunetsol.com>
2019-12-24 11:43:24 +0530

    employee

--- application/controllers/Employer_login.php
+++ application/controllers/Employer_login.php
@@ -49,8 +49,11 @@ class Employer_Login extends CI_Controller
              $this->session->set_flashdata('emp_msg', '<div class="alert alert-success alert-dismissable">Welcome Admin!</div>');
          }
          $data1['last_login']=date('Y-m-d H:i:s');
+             $whereres = "company_profile_id='$company_profile_id'";
         $this->Master_model->master_update($data1,'company_profile',$whereres);
-         
+        print_r($this->db->last_query());
+
+
                 redirect('employer/employee');
         } else {
             $this->session->set_flashdata('emp_msg',
