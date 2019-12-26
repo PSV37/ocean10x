5593c55da70f4e0e0e7e8ea86acb847e5ff5fab3
unknown <santu@edunetsol.com>
2019-12-26 10:54:37 +0530

    employee

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -2590,7 +2590,7 @@ public function interview_scheduler()
         $company=$this->session->userdata('company_name');
         $where1 = "employer_audit_record.company = '$company' ";
 
-        $data['result'] = $this->Master_model->getMaster('employer_audit_record', $where1, $join, $order = 'desc', $field = 'emp_updated_date', $select,$limit='10',$start=false, $search=false);
+        $data['result'] = $this->Master_model->getMaster('employer_audit_record', $where1, $join, $order = 'desc', $field = 'datetime', $select,$limit='10',$start=false, $search=false);
 
             $this->load->view('fontend/employer/audit',$data); 
 
