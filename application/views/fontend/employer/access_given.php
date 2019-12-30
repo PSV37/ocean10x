258775624c9191371cb5e82216cc07567a848da9
unknown <santu@edunetsol.com>
2019-12-30 12:16:09 +0530

    employee

--- application/views/fontend/employer/access_given.php
+++ application/views/fontend/employer/access_given.php
@@ -16,7 +16,7 @@
     <label class="control-label" for="email">Access given<span class="required">*</span></label>
       <table>
         <?php foreach ($access_data as $row) {
-          $data=explode(",", $row['access_to_employee'])
+          $data=explode(",", $row['access_to_employee']);
           print_r($data);
         } ?>
       </table>
