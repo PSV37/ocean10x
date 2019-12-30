c7e404b5b3dbfd445875a14339feafad55188c06
unknown <santu@edunetsol.com>
2019-12-30 15:17:46 +0530

    employee

--- application/controllers/Employer_login.php
+++ application/controllers/Employer_login.php
@@ -40,13 +40,14 @@ class Employer_Login extends CI_Controller
             $company_profile_id=$this->session->userdata('company_profile_id');
              $whereres = "company_profile_id='$company_profile_id'";
             $employer_data= $this->Master_model->get_master_row('company_profile',$select = FALSE,$whereres);
+             $where11['company_profile_id']=$company_profile_id;
+             
+        // $this->Master_model->master_update($update_data,'company_profile',$where11);
           
             if($employer_data['last_login']=="0000-00-00 00:00:00")
             {
                 $this->session->set_flashdata('emp_msg', '<div class="alert alert-success alert-dismissable">Thank You for joining “TheOcean” !</div>');
-                $where11['company_profile_id']=$company_profile_id;
-             
-        $this->Master_model->master_update($update_data,'company_profile',$where11);
+               
         // print_r($this->db->last_query());die;
 
 
@@ -54,10 +55,10 @@ class Employer_Login extends CI_Controller
             }else{
              $this->session->set_flashdata('emp_msg', '<div class="alert alert-success alert-dismissable">Welcome to TheOcean !<br>You are logged in as Administrator for '.$this->session->userdata('company_name').' !</div>');
              $this->session->set_flashdata('employee', '<div class="alert alert-success alert-dismissable">To start using TheOcean resources, we have created 3 users. Please enter their details !</div>');
-               $update_data=array('last_login'=>date('Y-m-d H:i:s'));
-          $where11['company_profile_id']=$company_profile_id;
+        //        $update_data=array('last_login'=>date('Y-m-d H:i:s'));
+        //   $where11['company_profile_id']=$company_profile_id;
              
-        $this->Master_model->master_update($update_data,'company_profile',$where11);
+        // $this->Master_model->master_update($update_data,'company_profile',$where11);
         // print_r($this->db->last_query());die;
 
 
