f1fb9ff6cdc70e00d8fc7ed83fdeb4c02500ab73
unknown <santu@edunetsol.com>
2019-12-31 16:05:11 +0530

    employee

--- application/views/fontend/layout/employer_left.php
+++ application/views/fontend/layout/employer_left.php
@@ -9,28 +9,28 @@
 
       <li> <a href="<?php echo base_url(); ?>employer" class=""> <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard </a> </li>
 
-      <li   onclick="record_audit('Edit Profile','profile_setting');"  data-level_id='Edit_Profile' id="Edit_Profile" value=""> <a href="#" class=""> <i class="fa fa-user-circle-o" aria-hidden="true" ></i> Edit Profile </a> </li>
+      <li   onclick="record_audit('Edit Profile');"  data-level_id='Edit_Profile' id="Edit_Profile" value=""> <a href="#" class=""> <i class="fa fa-user-circle-o" aria-hidden="true" ></i> Edit Profile </a> </li>
 
-      <li onclick="record_audit('Post New Job ');"> <a href="<?php echo base_url(); ?>employer/job-post" class=""> <i class="fa fa-pencil" aria-hidden="true"></i> Post New Job </a> </li>
+      <li onclick="record_audit('Post New Job','job-post');"> <a href="#" class=""> <i class="fa fa-pencil" aria-hidden="true"></i> Post New Job </a> </li>
 
       <li onclick="record_audit('Posted Job');"> <a href="<?php echo base_url() ?>employer/active-job"><i class="fa fa-check-square-o" aria-hidden="true"></i> Posted Job </a> </li>
 
-      <li onclick="record_audit('Pending Job');"> <a href="<?php echo base_url() ?>employer/pending-job" class=""><i class="fa fa-clock-o" aria-hidden="true"></i> Pending Job </a> </li>
+      <li onclick="record_audit('Pending Job','pending-job');"> <a href="<?php echo base_url() ?>employer/pending-job" class=""><i class="fa fa-clock-o" aria-hidden="true"></i> Pending Job </a> </li>
 
       <li onclick="record_audit('Question Bank');"> <a href="<?php echo base_url() ?>employer/all-questions" class=""><i class="fa fa-check-square-o" aria-hidden="true"></i> Question Bank</a> </li>
 
-      <li onclick="record_audit('Add New Question');"> <a href="<?php echo base_url() ?>employer/add-question" class=""><i class="fa fa-pencil" aria-hidden="true"></i> Add New Question</a></li>
+      <li onclick="record_audit('Add New Question','add-question');"> <a href="#" class=""><i class="fa fa-pencil" aria-hidden="true"></i> Add New Question</a></li>
 
-  	 <li onclick="record_audit('Import Question');"> <a href="<?php echo base_url() ?>employer/questionbank-import" class=""><i class="fa fa-upload" aria-hidden="true"></i>Import Question</a></li> 
+  	 <li onclick="record_audit('Import Question','questionbank-import');"> <a href="#" class=""><i class="fa fa-upload" aria-hidden="true"></i>Import Question</a></li> 
 
-  	 <li onclick="record_audit('Add Employee');"> <a href="<?php echo base_url() ?>employer/addemployee" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Employee</a></li>
+  	 <li onclick="record_audit('Add Employee','addemployee');"> <a href="#" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Employee</a></li>
 
-  	 <li onclick="record_audit('Employee');"> <a href="<?php echo base_url() ?>employer/allemployee" class=""><i class="fa fa-user" aria-hidden="true"></i> Employee</a></li>
+  	 <li onclick="record_audit('Employee');"> <a href="<?php echo base_url() ?>employer/allemployee" class=""><i class="fa fa-user" aria-hidden="true"></i>Employees</a></li>
 
       <li onclick="record_audit('Deactivated Employees');"> <a href="<?php echo base_url() ?>employer/deactivated-employee" class=""><i class="fa fa-user-times" aria-hidden="true"></i>
       Deactivated Employees</a></li>
 
-     <li onclick="record_audit(' Add Consultant');"> <a href="<?php echo base_url() ?>employer/add-new-consultant" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Consultant</a></li> 
+     <li onclick="record_audit(' Add Consultant','add-new-consultant');"> <a href="#" class=""><i class="fa fa-plus" aria-hidden="true"></i> Add Consultant</a></li> 
 
      <li onclick="record_audit('All Consultants');"> <a href="<?php echo base_url() ?>employer/show-all-consultant" class=""><i class="fa fa-user" aria-hidden="true"></i>All Consultants</a></li> 
 
@@ -40,7 +40,7 @@
 
      <li onclick="record_audit('CV Bank');"> <a href="<?php echo base_url() ?>employer/corporate-cv-bank" class=""><i class="fa fa-file-text" aria-hidden="true"></i><?php echo $this->company_profile_model->company_name($employer_id); ?> CV Bank</a></li> 
 
-     <li onclick="record_audit('Add Corporate Documents');"> <a href="<?php echo base_url() ?>add-Corporate-Documents" class=""><i class="fa fa-file" aria-hidden="true"></i> Add Corporate Documents</a></li> 
+     <li onclick="record_audit('Add Corporate Documents','add-Corporate-Documents');"> <a href="#" class=""><i class="fa fa-file" aria-hidden="true"></i> Add Corporate Documents</a></li> 
 
   	
     </ul>
@@ -100,9 +100,21 @@
                 }
         
             });
+     
+    }
+    else
+    {
       $.ajax({
-
-      })
+                type:'POST',
+                url:'<?php echo base_url();?>Employer/add_to_audit',
+                data:{var1:var1},
+                success:function(res){
+                  
+                 
+                    
+                }
+        
+            });
     }
   }
   function submit_password()
