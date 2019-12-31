c3e577ab146808abac4f302d4871a4f2a5b04918
unknown <santu@edunetsol.com>
2019-12-31 16:47:33 +0530

    employee

--- application/views/fontend/employer/employee_master.php
+++ application/views/fontend/employer/employee_master.php
@@ -240,7 +240,7 @@ $("#delete_btn").click(function(e)
 $("#deactivate_btn").click(function(e)
    { 
       var id=$('#status').val();
-      alert(id);
+      // alert(id);
 
       e.preventDefault();
       
