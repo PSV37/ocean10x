f8c6275ceedfda39bc69a5a875f64deab67101d5
unknown <santu@edunetsol.com>
2020-01-02 10:16:11 +0530

    employee

--- application/views/fontend/employer/suspended.php
+++ application/views/fontend/employer/suspended.php
@@ -43,7 +43,8 @@
         			  <th>Email Id</th>
         			  <th>Mobile No.</th>
         			  <th>Department</th>
-        			  <th>Status</th>
+                <th>Status</th>
+        			  <th>Activate</th>
               </thead>
               <tbody>
           		<?php $srno=0; foreach($result as $key){ $srno++; 
@@ -58,6 +59,8 @@
           				<td><?php echo $key['department_name']; ?></td>				
           		
                   <td style=""><button class="btn btn-danger"  name="status" id="status" value="<?php echo $key['emp_id'];?>" >Suspended</button></td>
+
+                  <td><button class="btn btn-warning" title='activate' data-toggle="modal" data-target="#activateModal"  name="activate" id="activate"    value="<?php echo $key['emp_id'];?>" >Activate</button></td>
                  
 
                 </tr>
@@ -92,11 +95,74 @@
 document.getElementsByClassName('form-control').innerHTML+="<br />";
 </script>
 <!-- delete model -->
- 
+ <div id="activateModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" align="center">Activate Employee</h4>
+        </div>
+        <form  id="del" autocomplete="off" enctype="multipart/formdata" method="POST">
+          <div class="modal-body" id="deleteContent">
+            <input type="hidden" name="del_id" id="del_id">
+            <div class="form-group">
+              <p><b>Are you sure want to activate Account? </b></p>
+            </div>
+          </div>
+          <center><div id='res'></div></center>
+          <div class="modal-footer">
+            <button class="btn btn-success submit" id="activate_btn" name="submit">Confirm</button>
+            <button type="button" class="btn btn-primary btn-md" data-dismiss="modal">Cancel</button>      
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 
   </section>
 <!-- /.content -->
 </div>
+
+<script >
+  $("#activate_btn").click(function(e)
+   { 
+      var id=$('#activate').val();
+      // alert(id);
+
+      e.preventDefault();
+      
+         $.ajax({ 
+                
+                url:'<?php echo base_url();?>Employer/activate',
+                   
+                    type: "POST",
+                    data: {
+                           id:id  
+                    },
+                    success: function(data)
+                    {
+
+                    // $("button#del_id").button('reset');
+                       $("#res").html('<div class="alert alert-danger"><button type="button" class="close">×</button>Employee Activated</div>');
+                          window.setTimeout(function() {
+                                $(".alert").fadeTo(500, 0).slideUp(500, function(){
+                                    $(this).remove(); 
+                                });
+                                location.reload();
+                            }, 1500);
+                          $('.alert .close').on("click", function(e){
+                                $(this).parent().fadeTo(500, 0).slideUp(500);
+                          });
+                          location.reload();
+
+                    }
+            });
+
+
+
+    })
+</script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 
