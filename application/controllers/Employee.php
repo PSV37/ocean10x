0fbe891b2b02a9df37708293d395df2d74ffac4b
unknown <santu@edunetsol.com>
2020-01-03 18:04:53 +0530

    employee

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -84,30 +84,32 @@ class Employee extends MY_Employee_Controller
                     $parameter=$old_array_keys[$i];
                     $old_data=$old_array_values[$i];
                     $new_data=$employee_data[$parameter];
-                    if (isset($new_data) && !empty($new_data)) {
-                        if (($old_data==$new_data) && (($new_data=='emp_updated_date') || ($new_data=='emp_updated_by')) )
-                        {
+                    print_r($old_data);
+                    print_r($new_data);
+                    // if (isset($new_data) && !empty($new_data)) {
+                    //     if (($old_data==$new_data) && (($new_data=='emp_updated_date') || ($new_data=='emp_updated_by')) )
+                    //     {
                             
-                        }
-                        else
-                        {
-                            $employee_name=$this->session->userdata('name');
-                            $action= str_replace("_", ' ', $parameter);
-                            $data=array('company'=>$company_name,
-                                       'action_taken_for'=>$employee_name,
-                                        'field_changed' =>$action,
-                                        'Action'=>$employee_name.' changed '.$action,
-                                        'datetime'=>date('Y-m-d H:i:s'),
-                                        'updated_by' =>$employee_name);
-                            $result=$this->Master_model->master_insert($data,'employer_audit_record');
-                            // print_r($this->db->last_query());die;
+                    //     }
+                    //     else
+                    //     {
+                    //         $employee_name=$this->session->userdata('name');
+                    //         $action= str_replace("_", ' ', $parameter);
+                    //         $data=array('company'=>$company_name,
+                    //                    'action_taken_for'=>$employee_name,
+                    //                     'field_changed' =>$action,
+                    //                     'Action'=>$employee_name.' changed '.$action,
+                    //                     'datetime'=>date('Y-m-d H:i:s'),
+                    //                     'updated_by' =>$employee_name);
+                    //         $result=$this->Master_model->master_insert($data,'employer_audit_record');
+                    //         // print_r($this->db->last_query());die;
 
-                        }
-                    }
+                    //     }
+                    // }
                     
                 }
 
-         $this->Master_model->master_update($employee_data,'employee',$where);
+         $this->Master_model->master_update($employee_data,'employee',$where);die;
         // $this->Master_model->master_insert($employee_data,'emp_record_history');
         // print_r($this->db->last_query());die;       //    
           $whereres = "emp_id='$employee_id'";
