d581d43dbd5b4c9a289b4478da71050c10ec0294
unknown <santu@edunetsol.com>
2020-01-04 17:22:53 +0530

    employee

--- application/views/fontend/employer/edit_employee.php
+++ application/views/fontend/employer/edit_employee.php
@@ -283,7 +283,7 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
       function getuseraccess(id){
       if(id){
           
-        
+        console.log(<?php echo $result['access_to_employee']; ?>)
               $.ajax({
                   type:'POST',
                   url:'<?php echo base_url();?>employer/get_access_data',
@@ -291,6 +291,7 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
                   success:function(res){
                       $('#user_accc').html(res);
                       $("#user_accc").selectpicker('refresh');
+
                   }
 
           
