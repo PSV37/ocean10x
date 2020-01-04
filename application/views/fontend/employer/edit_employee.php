98186b2301807b05a22b504067cd2e8f8770b1cc
unknown <santu@edunetsol.com>
2020-01-04 14:10:41 +0530

    employee

--- application/views/fontend/employer/edit_employee.php
+++ application/views/fontend/employer/edit_employee.php
@@ -73,8 +73,10 @@
                        <div class="col-md-4">
                         <div class="form-group">
                         <label for="exampleInputEmail1">Password<span class="required">*</span></label>
-                        <input type="Password" name="Password" id="Password" maxlength="15" class="form-control" value="<?php echo $result['password']; ?>"  >
-                        <span toggle="#password-field" class="fa fa-lg fa-eye field-icon toggle-password"></span>
+                         <input id="password-field" type="password" class="form-control" name="password" value="secret">
+              <span toggle="#password-field" class="fa fa-lg fa-eye field-icon toggle-password"></span>
+                        <!-- <input type="Password" name="Password" id="Password" maxlength="15" class="form-control" value="<?php echo $result['password']; ?>"  >
+                        <span toggle="#password-field" class="fa fa-lg fa-eye field-icon toggle-password"></span> -->
                         </div>
                       </div>
 									   <div class="col-md-4">
@@ -208,7 +210,7 @@ document.getElementsByClassName('form-control').innerHTML+="<br />";
     input.attr("type", "password");
   }
 });
-  
+
 </script>
  
 	  
