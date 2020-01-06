403ef1906dc34423e2c3c79b6f070db018da9070
unknown <santu@edunetsol.com>
2020-01-06 12:27:40 +0530

    employee

--- application/views/fontend/layout/employer_left.php
+++ application/views/fontend/layout/employer_left.php
@@ -98,7 +98,7 @@
 
       <li> <a href="#" class=""> <i class="fa fa-user" aria-hidden="true"></i>Profile Completed </a>
          <div class="progress bg-warning">
-                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style=" margin:5px;  width:<?php echo $profile_total;?>%" ><?php echo round($profile_total); ?> percent Profile completed.
+                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="   width:<?php echo $profile_total;?>%" ><?php echo round($profile_total); ?> percent Profile completed.
                 
                  </div>
                 </div>
