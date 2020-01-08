c9a0ec4dd5f4039a09003c50a51ed61afe0de74f
unknown <santu@edunetsol.com>
2020-01-08 12:23:21 +0530

    employee

--- application/views/fontend/employer/dashboard_main.php
+++ application/views/fontend/employer/dashboard_main.php
@@ -21,7 +21,7 @@
 </div>
 <!-- Page Title End --> 
 <?php if (!empty($this->session->flashdata('emp_msg'))) {?>
-<div id="smsg" class="alert alert-alert-dismissible fade in" style="float: right;">
+<div id="smsg" class="alert alert-alert-dismissible fade in">
   <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
   <strong><?php echo $this->session->flashdata('emp_msg');?></strong>
 </div>             
