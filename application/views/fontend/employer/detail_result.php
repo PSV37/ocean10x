ce2425eb7bf087dcda8ccbfe0ea9b33c851c9e6e
unknown <santu@edunetsol.com>
2020-01-09 12:45:23 +0530

    employee

--- application/views/fontend/employer/detail_result.php
+++ application/views/fontend/employer/detail_result.php
@@ -1,5 +1,5 @@
 
-           <p>Total questions=</p><?php print_r($exam_res['0']['total_questions']); ?>
+           <p>Total questions=<?php print_r($exam_res['0']['total_questions']); ?></p>
             <table class="table table-bordered table-striped" id="dataTables-example">
               <thead>
                 <tr>
