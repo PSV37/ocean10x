80c753670019e138c2f30320e774edd13a509eae
unknown <santu@edunetsol.com>
2020-02-21 12:55:16 +0530

    change

--- application/views/fontend/employee/consultant_master.php
+++ application/views/fontend/employee/consultant_master.php
@@ -62,7 +62,7 @@
                 <td>
                  &nbsp;&nbsp; <a href="<?php echo base_url();?>employee/edit_consultant?id=<?php echo $key['con_comp_map_id']; ?>"><i class="fa fa-pencil"></i></a>
                  &nbsp;&nbsp; <!--<a href="<?php echo base_url();?>emp/deletestatus?id=<?php echo $key['con_comp_map_id']; ?>"> <i class="fa fa-toggle-on"></i></a>-->
-                &nbsp;&nbsp; <a href='#' "title='Delete Record' data-toggle="modal" data-target="#deleteModal"  onclick="$('#del_id').val('<?php echo $key['con_comp_map_id'];?>');"><i class="fa fa-trash-o"></i></a>
+                &nbsp;&nbsp; <a href='#' title='Delete Record' data-toggle="modal" data-target="#deleteModal"  onclick="$('#del_id').val('<?php echo $key['con_comp_map_id'];?>');"><i class="fa fa-trash-o"></i></a>
 				</td>
               </tr>
 			<?php } ?>
