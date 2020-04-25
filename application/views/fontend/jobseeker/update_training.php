85caf2de5fb98ae4e76b1faa63cfe1f5e119a343
unknown <santu@edunetsol.com>
2020-04-25 14:57:45 +0530

    test

--- application/views/fontend/jobseeker/update_training.php
+++ application/views/fontend/jobseeker/update_training.php
@@ -535,3 +535,6 @@ function check_other(value)
 $('.mdb-select').materialSelect();
 });
 </script>            
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
