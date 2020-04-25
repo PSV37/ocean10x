379d06ab7c964cb1fa076f92c447853d4efb7429
unknown <santu@edunetsol.com>
2020-04-25 13:52:34 +0530

    test

--- application/views/fontend/jobseeker/update_training.php
+++ application/views/fontend/jobseeker/update_training.php
@@ -1,4 +1,13 @@
+<style type="text/css">
+  .select2-container .select2-selection--single{
+    height:34px !important;
+}
+.select2-container--default .select2-selection--single{
+         border: 1px solid #ccc !important; 
+     border-radius: 0px !important; 
+}
 
+</style>
           <div class="section lb">
                 <div class="container">
                     <div class="row">
@@ -530,8 +539,7 @@ function check_other(value)
 }
 
 	</script> 
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>   
+    
 <script>
     $('.select2').select2();
 </script>          
--- application/views/fontend/layout/seeker_header.php
+++ application/views/fontend/layout/seeker_header.php
@@ -28,6 +28,8 @@
 
     <!-- multiselect css -->
     <link href="<?php echo base_url(); ?>asset/css/bootstrap-multiselect.css" rel="stylesheet" type="text/css" />
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 
     <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> <![endif]-->
 
