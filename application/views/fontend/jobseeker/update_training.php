7fb56cdf5fe2ce1c139f62e9f6c04117d9054446
unknown <santu@edunetsol.com>
2020-04-27 09:47:33 +0530

    test

--- application/views/fontend/jobseeker/update_training.php
+++ application/views/fontend/jobseeker/update_training.php
@@ -550,4 +550,14 @@ function check_other(value)
   allowClear: true
   });
 });
-</script>         
+
+</script>   
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        // var country = ["Australia", "Bangladesh", "Denmark", "Hong Kong", "Indonesia", "Netherlands", "New Zealand", "South Africa"];
+        $("#country_id").select2();
+      });
+    </script>      
