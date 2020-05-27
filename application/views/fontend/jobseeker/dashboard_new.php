6923c224a32a4a207551467b9b7f6323f96f916c
supriyaL <supriya@itdivine.in>
2020-05-27 15:23:10 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -11,222 +11,92 @@
                          
     <div class="row">
      <div class="content col-md-3">
-  <div class="sidebar-content ps-container ps-theme-default ps-active-y" data-ps-id="86b12d91-8471-8c99-2c26-69633b9b43c8">
-          <div class="nav-container">
-            <ul id="main-menu-navigation" data-menu="menu-navigation" class="navigation navigation-main">
-              <li class="has-sub nav-item"><a href="#"><i class="icon-home"></i><span data-i18n="" class="menu-title">Dashboard</span><span class="tag badge badge-pill badge-danger mt-1">2</span></a>
-                <ul class="menu-content" style="">
-                  <li class=""><a href="dashboard-ecommerce.html" class="menu-item">eCommerce</a>
-                  </li>
-                  <li class=""><a href="dashboard-analytics.html" class="menu-item">Analytics</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-screen-desktop"></i><span data-i18n="" class="menu-title">Apps</span></a>
-                <ul class="menu-content" style="">
-                  <li class=""><a href="chat.html" class="menu-item">Chat</a>
-                  </li>
-                  <li class=""><a href="taskboard.html" class="menu-item">Taskboard</a>
-                  </li>
-                  <li class=""><a href="calendar.html" class="menu-item">Calendar</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-magnet"></i><span data-i18n="" class="menu-title">UI Kit</span></a>
-                <ul class="menu-content" style="">
-                  <li class=""><a href="color-palette.html" class="menu-item">Color Palette</a>
-                  </li>
-                  <li class=""><a href="grids.html" class="menu-item">Grid</a>
-                  </li>
-                  <li class=""><a href="typography.html" class="menu-item">Typography</a>
-                  </li>
-                  <li class=""><a href="syntax-highlighter.html" class="menu-item">Syntax Highlighter</a>
-                  </li>
-                  <li class=""><a href="helper-classes.html" class="menu-item">Helper Classes</a>
-                  </li>
-                  <li class=""><a href="text-utilities.html" class="menu-item">Text Utilities</a>
-                  </li>
-                  <li class="has-sub"><a href="#" class="menu-item">Icons</a>
-                    <ul class="menu-content">
-                      <li><a href="feather.html" class="menu-item">Feather</a>
-                      </li>
-                      <li><a href="font-awesome.html" class="menu-item">Font Awesome</a>
-                      </li>
-                      <li><a href="simple-line.html" class="menu-item">Simple Line</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class=" nav-item"><a href="cards.html"><i class="icon-layers"></i><span data-i18n="" class="menu-title">Cards</span></a>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-puzzle"></i><span data-i18n="" class="menu-title">Components</span></a>
-                <ul class="menu-content" style="">
-                  <li class="has-sub open"><a href="#" class="menu-item">Bootstrap</a>
-                    <ul class="menu-content">
-                      <li><a href="components-lists.html" class="menu-item">List</a>
-                      </li>
-                      <li><a href="components-buttons.html" class="menu-item">Buttons</a>
-                      </li>
-                      <li><a href="components-alerts.html" class="menu-item">Alerts</a>
-                      </li>
-                      <li><a href="components-badges.html" class="menu-item">Badges</a>
-                      </li>
-                      <li><a href="components-dropdowns.html" class="menu-item">Dropdowns</a>
-                      </li>
-                      <li><a href="components-inputgroups.html" class="menu-item">Input Groups</a>
-                      </li>
-                      <li><a href="components-media-objects.html" class="menu-item">Media Objects</a>
-                      </li>
-                      <li><a href="components-pagination.html" class="menu-item">Pagination</a>
-                      </li>
-                      <li><a href="components-progress.html" class="menu-item">Progress Bars</a>
-                      </li>
-                      <li><a href="components-modals.html" class="menu-item">Modals</a>
-                      </li>
-                      <li><a href="components-collapse.html" class="menu-item">Collapse</a>
-                      </li>
-                      <li class="active"><a href="components-accordion.html" class="menu-item">Accordion</a>
-                      </li>
-                      <li><a href="components-carousel.html" class="menu-item">Carousel</a>
-                      </li>
-                      <li><a href="components-datepicker.html" class="menu-item">Datepicker</a>
-                      </li>
-                      <li><a href="components-popover.html" class="menu-item">Popover</a>
-                      </li>
-                      <li><a href="components-tooltip.html" class="menu-item">Tooltip</a>
-                      </li>
-                      <li><a href="components-tabs.html" class="menu-item">Tabs</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="has-sub"><a href="#" class="menu-item">Extra</a>
-                    <ul class="menu-content">
-                      <li><a href="sweet-alerts.html" class="menu-item">Sweet Alert</a>
-                      </li>
-                      <li><a href="toastr.html" class="menu-item">Toastr</a>
-                      </li>
-                      <li><a href="upload.html" class="menu-item">Upload</a>
-                      </li>
-                      <li><a href="editor.html" class="menu-item">Editor</a>
-                      </li>
-                      <li><a href="dragndrop.html" class="menu-item">Drag and Drop</a>
-                      </li>
-                      <li><a href="tour.html" class="menu-item">Tour</a>
-                      </li>
-                      <li><a href="tags-input.html" class="menu-item">Input Tag</a>
-                      </li>
-                      <li><a href="switch.html" class="menu-item">Switch</a>
-                      </li>
-                      <li><a href="rating.html" class="menu-item">Rating</a>
-                      </li>
-                      <li><a href="typeahead.html" class="menu-item">Typeahead</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-doc"></i><span data-i18n="" class="menu-title">Forms</span><span class="tag badge badge-pill badge-primary mt-1">New</span></a>
-                <ul class="menu-content">
-                  <li class="has-sub"><a href="#" class="menu-item">Elements</a>
-                    <ul class="menu-content">
-                      <li><a href="inputs.html" class="menu-item">Inputs</a>
-                      </li>
-                      <li><a href="input-groups.html" class="menu-item">Input Groups</a>
-                      </li>
-                      <li><a href="input-grid.html" class="menu-item">Input Grid</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li class="has-sub"><a href="#" class="menu-item">Layouts</a>
-                    <ul class="menu-content">
-                      <li><a href="basic-forms.html" class="menu-item">Basic Forms</a>
-                      </li>
-                      <li><a href="horizontal-forms.html" class="menu-item">Horizontal Forms</a>
-                      </li>
-                      <li><a href="hidden-labels.html" class="menu-item">Hidden Labels</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a href="validation-forms.html" class="menu-item">Validation</a>
-                  </li>
-                  <li><a href="wizard-forms.html" class="menu-item">Wizard</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-grid"></i><span data-i18n="" class="menu-title">Tables</span></a>
-                <ul class="menu-content">
-                  <li><a href="regular-table.html" class="menu-item">Regular</a>
-                  </li>
-                  <li><a href="extended-table.html" class="menu-item">Extended</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-notebook"></i><span data-i18n="" class="menu-title">Data Tables</span></a>
-                <ul class="menu-content">
-                  <li><a href="dt-basic-initialization.html" class="menu-item">Basic Initialisation</a>
-                  </li>
-                  <li><a href="dt-advanced-initialization.html" class="menu-item">Advanced initialisation</a>
-                  </li>
-                  <li><a href="dt-styling.html" class="menu-item">Styling</a>
-                  </li>
-                  <li><a href="dt-data-sources.html" class="menu-item">Data Sources</a>
-                  </li>
-                  <li><a href="dt-api.html" class="menu-item">API</a>
-                  </li>
-                </ul>
-              </li>
-              <li class=" nav-item"><a href="google-map.html"><i class="icon-map"></i><span data-i18n="" class="menu-title">Google Map</span></a>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-pie-chart"></i><span data-i18n="" class="menu-title">Charts</span><span class="tag badge badge-pill badge-success white mt-1">2</span></a>
-                <ul class="menu-content">
-                  <li><a href="chartist.html" class="menu-item">Chartist</a>
-                  </li>
-                  <li><a href="chartjs.html" class="menu-item">ChartJs</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-sub nav-item"><a href="#"><i class="icon-docs"></i><span data-i18n="" class="menu-title">Pages</span></a>
-                <ul class="menu-content">
-                  <li><a href="forgot-password-page.html" class="menu-item">Forgot Password</a>
-                  </li>
-                  <li><a href="horizontal-timeline-page.html" class="menu-item">Horizontal Timeline</a>
-                  </li>
-                  <li><a href="vertical-timeline-page.html" class="menu-item">Vertical Timeline</a>
-                  </li>
-                  <li><a href="login-page.html" class="menu-item">Login</a>
-                  </li>
-                  <li><a href="register-page.html" class="menu-item">Register</a>
-                  </li>
-                  <li><a href="user-profile-page.html" class="menu-item">User Profile</a>
-                  </li>
-                  <li><a href="lock-screen-page.html" class="menu-item">Lock Screen</a>
-                  </li>
-                  <li><a href="invoice-page.html" class="menu-item">Invoice</a>
-                  </li>
-                  <li><a href="error-page.html" class="menu-item">Error</a>
-                  </li>
-                  <li><a href="coming-soon-page.html" class="menu-item">Coming Soon</a>
-                  </li>
-                  <li><a href="maintenance-page.html" class="menu-item">Maintenance</a>
-                  </li>
-                  <li><a href="gallery-page.html" class="menu-item">Gallery</a>
-                  </li>
-                  <li><a href="search.html" class="menu-item">Search</a>
-                  </li>
-                  <li><a href="faq.html" class="menu-item">FAQ</a>
-                  </li>
-                  <li><a href="knowledge-base.html" class="menu-item">Knowledge Base</a>
-                  </li>
-                </ul>
-              </li>
-              <li class=" nav-item"><a href="http://pixinvent.com/demo/convex-bootstrap-admin-dashboard-template/documentation"><i class="icon-book-open"></i><span data-i18n="" class="menu-title">Documentation</span></a>
-              </li>
-              <li class=" nav-item"><a href="https://pixinvent.ticksy.com/"><i class="icon-support"></i><span data-i18n="" class="menu-title">Support</span></a>
-              </li>
-            </ul>
-          </div>
-        <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 535px; right: 3px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 387px;"></div></div></div>
+         <!--Accordion wrapper-->
+		<!--Accordion wrapper-->
+<div class="accordion md-accordion" id="accordionEx1" role="tablist" aria-multiselectable="true">
+
+  <!-- Accordion card -->
+  <div class="card">
+
+    <!-- Card header -->
+    <div class="card-header" role="tab" id="headingTwo1">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
+        aria-expanded="false" aria-controls="collapseTwo1">
+        <h5 class="mb-0">
+          My Dashboard <i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>
+    </div>
+
+    <!-- Card body -->
+    <div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+      <ul>
+      <li> job alerat</li>
+       <li>profile view</li>
+       </ul>
+       </div>
+    </div>
+
+  </div>
+  <!-- Accordion card -->
+
+  <!-- Accordion card -->
+  <div class="card">
+
+    <!-- Card header -->
+    <div class="card-header" role="tab" id="headingTwo2">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
+        aria-expanded="false" aria-controls="collapseTwo21">
+        <h5 class="mb-0">
+          My Profile <i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>
+    </div>
+
+    <!-- Card body -->
+    <div id="collapseTwo21" class="collapse" role="tabpanel" aria-labelledby="headingTwo21"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+        <ul>
+      <li> job alerat</li>
+       <li>profile view</li>
+       </ul>
+      </div>
+    </div>
+
+  </div>
+  <!-- Accordion card -->
+
+  <!-- Accordion card -->
+  <div class="card">
+
+    <!-- Card header -->
+    <div class="card-header" role="tab" id="headingThree31">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
+        aria-expanded="false" aria-controls="collapseThree31">
+        <h5 class="mb-0">
+          Ocean Services<i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>   
+    </div>
+
+    <!-- Card body -->
+    <div id="collapseThree31" class="collapse" role="tabpanel" aria-labelledby="headingThree31"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+       <ul>
+      <li> job alerat</li>
+       <li>profile view</li>
+       </ul>
+      </div>
+    </div>
+
+  </div>
+  <!-- Accordion card -->
+   
+</div>
 <!-- Accordion wrapper -->
 <!-- Accordion wrapper -->
   	 </div>   
