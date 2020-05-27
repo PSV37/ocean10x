7e046c6dc552b6831dac22e245cb8165769f2b8a
supriyaL <supriya@itdivine.in>
2020-05-27 15:15:08 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -59,21 +59,10 @@
     <div id="collapseTwo21" class="collapse" role="tabpanel" aria-labelledby="headingTwo21"
       data-parent="#accordionEx1">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-        wolf
-        moon
-        officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-        Brunch
-        3
-        wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch
-        et.
-        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-        Ad
-        vegan
-        excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-        nesciunt
-        you probably haven't heard of them accusamus labore sustainable VHS.
+        <ul>
+      <li> job alerat</li>
+       <li>profile view</li>
+       </ul>
       </div>
     </div>
 
@@ -97,27 +86,16 @@
     <div id="collapseThree31" class="collapse" role="tabpanel" aria-labelledby="headingThree31"
       data-parent="#accordionEx1">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
-        wolf
-        moon
-        officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-        Brunch
-        3
-        wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-        shoreditch
-        et.
-        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-        Ad
-        vegan
-        excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth
-        nesciunt
-        you probably haven't heard of them accusamus labore sustainable VHS.
+       <ul>
+      <li> job alerat</li>
+       <li>profile view</li>
+       </ul>
       </div>
     </div>
 
   </div>
   <!-- Accordion card -->
-
+   
 </div>
 <!-- Accordion wrapper -->
 <!-- Accordion wrapper -->
