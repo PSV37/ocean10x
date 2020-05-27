11da13220d42947b02a8ac502c837ca59a85c819
supriyaL <supriya@itdivine.in>
2020-05-27 15:04:56 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -12,23 +12,24 @@
     <div class="row">
      <div class="content col-md-3">
          <!--Accordion wrapper-->
+		<!--Accordion wrapper-->
 <div class="accordion md-accordion" id="accordionEx1" role="tablist" aria-multiselectable="true">
 
   <!-- Accordion card -->
   <div class="card">
 
     <!-- Card header -->
-    <div class="card-header" role="tab" id="headingThree31">
-      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
-        aria-expanded="false" aria-controls="collapseThree31">
+    <div class="card-header" role="tab" id="headingTwo1">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
+        aria-expanded="false" aria-controls="collapseTwo1">
         <h5 class="mb-0">
-          Collapsible Group Item #3 <i class="fas fa-angle-down rotate-icon"></i>
+          Collapsible Group Item #1 <i class="fas fa-angle-down rotate-icon"></i>
         </h5>
       </a>
     </div>
 
     <!-- Card body -->
-    <div id="collapseThree31" class="collapse" role="tabpanel" aria-labelledby="headingThree31"
+    <div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1"
       data-parent="#accordionEx1">
       <div class="card-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
@@ -56,17 +57,17 @@
   <div class="card">
 
     <!-- Card header -->
-    <div class="card-header" role="tab" id="headingThree32">
-      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree32"
-        aria-expanded="false" aria-controls="collapseThree31">
+    <div class="card-header" role="tab" id="headingTwo2">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
+        aria-expanded="false" aria-controls="collapseTwo21">
         <h5 class="mb-0">
-          Collapsible Group Item #3 <i class="fas fa-angle-down rotate-icon"></i>
-        </h5>   
+          Collapsible Group Item #2 <i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
       </a>
-    </div>   
+    </div>
 
     <!-- Card body -->
-    <div id="collapseThree31" class="collapse" role="tabpanel" aria-labelledby="headingThree32"
+    <div id="collapseTwo21" class="collapse" role="tabpanel" aria-labelledby="headingTwo21"
       data-parent="#accordionEx1">
       <div class="card-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
@@ -130,7 +131,8 @@
 
 </div>
 <!-- Accordion wrapper -->
-  </div>   
+<!-- Accordion wrapper -->
+  	 </div>   
      
      
      
