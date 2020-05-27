6d5eb45d19c5c3fa989da529367a43857e6a737b
supriyaL <supriya@itdivine.in>
2020-05-27 18:04:05 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -12,10 +12,12 @@
 }
 .panel-group .panel{margin-bottom:15px;}
 
-.list-group-item{cursor:pointer;}
+.list-group-item{cursor:pointer;}   
 .panel-title>a{font-size:12px;}
 .panel-title>a:hover{text-decoration:none;}
-.summary{background-color:#fff;}
+.summary{background-color:#fff;
+		padding: 20px;
+   	    margin-right: 65px;}
 .userccount{background: #f5f5f5;}
 </style>
    
