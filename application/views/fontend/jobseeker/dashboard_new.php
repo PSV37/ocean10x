9c70c2a18ed7886369915405a30e0da08ebc29a6
supriyaL <supriya@itdivine.in>
2020-05-28 13:56:02 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -21,6 +21,7 @@ li.list-group-item:hover{color:#8ac640;}
    	    margin-right: 65px;
 		box-shadow: 2px 2px 3px 0px #ece9e9;
 }
+.summary h6{font-size:25px;}
    
 .lb{background-color:#f5f5f585;}
 .panel-default {   
