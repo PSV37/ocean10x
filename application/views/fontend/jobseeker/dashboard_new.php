2e49f81ca80bb81122f969227133fda5fe8c493d
supriyaL <supriya@itdivine.in>
2020-05-29 14:24:26 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -253,13 +253,36 @@ li.list-group-item {
                 
             <div class="row">
               <div class="col-md-12">
-                <div class="containe1r">
+                <div class="container">
 					<div class="row">
 		                <div class="col-md-12">
-		            	 	<div class="col-md-3 summary">
-		            	  	<i class="fa fa-briefcase" aria-hidden="true"></i><h6>45</h6>
-		            	  		<p>Saved Jobs</p>   
-		            	  	</div>
+		            	 	 <div class="col-lg-2 col-sm-6">
+                        		<div class="circle-tile">
+                            		<a href="#">
+                                        <div class="circle-tile-heading dark-blue">
+                                            <i class="fa fa-users fa-fw fa-3x"></i>
+                                        </div>
+                            		</a>
+                            			<div class="circle-tile-content dark-blue">
+                               			 <div class="circle-tile-description text-faded">
+                                   		 Users
+                                        </div>
+                                        <div class="circle-tile-number text-faded">
+                                            265
+                                            <span id="sparklineA"></span>
+                                        </div>
+                                        <a href="#" class="circle-tile-footer">More Info <i class="fa                 fa-chevron-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
 		            	  	<div class="col-md-3 summary">
 		            	  		<i class="fa fa-bell"></i><h6>10</h6>
 		            	  		<p>Job Alerts</p>
