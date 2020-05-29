468000e9d0c5f75775a8ad788419c445e55aaf3b
supriyaL <supriya@itdivine.in>
2020-05-29 09:38:08 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -104,7 +104,7 @@ li.list-group-item {
           <li class="nav-header">MY DASHBOARD</li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-             <a href="#set-5" class="hi-icon hi-icon-locked">Security</a>
+              <i class="nav-icon fas fa-file"></i>
               <p>Documentation</p>
             </a>    
           </li>
