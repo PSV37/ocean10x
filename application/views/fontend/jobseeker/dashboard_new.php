e77e75504ea139170a17d54161e197cf31f1326e
supriyaL <supriya@itdivine.in>
2020-05-29 09:28:36 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -100,15 +100,12 @@ li.list-group-item {
            
            
          
-          
-          <li class="nav-header">MY DASHBORD</li>
-         
+               
+          <li class="nav-header">MY DASHBOARD</li>
           <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
-              <i class="nav-icon far fa-image"></i>
-              <p>
-                Gallery
-              </p>
+            <a href="https://adminlte.io/docs/3.0" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p>Documentation</p>
             </a>
           </li>
           
