0a0977e5a4ec3d71147bd3f0b4b544c9db34395a
supriyaL <supriya@itdivine.in>
2020-05-30 10:55:03 +0530

    change

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -207,7 +207,8 @@ li.list-group-item {
 	height: 39px;
     width: 100%;
     background-image: linear-gradient(to right, #ace41e , #2196F3);
-	}  
+	}     
+.dark-blue img{margin:20px;}	 
 </style>     
       
    
