52ffdf8fd64b70a17ffaa03835589c33bc176ea1
unknown <santu@edunetsol.com>
2020-06-01 13:38:39 +0530

    ui

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -1111,7 +1111,7 @@ public function search(){
         $select_edu = "job_posting.job_title,job_posting.job_slugs,job_posting.job_position,job_posting.company_profile_id,js_saved_jobs.created_on,js_saved_jobs.job_post_id,js_saved_jobs.job_seeker_id,js_saved_jobs.id,city.city_name";
         $data['saved_job_data'] = $this->Master_model->getMaster("js_saved_jobs", $where_edu, $join_save, $order = false, $field = false, $select_edu,$limit=false,$start=false, $search=false);
 
-        // echo $this->db->last_query(); die;
+        echo $this->db->last_query(); die;
         $this->load->view('fontend/jobseeker/saved_jobs',$data);
     }
 
