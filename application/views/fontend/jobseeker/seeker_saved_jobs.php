5be5254526d6c1204270eefca22c5c2c11630480
unknown <santu@edunetsol.com>
2020-06-04 11:06:09 +0530

    new

--- application/views/fontend/jobseeker/seeker_saved_jobs.php
+++ application/views/fontend/jobseeker/seeker_saved_jobs.php
@@ -1,9 +1,9 @@
 <!--header--->
-<!-- <div class="container-fluid main-box_Scroll">
+<div class="container-fluid main-box_Scroll">
 
 <div clas="row">
 	<div class="col-md-12">
-    	<div class="col-md-3">
+    	<!-- <div class="col-md-3">
         <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
     <div class="inner-left-pannel"> -->
         
@@ -117,11 +117,7 @@
         
  <div class="col-md-9">
         <div class="row">
-   
-
-      
-      
-
+  
                 <div class="col-md-12">
                 
                     <div class="gradient_strip"></div>
--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -77,10 +77,10 @@
         </style>
 </head>
 <body>
-	<div class="container-fluid main-box_Scroll">
+	<!-- <div class="container-fluid main-box_Scroll"> -->
 
-<div clas="row">
-	<div class="col-md-12">
+<!-- <div clas="row"> -->
+	<!-- <div class="col-md-12"> -->
     	<div class="col-md-3">
         <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
 	    <div class="inner-left-pannel">
@@ -90,8 +90,8 @@
 		</aside>
 	</div>
 	</div>
-</div>
-</div>
+<!-- </div>
+</div> -->
 
 </body>
 
