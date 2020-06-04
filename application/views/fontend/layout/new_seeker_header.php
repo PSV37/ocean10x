f260ea019f9dc7def228f9ad8beca1cce582990b
unknown <santu@edunetsol.com>
2020-06-04 10:46:23 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -84,12 +84,12 @@
     	<div class="col-md-3">
         <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
 	    <div class="inner-left-pannel">
-	    <!-- </div>
+	    </div>
 		</aside>
 	</div>
 	</div>
 </div>
-</div> -->
+</div>
 
 </body>
 
