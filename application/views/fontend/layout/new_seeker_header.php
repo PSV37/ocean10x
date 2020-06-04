2f559aad25c27cd27c4793203894b2683b2d0309
unknown <santu@edunetsol.com>
2020-06-04 10:51:54 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/responsive.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/main.css">
     
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/new_ui.css">
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>asset/jc/css/jquery.Jcrop.css">
     <!-- Data Table  CSS -->
@@ -84,12 +84,12 @@
     	<div class="col-md-3">
         <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
 	    <div class="inner-left-pannel">
-	    </div>
+	    <!-- </div>
 		</aside>
 	</div>
 	</div>
 </div>
-</div>
+</div> -->
 
 </body>
 
