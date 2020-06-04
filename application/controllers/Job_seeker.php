5b9fe49e19ce6ab073d4c6a4de991804837a7a35
unknown <santu@edunetsol.com>
2020-06-04 16:16:45 +0530

    new

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -1121,16 +1121,29 @@ public function search(){
             elseif ($type=='all') {
                  $where_edu="js_saved_jobs.job_seeker_id='$jobseeker_id'";
             }
+                 $join_save = array(
+                'job_posting' => 'job_posting.job_post_id=js_saved_jobs.job_post_id | LFET OUTER',
+                // 'city' => 'city.id=job_posting.city_id | LFET OUTER',
+                );
+               
+                $select_edu = "job_posting.job_title,job_posting.job_slugs,job_posting.job_position,job_posting.company_profile_id,js_saved_jobs.created_on,js_saved_jobs.job_post_id,js_saved_jobs.job_seeker_id,js_saved_jobs.id,job_posting.city_id";
+                $data['saved_job_data'] = $this->Master_model->getMaster("js_saved_jobs", $where_edu, $join_save, $order = false, $field = false, $select_edu,$limit=false,$start=false, $search=false);
+        }
+        else
+        {
+                 $where_edu="js_saved_jobs.job_seeker_id='$jobseeker_id'";
+
+             $join_save = array(
+            'job_posting' => 'job_posting.job_post_id=js_saved_jobs.job_post_id | LFET OUTER',
+            // 'city' => 'city.id=job_posting.city_id | LFET OUTER',
+            );
+           
+            $select_edu = "job_posting.job_title,job_posting.job_slugs,job_posting.job_position,job_posting.company_profile_id,js_saved_jobs.created_on,js_saved_jobs.job_post_id,js_saved_jobs.job_seeker_id,js_saved_jobs.id,job_posting.city_id";
+            $data['saved_job_data'] = $this->Master_model->getMaster("js_saved_jobs", $where_edu, $join_save, $order = false, $field = false, $select_edu,$limit=false,$start=false, $search=false);
         }
 
        
-        $join_save = array(
-            'job_posting' => 'job_posting.job_post_id=js_saved_jobs.job_post_id | LFET OUTER',
-            // 'city' => 'city.id=job_posting.city_id | LFET OUTER',
-        );
        
-        $select_edu = "job_posting.job_title,job_posting.job_slugs,job_posting.job_position,job_posting.company_profile_id,js_saved_jobs.created_on,js_saved_jobs.job_post_id,js_saved_jobs.job_seeker_id,js_saved_jobs.id,job_posting.city_id";
-        $data['saved_job_data'] = $this->Master_model->getMaster("js_saved_jobs", $where_edu, $join_save, $order = false, $field = false, $select_edu,$limit=false,$start=false, $search=false);
 
         // echo $this->db->last_query(); die;
         // $this->load->view('fontend/jobseeker/saved_jobs',$data);
