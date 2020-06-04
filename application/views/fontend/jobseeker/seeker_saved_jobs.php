1ebfc0940fabdc24163ce177060c44c8e8acd8bc
unknown <santu@edunetsol.com>
2020-06-04 10:57:50 +0530

    new

--- application/views/fontend/jobseeker/seeker_saved_jobs.php
+++ application/views/fontend/jobseeker/seeker_saved_jobs.php
@@ -117,8 +117,9 @@
         
  <div class="col-md-9">
         <div class="row">
-   
+    <div class="inner-left-pannel"> 
       <?php $this->load->view('fontend/layout/seeker_left_menu.php'); ?>
+    </div>
 
       
       
