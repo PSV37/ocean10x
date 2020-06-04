f8c09ced855669e0dc7d1015833e6a785e610203
unknown <santu@edunetsol.com>
2020-06-04 10:49:35 +0530

    new

--- application/views/fontend/jobseeker/seeker_saved_jobs.php
+++ application/views/fontend/jobseeker/seeker_saved_jobs.php
@@ -112,13 +112,26 @@
         
         </div> -->
         <?php 
-    $this->load->view('fontend/layout/new_seeker_header.php');
+    // $this->load->view('fontend/layout/new_seeker_header.php');
 ?> 
         
  <div class="col-md-9">
         <div class="row">
-          
+          <div class="container-fluid main-box_Scroll">
+
+<div clas="row">
+  <div class="col-md-12">
+      <div class="col-md-3">
+        <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
+      <div class="inner-left-pannel">
       <?php $this->load->view('fontend/layout/seeker_left_menu.php'); ?>
+        
+      </div>
+    </aside>
+  </div>
+  </div>
+</div>
+</div>
       
 
                 <div class="col-md-12">
