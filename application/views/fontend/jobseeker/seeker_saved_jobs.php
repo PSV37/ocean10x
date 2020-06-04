1ffb398b62786ead8e8a700b6f6dd923b59085e0
supriyaL <supriya@itdivine.in>
2020-06-04 10:02:01 +0530

    change

---
+++ application/views/fontend/jobseeker/seeker_saved_jobs.php
@@ -0,0 +1,246 @@
+<!--header--->
+<div class="container-fluid main-box_Scroll">
+
+<div clas="row">
+	<div class="col-md-12">
+    	<div class="col-md-3">
+        <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
+    <div class="inner-left-pannel">
+        
+<!---header-->        
+        
+        
+        <!-- menus START -->
+        <div class="my-moving-parts">
+            <div class="my-param-content"></div>
+            <div class="my-normal-content">
+               <div class="inner-tabs-navigation" data-active="menu">
+                </div>
+                <div class="inner-tabs">
+                    <div class="account-tab">
+                        <div class="language-selection" title="Change language">
+                                        <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                							<span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                          					  <i class="flag flag-us"></i> 
+                       							 </a>
+                         					 </span>
+                						</div>
+                  		</div>
+                    </div>
+                    
+                    <div class="menu-tab">
+                        
+                        <nav class="menu-principal">
+                        <div class="menu_logo" style="height: 115px;">
+                        	<img src="https://www.consultnhire.com/files/1506847224_00024vs-logo.jpg" />
+                        </div>
+                            <ul class="menu-principal-list" style="">
+                   				 <li class="active">
+           							 <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                    <span class="icon-container">
+                  					 	 <i class="fas fa-tachometer-alt"></i>
+               					    </span>
+                                	<span class="text item">Dashboard</span>
+                                    </a>
+        						</li>
+                    			<li>
+           							 <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                    <span class="icon-container">
+                   						<i class="fas fa-user-alt"></i>
+               					   </span>
+                                <span class="text item">My profil</span>
+                                      </a>
+       						   </li>
+                               
+                   			  <li>
+           					 <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                               <span class="icon-container">
+                  				 <i class="fas fa-phone-volume"></i>
+               				 </span>
+                                <span class="text item">Contacts</span>
+                             </a>
+      						  </li>
+                   				 <li>
+           						 <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                    <span class="icon-container">
+                  					  <i class="fas fa-filter"></i>
+              						  </span>
+                                <span class="text item">Recruitments </span>
+                                  </a>
+								</li>
+                   				 <li>
+            						<a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                    <span class="icon-container">
+                  					  <i class="fas fa-map-signs"></i>
+              						  </span>
+                                <span class="text item">Mobility</span>
+                                      </a>
+       							 </li>
+                    				<li>
+            				<a data-dl-view="true" data-dl-title="Predictive models" href="/jobprofile">
+                            <span class="icon-container">
+                  			   <i class="fas fa-th-large"></i>
+              				  </span>
+                                <span class="text item">Predictive models</span>
+                             </a>
+       							 </li>
+                  				  <li>
+           						 <a data-dl-view="true" data-dl-title="Talent mapper" href="/talentmapper/generate">
+                                    <span class="icon-container">
+                   				 <i class="fas fa-th-large"></i>
+              					  </span>
+                                <span class="text item"> Talent mapper</span>
+                                 </a>
+       							 </li>
+                           		 <li>
+          					  <a data-dl-view="true" data-dl-title="Settings" href="/params" class="hidden-xs hidden-sm                                       hidden-md hidden-lg">
+                                <span class="icon-container">
+                   			    <i class="fas fa-cog"></i>
+                				</span>
+                                <span class="text item">Settings</span>
+                                            </a>
+       							 </li>
+   							 </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- menus END -->
+    </div>
+        </aside>
+        
+        </div>
+        
+ <div class="col-md-9">
+        <div class="row">
+                <div class="col-md-12">
+                
+                    <div class="gradient_strip"></div>
+                    <div class="text-grad">
+                    <h1 class="ml10">
+                      <span class="text-wrapper">
+                        <span class="letters">My Profile</span>
+                         <p>Profile</p>
+                      </span>
+                    </h1>
+                    </div>
+                    
+                   
+        		
+            	<h3 class="profile_heading">SAVED JOB</h3>
+             
+                  <div class="seperate-btn">   
+                    <button class="all_b active_save_btn">All</button>  
+                    <button class="sort_b">Sort</button>  
+                  </div>
+                  
+             
+             
+            <div class="col-md-12">
+            <div class="col-md-9">
+            	<div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                
+            </div>
+            <div class="col-md-3"></div>
+            	</div>
+            </div>
+              
+              
+                 
+			</div>
+            
+            
+            
+            
+            	
+          </div>
+          
+         
+          
+  </div>               
+              
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script> 
+<script>
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
+ <script>
+ // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: ""})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  })
+ </script>
+ 
