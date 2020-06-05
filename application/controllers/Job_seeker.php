906e0612269294ca564ac9cc9304e3475d927902
unknown <santu@edunetsol.com>
2020-06-05 15:04:43 +0530

    new

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -38,8 +38,10 @@ class Job_seeker extends MY_Seeker_Controller
 	
 	public function seeker_info()
     {
-			
-        $this->load->view('fontend/jobseeker/jobseeker_profile');
+			$city = $this->Master_model->getMaster('city',$where=false);
+            $country = $this->Master_model->getMaster('country',$where=false);
+            $state = $this->Master_model->getMaster('state',$where=false);
+        $this->load->view('fontend/jobseeker/jobseeker_profile'compact('city', 'country', 'state'),true);
     }
 	
 
