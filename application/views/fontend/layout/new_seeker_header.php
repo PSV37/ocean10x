aa21c6082348c7cceddd8682a62a58fbb26e9f45
unknown <santu@edunetsol.com>
2020-06-05 10:29:00 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/font-awesome.css"> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/responsive.css">
-    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/main.css"> -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/main.css">
      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/new_ui.css">
 
 
