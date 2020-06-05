2db1d84eda74e13e560035a2097db5b2d8c7b2f0
supriyaL <supriya@itdivine.in>
2020-06-05 16:57:11 +0530

    change

---
+++ application/views/fontend/layout/jobseeker_footer.php
@@ -0,0 +1,59 @@
+
+                		<!-- Footer -->
+	<div class="col-md-12 end-footer">
+<div class="row">
+<div class="col-md-4 p-footer">
+<h5>About consultnHire</h5>
+					<ul class="list-unstyled quick-links">
+						<p>Working with us has many benefits for you, your business and your stakeholders. We are known for our creative, yet professional approach</p>
+					</ul>
+
+</div>
+<div class="col-md-4 p-footer">
+<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Home</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Search Jobs</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Views All Jobs</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Contact Us</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;report a bug/Abuse</a></li>
+					</ul>
+
+</div>
+<div class="col-md-4 p-footer">
+<h5>About Company</h5>
+					<ul class="list-unstyled quick-links ">
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;About Company</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;My Account</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Terms Of Use</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Help</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Privacy Commitment</a></li>
+					</ul>
+</div>
+
+<div class="row">
+
+<div class="footer-icons">
+
+<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook-f"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-linkedin-in"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-phone-square-alt"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-envelope-open"></i></a></li>
+						
+					</ul>
+</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					
+					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+				</div>
+				<hr>
+			</div>
+
+
+
+</div>
+
+</div>
+	<!-- ./Footer -->
