2c8f25608f57a9673964e2c15dc8509dd3ba0adb
unknown <santu@edunetsol.com>
2020-06-05 19:00:17 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -49,7 +49,7 @@
     	<div class="col-md-3">
         <aside id="left-panel" style="overflow:scroll; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px #00000033;height:100vh;">
 	    <div class="inner-left-pannel">
-      <?php $this->load->view('fontend/layout/seeker_left_menu.php'); ?>
+         <?php $this->load->view('fontend/layout/seeker_left_menu.php'); ?>
       
 
 	    </div>
