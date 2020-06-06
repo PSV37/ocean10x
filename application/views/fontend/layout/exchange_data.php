700f9bccba7018b3114558820ee0382666580f52
supriyaL <supriya@itdivine.in>
2020-06-06 18:50:45 +0530

    change

--- application/views/fontend/layout/exchange_data.php
+++ application/views/fontend/layout/exchange_data.php
@@ -1,4 +1,3 @@
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <!---header---->
 
 <div class="container-fluid gradient_strip" >
@@ -16,19 +15,35 @@
      </div>          
 </div>
 
+
+	 <div class="switch switch-yellow">
+      <input type="radio" class="switch-input" name="view3" value="week3" id="week3" checked>
+      <label for="week3" class="switch-label switch-label-off">Job Search</label>
+      <input type="radio" class="switch-input" name="view3" value="month3" id="month3">
+      <label for="month3" class="switch-label switch-label-on">People Search</label>
+      <span class="switch-selection"></span>
+    </div>
+
+
+
+
+
+
 	<div class="social-media">
     <!---mail-box-->
     <div class="notification">
-    	<i class="fas fa-envelope"></i>
+    	<i class="fas fa-comment-alt"></i><br>
+        Messaging
     </div>    
     <!---mail box-end-->
     
     <!---notification-->
-     <div class="notification">
-    	<i class="fas fa-bell"></i>
+     <div class="bell">
+    	<i class="fas fa-bell"></i><br>
+        Notifications
    	 </div>
      <!--notification-end-->
-     <!---profile--->
+     <!---profile--->   
      <div class="profile">
      <i class="fas fa-user-circle"></i>
      <b>supriya</b>
@@ -42,353 +57,3 @@
 
 <!---header end--->
 
-   
-<div class="container-fluid">
-	<div class="container">
-        <div class="col-md-12">
-        	<div class="col-md-3">
-            <aside id="left-panel" style="margin-top:75px;
-                 margin-left: 14px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
-            z-index: 999;vertical-align:baseline;">
-            <div class="inner-left-pannel">
-                
-                
-                <!-- WHAT MOVES START -->
-                <div class="my-moving-parts">
-                    <div class="my-param-content"></div>
-                    <div class="my-normal-content">
-                       <div class="inner-tabs-navigation" data-active="menu">
-                        </div>
-                        <div class="inner-tabs">
-                            <div class="account-tab">
-                                <div class="language-selection" title="Change language">
-                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
-                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
-                                                      <i class="flag flag-us"></i> 
-                                                         </a>
-                                                     </span>
-                                                </div>
-                                </div>
-                            </div>
-                            
-                            <div class="menu-tab">
-                               
-                                
-                                
-                                <nav class="menu-principal">
-                                
-                                    <ul class="menu-principal-list" style="">
-                                         <li class="active">
-                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
-                                            <span class="icon-container">
-                                                 <i class="fas fa-tachometer-alt"></i>
-                                            </span>
-                                            <span class="text item">Dashboard</span>
-                                            </a>
-                                        </li>
-                                        <li>
-                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
-                                            <span class="icon-container">
-                                                <i class="fas fa-user-alt"></i>
-                                           </span>
-                                        <span class="text item">My Ocean profil</span>
-                                              </a>
-                                       </li>
-                                       
-                                      <li>
-                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
-                                       <span class="icon-container">
-                                         <i class="fas fa-phone-volume"></i>
-                                     </span>
-                                        <span class="text item">OcearnHunt Activities</span>
-                                     </a>
-                                      </li>
-                                         <li>
-                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
-                                            <span class="icon-container">
-                                              <i class="fas fa-filter"></i>
-                                              </span>
-                                        <span class="text item">Skill Upgrade</span>
-                                          </a>
-                                        </li>
-                                         <li>
-                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
-                                            <span class="icon-container">
-                                              <i class="fas fa-map-signs"></i>
-                                              </span>
-                                        <span class="text item">Become an Ocean champ</span>
-                                              </a>
-                                         </li>
-                                            
-                                          
-                                         
-                                     </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- WHAT MOVES END -->
-            </div>
-                </aside>
-            </div>
-            <div class="col-md-6 mid-section">
-            <div class="row">    
-            <div class="col-md-12 bd-1">
-                   
-            <div class="dash-box-w">
-             <h3 class="heading-dash">My Dashboard</h3>
-             
-             <div class="col-md-4 card-lb">
-                        <div class="card text-white bg-primary o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                           <i class="fas fa-fw fa-download"></i>
-                            </div>
-                            <span>Saved Job</span>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:2px;">10</span>
-                            </a>
-                            </div>
-                        </div>
-              <div class="col-md-4 card-lb">
-                        	<div class="card text-white bg-danger o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                          <i class="fas fa-volume-up fa-fw"></i>
-                            </div>
-                            <div >Job Alerts</div>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:22px;">20</span>
-                            </a>
-                            </div>
-                        </div>
-                 <div class="col-md-4 card-lb">
-                        	<div class="card text-white bg-warning o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                           <i class="fas fa-users fa-fw"></i>
-                            </div>
-                            <div>Profile Views</div>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:22px;">20</span>
-                            </a>
-                            </div>
-                        </div> 
-                     
-                     <div class="col-md-4 card-lb">
-                        <div class="card text-white bg-bluish o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                           <i class="fas fa-fw fa-download"></i>
-                            </div>
-                            <span>Article Views</span>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:22px;">10</span>
-                            </a>
-                            </div>
-                        </div>
-                        <div class="col-md-4 card-lb">
-                        	<div class="card text-white bg-link o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                          <i class="fas fa-volume-up fa-fw"></i>
-                            </div>
-                            <div>courses Completed</div>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:22px;">20</span>
-                            </a>
-                            </div>
-                        </div>
-                          
-                        <div class="col-md-4 card-lb">
-                        	<div class="card text-white bg-green o-hidden h-100">
-                            <div class="card-body">
-                            <div class="card-body-icon">
-                           <i class="fas fa-users fa-fw"></i>
-                            </div>
-                            <div>News Feed</div>
-                            </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
-                            <span class="float-left" style="font-size:22px;">20</span>
-                            </a>
-                            </div>
-                        </div>
-                        
-                              
-                  </div>                
-             </div>
-             
-             <div class="col-md-12 bd-2">
-             <div class="job-voucher alert alert-dismissible">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
-                   <div class="job_title">
-                   The person/job specification can be presented as a stand-alone 
-                   </div> 
-                    <div class="organization">
-                    	IT Company
-                    </div>
-                    <div class="location">
-                    	Pune ,Kalyani nagar
-                    </div>
-                    <div class="apply_job_btn">Apply job</div>
-                    <button class="job_dis_btn">Details</button>
-                </div>
-                <div class="job-voucher alert alert-dismissible">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
-                   <div class="job_title">
-                   The person/job specification can be presented as a stand-alone 
-                   </div> 
-                    <div class="organization">
-                    	IT Company
-                    </div>
-                    <div class="location">
-                    	Pune ,Kalyani nagar
-                    </div>
-                    <div class="apply_job_btn">Apply job</div>
-                    <button class="job_dis_btn">Details</button>
-                </div>
-                <div class="job-voucher alert alert-dismissible">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
-                   <div class="job_title">
-                   The person/job specification can be presented as a stand-alone 
-                   </div> 
-                    <div class="organization">
-                    	IT Company
-                    </div>
-                    <div class="location">
-                    	Pune ,Kalyani nagar
-                    </div>
-                    <div class="apply_job_btn">Apply job</div>
-                    <button class="job_dis_btn">Details</button>
-                </div>
-                <div class="job-voucher alert alert-dismissible">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
-                   <div class="job_title">
-                   The person/job specification can be presented as a stand-alone 
-                   </div> 
-                    <div class="organization">
-                    	IT Company
-                    </div>
-                    <div class="location">
-                    	Pune ,Kalyani nagar
-                    </div>
-                    <div class="apply_job_btn">Apply job</div>
-                    <button class="job_dis_btn">Details</button>
-                </div>
-                <div class="job-voucher alert alert-dismissible">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
-                   <div class="job_title">
-                   The person/job specification can be presented as a stand-alone 
-                   </div> 
-                    <div class="organization">
-                    	IT Company
-                    </div>
-                    <div class="location">
-                    	Pune ,Kalyani nagar
-                    </div>
-                    <div class="apply_job_btn">Apply job</div>
-                    <button class="job_dis_btn">Details</button>
-                </div>
-             </div>
-             
-             
-            </div>
-           </div>
-            <div class="col-md-3 ">
-            <div class="progress-bar-left">
-            <h3 class="heading-dash_profile">PROFILE LEVEL</h3>
-                <div class="progress yellow">
-                <span class="progress-left">
-                    <span class="progress-bar"></span>
-                </span>
-                <span class="progress-right">
-                    <span class="progress-bar"></span>
-                </span>
-                <div class="progress-value">75%</div>
-            </div>
-      
-        	
-       			 <div class="paragraph_p_level">
-       	<p>Create your own, and see what different functions produce. Get to understand what is really happening. What type of Graph do you want</p> 
-        </div>
-            </div>
-        </div>
-		
-	</div>
-    
-    
-    </div>
-    <!-- Footer -->
- <div class="container-fluid end-footer">   
-	
-    <div class="container">
-    <div class="col-md-12 ">
-<div class="row">
-<div class="col-md-4 p-footer">
-<h5>About consultnHire</h5>
-					<ul class="list-unstyled quick-links">
-						<p>Working with us has many benefits for you, your business and your stakeholders. We are known for our creative, yet professional approach</p>
-					</ul>
-
-</div>
-<div class="col-md-4 p-footer">
-<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Search Jobs</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Views All Jobs</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Contact Us</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;report a bug/Abuse</a></li>
-					</ul>
-
-</div>
-<div class="col-md-4 p-footer">
-<h5>About Company</h5>
-					<ul class="list-unstyled quick-links ">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;About Company</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;My Account</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Terms Of Use</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Help</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Privacy Commitment</a></li>
-					</ul>
-</div>
-
-<div class="row">
-
-<div class="footer-icons">
-
-<ul class="list-unstyled list-inline social text-center">
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook-f"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-linkedin-in"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-phone-square-alt"></i></a></li>
-						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-envelope-open"></i></a></li>
-						
-					</ul>
-</div>
-
-				
-				
-			</div>
-
-
-</div>
-</div>
-</div>
-</div>
-	<!-- ./Footer -->
-</div>
-   
-   
-   
-</body>
