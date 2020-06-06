eeb9c28b7159d0204a453c7ae0924e1035a371a1
unknown <santu@edunetsol.com>
2020-06-06 15:44:49 +0530

    new

--- application/views/fontend/layout/jobseeker_footer.php
+++ application/views/fontend/layout/jobseeker_footer.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/font-awesome.css"> 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/responsive.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/main.css">
+     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/seeker_dashboard.css">
  
  	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/jobseeker_footer.css">
     
