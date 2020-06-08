6b7341b6dc3beedf0cb377bdb78eefbb1594ce50
unknown <santu@edunetsol.com>
2020-06-08 11:13:08 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -129,9 +129,9 @@
 </script>
 
 
-<script src="<?php echo base_url().'assets/js/jquery-3.3.1.js'?>" type="text/javascript"></script>
-    <script src="<?php echo base_url().'assets/js/bootstrap.js'?>" type="text/javascript"></script>
-    <script src="<?php echo base_url().'assets/js/jquery-ui.js'?>" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>asset/js/jquery-ui.js" type="text/javascript"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $( "#search" ).autocomplete({
