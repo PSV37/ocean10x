113e182e815a639aaa6070129e5d5b3fcd4a6ee1
unknown <santu@edunetsol.com>
2020-06-08 09:55:44 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -116,7 +116,7 @@
                             </div>
                             <span>Saved Job</span>
                             </div>
-                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <a class="card-footer text-white clearfix small z-1" href="<?php echo base_url(); ?>seeker/my-saved-jobs">">
                             <span class="float-left" style="font-size:22px;"><?php echo $saved_jobs; ?></span>
                             
                             </a>
