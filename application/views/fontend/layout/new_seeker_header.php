fbcac81a782c33d792a7822806220a70fafc3912
unknown <santu@edunetsol.com>
2020-06-08 15:22:09 +0530

    new

--- application/views/fontend/layout/new_seeker_header.php
+++ application/views/fontend/layout/new_seeker_header.php
@@ -145,11 +145,12 @@
         $(document).ready(function(){
 
            // alert(search);
-            $( "#search" ).autocomplete({
-              // alert($('#search_value').val());
+            alert($('#search_value').val());
+            $("#search").autocomplete({
+             
               // source:"product_auto_complete.php?postcode=" + $('#zipcode').val(),
               source: "<?php echo base_url();?>job_seeker/search_people_job?search=" + $('#search_value').val(),
-              extraParams: { search: search }
+             
             });
         });
     </script>
