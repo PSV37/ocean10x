0dd2d5034c684c41bd9cc0ab220a6a97f43ffa21
unknown <santu@edunetsol.com>
2020-06-10 10:33:29 +0530

    new

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -77,6 +77,7 @@ class Job_seeker extends MY_Seeker_Controller
 
             $where['edu_level_id'] = '3';
             $data['gddspecial'] = $this->Master_model->getMaster('education_specialization',$where);
+            $data['course'] = $this->Master_model->getMaster('course',$where=false);
 
            
 
