55e0a6958cfc4d2d77617616cbe0b678466fc76c
supriyaL <supriya@itdivine.in>
2020-06-10 18:22:32 +0530

    change

---
+++ application/views/fontend/job/job_detail.php
@@ -0,0 +1,295 @@
+<!---header---->
+
+<div class="container-fluid gradient_strip" >
+<div class="container">
+<div class="col-md-12">
+	<div class="col-md-3">
+   <div class="menu_logo">
+      <img src="http://www.tele-kinetics.com/assets/img/logo.png" />
+   </div> 
+   </div>
+   
+		
+   <div class="col-md-2">
+   <div class="text-grad">
+
+   <div class="sear-bar">
+   <form class="search-form">
+  <input type="search">
+ <i class="fas fa-search"></i>
+</form>
+     </div>          
+</div>
+	</div>
+
+<div class="col-md-3">
+
+	 <div class="switch switch-yellow">
+      <input type="radio" class="switch-input" name="view3" value="week3" id="week3" checked>
+      <label for="week3" class="switch-label switch-label-off">Job Search</label>
+      <input type="radio" class="switch-input" name="view3" value="month3" id="month3">
+      <label for="month3" class="switch-label switch-label-on">People Search</label>
+      <span class="switch-selection"></span>
+    </div>
+</div>
+
+
+
+<div class="col-md-4">     
+
+	<div class="social-media">
+    <!---mail-box-->
+    <div class="notification">
+    	<i class="fas fa-comment-alt"></i><br>
+        Messaging
+    </div>    
+    <!---mail box-end-->
+    
+    <!---notification-->
+     <div class="bell">
+    	<i class="fas fa-bell"></i><br>
+        Notifications
+   	 </div>
+     <!--notification-end-->
+     <!---profile--->   
+   <!---  <div class="profile">
+    <i class="fas fa-user-circle"></i>
+     <b>supriya</b>
+     </div>--->
+     
+     
+     <!---end-profile-->
+    </div>
+    </div>
+    
+    
+    
+    </div>
+  </div>
+
+</div>
+    
+<!---header end--->
+
+<div class="container-fluid">
+	<div class="container">
+        <div class="col-md-12">
+        	<div class="col-md-3 main-height">
+            
+            <aside id="left-panel" style="margin-top:75px;
+                 margin-left: -25px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
+            z-index: 999;vertical-align:baseline;">
+            <div class="inner-left-pannel">
+                
+                
+                <!-- WHAT MOVES START -->
+                <div class="my-moving-parts">
+                    <div class="my-param-content"></div>
+                    <div class="my-normal-content">
+                       <div class="inner-tabs-navigation" data-active="menu">
+                        </div>
+                        <div class="inner-tabs">
+                            <div class="account-tab">
+                                <div class="language-selection" title="Change language">
+                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                                                      <i class="flag flag-us"></i> 
+                                                         </a>
+                                                     </span>
+                                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="menu-tab">
+                               
+                                
+                                
+                                <nav class="menu-principal">
+                                
+                                    <ul class="menu-principal-list" style="">
+                                         <li class="active">
+                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                            <span class="icon-container">
+                                                 <i class="fas fa-tachometer-alt"></i>
+                                            </span>
+                                            <span class="text item">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                            <span class="icon-container">
+                                                <i class="fas fa-user-alt"></i>
+                                           </span>
+                                        <span class="text item">My Ocean profil</span>
+                                              </a>
+                                       </li>
+                                       
+                                      <li>
+                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                                       <span class="icon-container">
+                                         <i class="fas fa-phone-volume"></i>
+                                     </span>
+                                        <span class="text item">OcearnHunt Activities</span>
+                                     </a>
+                                      </li>
+                                         <li>
+                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                            <span class="icon-container">
+                                              <i class="fas fa-filter"></i>
+                                              </span>
+                                        <span class="text item">Skill Upgrade</span>
+
+                                          </a>
+                                        </li>
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Become an Ocean champ</span>
+                                              </a>
+                                         </li>
+                                            
+                                          
+                                         
+                                     </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- WHAT MOVES END -->
+            </div>
+                </aside>
+            </div>
+			
+            	<div class="col-md-9 details_box">
+                	<div class="job-logo">
+                 <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="job-disc-logo"/>
+                    </div>
+                     <div class="row">
+              <div class="col-md-4">
+                <ul class="jobinfolist">
+                  <li>
+                    <h4>Job Title</h4>
+                    <strong>: 3</strong></li>
+                  <li>
+                    <h4>Job Status</h4>
+                    <strong>: IT &amp; Telecommunication</strong></li>
+                  <li>
+                    <h4>Job Level</h4>
+                    <strong>: Medium</strong></li>
+                                   
+                    
+                  
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <ul class="jobinfolist">
+                  <li>
+                    <h4>Organization:</h4>
+                    <strong>: Rs.12k-40k</strong></li>
+                  <li>
+                    <h4>Job Type</h4>
+                    <strong>: IT &amp; Telecommunication</strong></li>
+                  <!-- <li>
+                    <h4>Preferred Age:</h4> 
+                    <strong>0 (years)</strong></li> -->
+                  <li>
+                    <h4>Salary</h4>
+                    <strong>:10k</strong></li>
+                  
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <ul class="jobinfolist">
+                  <li>
+                    <h4>Location</h4>
+                    <strong>: Pune, Bangaluru, Mumbai</strong></li>
+                  <li>
+                    <h4>Experience</h4>
+                    <strong>: 1</strong></li>
+                  <!-- <li>
+                    <h4>Preferred Age:</h4> 
+                    <strong>0 (years)</strong></li> -->
+                  
+                </ul>
+              </div>
+              
+              
+            </div>
+            <div class="jd">
+            <h4>Job Description :</h4>
+            <p>A job description is an internal document that clearly states the essential job requirements, job duties, job responsibilities, and skills required to perform a specific role. A more detailed job description will cover how success is measured in the role so it can be used during performance evaluations.</p>
+              </div>
+              
+              <div class="jd-require">
+            <h4>Job Requirement</h4>
+            <button class="apply-cv" id="b3">Apply with Ocean cv</button>
+              </div>
+                     </div>
+            
+           
+           </div>
+       </div>
+       
+       <!-- Footer -->
+ <div class="container-fluid end-footer">   
+	
+    <div class="container">
+    <div class="col-md-12 ">
+<div class="row">
+<div class="col-md-4 p-footer">
+<h5>About consultnHire</h5>
+					<ul class="list-unstyled quick-links">
+						<p>Working with us has many benefits for you, your business and your stakeholders. We are known for our creative, yet professional approach</p>
+					</ul>
+
+</div>
+<div class="col-md-4 p-footer">
+<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Home</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Search Jobs</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Views All Jobs</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Contact Us</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;report a bug/Abuse</a></li>
+					</ul>
+
+</div>
+<div class="col-md-4 p-footer">
+<h5>About Company</h5>
+					<ul class="list-unstyled quick-links ">
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;About Company</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;My Account</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Terms Of Use</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Help</a></li>
+						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-angle-double-right"></i>&emsp;Privacy Commitment</a></li>
+					</ul>
+</div>
+
+<div class="row">
+
+<div class="footer-icons">
+
+<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook-f"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-linkedin-in"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-phone-square-alt"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fas fa-envelope-open"></i></a></li>
+						
+					</ul>
+</div>
+
+				
+				
+			</div>
+
+
+</div>
+</div>
+</div>
+</div>
+	<!-- ./Footer -->
+  </div>   
+  
