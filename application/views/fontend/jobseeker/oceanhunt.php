51b40dee851ff02f51f32337120134258029ab2c
unknown <santu@edunetsol.com>
2020-06-11 17:08:49 +0530

    new

--- application/views/fontend/jobseeker/oceanhunt.php
+++ application/views/fontend/jobseeker/oceanhunt.php
@@ -78,27 +78,51 @@
                 </div>
    </div>
     <div id="activity2" class="tab-pane fade">
-      <div class="invi-div">
-                            <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="invitation-img"/>
+     
+           <?  if (!empty($applicationlist)): foreach ($forward_applicationlist as $forward_applicaiton) :
+                // for ($i=0; $i <sizeof($forward_applicationlist) ; $i++) { 
+                $singlejob    = $this->job_posting_model->get_job_details_employer($forward_applicaiton->job_post_id);
+
+
+                            $sr_no++; ?>
+                        <div class="invi-div">
+                            <img src="<?php echo base_url()?>upload/<?php echo $this->company_profile_model->company_logoby_id($forward_applicaiton->company_id); ?>" class="invitation-img"/>
                             <div class="info-invitation">
-                                <p class="head-invi">php developer</p>
-                                <span class="salary-info">Slaray: 20k-30k<span>
-                                <p>text test</p>
+                                <p class="head-invi"><?php echo $this->job_posting_model->job_title_by_name($forward_applicaiton->job_post_id); ?></p>
+
+                                <span class="salary-info">Slaray: <?php echo $this->job_posting_model->job_salary_by_id($forward_applicaiton->job_post_id); ?><span>
+
+                                <p>Company name:<?php echo $this->company_profile_model->company_name($forward_applicaiton->company_id); ?></p>
                                  <div class="detail-b">Details</div>
                                     <div class="last-row-invitation">
                                     <ul>
-                                        <li><div class="location-inv"><i class="fas fa-map-marker-alt"></i></div></li>
-                                       <li> <div class="year-inv"><i class="fas fa-save"></i>&emsp;2 years</div></li>
-                                        <li> <div class="calender-inv"><i class="far fa-calendar-alt"></i>&emsp;7 hr ago</div></li>
-                                        <li> <div class="fulltime-inv"><i class="fas fa-clock"></i>&emsp;full time</div></li>
+                                        <li><div class="location-inv"><i class="fas fa-map-marker-alt"></i><?php echo $forward_applicaiton->city_id;  ?></div></li>
+                                       <li> <div class="year-inv"><i class="fas fa-save"></i>&emsp;<?php echo $forward_applicaiton->experience;  ?> years</div></li>
+                                        <li> <div class="calender-inv"><i class="far fa-calendar-alt"></i>&emsp; <?php if(!is_null($forward_applicaiton->created_at)) { $mtime = time_ago_in_php($forward_applicaiton->created_at);
+                            echo $mtime;} ?></div></li>
+                                        <li> <div class="fulltime-inv"><i class="fas fa-clock"></i>&emsp;<?php echo $forward_applicaiton->job_nature_name;  ?></div></li>
                                     </ul>
                                     </div>
                             
-                                <button class="apply-invi">Apply Now</button>
+                                <button class="apply-invi">Apply</button>
                             </div>
                             <div class="clear"></div>   
-                        </div>
-                        
+                              </div>
+                        <?php
+                  // $sr_no++;
+                   // }
+
+              endforeach;
+            ?>
+            <?php else : ?> 
+            
+                <div>
+                    <strong>There is no data to display</strong>
+                  
+                </div>
+             
+              
+            <?php endif; ?>
         <div class="invi-div">
                             <img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="invitation-img"/>
                             <div class="info-invitation">
