1579789e8e21834a81e56743731b8c5cd643888e
supriyaL <supriya@itdivine.in>
2020-06-12 10:11:49 +0530

    change

---
+++ application/views/fontend/exam/oceantest_instructions.php
@@ -0,0 +1,87 @@
+
+<div class="container-fluid header-image">
+
+<div class="profile-round">
+<img src="https://image.freepik.com/free-vector/woman-profile-cartoon_18591-58480.jpg" class="rounded-circle" alt="Cinque Terre" width="304" height="236"> 
+<div class="info">
+	<p>Hellow </p>
+    <span>welcome to OceanHunt</span>
+</div>
+
+</div>
+</div>
+<div class="container username">
+<p>supriya@123</p>
+
+</div>
+
+<div class="container">
+<div class="col-md-12">
+<div class="col-md-2"></div>
+<div class="col-md-10">
+<div class="stepwizard">
+    <div class="stepwizard-row">
+        <div class="stepwizard-step">
+            <button type="button" class="btn btn-primary btn-default btn-circle" >1</button>
+            <p>Cart</p>
+        </div>
+        <div class="stepwizard-step">
+            <button type="button" class="btn  btn-circle" disabled="disabled">2</button>
+            <p>Shipping</p>
+        </div>
+        <div class="stepwizard-step">
+            <button type="button" class="btn  btn-circle" disabled="disabled">3</button>
+            <p>Payment</p>
+        </div>
+        <div class="stepwizard-step">
+            <button type="button" class="btn btn-circle" disabled="disabled">3</button>
+            <p>Shipping</p>
+        </div>
+        <div class="stepwizard-step">
+            <button type="button" class="btn btn-circle" disabled="disabled">5</button>
+            <p>Shipping</p>
+        </div> 
+    </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid skills">
+<div class="container">
+	<h5 style="font-size: medium;
+    color: #828282;
+    margin-left: -15px;">Choose your programmig prefered languages</h5>
+    
+    <div class="row">
+    <div class="col-md-2 bo-c">c++</div>
+    <div class="col-md-2 bo-c">c</div>
+    <div class="col-md-2 bo-c">Bootstrap</div>
+    <div class="col-md-2 bo-c">Javascript</div>
+    <div class="col-md-2 bo-c">query</div>
+   
+    
+    </div>
+    
+    <div class="row">
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+   
+    
+    </div>
+    
+     <div class="row">
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 bo-c"></div>
+    <div class="col-md-2 show-b">Show more</div>
+   
+    
+    </div>
+    
+</div>
+</div>
