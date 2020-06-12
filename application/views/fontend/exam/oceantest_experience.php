565581e218dc6235a500b0eb7dd00f20416798e5
supriyaL <supriya@itdivine.in>
2020-06-12 11:48:45 +0530

    change

---
+++ application/views/fontend/exam/oceantest_experience.php
@@ -0,0 +1,32 @@
+<div class="container-fluid">
+	<div class="container">
+    	<div class="student-pro">
+        <h6>Are you a student or professional ?</h6>
+        <div class="m">
+        <div class="student-b"><i class="fas fa-graduation-cap"></i><p>Student</p></div>
+        <div class="professional-b"><i class="fas fa-user-graduate"></i>professional</div>
+        </div>
+        </div>
+    </div>
+</div>   
+<div class="container-fluid" style="background-color:aliceblue;margin-top:60px;">
+    <div class="container experience">
+    <h5>How many experience do you have ?</h5>
+    
+    
+    <div class="col-md-12 row rexp">
+    <div class="col-md-1 exp-box">1</div>
+    <div class="col-md-1 exp-box">2</div>
+    <div class="col-md-1 exp-box">3</div>
+    <div class="col-md-1 exp-box">4</div>
+    <div class="col-md-1 exp-box">5</div>
+    <div class="col-md-1 exp-box">6</div>
+    <div class="col-md-1 exp-box">7</div>
+    <div class="col-md-1 exp-box">8</div>
+    <div class="col-md-1 exp-box">9</div>
+    <div class="col-md-1 exp-box">10+</div>
+    </div>
+        <button class="pro-btn">Proceed to dashboard</button>
+
+    </div>
+</div>
