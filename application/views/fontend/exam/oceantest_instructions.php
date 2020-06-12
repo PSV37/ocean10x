5ea05963094d67a1da65b03db830b42c8cc952b9
unknown <santu@edunetsol.com>
2020-06-12 11:04:55 +0530

    new

--- application/views/fontend/exam/oceantest_instructions.php
+++ application/views/fontend/exam/oceantest_instructions.php
@@ -1,7 +1,4 @@
-<?php 
-    $this->load->view('fontend/layout/new_seeker_header.php');
-
-?>  
+   <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/oceantest.css"> 
    
 <div class="container-fluid header-image">
 
