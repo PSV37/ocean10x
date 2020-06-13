3d45eff05ba6affbffa4e15c0a196774b3468b99
supriyaL <supriya@itdivine.in>
2020-06-13 10:24:18 +0530

    change

---
+++ application/views/fontend/employer/employer_dashboard.php
@@ -0,0 +1,397 @@
+
+<!---header-->
+<div class="container-fluid gradient_strip" >
+<div class="container">
+<div class="col-md-12">
+	<div class="col-md-3">
+   <div class="menu_logo">
+      <img src="http://www.tele-kinetics.com/assets/img/logo.png" />
+   </div> 
+   </div>
+   
+		
+   <div class="col-md-2">
+ 
+
+   <div class="sear-bar">
+   <form class="search-form">
+  <input type="search">
+ <i class="fas fa-search"></i>
+</form>
+     </div>          
+
+	</div>
+
+<div class="col-md-3">
+
+	 <div class="switch switch-yellow">
+      <input type="radio" class="switch-input" name="view3" value="week3" id="week3" checked>
+      <label for="week3" class="switch-label switch-label-off">Job Search</label>
+      <input type="radio" class="switch-input" name="view3" value="month3" id="month3">
+      <label for="month3" class="switch-label switch-label-on">People Search</label>
+      <span class="switch-selection"></span>
+    </div>
+</div>
+
+<div class="col-md-1">
+	 <div class="notification">
+    	<i class="fas fa-comment-alt"></i><br>
+        Messaging
+    </div>    
+   
+</div>
+
+<div class="col-md-1">
+	 <div class="bell">
+    	<i class="fas fa-bell"></i><br>
+        Notifications
+   	 </div>  
+   
+</div>     
+    
+    <div class="col-md-2">
+    	 <div class="dropdown">
+  <i class="fas fa-user-circle"></i>&emsp;<a class=" dropdown-toggle" data-toggle="dropdown">
+    
+    <span class="caret"></span>
+    <p class="profile-accoutnt-p">supriya</p>
+    </a>
+    <ul class="dropdown-menu">
+      <li><a href="#"><i class="fas fa-user"></i></a> My Profile</li>
+      <li><a href="#"><i class="fas fa-lock"></i></a>Change Password</li>
+      <li><a href="#"><i class="fas fa-power-off"></i></a>Logout</li>
+    </ul>
+  </div>
+    </div>
+    
+    </div>
+  </div>
+
+</div>
+<!---header--->
+
+<div class="container-fluid main-d">
+	<div class="container">
+        <div class="col-md-12">
+        	<div class="col-md-3">
+            
+            <aside id="left-panel" style="margin-top:75px;
+                 margin-left: 14px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
+            z-index: 999;vertical-align:baseline;">
+            <div class="inner-left-pannel">
+                
+                
+                <!-- WHAT MOVES START -->
+                <div class="my-moving-parts">
+                    <div class="my-param-content"></div>
+                    <div class="my-normal-content">
+                       <div class="inner-tabs-navigation" data-active="menu">
+                        </div>
+                        <div class="inner-tabs">
+                            <div class="account-tab">
+                                <div class="language-selection" title="Change language">
+                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                                                      <i class="flag flag-us"></i> 
+                                                         </a>
+                                                     </span>
+                                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="menu-tab">
+                               
+                                
+                                
+                                <nav class="menu-principal">
+                                
+                                    <ul class="menu-principal-list" style="">
+                                         <li class="active">
+                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                            <span class="icon-container">
+                                                 <i class="fas fa-tachometer-alt"></i>
+                                            </span>
+                                            <span class="text item">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                            <span class="icon-container">
+                                                <i class="fas fa-user-alt"></i>
+                                           </span>
+                                        <span class="text item">CV Bank</span>
+                                              </a>
+                                       </li>
+                                       
+                                      <li>
+                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                                       <span class="icon-container">
+                                         <i class="fas fa-phone-volume"></i>
+                                     </span>
+                                        <span class="text item">Post New Job</span>
+                                     </a>
+                                      </li>
+                                         <li>
+                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                            <span class="icon-container">
+                                              <i class="fas fa-filter"></i>
+                                              </span>
+                                        <span class="text item">Active Job Post</span>
+
+                                          </a>
+                                        </li>
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Pending Jobs</span>
+                                              </a>
+                                         </li>
+                                            
+                                          
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Question Bank</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Employee Management</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Company / Recruiter</span>
+                                              </a>
+                                         </li>
+                                         
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">OceanChamp</span>
+                                              </a>
+                                         </li>
+                                         
+                                         
+                                     </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- WHAT MOVES END -->
+            </div>
+                </aside>
+            </div>
+            
+            <div class="col-md-6 mid-section">
+            <div class="row">    
+            <div class="col-md-12 bd-1">
+                   
+            <div class="dash-box-w">
+             <h3 class="heading-dash">My Dashboard</h3>
+             
+             <div class="col-md-4 card-lb">
+                        <div class="card text-white bg-primary o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                           <i class="fas fa-fw fa-download"></i>
+                            </div>
+                            <span>Saved Job</span>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:2px;">10</span>
+                            </a>
+                            </div>
+                        </div>
+              <div class="col-md-4 card-lb">
+                        	<div class="card text-white bg-danger o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                          <i class="fas fa-volume-up fa-fw"></i>
+                            </div>
+                            <div >Job Alerts</div>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:22px;">20</span>
+                            </a>
+                            </div>
+                        </div>
+                 <div class="col-md-4 card-lb">
+                        	<div class="card text-white bg-warning o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                           <i class="fas fa-users fa-fw"></i>
+                            </div>
+                            <div>Profile Views</div>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:22px;">20</span>
+                            </a>
+                            </div>
+                        </div> 
+                     
+                     <div class="col-md-4 card-lb">
+                        <div class="card text-white bg-bluish o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                           <i class="fas fa-fw fa-download"></i>
+                            </div>
+                            <span>Article Views</span>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:22px;">10</span>
+                            </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4 card-lb">
+                        	<div class="card text-white bg-link o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                          <i class="fas fa-volume-up fa-fw"></i>
+                            </div>
+                            <div>courses Completed</div>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:22px;">20</span>
+                            </a>
+                            </div>
+                        </div>
+                          
+                        <div class="col-md-4 card-lb">
+                        	<div class="card text-white bg-green o-hidden h-100">
+                            <div class="card-body">
+                            <div class="card-body-icon">
+                           <i class="fas fa-users fa-fw"></i>
+                            </div>
+                            <div>News Feed</div>
+                            </div>
+                            <a class="card-footer text-white clearfix small z-1" href="#">
+                            <span class="float-left" style="font-size:22px;">20</span>
+                            </a>
+                            </div>
+                        </div>
+                        
+                              
+                  </div>                
+             </div>
+             
+             <div class="col-md-12 bd-2">
+             <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+                <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                   The person/job specification can be presented as a stand-alone 
+                   </div> 
+                    <div class="organization">
+                    	IT Company
+                    </div>
+                    <div class="location">
+                    	Pune ,Kalyani nagar
+                    </div>
+                    <div class="apply_job_btn">Apply job</div>
+                    <button class="job_dis_btn">Details</button>
+                </div>
+             </div>
+             
+             
+            </div>
+           </div>
+           
+            <div class="col-md-3 pro-bar">
+          
+            <h3 class="heading-dash_profile">PROFILE LEVEL</h3>
+                <div class="progress yellow">
+                <span class="progress-left">
+                    <span class="progress-bar"></span>
+                </span>
+                <span class="progress-right">
+                    <span class="progress-bar"></span>
+                </span>
+                <div class="progress-value">75%</div>
+            </div>
+      
+        	
+       			 <div class="paragraph_p_level">
+       	<p>Create your own, and see what different functions produce. Get to understand what is really happening. What type of Graph do you want</p> 
+        </div>
+            </div>
+            
+         </div>
+     </div>
+</div>   
+
