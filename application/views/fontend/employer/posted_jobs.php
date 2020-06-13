dbbe2c7acb82e38464dffbce32a6624ac6cc2fa4
supriyaL <supriya@itdivine.in>
2020-06-13 18:41:30 +0530

    change

---
+++ application/views/fontend/employer/posted_jobs.php
@@ -0,0 +1,441 @@
+<style>
+@charset "utf-8";
+/* CSS Document */
+
+.btn_all {
+    color: #539617;
+    background-color: #fff;
+    height: 35px;
+    width: 70px;
+    border: solid 1px;
+    border-radius: 50px;
+}
+	
+.btn_all:hover {
+    color: #fff;
+    background-color: #a6e026;
+    border-color: #87cf56;
+}
+.all_dropdown{float:left;margin-right:20px;}
+.btn_sort{
+	color: #319fda;
+    background-color: #fff;
+    height: 35px;
+    width: 70px;
+    border: solid 1px;
+    border-radius: 50px;
+	}
+.btn_sort:hover{
+	color: #fff;
+    background-color: #44afe1;
+    border-color: #168bd8;}	
+
+.hide_btn{margin-top:20px;}
+#myDIV {
+  width: 100%;
+  
+  background-color:#eef7fa;
+  margin-top: 20px;
+}
+.horizontal_box {
+    width: 7%;
+    background-color: #a9e224;
+    height: 140px;
+    padding: 0px;
+}
+.try_bt{
+height: 30px;
+    width: 70px;
+    border: none;
+    background-color: #a3df2d;
+    color: #fff;
+	
+	}
+.horizontal_box{position:relative;
+width: 3%;
+    background-color: #a9e224;
+    height: 270px;
+    padding: 0px;
+    margin-left: 15px;}	
+.p-i {
+    position: absolute;
+    top: 19px;
+    right: -24px;
+    background-color: #fff;
+    height: 50px;
+    width: 50px;
+    line-height: 50px;
+    border-radius: 35px;
+    border: solid 2px #73b102;
+	text-align:center;
+}
+.details-box {
+    padding: 30px 0px;
+}
+.head-box strong{line-height:3;}
+.head-box-a strong{line-height:3;
+font-weight: 400;}
+.bg-lb:hover .p-i{color:#fff;
+				background-color:#62a910;}
+button.apply-cv-btn {
+    padding: 8px 20px;
+    border-radius: 40px;
+    border: solid 2px #F44336;
+    background-color: #fff;
+    color: #000;
+}	
+button.apply-cv-btn:hover{background-color:#F44336;
+/*new*/
+
+color:#fff;}
+.all_b {
+    float: left;
+    margin-right: 30px;
+    background-color: #fff;
+   padding: 3px 32px;
+    border-radius: 20px;
+    border: solid 1px #a7e126;
+}
+.sort_b {
+    background-color: #fff;
+   padding: 3px 32px;
+    border-radius: 20px;
+    border: solid 1px #a7e126;
+}
+.seperate-btn {
+    margin-top:34px;
+}
+.active_save_btn{background-color:#a7e126;
+color:#fff;}
+.sort_b:hover{background-color:#a7e126;
+color:#fff;}
+.all_b:hover{background-color:#a7e126;
+color:#fff;}
+.job-voucher{background-color:#fff;border-radius:3px;height:130px;padding: 15px 20px 15px 20px;box-shadow: 0px 2px 6px #00000038;
+line-height:20px;}
+.dimen_img-s{height:60px;
+			width:60px;float:left;margin-right: 20px;
+			margin-bottom:20px;}
+
+
+
+
+
+.dimen_img-s{height:60px;
+width:60px;}	
+.job_title{font-size:18px;
+		cursor: pointer;
+		margin-bottom:10px;
+		}
+.job_title:hover{text-decoration:underline;}		
+.organization {
+    color: #868383;
+	font-size: 12px;
+}
+.location{color: #868383;
+font-size: 12px;
+}
+.alert-dismissable .close, .alert-dismissible .close {
+    position: relative;
+    top: 0px;
+    right: 0px;
+    color: inherit;
+}
+.job_dis_btn{padding: 2px 16px;
+    border: none;
+    margin-left: 79px;
+   
+	font-size:12px;}
+.job_dis_btn:hover{
+background-color: #81c3f8;
+    color: #fff;
+}
+.apply_job_btn {
+    float: right;
+    background-color: #fff;
+    padding: 2px 21px;
+    border: solid 1px #18c5bd;
+    color: #159d96;
+    font-weight: 700;
+    font-size: 11px;
+    cursor: pointer;
+    border-radius: 29px;
+    margin-top: -5px;
+}
+.apply_job_btn:hover{
+	 background-color:#159d96;
+	 color: #fff;
+	 transition:0.7s;
+	}
+.bd-2 {
+    margin-top: 20px;
+	padding:0px;
+}	
+/*end*/
+
+
+.col-md-6.active-job {
+    margin-top: 77px;
+}
+.dropdown.right-arrow {
+    position: absolute;
+    top: -3px;
+    right: 69px;
+    border: none;
+}
+.btn{padding:0px;}
+.btn-default{border:none;font-size: 20px;}
+.btn-default:hover{
+	background-color:#fff;
+	border:none;}
+.dropdown-menu li {
+    font-size: 11px;
+    padding: 8px 13px;
+    cursor: pointer;
+}	
+</style>
+
+
+
+
+
+<!---header-->
+<div class="container-fluid gradient_strip" >
+<div class="container">
+<div class="col-md-12">
+	<div class="col-md-3">
+   <div class="menu_logo">
+      <img src="http://www.tele-kinetics.com/assets/img/logo.png" />
+   </div> 
+   </div>
+   
+		
+   <div class="col-md-2">
+ 
+
+   <div class="sear-bar">
+   <form class="search-form">
+  <input type="search">
+ <i class="fas fa-search"></i>
+</form>
+     </div>          
+
+	</div>
+
+<div class="col-md-3">
+
+	 <div class="switch switch-yellow">
+      <input type="radio" class="switch-input" name="view3" value="week3" id="week3" checked>
+      <label for="week3" class="switch-label switch-label-off">Job Search</label>
+      <input type="radio" class="switch-input" name="view3" value="month3" id="month3">
+      <label for="month3" class="switch-label switch-label-on">People Search</label>
+      <span class="switch-selection"></span>
+    </div>
+</div>
+
+<div class="col-md-1">
+	 <div class="notification">
+    	<i class="fas fa-comment-alt"></i><br>
+        Messaging
+    </div>    
+   
+</div>
+
+<div class="col-md-1">
+	 <div class="bell">
+    	<i class="fas fa-bell"></i><br>
+        Notifications
+   	 </div>  
+   
+</div>     
+    
+    <div class="col-md-2">
+    	 <div class="dropdown">
+  <i class="fas fa-user-circle"></i>&emsp;<a class=" dropdown-toggle" data-toggle="dropdown">
+    
+    <span class="caret"></span>
+    <p class="profile-accoutnt-p">supriya</p>
+    </a>
+    <ul class="dropdown-menu">
+      <li><a href="#"><i class="fas fa-user"></i></a> My Profile</li>
+      <li><a href="#"><i class="fas fa-lock"></i></a>Change Password</li>
+      <li><a href="#"><i class="fas fa-power-off"></i></a>Logout</li>
+    </ul>
+  </div>
+    </div>
+    
+    </div>
+  </div>
+
+</div>
+<!---header--->
+
+
+<div class="container-fluid main-d">
+	<div class="container">
+        <div class="col-md-12">
+        	<div class="col-md-3">
+            
+            <aside id="left-panel" style="margin-top:75px;
+                 margin-left: 14px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
+            z-index: 999;vertical-align:baseline;">
+            <div class="inner-left-pannel">
+                
+                
+                <!-- WHAT MOVES START -->
+                <div class="my-moving-parts">
+                    <div class="my-param-content"></div>
+                    <div class="my-normal-content">
+                       <div class="inner-tabs-navigation" data-active="menu">
+                        </div>
+                        <div class="inner-tabs">
+                            <div class="account-tab">
+                                <div class="language-selection" title="Change language">
+                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                                                      <i class="flag flag-us"></i> 
+                                                         </a>
+                                                     </span>
+                                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="menu-tab">
+                               
+                                
+                                
+                                <nav class="menu-principal">
+                                
+                                    <ul class="menu-principal-list" style="">
+                                         <li class="active">
+                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                            <span class="icon-container">
+                                                 <i class="fas fa-tachometer-alt"></i>
+                                            </span>
+                                            <span class="text item">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                            <span class="icon-container">
+                                                <i class="fas fa-user-alt"></i>
+                                           </span>
+                                        <span class="text item">CV Bank</span>
+                                              </a>
+                                       </li>
+                                       
+                                      <li>
+                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                                       <span class="icon-container">
+                                         <i class="fas fa-phone-volume"></i>
+                                     </span>
+                                        <span class="text item">Post New Job</span>
+                                     </a>
+                                      </li>
+                                         <li>
+                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                            <span class="icon-container">
+                                              <i class="fas fa-filter"></i>
+                                              </span>
+                                        <span class="text item">Active Job Post</span>
+
+                                          </a>
+                                        </li>
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Pending Jobs</span>
+                                              </a>
+                                         </li>
+                                            
+                                          
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Question Bank</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Employee Management</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Company / Recruiter</span>
+                                              </a>
+                                         </li>
+                                         
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">OceanChamp</span>
+                                              </a>
+                                         </li>
+                                         
+                                         
+                                     </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- WHAT MOVES END -->
+            </div>
+                </aside>
+            </div>
+            
+            <div class="col-md-6 active-job">
+            
+            <div class="job-voucher alert alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                	<img src="https://media-exp1.licdn.com/dms/image/C4E0BAQHbWPfQdNw1EA/company-logo_200_200/0?e=2159024400&v=beta&t=fWMuJX9leYFsDf-weERHLyIPfRh4aCOwx8wygmhad9Q" class="dimen_img-s" />
+                   <div class="job_title">
+                  php developer
+                   </div> 
+                    <div class="organization">
+                    	Published : 11 july
+                    </div>
+                    <div class="location">
+                    	Deadline :19 july
+                    </div>
+                    <div class="apply_job_btn">forward</div>
+                   
+                   
+                   	<div class="dropdown right-arrow">
+                        <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">...
+                       </button>
+                        <ul class="dropdown-menu">
+                          
+                          <li><a href="#">Copy link to post</a></li>
+                          <li><a href="#">save job</a></li>
+                          <li><a href="#">JavaScript</a></li>
+                          
+                        </ul>
+                      </div>
+                   
+                </div>
+            
+            
+            
+            </div>
+			<div class="col-md-3">
+            <!--future use-->
+            </div>            
+		</div>
+    </div>
+</div>        
