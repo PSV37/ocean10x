71641ad63e36feea22fef43605baeebe4ef6b594
unknown <santu@edunetsol.com>
2020-06-15 15:13:03 +0530

    new

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -531,8 +531,8 @@ class Exam extends MY_Seeker_Controller
            break;
         }
 
-        print_r($option);die;
-        // print_r($data['questions']);
+        // print_r($option);
+        // print_r($data['questions']['answer']);die;
        
         for($q=0;$q<sizeof($data['questions']['answer']);$q++)
         {
