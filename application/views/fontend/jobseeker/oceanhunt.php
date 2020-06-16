57eeee32611c85ca0e8ebd463b963c04903845ed
unknown <santu@edunetsol.com>
2020-06-16 16:38:23 +0530

    new

--- application/views/fontend/jobseeker/oceanhunt.php
+++ application/views/fontend/jobseeker/oceanhunt.php
@@ -51,7 +51,7 @@
                                     </ul>
                                     </div>
                             
-                                <button class="apply-invi">Apply</button>
+                                <button class="apply-invi" data-toggle="modal" data-target="#ApplyJob1<?php echo $$forward_applicaiton->job_post_id; ?>">Apply</button>
                             </div>
                             <div class="clear"></div>   
                               </div>
@@ -77,6 +77,69 @@
                 <button class="more-btn">show more</button>
                 </div>
    </div>
+   <?php foreach ($applicationlist as $applicaiton) : ?>
+ <div id="ApplyJob1<?php echo $applicaiton->job_post_id; ?>" class="modal fade" role="dialog">
+  <div class="modal-dialog"> 
+    
+    <!-- Modal content-->
+    
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Apply Job</h4>
+      </div>
+      <div class="modal-body">
+        <form  class="form-horizontal" action="<?php echo base_url() ?>job/apply_job" method="post" style="padding: 30px;">
+          <input type="hidden" name="forward_status" class="form-control" id="forward_status" value="<?php if(!empty($forward_status)){ foreach($forward_status as $frow){
+            echo $frow['forword_job_status'];
+          } }?>" placeholder="">
+          <input type="hidden" name="job_apply_id" class="form-control" id="job_apply_id" value="<?php if(!empty($forward_status)){ foreach($forward_status as $frow){
+            echo $frow['job_apply_id'];
+          } }?>" placeholder="">
+                   
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email">User Name:</label>
+            <div class="col-sm-8">
+              <input type="text" name="js_career_salary" disabled="" class="form-control" id="js_career_salary" placeholder=""
+
+                   value="<?php if(!empty($jobseeker_id)){ echo $this->Job_seeker_model->jobseeker_name($jobseeker_id);} ?>">
+            </div>
+          </div>
+          <input type="hidden" name="job_seeker_id" value="<?php echo $jobseeker_id ?>">
+          <input type="hidden" name="job_post_id" value="<?php echo $applicaiton->job_post_id; ?>">
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email">Company Name:</label>
+            <div class="col-sm-8">
+              <input type="text" name="js_career_salary" disabled="" class="form-control" id="js_career_salary" placeholder="" value="<?php $company_id=$applicaiton->company_profile_id;
+                         echo $this->company_profile_model->company_name($company_id);?>">
+            </div>
+          </div>
+          <input type="hidden" name="company_id" value="<?php echo $company_id; ?>">
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email"> Expected Salary:</label>
+            <div class="col-sm-8">
+              <input type="text" name="expected_salary" required class="form-control" id="avaliable" placeholder="Expected Salary"
+
+                   >
+            </div>
+          </div>
+          <?php $test=$applicaiton->is_test_required;
+            if ($test=='Yes') {
+          ?>
+          <div><b>Note: This job has a Online Test.</b></div>
+          <div class="panel-body"></div>
+        <?php }else{} ?>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    
+    </div>
+  </div>
+</div>
+<?php  endforeach;  ?>
     <div id="activity2" class="tab-pane fade">
      
            <?  if (!empty($applicationlist)): foreach ($applicationlist as $forward_applicaiton) :
@@ -107,7 +170,7 @@
                                     </ul>
                                     </div>
                             
-                                <button class="apply-invi">Apply</button>
+                                <button class="apply-invi" data-toggle="modal" data-target="#ApplyJob2<?php echo $$forward_applicaiton->job_post_id; ?>">Apply</button>
                             </div>
                             <div class="clear"></div>   
                               </div>
@@ -146,7 +209,69 @@
       
  </div>           
 			
- 
+ <?php foreach ($applicationlist as $applicaiton) : ?>
+ <div id="ApplyJob2<?php echo $applicaiton->job_post_id; ?>" class="modal fade" role="dialog">
+  <div class="modal-dialog"> 
+    
+    <!-- Modal content-->
+    
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Apply Job</h4>
+      </div>
+      <div class="modal-body">
+        <form  class="form-horizontal" action="<?php echo base_url() ?>job/apply_job" method="post" style="padding: 30px;">
+          <input type="hidden" name="forward_status" class="form-control" id="forward_status" value="<?php if(!empty($forward_status)){ foreach($forward_status as $frow){
+            echo $frow['forword_job_status'];
+          } }?>" placeholder="">
+          <input type="hidden" name="job_apply_id" class="form-control" id="job_apply_id" value="<?php if(!empty($forward_status)){ foreach($forward_status as $frow){
+            echo $frow['job_apply_id'];
+          } }?>" placeholder="">
+                   
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email">User Name:</label>
+            <div class="col-sm-8">
+              <input type="text" name="js_career_salary" disabled="" class="form-control" id="js_career_salary" placeholder=""
+
+                   value="<?php if(!empty($jobseeker_id)){ echo $this->Job_seeker_model->jobseeker_name($jobseeker_id);} ?>">
+            </div>
+          </div>
+          <input type="hidden" name="job_seeker_id" value="<?php echo $jobseeker_id ?>">
+          <input type="hidden" name="job_post_id" value="<?php echo $applicaiton->job_post_id; ?>">
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email">Company Name:</label>
+            <div class="col-sm-8">
+              <input type="text" name="js_career_salary" disabled="" class="form-control" id="js_career_salary" placeholder="" value="<?php $company_id=$applicaiton->company_profile_id;
+                         echo $this->company_profile_model->company_name($company_id);?>">
+            </div>
+          </div>
+          <input type="hidden" name="company_id" value="<?php echo $company_id; ?>">
+          <div class="form-group">
+            <label class="control-label col-sm-4" for="email"> Expected Salary:</label>
+            <div class="col-sm-8">
+              <input type="text" name="expected_salary" required class="form-control" id="avaliable" placeholder="Expected Salary"
+
+                   >
+            </div>
+          </div>
+          <?php $test=$applicaiton->is_test_required;
+            if ($test=='Yes') {
+          ?>
+          <div><b>Note: This job has a Online Test.</b></div>
+          <div class="panel-body"></div>
+        <?php }else{} ?>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    
+    </div>
+  </div>
+</div>
+<?php  endforeach;  ?>
  
  
  
