6e0d527e5ba5d944a92856f652016ef6a1992310
unknown <santu@edunetsol.com>
2020-06-17 14:33:01 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -819,7 +819,7 @@ function getstate(){
                $result = '';
                 if(!empty($skills)){ 
                     foreach($skills as $skill_row){
-                      $result .="<button> <input type='checkbox' name='skill_set[]' style='height:15px; width:20px;' id='skill_set' value=".$skill_row['id']." checked> ".$skill_row['skill_name']."</button>";
+                      $result .="<button type='button'> <input type='checkbox' name='skill_set[]' style='height:15px; width:20px;' id='skill_set' value=".$skill_row['id']." checked> ".$skill_row['skill_name']."</button>";
 
                     }
                 }else{
