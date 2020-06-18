fe46968db0a3ca7ea00ed04da0536714e793292b
supriyaL <supriya@itdivine.in>
2020-06-18 10:12:57 +0530

    change

---
+++ application/views/fontend/employer/questionbank.php
@@ -0,0 +1,271 @@
+<div class="container-fluid main-d">
+	<div class="container">
+        <div class="col-md-12">
+        	<div class="col-md-3">
+            
+            <aside id="left-panel" style="margin-top:75px;
+                 margin-left: 14px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
+            z-index: 999;vertical-align:baseline;">
+            <div class="inner-left-pannel">
+                
+                
+                <!-- WHAT MOVES START -->
+                <div class="my-moving-parts">
+                    <div class="my-param-content"></div>
+                    <div class="my-normal-content">
+                       <div class="inner-tabs-navigation" data-active="menu">
+                        </div>
+                        <div class="inner-tabs">
+                            <div class="account-tab">
+                                <div class="language-selection" title="Change language">
+                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                                                      <i class="flag flag-us"></i> 
+                                                         </a>
+                                                     </span>
+                                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="menu-tab">
+                               
+                                
+                                
+                                <nav class="menu-principal">
+                                
+                                    <ul class="menu-principal-list" style="">
+                                         <li class="active">
+                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                            <span class="icon-container">
+                                                 <i class="fas fa-tachometer-alt"></i>
+                                            </span>
+                                            <span class="text item">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                            <span class="icon-container">
+                                                <i class="fas fa-user-alt"></i>
+                                           </span>
+                                        <span class="text item">CV Bank</span>
+                                              </a>
+                                       </li>
+                                       
+                                      <li>
+                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                                       <span class="icon-container">
+                                         <i class="fas fa-phone-volume"></i>
+                                     </span>
+                                        <span class="text item">Post New Job</span>
+                                     </a>
+                                      </li>
+                                         <li>
+                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                            <span class="icon-container">
+                                              <i class="fas fa-filter"></i>
+                                              </span>
+                                        <span class="text item">Active Job Post</span>
+
+                                          </a>
+                                        </li>
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Pending Jobs</span>
+                                              </a>
+                                         </li>
+                                            
+                                          
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Question Bank</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Employee Management</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Company / Recruiter</span>
+                                              </a>
+                                         </li>
+                                         
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">OceanChamp</span>
+                                              </a>
+                                         </li>
+                                         
+                                         
+                                     </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- WHAT MOVES END -->
+            </div>
+                </aside>
+            </div>
+            
+            <div class="col-md-9 question-bank">
+            	<div class="header-bookbank">
+                		Question Bank
+                 </div>
+                	
+                <div class="question-box">
+                	<div class="panel-heading"> 
+                    <span style="color:#949694;">Java<span>(Core java)</span></span>                 
+                  <p class="question">
+Which among the following statements regarding the Surrogacy (Regulation)
+bill, 2016 is incorrect ?
+                  </p>
+             </div><!--.panel-heading-->
+        
+                    <div class = "panel-body">
+                    <ul class = "list-group">
+                        <li class = "list-group-item">
+                              <div class="checkbox">
+                                <input type="checkbox" id="checkbox" />
+                                <label for="checkbox">
+                                    1 and 2 only
+                                </label>
+                            </div>
+                        </li>
+                        <li class = "list-group-item">
+                              <div class="checkbox">
+                                <input type="checkbox" id="checkbox" />
+                                <label for="checkbox">
+                                    2 and 3 only
+                                </label>
+                            </div>
+                        </li>
+                        <li class = "list-group-item">
+                              <div class="checkbox">
+                                <input type="checkbox" id="checkbox" />
+                                <label for="checkbox">
+                                    1 and 3 only
+                                </label>
+                            </div>
+                        </li>
+                        <li class = "list-group-item">
+                              <div class="checkbox">
+                                <input type="checkbox" id="checkbox" />
+                                <label for="checkbox">
+                                    1, 2 and 3
+                                </label>
+                            </div>
+                        </li>
+                       </ul>
+                     </div>
+               		
+                    	<p>
+  <a class="toggle btn " href="#example">show answer</a>
+</p>
+
+<div class="toggle-content" id="example">
+  Here's some text we want to toggle visibility of.
+</div>
+                    
+                     <div class="btn-group">
+      <a href="#" class="btn btn-inverse disabled"><i class="far fa-edit"></i></a>
+      <a href="#" class="btn btn-inverse disabled"><i class="fas fa-trash-alt"></i></a>
+  </div>                            
+				</div>  
+        
+            
+</div>
+</div>
+</div>
+<script>
+
+// Show an element
+var show = function (elem) {
+
+	// Get the natural height of the element
+	var getHeight = function () {
+		elem.style.display = 'block'; // Make it visible
+		var height = elem.scrollHeight + 'px'; // Get it's height
+		elem.style.display = ''; //  Hide it again
+		return height;
+	};
+
+	var height = getHeight(); // Get the natural height
+	elem.classList.add('is-visible'); // Make the element visible
+	elem.style.height = height; // Update the max-height
+
+	// Once the transition is complete, remove the inline max-height so the content can scale responsively
+	window.setTimeout(function () {
+		elem.style.height = '';
+	}, 350);
+
+};
+
+// Hide an element
+var hide = function (elem) {
+
+	// Give the element a height to change from
+	elem.style.height = elem.scrollHeight + 'px';
+
+	// Set the height back to 0
+	window.setTimeout(function () {
+		elem.style.height = '0';
+	}, 1);
+
+	// When the transition is complete, hide it
+	window.setTimeout(function () {
+		elem.classList.remove('is-visible');
+	}, 350);
+
+};
+
+// Toggle element visibility
+var toggle = function (elem, timing) {
+
+	// If the element is visible, hide it
+	if (elem.classList.contains('is-visible')) {
+		hide(elem);
+		return;
+	}
+
+	// Otherwise, show it
+	show(elem);
+	
+};
+
+// Listen for click events
+document.addEventListener('click', function (event) {
+
+	// Make sure clicked element is our toggle
+	if (!event.target.classList.contains('toggle')) return;
+
+	// Prevent default link behavior
+	event.preventDefault();
+
+	// Get the content
+	var content = document.querySelector(event.target.hash);
+	if (!content) return;
+
+	// Toggle the content
+	toggle(content);
+
+}, false);
+</script>
