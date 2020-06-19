3b44bc802212172cdfca6de6320ea577089c0904
supriyaL <supriya@itdivine.in>
2020-06-19 15:02:43 +0530

    change

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -219,11 +219,11 @@ input{
                       if(!empty($job_info->experience)){ echo $job_info->experience;
                        }
                     ?>" autocomplete="off" required> -->
-                    <div class=" col-md-2 formrow" style="margin-top:37px;">
+                    <div class="formrow" style="margin-top:37px;">
                    
                     	 <input class="form-control" min="1" type="text" />
                     </div>
-                     <div class="col-md-2 formrow">
+                     <div class=" formrow">
                     	 <input class="form-control" min="1" type="text" />
                     </div>    
                                      
