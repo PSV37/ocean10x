7039f0f62d9c4b4cc2640ea3d5dfdc11f4af1640
supriyaL <supriya@itdivine.in>
2020-06-19 14:32:38 +0530

    change

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -294,14 +294,14 @@ section {
           
 
           <section>
-             <div class="col-md-3 col-sm-12">
+             <div class="col-md-2 col-sm-12">
                   <div class="formrow">
                      <label class="control-label ">Salary Range<span class="required"> * </span> </label>
                    
                   <input class="form-control" min="1" type="number" maxlength="2" name="salary range" placeholder="" />
                </div>
-               </div>
-                <div class="col-md-3 col-sm-12">
+               </div>     
+                <div class="col-md-2 col-sm-12">
                <div class="formrow" style="margin-top:37px;">
 
                <input class="form-control" min="1" type="number" maxlength="2" name="salary range"  placeholder=""/>
