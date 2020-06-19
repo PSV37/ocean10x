f8ec4d902d6e0263a6f057c5594b511106c50930
unknown <santu@edunetsol.com>
2020-06-19 17:57:55 +0530

    new

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -295,18 +295,18 @@ span.options_beni {
 
                  <div class="col-md-3 col-sm-12">
                   <div class="formrow">
-                    <label class="control-label ">salary Range<span class="required"> *</span> </label>
+                    <label class="control-label ">Salary Range<span class="required"> *</span> </label>
                  <!--  <input class="form-control" type="text" name="experience" maxlength="2" value="<?php 
                       if(!empty($job_info->experience)){ echo $job_info->experience;
                        }
                     ?>" autocomplete="off" required> -->
                     <div class="col-md-3 formrow" style="width:80px;margin-left:-14px;">
                    
-                       <input class="form-control allownumericwithdecimal" min="1" maxlength="2" type="text" name="sal_from" />
+                       <input class="form-control allownumericwithdecimal" min="1" maxlength="2" type="text" name="exp_from" />
                     </div>
                     
                      <div class="col-md-3 formrow" style="width:80px;margin-left:-19px;">
-                       <input class="form-control allownumericwithdecimal" min="1" maxlength="2" type="text" name="sal_to" />
+                       <input class="form-control allownumericwithdecimal" min="1" maxlength="2" type="text" name="exp_to" />
                     </div>    
                                      
                   </div>    
