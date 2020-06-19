82a330d5a9012a4cd3c3503a9c99382160a0d431
unknown <santu@edunetsol.com>
2020-06-19 11:59:42 +0530

    new

--- application/views/fontend/employer/posted_jobs.php
+++ application/views/fontend/employer/posted_jobs.php
@@ -243,43 +243,43 @@ $company_profile_id = $this->session->userdata('company_profile_id');
                  <?php if (!empty($company_active_jobs)): foreach ($company_active_jobs as $v_companyjobs) : ?>
                   <div class="card">
                     <div class="front">
-                      <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Rivian_company_logo.jpg" style="height:40px; width:40px;border-radius:5px;float:left" />
+                      <img src="<?php echo base_url() ?>upload/<?php echo $this->company_profile_model->company_logoby_id($company_profile_id);?>" style="height:40px; width:40px;border-radius:5px;float:left" />
                       <div class="job-info">
-                        <p class="job_title">Software development Leader</p>
+                        <p class="job_title"><?php echo $v_companyjobs->job_title; ?></p>
                       </div>   
                       <div class="icon-info">
                         <li class="left-icon-title"><i class="fas fa-map-marker-alt"></i></li>
-                        <li class="right-icon-title"> &emsp;Mumbai ,pune</li>
+                        <li class="right-icon-title"> &emsp;<?php echo $v_companyjobs->city_id; ?></li>
                         <li class="left-icon-title"><i class="fas fa-briefcase"></i></li>
-                        <li class="right-title" style="width:100%;"> &emsp;2 years(experience)</li>
+                        <li class="right-title" style="width:100%;"> &emsp;<?php echo $v_companyjobs->experience; ?>(experience)</li>
                         <div class="clear"></div>
                       </div>
                       <div class="following-info">
                         <li class="left-title"
-                          >Job Roll</li><li class="right-title">&nbsp;: java Developer</li>
-                          <li class="left-title">Engagement</li><li class="right-title">&nbsp;: Full time</li>
-                          <li class="left-title">Domain</li><li class="right-title">&nbsp;:IT</li>
+                          >Job Roll</li><li class="right-title">&nbsp;: <?php echo $v_companyjobs->job_role; ?></li>
+                          <li class="left-title">Engagement</li><li class="right-title">&nbsp;: <?php echo $v_companyjobs->job_nature_name; ?></li>
+                          <li class="left-title">Domain</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->job_category_name; ?></li>
                           <li class="left-title">Role Type </li><li class="right-title">&nbsp;:</li>
                           <li class="left-title">Dummy1</li><li class="right-title">&nbsp;:</li>
                           <li class="left-title">Dummy2</li><li class="right-title">&nbsp;:</li>
                           <div class="clear"></div>
                       </div>
                       <div class="following-info2">
-                        <li class="left-title">Education</li><li class="right-title">&nbsp;: M Tech</li>
-                        <li class="left-title">Specialization</li><li class="right-title">&nbsp;:</li>
-                        <li class="left-title">experience</li><li class="right-title">&nbsp;:</li>
+                        <li class="left-title">Education</li><li class="right-title">&nbsp;: <?php echo $v_companyjobs->education; ?></li>
+                        <li class="left-title">Specialization</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->education_specialization; ?></li>
+                        <li class="left-title">experience</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->experience; ?></li>
                         <li class="left-title">Joining ETA</li><li class="right-title">&nbsp;:30 days</li>
-                        <li class="left-title">Benifits</li><li class="right-title">&nbsp;: </li>
-                        <li class="left-title">Skill sets</li><li class="right-title">&nbsp;: </li>
+                        <li class="left-title">Benifits</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->benefits; ?> </li>
+                        <li class="left-title">Skill sets</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->skills_required; ?> </li>
                         <div class="clear"></div>
                       </div>
                       <div class="following-info3">
                         <li class="left-title">JD attached&nbsp;<i class="fas fa-link"></i></li><li class="right-title">&nbsp;: yes</li>
-                        <li class="left-title">CTC</li><li class="right-title">&nbsp;:</li>
-                        <li class="left-title">Vacancies</li><li class="right-title">&nbsp;: 4</li>
-                        <li class="left-title">Ocean Test</li><li class="right-title">&nbsp;:yes</li>
-                        <li class="left-title">Published on</li><li class="right-title">&nbsp;:10th july</li>
-                        <li class="left-title">Job expiry</li><li class="right-title">&nbsp;:12th july</li>
+                        <li class="left-title">CTC</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->salary_range; ?></li>
+                        <li class="left-title">Vacancies</li><li class="right-title">&nbsp;: <?php echo $v_companyjobs->no_jobs; ?></li>
+                        <li class="left-title">Ocean Test</li><li class="right-title">&nbsp;:<?php echo $v_companyjobs->is_test_required; ?></li>
+                        <li class="left-title">Published on</li><li class="right-title">&nbsp;:<?php if(!is_null($v_companyjobs->created_at)) { echo date('F j Y',strtotime($v_companyjobs->created_at)); } ?></li>
+                        <li class="left-title">Job expiry</li><li class="right-title">&nbsp;:<?php if(!is_null($v_companyjobs->job_deadline)) { echo date('F j Y',strtotime($v_companyjobs->job_deadline)); } ?></li>
                         <div class="clear"></div>
                       </div>
                                    
