a9e85ffdff813deb3297659f2940518c93dee802
unknown <santu@edunetsol.com>
2020-06-19 16:28:59 +0530

    new

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -294,36 +294,6 @@ span.options_beni {
               </div>
 
                
-             <div class="col-md-2 col-sm-12">
-                  <div class="formrow">
-                     <label class="control-label ">Salary Range<span class="required"> * </span> </label>
-                   
-                  <input class="form-control" min="1" type="number" maxlength="2" name="salary range" placeholder="" />
-               </div>
-              
-               
-               </div>    
-              To <div class="col-md-2 col-sm-12">
-                <div class="formrow" style="margin-top:37px;">
-
-             <input class="form-control" min="1" type="number" maxlength="2" name="salary range"  placeholder=""/>
-        </div>
-                </div>
-               
-             <div class="col-md-6 col-sm-12">
-                  <div class="formrow">
-                    <label></label>
-                    <input type="radio" name="salary_range">
-
-                   
-                   <!--  <input type="text" id="salary_range" name="salary_range" onkeyup="javascript:changeSalary();" class="form-control" min="1" autocomplete="off"><?php echo form_error('salary_range'); ?> -->
-                  </div>
-                </div>
-
-           
-             
-              
-
              
                       
               </div>
@@ -357,36 +327,6 @@ span.options_beni {
           <section>
 
             
-
-             <div class="col-md-2 col-sm-12">
-                  <div class="formrow">
-                     <label class="control-label ">Salary Range<span class="required"> * </span> </label>
-                   
-                  <input class="form-control" min="1" type="number" maxlength="2" name="salary range" placeholder="" />
-               </div>
-              
-               
-               </div>    
-               <div class="col-md-2 col-sm-12">
-                <div class="formrow" style="margin-top:37px;">
-
-             <input class="form-control" min="1" type="number" maxlength="2" name="salary range"  placeholder=""/>
-				</div>
-                </div>
-               
-             <div class="col-md-6 col-sm-12">
-                  <div class="formrow">
-                    <label></label>
-                    <input type="radio" name="salary_range">
-
-                   
-                   <!--  <input type="text" id="salary_range" name="salary_range" onkeyup="javascript:changeSalary();" class="form-control" min="1" autocomplete="off"><?php echo form_error('salary_range'); ?> -->
-                  </div>
-                </div>
-
-           
-             
-              
 
                  <div class="col-md-12 col-sm-12">
                    <div class="formrow">
