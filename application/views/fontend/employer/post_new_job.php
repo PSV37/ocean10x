8795d30d97d6499d0f37338b3535628e33fa8203
supriyaL <supriya@itdivine.in>
2020-06-19 15:22:16 +0530

    change

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -327,10 +327,10 @@ input{
                   <input class="form-control" min="1" type="number" maxlength="2" name="salary range" placeholder="" />
                </div>
                </div>     
-               | <div class="col-md-2 col-sm-12">
+                <div class="col-md-2 col-sm-12">
                <div class="formrow" style="margin-top:37px;">
 
-               <input class="form-control" min="1" type="number" maxlength="2" name="salary range"  placeholder=""/>
+              | <input class="form-control" min="1" type="number" maxlength="2" name="salary range"  placeholder=""/>
 				</div>
                 </div>
                
