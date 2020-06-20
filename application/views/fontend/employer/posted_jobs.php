20cedd502ffcb44d80fc63ad4ffc225dd77d38dd
unknown <santu@edunetsol.com>
2020-06-20 12:27:10 +0530

    new

--- application/views/fontend/employer/posted_jobs.php
+++ application/views/fontend/employer/posted_jobs.php
@@ -324,8 +324,8 @@ $company_profile_id = $this->session->userdata('company_profile_id');
                         <div class="clear"></div>
                       </div>
 
-                          <!-- <div id="skills"> -->
-                             <span id="skills">Skill sets</span>:
+                          <div id="skills">
+                             <span>Skill sets</span>:
                              <?php 
                              $sk=$v_companyjobs->skills_required;
                              if (isset($sk) && !empty($sk)) {
@@ -346,8 +346,8 @@ $company_profile_id = $this->session->userdata('company_profile_id');
                             
                               ?>
 
-                           <!--  <div class="clear"></div>
-                          </div> -->         
+                            <div class="clear"></div>
+                          </div>         
                                    
                 <button class="detail-btn">details</button>
                <?php  if ($v_companyjobs->job_deadline > date('Y-m-d')){
