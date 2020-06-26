d134a34af2076dd8adf67b2471b01e1938314756
supriyaL <supriya@itdivine.in>
2020-06-26 12:14:43 +0530

    change

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -869,7 +869,7 @@ li.bullet {
     <div id="menu1" class="tab-pane fade">
     
       <div class="education_header" style="position:relative;">
-          <img src="https://www.jetchill.com/wp-content/uploads/2017/05/blog-header-background.jpg" style="width:100%;position:relative;"></img>
+          <img src="" style="width:100%;position:relative;"></img>
             <div class="icon-education" style="position:absolute;bottom:23px;right:53%;">
             <i class="fas fa-graduation-cap edu-i"></i>
             </div>
