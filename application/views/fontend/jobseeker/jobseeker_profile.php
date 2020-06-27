5e54122d95dc1655b93449a1e299532bb89da480
unknown <santu@edunetsol.com>
2020-06-27 14:44:47 +0530

    new

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -52,8 +52,8 @@
             <div class="tab-content">
                <div id="menu5" class="tab-pane fade">
                   <div class="education_header" style="position:relative;">
-                     <img src="https://www.sassm.in/education/images/blog-header.jpg" style="width:100%;position:relative;height:65px;">
-                     <div class="icon-education" style="position:absolute;bottom:23px;right:53%;">
+                    <img src="https://www.sassm.in/education/images/blog-header.jpg" style="width:100%;position:relative;height:65px;">
+                    <div class="icon-education" style="position:absolute;bottom:23px;right:53%;">
                         <i class="fas fa-graduation-cap edu-i"></i>
                      </div>
                   </div>
