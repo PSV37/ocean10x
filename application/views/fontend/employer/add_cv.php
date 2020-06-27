4b457ddd8e9e1cd0fcea9744a6e4af2dfbb85552
unknown <santu@edunetsol.com>
2020-06-27 15:24:25 +0530

    new

--- application/views/fontend/employer/add_cv.php
+++ application/views/fontend/employer/add_cv.php
@@ -184,6 +184,9 @@
          </div>
      </div>
  </div>
+ <script src="<?php echo base_url(); ?>assets/js/jquery-3.3.1.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>asset/js/jquery-ui.js" type="text/javascript"></script>
 <script type="text/javascript">
       $(function() {
       $("#candidate_email").autocomplete({
