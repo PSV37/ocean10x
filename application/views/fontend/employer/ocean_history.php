15e35a6156b18d810f167b4f82dc541ac55bad8b
supriyaL <supriya@itdivine.in>
2020-06-30 10:29:24 +0530

    change

---
+++ application/views/fontend/employer/ocean_history.php
@@ -0,0 +1,363 @@
+<div class="container-fluid main-d">
+	<div class="container">
+        <div class="col-md-12">
+        	<div class="col-md-3">
+            
+            <aside id="left-panel" style="margin-top:75px;
+                 margin-left: 14px;height:auto; border-right: 1px solid rgba(240, 240, 240, 0.3);box-shadow: 2px 2px 4px 0px   #00000033;position: fixed;
+            z-index: 999;vertical-align:baseline;">
+            <div class="inner-left-pannel">
+                
+                
+                <!-- WHAT MOVES START -->
+                <div class="my-moving-parts">
+                    <div class="my-param-content"></div>
+                    <div class="my-normal-content">
+                       <div class="inner-tabs-navigation" data-active="menu">
+                        </div>
+                        <div class="inner-tabs">
+                            <div class="account-tab">
+                                <div class="language-selection" title="Change language">
+                                                <div class="btn-header transparent pull-right dropdown" style="margin-top: -1px;">
+                                                    <span><a href="#" class="dropdown-toggle locale" data-toggle="dropdown">
+                                                      <i class="flag flag-us"></i> 
+                                                         </a>
+                                                     </span>
+                                                </div>
+                                </div>
+                            </div>
+                            
+                            <div class="menu-tab">
+                               
+                                
+                                
+                                <nav class="menu-principal">
+                                
+                                    <ul class="menu-principal-list" style="">
+                                         <li class="active">
+                                             <a data-dl-view="true" data-dl-title="Dashboard" href="#">
+                                            <span class="icon-container">
+                                                 <i class="fas fa-tachometer-alt"></i>
+                                            </span>
+                                            <span class="text item">Dashboard</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                             <a data-dl-view="true" data-dl-title="My profile" href="/candidate/detail">
+                                            <span class="icon-container">
+                                                <i class="fas fa-user-alt"></i>
+                                           </span>
+                                        <span class="text item">CV Bank</span>
+                                              </a>
+                                       </li>
+                                       
+                                      <li>
+                                     <a data-dl-view="true" data-dl-title="Contacts" href="/candidate">
+                                       <span class="icon-container">
+                                         <i class="fas fa-phone-volume"></i>
+                                     </span>
+                                        <span class="text item">Post New Job</span>
+                                     </a>
+                                      </li>
+                                         <li>
+                                         <a data-dl-view="true" data-dl-title="Recruitments" href="/campaign">
+                                            <span class="icon-container">
+                                              <i class="fas fa-filter"></i>
+                                              </span>
+                                        <span class="text item">Active Job Post</span>
+
+                                          </a>
+                                        </li>
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Pending Jobs</span>
+                                              </a>
+                                         </li>
+                                            
+                                          
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Question Bank</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Employee Management</span>
+                                              </a>
+                                         </li>
+                                         
+                                          <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">Company / Recruiter</span>
+                                              </a>
+                                         </li>
+                                         
+                                         <li>
+                                            <a data-dl-view="true" data-dl-title="Mobility" href="/jobprofile/generate">
+                                            <span class="icon-container">
+                                              <i class="fas fa-map-signs"></i>
+                                              </span>
+                                        <span class="text item">OceanChamp</span>
+                                              </a>
+                                         </li>
+                                         
+                                         
+                                     </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- WHAT MOVES END -->
+            </div>
+                </aside>
+            </div>
+            
+            <div class="col-md-6 employ_tab">
+            	
+          
+  
+
+  <ul id="myTabs" class="nav nav-pills nav-justified" role="tablist" data-tabs="tabs">
+    <li class="active"><a href="#Commentary" data-toggle="tab">New Bussness Connnection</a></li>
+    <li><a href="#Videos" data-toggle="tab">My Bussness Connnection</a></li>
+   
+  </ul>
+  
+  
+  
+
+  
+  
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane fade in active" id="Commentary">
+    <!---
+    
+    <div class="pull-left">
+<select 0id="campaign_campaignRules_country_0_countrySelect" name="campaign[campaignRules][country][0][countrySelect][]" multiple="multiple" style="display: none;">
+<option value="AF" selected="selected">Afghanistan</option>
+<option value="AL" selected="selected">Albania</option>
+<option value="DZ" selected="selected">Algeria</option>
+<option value="AS" selected="selected">American Samoa</option>
+<option value="AD" selected="selected">Andorra</option>
+<option value="AO" selected="selected">Angola</option>
+<option value="AI" selected="selected">Anguilla</option>
+<option value="AQ" selected="selected">Antarctica</option>
+<option value="AG" selected="selected">Antigua and Barbuda</option>
+<option value="AR" selected="selected">Argentina</option>
+<option value="AM" selected="selected">Armenia</option>
+<option value="AW" selected="selected">Aruba</option>
+<option value="AU" selected="selected">Australia</option>
+<option value="AT" selected="selected">Austria</option>
+<option value="AZ" selected="selected">Azerbaijan</option>
+<option value="BS" selected="selected">Bahamas</option>
+<option value="BH" selected="selected">Bahrain</option>
+<option value="BD" selected="selected">Bangladesh</option>
+<option value="BB" selected="selected">Barbados</option>
+<option value="BY" selected="selected">Belarus</option>
+<option value="BE" selected="selected">Belgium</option>
+<option value="BZ" selected="selected">Belize</option>
+<option value="BJ" selected="selected">Benin</option>
+<option value="AX" selected="selected">Åland Islands</option>
+
+
+</select>
+
+<div class="btn-group" style="width: 300px;"><button type="button" class="multiselect dropdown-toggle form-control btn btn-default" data-toggle="dropdown" style="font-size:12px;background-color: aliceblue;border:none;width: 200px; overflow: hidden; text-overflow: ellipsis;">
+
+<span class="multiselect-selected-text">Search</span>
+ <b class="caret"></b></button>
+ <ul id="country-multiselect" class="multiselect-container dropdown-menu" style="padding:0px 10px;max-height: 250px; overflow-y: auto; overflow-x: hidden;">
+ <li class="multiselect-item filter" value="0">
+ <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+ <input class="form-control multiselect-search" type="text" placeholder="Search">
+ <span class="input-group-btn">
+ <button class="btn btn-default multiselect-clear-filter" type="button"><i class="glyphicon glyphicon-remove"></i></button>
+ </span></div></li><li class="multiselect-item multiselect-all active"><a tabindex="0" class="multiselect-all"><label class="checkbox"><input type="checkbox" value="multiselect-all"> Select all</label>
+ </a></li>
+ <li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="AF"> Afghanistan</label></a></li><li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="AL"> Albania</label></a></li>
+ <li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="DZ"> Algeria</label></a></li>
+ <li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="AS"> American Samoa</label></a></li>
+ <li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="AD"> Andorra</label></a></li><li class="active filter-hidden" style="display: none;"><a tabindex="0"><label class="checkbox"><input type="checkbox" value="AO"> Angola</label></a></li>
+</ul></div></div>
+    --->
+    <div class="row">
+    <div class="col col-sm-3">                                 
+						<select class="form-control" name="exp" id="exp_header">
+							<option>Skills</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option><option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option><option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option><option value="40">40</option><option value="41">41</option><option value="42">42</option><option value="43">43</option><option value="44">44</option><option value="45">45</option><option value="46">46</option><option value="47">47</option><option value="48">48</option><option value="49">49</option><option value="50">50</option>                                         
+						</select>                         
+					</div>
+                    
+              <div class="col col-sm-3">                                 
+						<div class="placeholder_cmmn" id="auto_loc_wrap_srp">    
+ <input type="text" class="form-control" tabindex="3" monstab="3" placeholder="Location" onfocus="if(this.value==&quot;&quot;)this.value=&quot;&quot;" onblur="if(this.value=='')this.value=''" id="lmy_header" name="lmy">
+ </div>
+
+                                                     
+					</div>  
+                    
+                    
+                    
+                    <div class="col col-sm-3">                                 
+						<button class="saerch_btn">search</button>
+
+                                                     
+					</div>   
+       </div>            
+                    
+                        
+    
+    <div class="col-md-12" style="margin-top:30px;padding-right:0px;padding-left:0px;">
+    <!--
+    <div class="card1">
+    <div class="img-logo">
+    	<img src="https://www.consultnhire.com/upload/4ce62c1e36ede93d46de44f1de63c4f0.jpg" style="height:50px; width:50px;border-radius:5px;border:solid 1px #eae9e9b8;margin-right:15px;float:left;">
+    	</div>
+        <p class="title">somson</p>
+    		<div class="icon-info">
+                        <li class="left-icon-title"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></li>
+                        <li class="right-icon-title">  pune</li>
+                        <div class="clear"></div>
+                      </div>
+             <div class="icon-info2">
+                        <li class="left-icon-title">Since</li>
+                        <li class="right-icon-title">:&emsp;1980</li>
+                        <div class="clear"></div>
+               </div> 
+               
+               <div class="following-info">
+                        <li class="left-title">Job Roll</li><li class="right-title">&nbsp;: 1</li>
+                          <li class="left-title">Engagement</li>
+                          <li class="right-title">&nbsp;: Hourly</li>
+                          
+                          <div class="clear"></div>
+                      </div>
+               
+               
+               
+               
+                           
+    </div>
+    --->
+    <label>
+                    <input type="checkbox" />
+                        <div class="card">
+                            <div class="front">
+                            	
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Rivian_company_logo.jpg" style="height:40px; width:40px;border-radius:5px;float:left" />
+                                
+                                <div class="job-info">
+                                	<p class="job_title">Software development Leader</p>
+                                    <div class="a"> 
+                                <li class="left-title" style="margin-left: 3px;width:27px"
+                                    ><i class="fas fa-map-marker-alt" aria-hidden="true"></i></li><li class="right-title" style="margin-left:23px;">&nbsp;: Mumbai</li>
+                                    </div>
+                                 
+                                </div>   
+                                
+                                  
+                                <div class="following-info">
+                                	<li class="left-title"
+                                    >Email</li><li class="right-title">&nbsp;: hgdgh@gmail.com</li>
+                                   
+                                     <li class="left-title">Contact</li><li class="right-title">&nbsp;: 9979799898</li>
+                                    
+
+                                     <li class="left-title">Domains</li><li class="right-title">&nbsp;:IT</li>
+                                     
+                                     
+                                      
+                                      <div class="clear"></div>
+                                   </div>
+                                   
+                                   <div class="following-info2">
+                                   <li class="left-title">Since</li><li class="right-title">&nbsp;:1980</li>
+                                	<li class="left-title">Hiring Level </li><li class="right-title">&nbsp;:</li>
+                                     <li class="left-title">Tetorries</li><li class="right-title">&nbsp;:Domastic</li>
+                                     
+                                     
+                                      
+                                      <div class="clear"></div>
+                                   </div>
+                                   
+                                   
+                                   
+                                  
+								<button class="detail-btn">Connect</button>
+                                 <button class="detail-btn">Send Message</button>
+								
+                                   
+                                    
+                               
+
+                                
+                                </div>   	
+                            
+                                    
+                             
+                             
+                            	
+                               
+                            
+                            
+                            </div>
+                            <div class="back"></div>
+                            
+                        </div>
+                </label>
+    
+    
+    
+    
+    </div>
+   
+   
+    <div role="tabpanel" class="tab-pane fade" id="Videos">
+    
+    <div class="table-responsive">
+            
+          </div>
+    
+    </div>
+    <div role="tabpanel" class="tab-pane fade" id="Events">
+    
+    
+    <div class="table-responsive">
+            
+          </div>
+    
+    </div>
+     <div role="tabpanel" class="tab-pane fade" id="music">
+    
+    
+    
+    
+    </div>
+  </div>
+
+
+
+
+
+
+            
+            
+            
+            
+            </div>
+		</div>
+     </div>
+</div>
