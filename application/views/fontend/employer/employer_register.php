c6f6067f28a29b285631c03b4b9716723f714295
supriyaL <supriya@itdivine.in>
2020-07-03 21:24:45 +0530

    change

--- application/views/fontend/employer/employer_register.php
+++ application/views/fontend/employer/employer_register.php
@@ -198,19 +198,19 @@
 
                        
                 
-        </ul>
+        </ul>    
       </div>
     </nav>
   </div>
 
 <ul class="sidenav" id="mobile-navbar">
-    <li><a href="#">MENÚ</a></li>
+    <li><a href="#">Home</a></li>
     <hr />
-    <li><a href="#">Inicio</a></li>
+    <li><a href="#">About Us</a></li>
     <li><a href="#">Portafolio</a></li>
-    <li><a href="#">Acerca de mí</a></li>
-    <li><a href="#">Contacto</a></li>
-    <li><a href="#" class="btn white indigo-text">Inciar sesión</a></li>
+    <li><a href="#">Training</a></li>
+    <li><a href="#">Post Resume</a></li>
+    <li><a href="#" class="btn white indigo-text">Contact Us</a></li>
   </ul>
 </div>       
           
