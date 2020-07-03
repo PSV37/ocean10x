ed9ef5474a7bab6787079901d6aa796d5478f574
supriyaL <supriya@itdivine.in>
2020-07-03 18:34:13 +0530

    change

--- application/views/fontend/index.php
+++ application/views/fontend/index.php
@@ -15,9 +15,10 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link href="index-css.css" rel="stylesheet" type="text/css" />
 
-      <style>
+
+<style>
+
          .container{max-width:1260px !important;}
          .@media only screen and (min-width: 601px){
          .container {
@@ -168,225 +169,263 @@
          border-radius: 5px;     
          margin-bottom:18px;
          } 
-      </style>
-   </head>
-   <body>
-      <div class="navbar-fixed">
-         <nav class="nav-wrapper indigo">
-            <div class="container">
-               <a href="#" class="brand-logo"><img src="http://www.tele-kinetics.com/assets/img/logo.png" style="width:152px;margin-top:-4px;"></a>
-               <a href="#" class="sidenav-trigger" data-target="mobile-navbar">
-               <i class="material-icons">menu</i>  
-               </a>
-               <ul class="right hide-on-med-and-down" >
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Training</a></li>
-                  <li><a href="#">Post Resume</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li class="yellow" style="border-radius: 20px 0px 0px 20px;margin-left:60px;"><a href="<?php echo site_url('employer_login') ?>">Employer Login</a></li>
-                  <li class="yellow2" style="border-radius: 0px 20px 20px 0px;"><a href="<?php echo site_url('seeker-login') ?>">Candidate Login</a></li>
-               </ul>
-            </div>
-         </nav>
-      </div>
-      <ul class="sidenav" id="mobile-navbar">
-         <li><a href="#">MENÚ</a></li>
-         <hr />
-         <li><a href="#">Inicio</a></li>
-         <li><a href="#">Portafolio</a></li>
-         <li><a href="#">Acerca de mí</a></li>
-         <li><a href="#">Contacto</a></li>
-         <li><a href="#" class="btn white indigo-text">Inciar sesión</a></li>
-      </ul>
+      </style>    
+
+</head>  
+<body>
+<div class="container_fluid">
+<div class="navbar-fixed">
+    <nav class="nav-wrapper indigo">
       <div class="container">
-         <div class="col-md-12">
-            <div class="col-sm-6" style="padding:50px 0px;">
-               <h1 style="font-size: 29px; margin-bottom: 50px;margin-top:60px;">Welcome to the ocean opportunities !</h1>
-               <div class="anchor_link">
-                  <ul>
-                     <li>
-                        <div class="m_icon">
-                           <i class="fas fa-users"></i>
-                        </div>
-                        <p class="small_t">People Search</p>
-                     </li>
-                     <li>
-                        <div class="m_icon">
-                           <i class="fas fa-book-reader"></i>
-                        </div>
-                        <p class="small_t">Search Skills</p>
-                     </li>
-                     <li>
-                        <div class="m_icon">
-                           <i class="fas fa-briefcase"></i>
-                        </div>
-                        <p class="small_t">Job Search</p>
-                     </li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-sm-6" style="padding:50px 0px;">
-               <img src="https://image.freepik.com/free-vector/recruit-agent-analyzing-candidates_74855-4565.jpg" />
-            </div>
-         </div>
+        <a href="#" class="brand-logo"><img src="http://www.tele-kinetics.com/assets/img/logo.png" style="width:152px;margin-top:-4px;"></a>
+        <a href="#" class="sidenav-trigger" data-target="mobile-navbar">
+          <i class="material-icons">menu</i>  
+        </a>
+        <ul class="right hide-on-med-and-down" >
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Training</a></li>
+          <li><a href="#">Post Resume</a></li>  
+         
+           <li><a href="#">Contact Us</a></li>
+		
+        <li class="yellow" style="border-radius: 20px 0px 0px 20px;margin-left:60px;"><a href="https://www.consultnhire.com/employer_login">Employer Login</a></li>
+       <li class="yellow2" style="border-radius: 0px 20px 20px 0px;"><a href="https://www.consultnhire.com/employer_login">Candidate Login</a></li>
+
+                  
+            
+        </ul>
       </div>
-      <div class="container-fluid back_com">
-         <div class="container">
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">IT</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">Manufacturing</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">Retail</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">Automobile</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">IT</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-            <div class="col-md-2 ">
-               <div class="head">
-                  <p style="margin-top:20px;color:#000;">Reatail</p>
-                  <ul>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                     <li>It Divine</li>
-                  </ul>
-               </div>
-            </div>
-         </div>
-      </div>
-      <div class="container">
-         <div class="col-md-6">
-            <img src="https://image.freepik.com/free-vector/illustrated-corporatebusiness-people_53876-28562.jpg"/>
-         </div>
-         <div class="col-md-6">
-            <p style="margin-top:60px;color:#18c5bd;">LMS</p>
-            <h1 style="font-size: 29px; margin-bottom: 50px;">Enhance Your Skills On The Ocean Of Content !</h1>
-         </div>
-      </div>
-      <div class="container" style="border-top:solid 1px #f2f2f2ba;">
-         <div class="col-md-6">
-            <h1 style="font-size: 21px; margin-bottom:25px;margin-top:50px;">Looking For Professionals ??</h1>
-            <button class="your_job">Post Your Job</button>
-         </div>
-         <div class="col-md-6">
-            <img src="https://image.freepik.com/free-vector/email-messaging-email-marketing-campaign_183665-8.jpg" style="width:400px;margin-left:180px;"/>
-         </div>
-      </div>
-      <div class="container-fluid" style="background-color:#eaf8f9;">
-         <div class="container">
-            <div class="col-md-6">
-               <img src="https://image.freepik.com/free-vector/happy-character-winning-prize-with-flat-design_23-2147894434.jpg" style="width:290px;margin-left: 110px;"/>
-            </div>
-            <div class="col-md-6">
-               <h1 style="font-size: 24px; margin-bottom: 50px;margin-top:112px;">Looking To Become an OCEANCHAMP !</h1>
-            </div>
-         </div>
-      </div>
-      <div class="container" style="padding:55px 0px;">
-         <div class="col-md-3">
-            <div class="footer1">
-               <h5 style="font-weight: 600;">About ConsultnHire</h5>
-               <p style="color: #6d6c6c;font-size: 12px;letter-spacing: 1px;text-align: justify;line-height:2;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
-            </div>
-         </div>
-         <div class="col-md-3">
-            <div class="footer1">
-               <h5 style="font-weight: 600;">Quick Links</h5>
-               <li>Home</li>
-               <li>Search Job</li>
-               <li>View All Job</li>
-               <li>Contact Us</li>
-               <li>Report a bug/Abuse</li>
-            </div>
-         </div>
-         <div class="col-md-3">
-            <div class="footer1">
-               <h5 style="font-weight: 600;">Quick Links</h5>
-               <li>About Company</li>
-               <li>My Account</li>
-               <li>Help</li>
-               <li>Terms Of Use</li>
-               <li>Privercy Commitment</li>
-            </div>
-         </div>
-         <div class="col-md-3">
-            <div class="footer1 icons_w">
-               <h5 style="font-weight: 600;">Contact Info</h5>
-               <li><span><i class="fab fa-linkedin-in"></i></span>Linked</li>
-               <li><span><i class="far fa-paper-plane"></i></span>info@Consultnhire.com</li>
-               <li><span><i class="fas fa-mobile-alt"></i></span>7865342789</li>
-               <li><span><i class="fab fa-linkedin-in"></i></span>Contact Us</li>
-               <li><span><i class="fab fa-facebook-f"></i></span>Facebook</li>
-            </div>
-         </div>
-      </div>
->>>>>>> cb7cbef135bb4d796aa0454a93f0ccecdc880e90
-      <div class="container">
-         <p class="copyright" style="text-align:center;font-size:12px;">Copyright © 2020 ConsultnHire. All rights reserved.</p>
-      </div>
-      <!-- Compiled and minified JavaScript -->
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-      <script>
-         // Show/Hide sidenav mobile
-         document.addEventListener("DOMContentLoaded", function () {
-         $(document).ready(function () {
-           $(".sidenav").sidenav();
-         });
-         });
-      </script>
-   </body>
+    </nav>
+  </div>
+
+<ul class="sidenav" id="mobile-navbar">
+    <li><a href="#">MENÚ</a></li>
+    <hr />
+    <li><a href="#">Inicio</a></li>
+    <li><a href="#">Portafolio</a></li>
+    <li><a href="#">Acerca de mí</a></li>
+    <li><a href="#">Contacto</a></li>
+    <li><a href="#" class="btn white indigo-text">Inciar sesión</a></li>
+  </ul>
+</div>  
+ 
+ 
+<div class="continer">
+<div class="col-sm-6" style="padding:50px 0px;">
+<h1 style="font-size: 29px; margin-bottom: 50px;margin-top:60px;">Welcome to the ocean opportunities !</h1>
+<div class="anchor_link">
+<ul>
+<li>
+<div class="m_icon">
+<i class="fas fa-users"></i>
+</div>
+<p class="small_t">People Search</p>
+</li>
+<li>
+<div class="m_icon">
+<i class="fas fa-book-reader"></i>
+</div>
+<p class="small_t">Search Skills</p>
+
+</li>
+<li>
+<div class="m_icon">
+<i class="fas fa-briefcase"></i>
+</div>
+<p class="small_t">Job Search</p>
+
+</li>
+</ul>
+
+</div>
+
+
+
+</div>
+<div class="col-sm-6" style="padding:50px 0px;">
+<img src="https://image.freepik.com/free-vector/recruit-agent-analyzing-candidates_74855-4565.jpg" />
+
+</div>
+
+</div>
+ 
+ 
+ <div class="container-fluid back_com">
+<div class="container">
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">IT</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">Manufacturing</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">Retail</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">Automobile</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">IT</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+<div class="col-md-2 ">
+<div class="head">
+<p style="margin-top:20px;color:#000;">Reatail</p>
+<ul>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+<li>It Divine</li>
+
+</ul>
+</div>
+</div>
+
+</div>
+</div>
+ 
+ <div class="container">
+<div class="col-md-6">
+<img src="https://image.freepik.com/free-vector/illustrated-corporatebusiness-people_53876-28562.jpg"/>
+</div>
+<div class="col-md-6">
+<p style="margin-top:60px;color:#18c5bd;">LMS</p>
+<h1 style="font-size: 29px; margin-bottom: 50px;">Enhance Your Skills On The Ocean Of Content !</h1>
+</div>
+</div>   
+
+
+<div class="container" style="border-top:solid 1px #f2f2f2ba;">
+<div class="col-md-6">
+<h1 style="font-size: 21px; margin-bottom:25px;margin-top:50px;">Looking For Professionals ??</h1>
+<button class="your_job">Post Your Job</button>
+</div>
+<div class="col-md-6">
+<img src="https://image.freepik.com/free-vector/email-messaging-email-marketing-campaign_183665-8.jpg" style="width:400px;margin-left:180px;"/>
+</div>
+</div>
+<div class="container-fluid" style="background-color:#eaf8f9;">
+<div class="container">
+<div class="col-md-6">
+<img src="https://image.freepik.com/free-vector/happy-character-winning-prize-with-flat-design_23-2147894434.jpg" style="width:290px;margin-left: 110px;"/>
+</div>
+<div class="col-md-6">
+<h1 style="font-size: 24px; margin-bottom: 50px;margin-top:112px;">Looking To Become an OCEANCHAMP !</h1>
+</div>   
+</div>
+</div>
+
+<div class="container" style="padding:55px 0px;">
+
+<div class="col-md-3">  
+<div class="footer1">
+<h5 style="font-weight: 600;">About ConsultnHire</h5>
+<p style="color: #6d6c6c;font-size: 12px;letter-spacing: 1px;text-align: justify;line-height:2;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+</div>
+</div>
+<div class="col-md-3">
+<div class="footer1">
+<h5 style="font-weight: 600;">Quick Links</h5>
+<li>Home</li>
+<li>Search Job</li>
+<li>View All Job</li>
+<li>Contact Us</li>
+<li>Report a bug/Abuse</li>
+</div>
+</div>
+<div class="col-md-3">
+<div class="footer1">
+<h5 style="font-weight: 600;">Quick Links</h5>
+<li>About Company</li>
+<li>My Account</li>
+<li>Help</li>
+<li>Terms Of Use</li>
+<li>Privercy Commitment</li>
+</div>
+</div>
+
+
+
+<div class="col-md-3">
+<div class="footer1 icons_w">   
+<h5 style="font-weight: 600;">Contact Info</h5>
+<li><span><i class="fab fa-linkedin-in"></i></span>Linked</li>
+<li><span><i class="far fa-paper-plane"></i></span>info@Consultnhire.com</li>
+<li><span><i class="fas fa-mobile-alt"></i></span>7865342789</li>
+<li><span><i class="fab fa-linkedin-in"></i></span>Contact Us</li>
+<li><span><i class="fab fa-facebook-f"></i></span>Facebook</li>
+</div>
+</div>
+
+</div>
+<div class="container">
+<p class="copyright" style="text-align:center;font-size:12px;">Copyright © 2020 ConsultnHire. All rights reserved.</p>
+
+</div>
+
+  
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
+  <script>
+  // Show/Hide sidenav mobile
+document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function () {
+    $(".sidenav").sidenav();
+  });
+});
+  </script>
+</body>
 </html>
