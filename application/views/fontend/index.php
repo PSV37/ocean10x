af1775525278ad90ca631ab061180b8a099185f3
supriyaL <supriya@itdivine.in>
2020-07-03 17:27:15 +0530

    change

--- application/views/fontend/index.php
+++ application/views/fontend/index.php
@@ -2,10 +2,11 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
+     
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <meta name="author" content="">
-    
+
   <!--Import Google Icon Font-->    
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!-- Compiled and minified CSS -->
