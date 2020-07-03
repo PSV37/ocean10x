b92a288d8689c0af0c78f9396c80b02468091a64
supriyaL <supriya@itdivine.in>
2020-07-03 17:06:29 +0530

    change

--- application/views/fontend/index.php
+++ application/views/fontend/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="UTF-8">    
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
@@ -13,10 +13,9 @@
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link href="index-css.css" rel="stylesheet" type="text/css" />
+ 
 
 
-  <title>Navbar con MaterializeCSS</title>
 </head>
 <style>
 .container{max-width:1260px !important;}
