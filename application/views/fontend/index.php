62c58e4a7a123d33547c8c87a06dbcf711de2c2f
supriyaL <supriya@itdivine.in>
2020-07-03 17:09:28 +0530

    change

--- application/views/fontend/index.php
+++ application/views/fontend/index.php
@@ -2,11 +2,12 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">    
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="author" content="">
 
-
-  <!--Import Google Icon Font-->
+  <!--Import Google Icon Font-->    
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
