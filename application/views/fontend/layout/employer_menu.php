915190f391991578035b5c4c6be8667ed38c82d4
supriyaL <supriya@itdivine.in>
2020-07-04 11:37:34 +0530

    change

--- application/views/fontend/layout/employer_menu.php
+++ application/views/fontend/layout/employer_menu.php
@@ -355,6 +355,9 @@ $(function () {
     });
 });
 </script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
   <script>
   // Show/Hide sidenav mobile
 document.addEventListener("DOMContentLoaded", function () {
