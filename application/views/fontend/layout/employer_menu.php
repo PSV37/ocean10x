4f821f027d9852e97a985664dc523edb4b973165
supriyaL <supriya@itdivine.in>
2020-07-04 23:07:42 +0530

    change

--- application/views/fontend/layout/employer_menu.php
+++ application/views/fontend/layout/employer_menu.php
@@ -57,6 +57,21 @@
 <?php $activemenu = $this->session->userdata('activemenu'); ?>
 <div class="col-md-3">
 <div class="employer_menu">
+
+
+
+
+
+
+ <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+      <li><a href="#">HTML</a></li>
+      <li><a href="#">CSS</a></li>
+      <li><a href="#">JavaScript</a></li>
+    </ul>
+  </div>
             
             <aside id="left-panel" style="margin-top:9px;
                  margin-left: 14px;height:auto; 
