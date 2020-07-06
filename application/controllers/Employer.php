f37cde86253f81f6d63e9bf180a118a08593955a
unknown <santu@edunetsol.com>
2020-07-06 14:00:56 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -408,8 +408,8 @@ class Employer extends MY_Employer_Controller
             $this->form_validation->set_rules('job_nature', 'Engagement Model', 'required');
             $this->form_validation->set_rules('preffered_certificates', 'Certification Preferred', 'required');
             $this->form_validation->set_rules('job_test_requirment', 'Ocean Test Required', 'required');
-            // $this->form_validation->set_rules('salrange_from', 'Salary Range From', 'required|max_length[2]');
-            // $this->form_validation->set_rules('salrange_to', 'Salary Range To', 'required|max_length[2]');
+            $this->form_validation->set_rules('salrange_from', 'Salary Range From', 'required|max_length[2]');
+            $this->form_validation->set_rules('salrange_to', 'Salary Range To', 'required|max_length[2]');
            
 
             // $this->form_validation->set_rules('job_desc', 'job description', 'trim|callback_contactVerify[job_description]|xss_clean');
