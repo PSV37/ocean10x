1296d37d2eecb50ca1e23a5218d197221e7f389f
unknown <santu@edunetsol.com>
2020-07-06 17:42:11 +0530

    new

--- application/views/fontend/employer/job_preview.php
+++ application/views/fontend/employer/job_preview.php
@@ -306,8 +306,10 @@ p.right-title_detail {
                               
                               <div class="skils_benifit">
                               <li class="left-title_seperate">Benefit</li><li class="right-title_seperate">&nbsp;:
-                                print_r($benefits);
-                              <?php foreach ($benefits as $row) { ?>
+
+                              <?php
+                                 print_r($benefits);
+                               foreach ($benefits as $row) { ?>
                                  <label>
                                 <input type="checkbox" value="4" class="btn-default1" checked="" name="benefits[]">
                                 <span><?php echo $row; ?></span>
