d6c8ed87917123c927b96c303fbf94be8d4aea96
unknown <santu@edunetsol.com>
2020-07-07 17:05:32 +0530

    new

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -539,6 +539,7 @@ p#or {
                      <div id="errorbox"></div>
                      <div class=" formrow">
                         <label class="control-label">Upload JD <span class="required"> * </span></label>  <?php if (!empty($this->session->userdata('jd_file')) ) { ?>  <a id="jd_file"   style="margin-left: 10px;" href="<?php echo base_url() ?>upload/job_description/<?php echo $this->session->userdata('jd_file'); ?>" download>Job_description</a><span style="margin-left: 15px" onclick="cancel_jd();" ><i class="fa fa-times" aria-hidden="true"></i></span> <?php   } ?> 
+
                         <input type="file"  name="job_description" id="job_description" class="form-control" value=" <?php if (!empty($this->session->userdata('jd_file')) ) { echo $this->session->userdata('jd_file'); } ?>" > 
                      </div>
                   </div>
@@ -777,8 +778,8 @@ else if(value == 'other_skill' )
   document.getElementById('test').addEventListener('submit', function(event){
     // Get the length of the values of each input
     var jd = document.getElementById('jd').value.length;
-        job_description = document.getElementById('job_description').value.length;
-
+     var   job_description = document.getElementById('job_description').value.length;
+alert(document.getElementById('job_description').value);
     // If both fields are empty stop the form from submitting
     if( jd === 0 && job_description === 0 ) {
       event.preventDefault();
