4aefaf18fb6428f35703af96cc7850b1a0b1d705
unknown <santu@edunetsol.com>
2020-07-08 10:57:16 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -250,7 +250,7 @@ class Employer extends MY_Employer_Controller
             $this->form_validation->set_rules('salrange_from', 'Salary Range From', 'required|max_length[2]');
             $this->form_validation->set_rules('salrange_to', 'Salary Range To', 'required|max_length[2]');
 
-            $this->form_validation->set_rules('benefits', 'Salary Range To', 'required');
+            $this->form_validation->set_rules('benefits[]', 'Salary Range To', 'required');
          
 
             $this->form_validation->set_message('required', 'This field is mandatory');
