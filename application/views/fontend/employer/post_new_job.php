39b637a77b979a9c44dbbe3360d7ea5d7cc03580
unknown <santu@edunetsol.com>
2020-07-08 18:39:18 +0530

    new

--- application/views/fontend/employer/post_new_job.php
+++ application/views/fontend/employer/post_new_job.php
@@ -647,7 +647,7 @@ p#or {
              
           else
         error.insertAfter(element);
-      var file_val = document.getElementById('jd_session').val;
+
         },
         rules: {
             
@@ -662,8 +662,7 @@ p#or {
     <?php 
    
 
-     $jd_file_val = "<script>document.writeln(res);</script>";
-     echo  $jd_file_val;
+   
     if (empty($jd_file_val)) { ?>
         
         'job_description': {
