e219b282eec763adca0923c17054f87bba67cfb2
unknown <santu@edunetsol.com>
2020-07-13 19:27:54 +0530

    new

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1051,12 +1051,13 @@ button.btn.btn-default {
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-sm">
       <div class="modal-content">
+          <form method="post" action="<?php echo base_url(); ?>employer/add_cv_folder">
         <div class="modal-header">
           <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
           <h4 class="modal-title">Add folder</h4>
         </div>
         <div class="modal-body">
-         <form method="post" action="<?php echo base_url(); ?>employer/add_cv_folder">
+        
             <div class="col-md-12" style="margin-top: 20px;">
                <div class="row">
                  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
@@ -1085,13 +1086,14 @@ button.btn.btn-default {
                </div>
                </div>
             </div>
-         </form>
+        
          <!--  <p>This is a small modal.</p> -->
         </div>
         <div class="modal-footer">
           <button type="submit" class="btn btn-default" data-dismiss="modal">Add</button>
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         </div>
+         </form>
       </div>
     </div>
   </div>
