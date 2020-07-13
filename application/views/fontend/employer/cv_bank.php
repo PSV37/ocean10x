20a05486868f5c0d4f64803bfc388b91026656a0
unknown <santu@edunetsol.com>
2020-07-13 19:13:23 +0530

    new

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1021,8 +1021,8 @@ button.btn.btn-default {
                         <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-ellipsis-h" aria-hidden="true"></i>
                         </button>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1" style="top:47px;">
-                           <li id="div_frwrd"><a class="dropdown-item" href="#" id="div_frwrd" data-toggle="modal" data-target="#rotateModal<?php echo $cv_row['cv_id']; ?>" >Forward Job Post</a></li>
+                        <div id="div_frwrd" class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1" style="top:47px;">
+                           <li ><a class="dropdown-item" href="#" id="div_frwrd" data-toggle="modal" data-target="#rotateModal<?php echo $cv_row['cv_id']; ?>" >Forward Job Post</a></li>
                         <?php if(isset($cv_row['js_resume']) && !empty($cv_row['js_resume'])){ ?>
                            <li id="div_download"> <a class="dropdown-item"  href="<?php if(isset($cv_row['js_resume']) && !empty($cv_row['js_resume'])){ echo base_url(); echo 'upload/Resumes/'.$cv_row['js_resume']; } ?>" download >Download this cv</a></li>
                            <?php } ?>
@@ -1339,7 +1339,7 @@ $.expr[":"].contains = $.expr.createPseudo(function(arg) {
             $("input[name='bulk_forward']:checkbox").prop('checked',false);
              if (this.checked) 
             {
-                $("#div_download").hide();
+                $("#div_frwrd").hide();
                 $("#div_frwrd").show();
 
              
