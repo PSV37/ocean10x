e4836589b4c813b76796d6a1ec71738bd5cbf1fc
unknown <santu@edunetsol.com>
2020-07-13 17:06:03 +0530

    new

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1396,6 +1396,16 @@ $.expr[":"].contains = $.expr.createPseudo(function(arg) {
                  success: function(response){
                    // window.location = response;
                    alert(response);
+                   const url =response;
+                                  const a = document.createElement('a');
+                                  a.style.display = 'none';
+                                  a.href = url;
+                                  // the filename you want
+                                  a.download = 'Resumes.zip';
+
+                                  document.body.appendChild(a);
+                                  a.click();
+                                  window.URL.revokeObjectURL(url);
                  }
                });
 
