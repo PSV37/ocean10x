e24a21b6ea22bc6da46984e09c3ede51f71cf16e
unknown <santu@edunetsol.com>
2020-07-13 14:35:26 +0530

    new

--- application/views/fontend/layout/employer_menu.php
+++ application/views/fontend/layout/employer_menu.php
@@ -54,7 +54,9 @@
 
 
 </style>    
-<?php $activemenu = $this->session->userdata('activemenu'); ?>
+<?php $activemenu = $this->session->userdata('activemenu'); 
+$employer_id = $this->session->userdata('company_profile_id');
+?>
 <div class="col-md-3">
 <div class="employer_menu">
 
@@ -106,8 +108,43 @@
                                   </a>
                                   <div class="row tree well">
                                                  
-                                                     <ul>
-                                            <li>
+                                                   
+            <?php $wheres       = "status='1' AND company_id='$employer_id' and parent_id = '0'";
+            $folders     = $this->Master_model->getMaster('cv_folder', $where = $wheres); 
+              if (!empty($folders)) { 
+                foreach ($folders as $row) { ?>
+                <ul>
+                  <li>
+                <span><i class="fas fa-folder-open"></i> <?php echo $row['folder_name']; ?></span>
+                   <?php 
+                    $parent_id = $row['id']; 
+                   $where_child  = "status='1' AND company_id='$employer_id' and parent_id = '$parent_id'";
+                      $child_folders  = $this->Master_model->getMaster('cv_folder', $where = $where_child); 
+                       if (!empty($child_folders)) { 
+                          foreach ($child_folders as $row1) { ?>
+                            <ul>
+                              <li>
+                                <span><i class="fas fa-folder-open"></i> <?php echo $row['folder_name']; ?></span> 
+                                    <?php $cparent_id = $row1['id']; 
+                                     $where_child  = "status='1' AND company_id='$employer_id' and parent_id = '$cparent_id'";
+                                        $grand_child_folders  = $this->Master_model->getMaster('cv_folder', $where = $where_child); 
+                                         if (!empty($grand_child_folders)) { 
+                                            foreach ($grand_child_folders as $row2) { ?>
+                                              <ul>
+                                                <li>
+                                                      <span><i class="fas fa-folder-open"></i><?php echo $row2['folder_name']; ?></span>
+                                                </li>
+                                              </ul>
+                                        </li>
+                                        </ul>
+                                                
+                                         </li>
+                                            
+                                        </ul>     
+            <?php } } } }}}
+            ?>
+
+                                            <!-- <li>
                                                 <span><i class="fas fa-folder-open"></i> Parent</span>
                                                 <ul>
                                                     <li>
@@ -128,10 +165,10 @@
                                                         </ul>
                                                     </li>
                                                 </ul>
-                                                
-                                            </li>
+                                                 -->
+                                            <!-- </li>
                                             
-                                        </ul>  
+                                        </ul>  --> 
                                                    
                                                      
                                                     </div>
