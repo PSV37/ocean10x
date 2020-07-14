31300f918f0a642b347b293ae09fa74d1c929cb6
unknown <santu@edunetsol.com>
2020-07-14 17:33:43 +0530

    new

--- application/views/fontend/layout/employer_menu.php
+++ application/views/fontend/layout/employer_menu.php
@@ -454,8 +454,7 @@ $('#submenu').bind("contextmenu", function (event) {
 $(document).bind("mousedown", function (e) {
     
     // If the clicked element is not the menu
-    var name = e.target.getAttribute('data-action');
-    alert(name);
+   
     if (!$(e.target).parents(".custom-menu").length > 0) {
         
         // Hide it
