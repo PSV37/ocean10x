85a95d3b79144501da6c0aff250acf5e5171494e
unknown <santu@edunetsol.com>
2020-07-14 09:17:05 +0530

    new

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1027,7 +1027,7 @@ button.btn.btn-default {
                            <li id="div_download"> <a class="dropdown-item"  href="<?php if(isset($cv_row['js_resume']) && !empty($cv_row['js_resume'])){ echo base_url(); echo 'upload/Resumes/'.$cv_row['js_resume']; } ?>" download >Download this cv</a></li>
                            <?php } ?>
 
-                           <li> <a class="dropdown-item" href="#">Dummy 1</a></li>
+                           <li><a class="dropdown-item" class="dropdown-item" href="#"  data-toggle="modal" data-target="#move_cv<?php echo $cv_row['cv_id']; ?>" href="#">Move this CV</a></li>
                         </div>
                      </div>
                   </div>
@@ -1046,8 +1046,53 @@ button.btn.btn-default {
          <div class="col-md-3 right_side">
          <div class="row" style="text-align: justify;margin: 10 auto;width: fit-content;">
       
-  
+<?php foreach ($cv_bank_data as $cv_row) :  ?>
+<div class="modal fade" id="move_cv<?php echo $cv_row['cv_id']; ?>" role="dialog">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+          <form method="post" action="<?php echo base_url(); ?>employer/add_cv_folder">
+        <div class="modal-header">
+          <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
+          <h4 class="modal-title">Add folder</h4>
+        </div>
+        <div class="modal-body">
+        
+            
+            <div class="col-md-12">
+               <div class="row">
+                 <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <label class="mdl-textfield__label" for="sample3">Choose Folder</label>
+                  <!-- <input type="text"  name="job_title"  id="job_title" placeholder=""  id="subject" data-required="true" multiple style="display: inline-block; width: 100%;" required> -->
+                  <?php 
+                  $employer_id = $this->session->userdata('company_profile_id');
+                  $wheres  = "status='1' AND company_id='$employer_id' ";
+                     $folders     = $this->Master_model->getMaster('cv_folder', $where = $wheres); ?>
+                  <select class="form-control select2" name="parent">
+                     <option value="0">None</option>
 
+                     <?php foreach ($folders as $row) { ?>
+                      <option value="<?php echo $row['id'] ?>"><?php echo $row['folder_name'] ?></option>
+                     <? } ?>
+                    
+                  </select>
+               </div>
+               </div>
+            </div>
+        
+         <!--  <p>This is a small modal.</p> -->
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-default">Add</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+         </form>
+      </div>
+    </div>
+  </div>
+  <?php
+                  $key++;
+                    endforeach;  
+                  ?>
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-sm">
       <div class="modal-content">
