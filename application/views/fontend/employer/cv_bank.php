8ff9cb63f61af3cd026a15586c2ea1c47bea5036
unknown <santu@edunetsol.com>
2020-07-14 11:52:54 +0530

    new

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1136,8 +1136,9 @@ span.select2-selection.select2-selection--single {
                      <option value="0">None</option>
 
                      <?php foreach ($folders as $row) { 
-                        $p1= $this->Employer_Login_model->cv_folder($row['id']);
-                        $p2 = $this->Employer_Login_model->cv_folder($p1->parent_id); 
+                        $p1 = $this->Employer_Login_model->cv_folder(echo $row['id']);
+                        print_r($p1);
+                        // $p2 = $this->Employer_Login_model->cv_folder($p1->parent_id); 
                         if ($p2 == '0') { ?>
                            <option value="<?php echo $row['id']; ?>"><?php echo $row['folder_name'] ?></option>
                        
