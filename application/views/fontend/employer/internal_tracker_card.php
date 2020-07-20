9ee5b584fb21d99cf89bcf0adb264aa46c57f907
unknown <santu@edunetsol.com>
2020-07-20 16:06:15 +0530

    new

--- application/views/fontend/employer/internal_tracker_card.php
+++ application/views/fontend/employer/internal_tracker_card.php
@@ -11,7 +11,7 @@
                         
                             <!-- <tbody> -->
                               <p><?php echo $job_row1->datecreation; ?></p><br>
-                              <?php $forwarded_job_tracking_date = $this->job_posting_model->get_job_forwarded_candidate_by_date($job_id,$job_row->datecreation);
+                              <?php $forwarded_job_tracking_date = $this->job_posting_model->get_job_forwarded_candidate_by_date($job_id,$job_row1->datecreation);
                               echo $this->db->last_query();die;
 
                                foreach ($forwarded_job_tracking_date as $job_row) {
