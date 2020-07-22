440165ecf19c7a8b88411dda849e1427b0bd0a6d
unknown <santu@edunetsol.com>
2020-07-22 18:14:36 +0530

    new

--- application/views/fontend/layout/employer_menu.php
+++ application/views/fontend/layout/employer_menu.php
@@ -548,7 +548,7 @@ $employer_id = $this->session->userdata('company_profile_id');
     { 
      $("input").keypress(function(){
     alert('in');
-    somethingChanged = true;
+    // somethingChanged = true;
 
   //this code executes when the keypress event occurs.
 });
