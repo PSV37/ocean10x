f459157e632830954262c27c4f11aa12c1c0199d
unknown <santu@edunetsol.com>
2020-07-24 15:30:47 +0530

    new

--- application/views/fontend/employer/create_test.php
+++ application/views/fontend/employer/create_test.php
@@ -218,7 +218,7 @@ input.select2-search__field {
 <div class="modal fade" id="add_test" role="dialog">
     <div class="modal-dialog modal-sm">
       <div class="modal-content">
-          <form method="post" action="<?php echo base_url(); ?>employer/add_to_test">
+          <form method="post" id="js">
         <div class="modal-header">
           <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
           <h4 class="modal-title">Add To</h4>
