fcb9da0950810f59a4529761b77ecc9df5dc31bf
unknown <santu@edunetsol.com>
2020-07-24 18:35:29 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -2018,9 +2018,9 @@ class Employer extends MY_Employer_Controller
             $old_questions = explode(',', $old_question_data['questions']);
             $ques = explode(',', $up_date);
             $new_arr = array_unique (array_merge($old_questions,$ques));
-            print_r($old_questions);
-            print_r($ques); 
-            print_r(implode(',',$new_arr));die;
+            // print_r($old_questions);
+            // print_r($ques); 
+            // print_r(implode(',',$new_arr));die;
             $test_data['questions'] = implode(',',$new_arr);
             $test_data['updated_on'] = date('Y-m-d H:i:s', strtotime('+5 hours +30 minutes'));
             $this->Master_model->master_update($test_data, 'oceanchamp_tests',$where);
