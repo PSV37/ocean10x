8691694e752a512bf773374a3436c28388ee3caa
unknown <santu@edunetsol.com>
2020-07-24 17:10:49 +0530

    new

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -1980,7 +1980,9 @@ label {
                               </div>
                               <div class="form-group">
                                  <label class="control-label col-sm-3" for="email">End Date:</label>
-                                 <div class="col-sm-9"><input id="resDate_1" class="datepicker form-control" required name="end_date" value="<?php if (!empty($experinece->end_date)) { echo date('d/m/Y',strtotime($experinece->end_date)); }else{ echo "";} ?>" disabled="disabled">
+                                 <div class="col-sm-9"> <input id="resDate_1" class="datepicker form-control"  name="end_date" value="" disabled="disabled" value="<?php if (!empty($experinece->end_date)) { echo date('d/m/Y',strtotime($experinece->end_date)); }else{ echo "";} ?>">
+                                          </div>
+                                 <!-- <div class="col-sm-9"><input id="resDate_1" class="datepicker form-control" required name="end_date"  disabled="disabled"> -->
                                  </div>
                               </div>
                               <div class="form-group">
