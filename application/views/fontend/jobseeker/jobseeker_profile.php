245c9086db9a654f56d2c9807abd151afe7fb0b8
unknown <santu@edunetsol.com>
2020-07-24 16:39:46 +0530

    new

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -1975,7 +1975,7 @@ label {
                               <div class="form-group">
                                  <label class="control-label col-sm-3" for="email">Start Date:</label>
                                  <div class="col-sm-9"><input class="datepicker form-control" id="start_date_picker1" required name="start_date" value="<?php if (!empty($experinece->start_date)) { echo date('d-m-Y',strtotime($experinece->start_date)); } ?>">
-                                    <label><input type="checkbox" id="upChkDisable_1" onclick="disableUpperDP('1')" checked="checked">  Current Job</label>
+                                    <label><input type="checkbox" id="upChkDisable_1" onclick="disableUpperDP('1')">  Current Job</label>
                                  </div>
                               </div>
                               <div class="form-group">
