04d663313df43cf8e62cbec3c2c21b4fac268ec5
unknown <santu@edunetsol.com>
2020-07-27 18:34:39 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1132,7 +1132,7 @@ input[type="radio"] {
                  myQuestions.forEach(
       (currentQuestion, questionNumber) => {
 
-        const TIME_LIMIT = ${currentQuestion.time_for_question};
+        const TIME_LIMIT = currentQuestion.time_for_question;
                   let timePassed = 0;
                   let timeLeft = TIME_LIMIT;
                   let timerInterval = null;
