9f078f0691897a76554f04de9aea24d7ffd8abfd
unknown <santu@edunetsol.com>
2020-07-27 17:16:12 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1063,8 +1063,7 @@ input[type="radio"] {
                   function getTimeRemaining(endtime) {
                     var t = Date.parse(endtime) - Date.parse(new Date());
                     var seconds = Math.floor((t / 1000) % 60);
-                    var minutes = Math.floor((t / 1000 / 60) % 60);
-                    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+                    
                    
                     return {
                       'total': t,
