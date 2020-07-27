e78d1e398c1e2ee2be07ff23306e4a7c5c807c7e
ravitejaram <raviteja.mailaram@gmail.com>
2020-07-27 12:25:56 +0530

    internal_tracker23

--- application/views/fontend/employer/internal_tracker.php
+++ application/views/fontend/employer/internal_tracker.php
@@ -771,8 +771,10 @@ textarea#comment {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
 <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.10.0/additional-methods.js"></script>
 
+
 <script src="js/jquery.validate.min.js"></script>
 <script src="js/additional-methods.js"></script>
+
 
 
 <script>
