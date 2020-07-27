1b10168f82f0aedcafd3b877c7a3617132f36d9e
unknown <santu@edunetsol.com>
2020-07-27 12:11:23 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6027,10 +6027,10 @@ function update_external()
                 $questions = explode(',',$oceanchamp_tests['questions']);
                 print_r($questions);
              //    $i=0;
-             //    foreach ($questions as $row) {
+                foreach ($questions as $row) {
              //      // print_r($row);
              //      // print_r($i);
-             //      $where['ques_id']   = $row;
+                  $where['ques_id']   = $row;
              //  //      $join_emp  = array(
              //  //     'skill_master' => 'skill_master.id=questionbank.technical_id |left outer',
              //  //     'topic' => 'topic.topic_id=questionbank.topic_id |left outer',
@@ -6039,10 +6039,10 @@ function update_external()
              //  //     'lineitemlevel' => 'lineitemlevel.lineitemlevel_id=questionbank.lineitemlevel_id |left outer',
              //  //     'questionbank_answer' => 'questionbank_answer.question_id = questionbank.ques_id|LEFT OUTER'
              //  // );
-             //      $question_data  = $this->Master_model->get_master_row('questionbank', $select = FALSE, $where, $join = false);
+                  $question_data  = $this->Master_model->get_master_row('questionbank', $select = FALSE, $where, $join = false);
                 
-             //      array_push($all_questions, $question_data);
-              // }
+                  array_push($all_questions, $question_data);
+              }
           // }
              //    endforeach;  
              //    endif;               
