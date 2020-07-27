93523be09927c31cd235d1cc2e6bb389e5446860
unknown <santu@edunetsol.com>
2020-07-27 15:54:54 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -948,7 +948,7 @@ button:hover{
                           <form id="nextques" class="submit-form" action="<?php echo base_url();?>employer/insert_test_data" method="post">
 
                                   
-                              <h1>Quiz on Important Facts</h1>
+                            
                               <div class="quiz-container">
                                 <div id="quiz"></div>
                               </div>
@@ -989,17 +989,7 @@ button:hover{
               <input type="radio" name="question${questionNumber}" value="${letter}">
               ${letter} :
               ${currentQuestion.answers[letter]}
-            </label> '<div class="bullet">'+
-                        '<div class="line zero"></div>'+
-                        '<div class="line one"></div>'+
-                        '<div class="line two"></div>'+
-                        '<div class="line three"></div>'+
-                        '<div class="line four"></div>'+
-                        '<div class="line five"></div>'+
-                        '<div class="line six"></div>'+
-                        '<div class="line seven"></div>'+
-                      '</div>'+
-                    '</li>';`
+            </label>`
           );
         }
 
@@ -1139,6 +1129,168 @@ button:hover{
 })();
 
               </script>
+              <script>
+                  function getTimeRemaining(endtime) {
+                    var t = Date.parse(endtime) - Date.parse(new Date());
+                    var seconds = Math.floor((t / 1000) % 60);
+                    var minutes = Math.floor((t / 1000 / 60) % 60);
+                    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+                   
+                    return {
+                      'total': t,
+                     
+                      'hours': hours,
+                      'minutes': minutes,
+                      'seconds': seconds
+                    };
+                  }
+                  
+                  function initializeClock(id, endtime) {
+                    var clock = document.getElementById(id);
+                    
+                    var hoursSpan = clock.querySelector('.hours');
+                    var minutesSpan = clock.querySelector('.minutes');
+                    var secondsSpan = clock.querySelector('.seconds');
+                  
+                    function updateClock() {
+                      var t = getTimeRemaining(endtime);
+                  
+                    
+                      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+                      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+                      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+                  
+                      if (t.total <= 0) {
+                        clearInterval(timeinterval);
+                      }
+                    }
+                  
+                    updateClock();
+                    var timeinterval = setInterval(updateClock, 1000);
+                  }
+                  
+                  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+                  initializeClock('clockdiv', deadline);
+               </script>
+               <script>
+                  // Credit: Mateusz Rybczonec
+                  
+                  const FULL_DASH_ARRAY = 283;
+                  const WARNING_THRESHOLD = 10;
+                  const ALERT_THRESHOLD = 5;
+                  
+                  const COLOR_CODES = {
+                    info: {
+                      color: "green"
+                    },
+                    warning: {
+                      color: "orange",
+                      threshold: WARNING_THRESHOLD
+                    },
+                    alert: {
+                      color: "red",
+                      threshold: ALERT_THRESHOLD
+                    }
+                  };
+                  
+                  const TIME_LIMIT = 20;
+                  let timePassed = 0;
+                  let timeLeft = TIME_LIMIT;
+                  let timerInterval = null;
+                  let remainingPathColor = COLOR_CODES.info.color;
+                  
+                  document.getElementById("app").innerHTML = `
+                  <div class="base-timer">
+                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                      <g class="base-timer__circle">
+                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                        <path
+                          id="base-timer-path-remaining"
+                          stroke-dasharray="283"
+                          class="base-timer__path-remaining ${remainingPathColor}"
+                          d="
+                            M 50, 50
+                            m -45, 0
+                            a 45,45 0 1,0 90,0
+                            a 45,45 0 1,0 -90,0
+                          "
+                        ></path>
+                      </g>
+                    </svg>
+                    <span id="base-timer-label" class="base-timer__label">${formatTime(
+                      timeLeft
+                    )}</span>
+                  </div>
+                  `;
+                  
+                  startTimer();
+                  
+                  function onTimesUp() {
+                    clearInterval(timerInterval);
+                    $("#next").click();
+
+                  }
+                  
+                  function startTimer() {
+                    timerInterval = setInterval(() => {
+                      timePassed = timePassed += 1;
+                      timeLeft = TIME_LIMIT - timePassed;
+                      document.getElementById("base-timer-label").innerHTML = formatTime(
+                        timeLeft
+                      );
+                      setCircleDasharray();
+                      setRemainingPathColor(timeLeft);
+                  
+                      if (timeLeft === 0) {
+                        onTimesUp();
+                      }
+                    }, 1000);
+                  }
+                  
+                  function formatTime(time) {
+                    const minutes = Math.floor(time / 60);
+                    let seconds = time % 60;
+                  
+                    if (seconds < 10) {
+                      seconds = `0${seconds}`;
+                    }
+                  
+                    return `${minutes}:${seconds}`;
+                  }
+                  
+                  function setRemainingPathColor(timeLeft) {
+                    const { alert, warning, info } = COLOR_CODES;
+                    if (timeLeft <= alert.threshold) {
+                      document
+                        .getElementById("base-timer-path-remaining")
+                        .classList.remove(warning.color);
+                      document
+                        .getElementById("base-timer-path-remaining")
+                        .classList.add(alert.color);
+                    } else if (timeLeft <= warning.threshold) {
+                      document
+                        .getElementById("base-timer-path-remaining")
+                        .classList.remove(info.color);
+                      document
+                        .getElementById("base-timer-path-remaining")
+                        .classList.add(warning.color);
+                    }
+                  }
+                  
+                  function calculateTimeFraction() {
+                    const rawTimeFraction = timeLeft / TIME_LIMIT;
+                    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+                  }
+                  
+                  function setCircleDasharray() {
+                    const circleDasharray = `${(
+                      calculateTimeFraction() * FULL_DASH_ARRAY
+                    ).toFixed(0)} 283`;
+                    document
+                      .getElementById("base-timer-path-remaining")
+                      .setAttribute("stroke-dasharray", circleDasharray);
+                  }
+               </script>
             </div>
          </div>
       </div>
