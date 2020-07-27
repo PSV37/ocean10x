6cb571e7e01cb8a3d4ee83f2638bf3eda2eb2437
unknown <santu@edunetsol.com>
2020-07-27 11:52:22 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6017,25 +6017,26 @@ function update_external()
             $where_all = "oceanchamp_tests.status='1' AND oceanchamp_tests.company_id='$employer_id' AND test_id = '$test_id'";
 
             $oceanchamp_tests = $this->Master_model->get_master_row('oceanchamp_tests', $select = FALSE, $where = $where_all, $join = FALSE);
+
             $all_questions = array();
              $key = 1; if (!empty($oceanchamp_tests)): foreach ($oceanchamp_tests as $question) : 
                  
                 $questions = explode(',',$question['questions']);
-                // print_r($questions);
+                print_r($questions);
                 $i=0;
                 foreach ($questions as $row) {
                   // print_r($row);
                   // print_r($i);
                   $where['ques_id']   = $row;
-                   $join_emp  = array(
-                  'skill_master' => 'skill_master.id=questionbank.technical_id |left outer',
-                  'topic' => 'topic.topic_id=questionbank.topic_id |left outer',
-                  'subtopic' => 'subtopic.subtopic_id=questionbank.subtopic_id |left outer',
-                  'lineitem' => 'lineitem.lineitem_id=questionbank.lineitem_id |left outer',
-                  'lineitemlevel' => 'lineitemlevel.lineitemlevel_id=questionbank.lineitemlevel_id |left outer',
-                  'questionbank_answer' => 'questionbank_answer.question_id = questionbank.ques_id|LEFT OUTER'
-              );
-                  $question_data  = $this->Master_model->get_master_row('questionbank', $select = FALSE, $where, $join = $join_emp);
+              //      $join_emp  = array(
+              //     'skill_master' => 'skill_master.id=questionbank.technical_id |left outer',
+              //     'topic' => 'topic.topic_id=questionbank.topic_id |left outer',
+              //     'subtopic' => 'subtopic.subtopic_id=questionbank.subtopic_id |left outer',
+              //     'lineitem' => 'lineitem.lineitem_id=questionbank.lineitem_id |left outer',
+              //     'lineitemlevel' => 'lineitemlevel.lineitemlevel_id=questionbank.lineitemlevel_id |left outer',
+              //     'questionbank_answer' => 'questionbank_answer.question_id = questionbank.ques_id|LEFT OUTER'
+              // );
+                  $question_data  = $this->Master_model->get_master_row('questionbank', $select = FALSE, $where, $join = false);
                 
                   array_push($all_questions, $question_data);
               }
