e04c73f5e15366577ccc8a862f8cde7780a821a3
unknown <santu@edunetsol.com>
2020-07-28 18:06:40 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1154,7 +1154,31 @@ input[type="radio"] {
                     let timeLeft = TIME_LIMIT;
                     let timerInterval = null;
                     let remainingPathColor = COLOR_CODES.info.color;
- 
+ document.getElementById("app").innerHTML = `
+                  <div class="base-timer">
+                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                      <g class="base-timer__circle">
+                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                        <path
+                          id="base-timer-path-remaining"
+                          stroke-dasharray="283"
+                          class="base-timer__path-remaining ${remainingPathColor}"
+                          d="
+                            M 50, 50
+                            m -45, 0
+                            a 45,45 0 1,0 90,0
+                            a 45,45 0 1,0 -90,0
+                          "
+                        ></path>
+                      </g>
+                    </svg>
+                    <span id="base-timer-label" class="base-timer__label">${formatTime(
+                      timeLeft
+                    )}</span>
+                  </div>
+                  `;
+                  
+                  startTimer();
   
  var myQuestions = <?php echo json_encode($all_questions); ?>;
 // console.log(JSON.parse(myQuestions));
@@ -1230,31 +1254,7 @@ input[type="radio"] {
                   
                   
                   
-                  document.getElementById("app").innerHTML = `
-                  <div class="base-timer">
-                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                      <g class="base-timer__circle">
-                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-                        <path
-                          id="base-timer-path-remaining"
-                          stroke-dasharray="283"
-                          class="base-timer__path-remaining ${remainingPathColor}"
-                          d="
-                            M 50, 50
-                            m -45, 0
-                            a 45,45 0 1,0 90,0
-                            a 45,45 0 1,0 -90,0
-                          "
-                        ></path>
-                      </g>
-                    </svg>
-                    <span id="base-timer-label" class="base-timer__label">${formatTime(
-                      timeLeft
-                    )}</span>
-                  </div>
-                  `;
                   
-                  startTimer();
 
 
                   function onTimesUp() {
