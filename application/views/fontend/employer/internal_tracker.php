1e4ee93ec8415819ae88d3754c01944051d88666
ravitejaram <raviteja.mailaram@gmail.com>
2020-07-28 12:23:35 +0530

    internal_tracker_a05

--- application/views/fontend/employer/internal_tracker.php
+++ application/views/fontend/employer/internal_tracker.php
@@ -1091,4 +1091,18 @@ $(".allowalphabates").keypress(function (e) {
      });
 
 
+$(document).on("keypress keyup blur", ".allowalphabatesspace", function (e){
+         var regex = new RegExp("^[a-zA-Z ]*$");
+         var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+         if (regex.test(str)) {
+             return true;
+         }
+         else
+         {
+         e.preventDefault();
+         return false;
+         }
+     });
+
+
 </script>
