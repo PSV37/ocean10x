b47ec10e00709096eae5a0cb0c16bf0bb60915d9
unknown <santu@edunetsol.com>
2020-07-28 10:17:05 +0530

    new

--- application/views/fontend/employer/ocean_test_instructions.php
+++ application/views/fontend/employer/ocean_test_instructions.php
@@ -10,11 +10,12 @@
             <!-- <form id="submit" class="submit-form" action="" method="post"> -->
                <form method="post" action="<?php echo base_url(); ?>employer/ocean_test_start/<?php if(!empty($test_id))echo base64_encode($test_id); ?>"> 
                            <div class="col-md-9 instruction_text">
-                              
+                              <?php print_r($oceanchamp_tests); ?>
+
                <h4 style="margin-bottom:20px;font-size:22px;color:#14a9a2;">Before You Start The Test Carefully Read The Instructions Below !</h4>
                <li>This is a FREE online test. DO NOT pay money to anyone to attend this test.</li>
                <li>All Questions are compulsory.</li>
-               <li>Total number of questions : <?php echo $oceanchamp_tests->total_questions; ?></li>
+               <li>Total number of questions : <?php echo $oceanchamp_tests['total_questions']; ?></li>
                <li>Time alloted :  <?php echo $oceanchamp_tests->test_duration; ?> Seconds.</li>
                <li>Each question carry 1 mark, no negative marks.</li>
                <li>DO NOT refresh the page.</li>
