380c1d38867469f64be287088aeed0c764ea6b83
unknown <santu@edunetsol.com>
2020-07-28 15:19:02 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1071,7 +1071,8 @@ input[type="radio"] {
           </div>`
         );
         timer.push(
-          `<div class="base-timer">
+            `<div class="slide">
+          <div class="base-timer">
                     <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                       <g class="base-timer__circle">
                         <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
@@ -1091,6 +1092,7 @@ input[type="radio"] {
                     <span id="base-timer-label" class="base-timer__label">${formatTime(
                       timeLeft
                     )}</span>
+                  </div>
                   </div>
                   `
         );
