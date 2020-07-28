58fa938741418152f99cc0f3248b9d09c6fd33c6
unknown <santu@edunetsol.com>
2020-07-28 15:23:41 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -992,7 +992,52 @@ input[type="radio"] {
                   // startTimer();
 
 
-                  function onTimesUp() {
+                 
+                  
+  
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+             <div class="time"> ${currentQuestion.time_for_question}</div>
+          </div>`
+        );
+        timer.push(
+            `<div class="slide">
+          <div class="base-timer">
+                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                      <g class="base-timer__circle">
+                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                        <path
+                          id="base-timer-path-remaining"
+                          stroke-dasharray="283"
+                          class="base-timer__path-remaining ${remainingPathColor}"
+                          d="
+                            M 50, 50
+                            m -45, 0
+                            a 45,45 0 1,0 90,0
+                            a 45,45 0 1,0 -90,0
+                          "
+                        ></path>
+                      </g>
+                    </svg>
+                    <span id="base-timer-label" class="base-timer__label">${formatTime(
+                      timeLeft
+                    )}</span>
+                  </div>
+                  </div>
+                  `
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+     document.getElementById("app").innerHTML = timer;
+      startTimer();
+     
+                 function onTimesUp() {
                     clearInterval(timerInterval);
                     $("#next").click();
                     // startTimer();
@@ -1060,50 +1105,6 @@ input[type="radio"] {
                   }
                   
                  
-                  
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="slide">
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join("")} </div>
-             <div class="time"> ${currentQuestion.time_for_question}</div>
-          </div>`
-        );
-        timer.push(
-            `<div class="slide">
-          <div class="base-timer">
-                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                      <g class="base-timer__circle">
-                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-                        <path
-                          id="base-timer-path-remaining"
-                          stroke-dasharray="283"
-                          class="base-timer__path-remaining ${remainingPathColor}"
-                          d="
-                            M 50, 50
-                            m -45, 0
-                            a 45,45 0 1,0 90,0
-                            a 45,45 0 1,0 -90,0
-                          "
-                        ></path>
-                      </g>
-                    </svg>
-                    <span id="base-timer-label" class="base-timer__label">${formatTime(
-                      timeLeft
-                    )}</span>
-                  </div>
-                  </div>
-                  `
-        );
-      }
-    );
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-     document.getElementById("app").innerHTML = timer;
-     
-                
   }
 
   function showResults(){
