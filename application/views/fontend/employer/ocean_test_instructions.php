9ad203484ac0d001e1def1702dd72713b2bf0c84
unknown <santu@edunetsol.com>
2020-07-28 09:59:53 +0530

    new

--- application/views/fontend/employer/ocean_test_instructions.php
+++ application/views/fontend/employer/ocean_test_instructions.php
@@ -10,6 +10,7 @@
             <!-- <form id="submit" class="submit-form" action="" method="post"> -->
                <form method="post" action="<?php echo base_url(); ?>employer/ocean_test_start/<?php if(!empty($test_id))echo base64_encode($test_id); ?>"> 
                            <div class="col-md-9 instruction_text">
+                              
                <h4 style="margin-bottom:20px;font-size:22px;color:#14a9a2;">Before You Start The Test Carefully Read The Instructions Below !</h4>
                <li>This is a FREE online test. DO NOT pay money to anyone to attend this test.</li>
                <li>All Questions are compulsory.</li>
