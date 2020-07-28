18fbf9835a4128d0e9879f616405dd494e2be130
unknown <santu@edunetsol.com>
2020-07-28 17:50:12 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -984,39 +984,13 @@ input[type="radio"] {
                  // const TIME_LIMIT = currentQuestion.;
                  const TIME_LIMIT = currentQuestion.time_for_question;
                   // const TIME_LIMIT = 5;
-  let timePassed = 0;
-  let timeLeft = TIME_LIMIT;
-  let timerInterval = null;
-  let remainingPathColor = COLOR_CODES.info.color;
+                    let timePassed = 0;
+                    let timeLeft = TIME_LIMIT;
+                    let timerInterval = null;
+                    let remainingPathColor = COLOR_CODES.info.color;
 
-                 // alert(currentQuestion.question);
-                  
-                  
-                  // document.getElementById("app").innerHTML = `
-                  // <div class="base-timer">
-                  //   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                  //     <g class="base-timer__circle">
-                  //       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-                  //       <path
-                  //         id="base-timer-path-remaining"
-                  //         stroke-dasharray="283"
-                  //         class="base-timer__path-remaining ${remainingPathColor}"
-                  //         d="
-                  //           M 50, 50
-                  //           m -45, 0
-                  //           a 45,45 0 1,0 90,0
-                  //           a 45,45 0 1,0 -90,0
-                  //         "
-                  //       ></path>
-                  //     </g>
-                  //   </svg>
-                  //   <span id="base-timer-label" class="base-timer__label">${formatTime(
-                  //     timeLeft
-                  //   )}</span>
-                  // </div>
-                  // `;
+                 
 
-                  // startTimer();
 
 
                   
@@ -1028,7 +1002,7 @@ input[type="radio"] {
           `<div class="slide"  id="${questionNumber}">
             <div class="question" id="${questionNumber}"> ${currentQuestion.question} </div>
             <div class="answers"> ${answers.join("")} </div>
-          
+          <input type = "hidden" id="timer${questionNumber}" value="currentQuestion.time_for_question"
           </div>`
         );
         timer.push(
@@ -1067,7 +1041,7 @@ input[type="radio"] {
 
     // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
-     document.getElementById("app").innerHTML = timer;
+     // document.getElementById("app").innerHTML = timer;
      document.getElementById("skip").innerHTML = skip;
 
      
@@ -1155,8 +1129,71 @@ input[type="radio"] {
   const quizContainer = document.getElementById('quiz');
   const resultsContainer = document.getElementById('results');
   const submitButton = document.getElementById('submit');
- 
-  function onTimesUp() {
+  const TIME_LIMIT = document.getElementById('timer'+currentSlide);
+
+  // Credit: Mateusz Rybczonec
+             
+                  
+                  const FULL_DASH_ARRAY = 283;
+                  const WARNING_THRESHOLD = 10;
+                  const ALERT_THRESHOLD = 5;
+                  
+                  const COLOR_CODES = {
+                    info: {
+                      color: "green"
+                    },
+                    warning: {
+                      color: "orange",
+                      threshold: WARNING_THRESHOLD
+                    },
+                    alert: {
+                      color: "red",
+                      threshold: ALERT_THRESHOLD
+                    }
+                  };
+                   
+
+                          // variable to store the list of possible answers
+                         
+
+                       
+                 // const TIME_LIMIT = currentQuestion.;
+                 const TIME_LIMIT = 20;
+                  let timePassed = 0;
+                  let timeLeft = TIME_LIMIT;
+                  let timerInterval = null;
+                  let remainingPathColor = COLOR_CODES.info.color;
+                  
+                  
+                  
+                  document.getElementById("app").innerHTML = `
+                  <div class="base-timer">
+                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                      <g class="base-timer__circle">
+                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                        <path
+                          id="base-timer-path-remaining"
+                          stroke-dasharray="283"
+                          class="base-timer__path-remaining ${remainingPathColor}"
+                          d="
+                            M 50, 50
+                            m -45, 0
+                            a 45,45 0 1,0 90,0
+                            a 45,45 0 1,0 -90,0
+                          "
+                        ></path>
+                      </g>
+                    </svg>
+                    <span id="base-timer-label" class="base-timer__label">${formatTime(
+                      timeLeft
+                    )}</span>
+                  </div>
+                  `;
+                  
+                  startTimer();
+
+
+                  function onTimesUp() {
                     clearInterval(timerInterval);
                     $("#next").click();
                     // startTimer();
@@ -1222,7 +1259,8 @@ input[type="radio"] {
                       .getElementById("base-timer-path-remaining")
                       .setAttribute("stroke-dasharray", circleDasharray);
                   }
-                  
+ 
+  
  var myQuestions = <?php echo json_encode($all_questions); ?>;
 // console.log(JSON.parse(myQuestions));
  console.log(myQuestions);
