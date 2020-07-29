a7a9aa10e448dc02e88542a92b844ade5e5a4e88
unknown <santu@edunetsol.com>
2020-07-29 15:18:17 +0530

    new

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -955,7 +955,7 @@ input[type="radio"] {
               ${letter} :
               ${currentQuestion.answers[letter]}
             </label>
-             <input type = "hidden" id="timer${questionNumber}" value="${currentQuestion.time_for_question}">`
+            `
           );
         }
 
@@ -1097,29 +1097,7 @@ input[type="radio"] {
           </div>`
         );
         timer.push(
-      `<div class="slide1">
-          <div class="base-timer">
-                    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                      <g class="base-timer__circle">
-                        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-                        <path
-                          id="base-timer-path-remaining"
-                          stroke-dasharray="283"
-                          class="base-timer__path-remaining ${remainingPathColor}"
-                          d="
-                            M 50, 50
-                            m -45, 0
-                            a 45,45 0 1,0 90,0
-                            a 45,45 0 1,0 -90,0
-                          "
-                        ></path>
-                      </g>
-                    </svg>
-                    <span id="base-timer-label" class="base-timer__label">${formatTime(
-                      timeLeft
-                    )}</span>
-                  </div>
-                  </div>`
+      ` <input type = "hidden" id="timer${questionNumber}" value="${currentQuestion.time_for_question}">`
                     );
          startTimer();
 
