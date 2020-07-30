c05ea00b5f360723baf6c9d48a2c2702dc7a582c
ravitejaram <raviteja.mailaram@gmail.com>
2020-07-30 22:12:13 +0530

    job_seeker30

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -3974,7 +3974,7 @@ $(document).ready(function () {
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script-->
 <script src="dist/js/multi_select.js"></script>
 <script>
       $(document).ready(function () {
