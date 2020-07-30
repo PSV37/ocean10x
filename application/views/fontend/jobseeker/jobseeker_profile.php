39ced66dc89d98b9215b6247372912a67666821f
ravitejaram <raviteja.mailaram@gmail.com>
2020-07-30 12:01:37 +0530

    job_seeker12

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -1935,8 +1935,8 @@ label {
                                    
                         
                         ?>
-                    <<div class="modal fade" id="myModal5" role="dialog">
-                        <div class="modal-dialog modal-md">>
+                    <!--<div class="modal fade" id="myModal5" role="dialog">
+                        <div class="modal-dialog modal-md">-->
                      <div class="modal-content">
                         <div class="modal-header">
                            <button type="button" class="close" data-dismiss="modal">&times;</button>
