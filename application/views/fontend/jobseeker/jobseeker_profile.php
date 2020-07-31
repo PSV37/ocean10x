e7b5d4f234b0b70a580b665ce52db654d2a6512e
ravitejaram <raviteja.mailaram@gmail.com>
2020-07-31 10:43:35 +0530

    job_seeker355

--- application/views/fontend/jobseeker/jobseeker_profile.php
+++ application/views/fontend/jobseeker/jobseeker_profile.php
@@ -1484,7 +1484,7 @@ label {
                                          echo $js_personal_info->work_permit_countries;
                                          }
                                        ?>">
-                                       
+
                                 <input type="text" name="other_country_work_permit" class="allowalphanumeric form-control" id="tokenfield" style="display: inline-block;"  placeholder="Enter Location" onkeydown="check_key();"
                         value="<?php if(!empty($job_info->city_id) ){echo $job_info->city_id; } ?><?php echo set_value('city_id'); ?>"><?php echo form_error('city_id'); ?>
                    
@@ -3958,3 +3958,23 @@ $(document).ready(function () {
 
 
 
+<script> 
+   $(document).ready(function() { 
+   
+     $('#other_terxtbx').hide();
+     $('#skl_btn').hide();
+     $('#other_skills').hide();
+      $('#training_title1').hide();
+      var id=document.getElementById('job_role').value;
+      // alert(id);
+      getSkillsdetails(id);
+   
+   $("#tokenfield").keypress(function(event){
+         var inputValue = event.charCode;
+         alert(inputValue);
+         if(!(inputValue >= 65 && inputValue <= 120) && (inputValue != 32 && inputValue != 0)){
+             event.preventDefault();
+         }
+     });
+
+   </script>
