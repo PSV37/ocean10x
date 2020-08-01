3eafd8adebbed443dcf264c338643c762860f897
unknown <santu@edunetsol.com>
2020-08-01 12:51:26 +0530

    new

--- application/views/fontend/employer/employer_dashboard.php
+++ application/views/fontend/employer/employer_dashboard.php
@@ -273,17 +273,18 @@ body {font-family: Arial, Helvetica, sans-serif;}
        			 <div class="paragraph_p_level">
        	
         </div>
+        <button class="open-button" onclick="openForm()">Chat</button>
         <div class="chat-popup" id="myForm">
-  <form action="/action_page.php" class="form-container">
-    <h1>Chat</h1>
+              <form action="/action_page.php" class="form-container">
+                <h1>Chat</h1>
 
-    <label for="msg"><b>Message</b></label>
-    <textarea placeholder="Type message.." name="msg" required></textarea>
+                <label for="msg"><b>Message</b></label>
+                <textarea placeholder="Type message.." name="msg" required></textarea>
 
-    <button type="submit" class="btn">Send</button>
-    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-  </form>
-</div>
+                <button type="submit" class="btn">Send</button>
+                <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+              </form>
+            </div>
             </div>
             
          </div>
