9e850c4fffe880120fbeb20a80ed819f7b285edd
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-01 18:52:59 +0530

    review_resume17

--- application/views/fontend/employer/employer_login.php
+++ application/views/fontend/employer/employer_login.php
@@ -251,7 +251,7 @@ input:-internal-autofill-selected{background-color:#fff !important;}
       <label>Password </label>
       <!--span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span-->
       <!--span toggle="#password-field" class="fa fa-lg fa-eye-slash field-icon toggle-password"></span-->
-      <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+      <!--span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span-->
       <i id="eyeIcon" class="fa fa-eye-slash toggle-password"></i>
 
     </div>    
