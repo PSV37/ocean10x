72148634ec4db753d041abd3d163328249629d30
unknown <santu@edunetsol.com>
2020-08-03 18:19:55 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -89,7 +89,188 @@
     $this->load->view('fontend/layout/new_seeker_header.php');
 
 ?>  
-   
+  <style>
+    .chatperson{
+  display: block;
+  border-bottom: 1px solid #eee;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  overflow: hidden;
+  margin-bottom: 15px;
+  padding: 4px;
+}
+.chatperson:hover{
+  text-decoration: none;
+  border-bottom: 1px solid orange;
+}
+.namechat {
+    display: inline-block;
+    vertical-align: middle;
+}
+.chatperson .chatimg img{
+  width: 40px;
+  height: 40px;
+  background-image: url('http://i.imgur.com/JqEuJ6t.png');
+}
+.chatperson .pname{
+  font-size: 18px;
+  padding-left: 5px;
+}
+.chatperson .lastmsg{
+  font-size: 12px;
+  padding-left: 5px;
+  color: #ccc;
+}
+
+/*body{
+    height:400px;
+    position: fixed;
+    bottom: 0;
+}*/
+.col-md-2, .col-md-10{
+    padding:0;
+}
+.panel{
+    margin-bottom: 0px;
+}
+.chat-window{
+    bottom:0;
+    position:fixed;
+    float:right;
+    margin-left:10px;
+}
+.chat-window > div > .panel{
+    border-radius: 5px 5px 0 0;
+}
+.icon_minim{
+    padding:2px 10px;
+}
+.msg_container_base{
+  background: #e5e5e5;
+  margin: 0;
+  padding: 0 10px 10px;
+  max-height:300px;
+  overflow-x:hidden;
+}
+.top-bar {
+  background: #666;
+  color: white;
+  padding: 10px;
+  position: relative;
+  overflow: hidden;
+}
+.msg_receive{
+    padding-left:0;
+    margin-left:0;
+}
+.msg_sent{
+    padding-bottom:20px !important;
+    margin-right:0;
+}
+.messages {
+  background: white;
+  padding: 10px;
+  border-radius: 2px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.2);
+  max-width:100%;
+}
+.messages > p {
+    font-size: 13px;
+    margin: 0 0 0.2rem 0;
+  }
+.messages > time {
+    font-size: 11px;
+    color: #ccc;
+}
+.msg_container {
+    padding: 10px;
+    overflow: hidden;
+    display: flex;
+}
+img {
+    display: block;
+    width: 100%;
+}
+.avatar {
+    position: relative;
+}
+.base_receive > .avatar:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 0;
+    height: 0;
+    border: 5px solid #FFF;
+    border-left-color: rgba(0, 0, 0, 0);
+    border-bottom-color: rgba(0, 0, 0, 0);
+}
+
+.base_sent {
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+.base_sent > .avatar:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 0;
+    border: 5px solid white;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    box-shadow: 1px 1px 2px rgba(black, 0.2); // not quite perfect but close
+}
+
+.msg_sent > time{
+    float: right;
+}
+
+
+
+.msg_container_base::-webkit-scrollbar-track
+{
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
+}
+
+.msg_container_base::-webkit-scrollbar
+{
+    width: 12px;
+    background-color: #F5F5F5;
+}
+
+.msg_container_base::-webkit-scrollbar-thumb
+{
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    background-color: #555;
+}
+
+.btn-group.dropup{
+    position:fixed;
+    left:0px;
+    bottom:0;
+}
+.panel-heading.top-bar {
+    background-color: #18c5bd;
+}
+button#btn-chat {
+    height: 30px;
+    background-color: #18c5bd;
+    border: none;
+}
+div#myForm1 {
+    display: block;
+    max-width: 350px;
+    float: right;
+    margin-left: 290px;
+    max-height: 300;
+    overflow-y: auto;
+}
+  </style> 
 <div class="container-fluid">
     <div class="container">
         <div class="col-md-12">
@@ -291,9 +472,8 @@
 
 
                 <div class="panel-body msg_container_base" >
-                   <input type="search" name="search_connection" placeholder="search new connection" id="search_connection" style="display: none;
-  border-radius: 0;margin-top: 43px;max-width: 88%;margin-left: 2px; color: black;">
-  <button class="btn btn-primary btn-sm" id="connection_btn" style="display: none;float: right;margin-right: -9px;margin-top: 1px;height: 36px;background-color: #18c5bd;border: none;"><i class="fa fa-plus fa-1x" onclick="add_connection();" aria-hidden="true"></i></button>
+                  <input type="search" name="search_connection" placeholder="search new connection" id="search_connection" style="display: none; border-radius: 0;margin-top: 43px;max-width: 88%;margin-left: 2px; color: black;">
+                  <button class="btn btn-primary btn-sm" id="connection_btn" style="display: none;float: right;margin-right: -9px;margin-top: 1px;height: 36px;background-color: #18c5bd;border: none;"><i class="fa fa-plus fa-1x" onclick="add_connection();" aria-hidden="true"></i></button>
                     <input type="hidden" name="job_seeker_id" value="" id="auto-value">
                     <?php foreach ($chatbox as $row) {?>
 
@@ -408,3 +588,94 @@
             }
         });
 </script> 
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+function send_msg(id)
+{
+  alert(id);
+  var message = $('#btn-input').val();
+  $.ajax({
+              url: "<?php echo base_url();?>employer/send_message",
+              type: "POST",
+              data: {id:id,message:message},
+              // contentType:false,
+              // processData:false,
+               // dataType: "json",
+              success: function(data)
+              {
+                $('#myForm1').html(data);
+              }
+        });
+}
+
+function show_box(id){
+  // var id = $('#auto-value').val();
+  alert(id);
+   $.ajax({
+              url: "<?php echo base_url();?>employer/get_messages",
+              type: "POST",
+              data: {id:id},
+              // contentType:false,
+              // processData:false,
+               // dataType: "json",
+              success: function(data)
+              {
+                 document.getElementById("myForm1").style.display = "block";
+
+                $('#myForm1').html(data);
+              }
+        });
+   
+}
+
+function opensearch(){
+    document.getElementById("search_connection").style.display = "block";
+    document.getElementById("connection_btn").style.display = "block";
+}
+$("#search_connection").autocomplete({
+             
+              source: "<?php echo base_url();?>Employer/search_people",
+             minLength: 2,
+              // append: "#rotateModal",
+              focus: function(event, ui) {
+               // prevent autocomplete from updating the textbox
+               event.preventDefault();
+               // manually update the textbox
+               // alert(source);
+               $(this).val(ui.item.label);
+            },
+            select: function(event, ui) {
+               // prevent autocomplete from updating the textbox
+               event.preventDefault();
+               // manually update the textbox and hidden field
+               $(this).val(ui.item.label);
+               $("#auto-value").val(ui.item.value);
+            }
+            
+           });
+function add_connection()
+{
+  var id = $('#auto-value').val();
+  var name = $('#search_connection').val();
+  alert(id);
+   $.ajax({
+              url: "<?php echo base_url();?>employer/add_new_connection",
+              type: "POST",
+              data: {id:id,name:name},
+              // contentType:false,
+              // processData:false,
+               // dataType: "json",
+              success: function(data)
+              {
+                $('#myForm').html(data);
+              }
+        });
+}
+</script>
