10b6f6cd0b327b0f15111cb87ee6ab30517418ec
unknown <santu@edunetsol.com>
2020-08-03 15:32:06 +0530

    new

--- application/views/fontend/employer/chatting_card.php
+++ application/views/fontend/employer/chatting_card.php
@@ -6,7 +6,7 @@
                     </div>
                     <span style="float: right;" onclick="closeForm('myForm1')"><i  class="fa fa-close"></i></span>
                 </div>
-                <?php foreach ($chatbox as $row) {  ?>
+                <?php foreach ($chatbox as $row) { $date = date('Y-md', $row['created_date']); ?>
                   
               
                 <div class="panel-body msg_container_base">
@@ -16,8 +16,11 @@
                     <div class="row msg_container base_sent">
                         <div class="col-md-10 col-xs-10">
                             <div class="messages msg_sent">
-                                <p><?php echo $row['msg'] ?></p>
-                                <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                                <p><?php echo $row['msg']; ?></p>
+                                <time datetime="2009-11-13T20:00"><?php if ($date == date('Y-m-d')) { echo $row['created_date']->diff(new DateTime('Y-m-d H:i:s'));
+                                   }else{
+                                   echo $row['created_date'];
+                                   } ?></time>
                             </div>
                         </div>
                         <div class="col-md-2 col-xs-2 avatar">
@@ -31,7 +34,7 @@
                         </div>
                         <div class="col-md-10 col-xs-10">
                             <div class="messages msg_receive">
-                                <p><?php echo $row['msg'] ?></p>
+                                <p><?php echo $row['msg']; ?></p>
                                 <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
                             </div>
                         </div>
