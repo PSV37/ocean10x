2072609b8f384f7407d58b755a5b043d434b2b78
unknown <santu@edunetsol.com>
2020-08-04 17:36:11 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -736,7 +736,7 @@ function opensearch(){
     document.getElementById("search_connection").style.display = "block";
     document.getElementById("connection_btn").style.display = "block";
 }
-$(document).on("keypress keyup blur", "#btn-input", function (e){
+$(document).on("keypress", "#btn-input", function (e){
     if (event.which == 13) {
         // validate();
         // alert("You pressed enter");
