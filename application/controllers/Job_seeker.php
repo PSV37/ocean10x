b09a6c2be6fb4b4316379a38fc3c207441f1b003
unknown <santu@edunetsol.com>
2020-08-04 16:11:00 +0530

    new

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -2022,13 +2022,14 @@ public function user_profile()
 
         if ($check['type'] == 'js' && $check['created_by'] == $this->session->userdata('job_seeker_id')) {
             $Join_data      = array(
-            'company_profile' => 'company_profile.company_profile_id = emp_js_connection.emp_id|Left OUTER ');
+           'js_info' => 'js_info.job_seeker_id = emp_js_connection.emp_id|Left OUTER ');
            
         }
         else
         {
           $Join_data      = array(
-            'js_info' => 'js_info.job_seeker_id = emp_js_connection.emp_id|Left OUTER ');   
+            
+             'company_profile' => 'company_profile.company_profile_id = emp_js_connection.emp_id|Left OUTER ');   
         }
 
         $whereres   = " emp_js_connection_id = '$connection_id'";
