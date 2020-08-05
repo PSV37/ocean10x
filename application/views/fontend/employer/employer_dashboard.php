42f9481b42861bad2e09746a344880cb546e02a7
unknown <santu@edunetsol.com>
2020-08-05 16:36:25 +0530

    new

--- application/views/fontend/employer/employer_dashboard.php
+++ application/views/fontend/employer/employer_dashboard.php
@@ -557,7 +557,7 @@ div#myForm {
 </div>   
 
 <script>
-$( document ).ready(function() {
+// $( document ).ready(function() {
 setInterval(function(){ 
      if ( $('#myForm1').css('display') == 'block')
       {
@@ -566,7 +566,7 @@ setInterval(function(){
       }
    
     }, 3000);
-});
+// });
   // setInterval("my_function();",10000);
 function openForm() {
   document.getElementById("myForm").style.display = "block";
