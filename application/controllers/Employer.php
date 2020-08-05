cbb0b0649c7d1c267140ad64d392e1cbbc775120
unknown <santu@edunetsol.com>
2020-08-05 09:16:08 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -31,31 +31,16 @@ class Employer extends MY_Employer_Controller
         $this->session->set_userdata($data);
         $company_info = $this->company_profile_model->get($employer_id);
          $wheremsg = "created_by='$employer_id'";
-           
-        //   $Join_data      = array(
-        //     'js_info' => 'js_info.job_seeker_id = emp_js_connection.js_id|Left OUTER '
-                
-        //  ); 
-        //  $whereres   = "emp_id='$employer_id'";
-        // $chatbox = $this->Master_model->getMaster('emp_js_connection', $where =  $whereres, $join = $Join_data, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
-
+        
         $whereres   = "emp_id='$employer_id'";
         $check = $this->Master_model->get_master_row('emp_js_connection', $select = FALSE, $whereres,$Join_data);
-        // print_r($check['type']);die;
-
-
-        // if ($check['type'] == 'js') {
-        //     $Join_data      = array(
-        //     'js_info' => 'js_info.job_seeker_id = emp_js_connection.js_id|Left OUTER ');
-        // }
-        // else
-        // {
-        //     $Join_data      = array(
-        //     'company_profile' => 'company_profile.company_profile_id = emp_js_connection.js_id|Left OUTER ');
-        // }
+       
+            $Join_data      = array(
+            'emp_js_connection' => 'emp_js_connection.emp_js_connection_id = messaging.connection_id|Left OUTER ');
+       
 
         $whereres   = "emp_id='$employer_id'";
-        $chatbox = $this->Master_model->getMaster('emp_js_connection', $where =  $whereres, $join = false, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
+        $chatbox = $this->Master_model->getMaster('messaging', $where =  $whereres, $join = $Join_data, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
         
         $this->load->view('fontend/employer/employer_dashboard', compact('company_info','chatbox'));
         
