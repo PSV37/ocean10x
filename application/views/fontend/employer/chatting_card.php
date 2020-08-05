04c74a1745e7ff8db03b17276e0df40dc6317dc0
unknown <santu@edunetsol.com>
2020-08-05 17:02:51 +0530

    new

--- application/views/fontend/employer/chatting_card.php
+++ application/views/fontend/employer/chatting_card.php
@@ -50,7 +50,7 @@
                     <div class="input-group">
                         <input id="btn-input" type="text" class="form-control input-sm chat_input" placeholder="Write your message here..." />
                         <span class="input-group-btn">
-                        <button class="btn btn-primary btn-sm" id="btn-chat" onclick="send_msg(<?php echo $check['emp_js_connection_id']; ?>);"><i class="fa fa-send fa-1x" aria-hidden="true"></i></button>
+                        <button class="btn btn-primary btn-sm" value="<?php echo $check['emp_js_connection_id']; ?>" id="btn-chat" onclick="send_msg(<?php echo $check['emp_js_connection_id']; ?>);"><i class="fa fa-send fa-1x" aria-hidden="true"></i></button>
                         </span>
                     </div>
                 </div>
