f23d8088b9a5692966765ceb97a1b852fb652350
unknown <santu@edunetsol.com>
2020-08-06 16:45:04 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -756,7 +756,7 @@ function send_msg(id)
 }
 
 function show_box(id){
-  // var id = $('#auto-value').val();
+  var id = $('#auto-value').val();
   // alert(id);
   
    $.ajax({
@@ -771,6 +771,8 @@ function show_box(id){
                  document.getElementById("myForm1").style.display = "block";
 
                 $('#myForm1').html(data);
+               var cid = $('#connection_id').val();
+               alert(cid);
               }
         });
      //  setInterval(function(){ 
