edfd22673784bb43338eb330d4bca0b6be7957af
unknown <santu@edunetsol.com>
2020-08-06 17:45:12 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -579,7 +579,7 @@ z-index: 1000;
                         <div class="col-md-2 col-xs-2 avatar">
                             <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" class=" img-responsive ">
                         </div>
-                        <div class="col-md-10 col-xs-10" onclick="show_box(<?php echo $row['emp_js_connection_id']; ?>);">
+                        <div class="col-md-10 col-xs-10" onclick="show_box(<?php echo $row['emp_js_connection_id']; ?>);get_list();">
                             <div class="messages msg_receive">
 
                                <?php $js_id = $this->session->userdata('job_seeker_id');
