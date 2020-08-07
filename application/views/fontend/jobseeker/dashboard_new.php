0a59cb8a5e739fcf0295e256857d1423ce11a330
unknown <santu@edunetsol.com>
2020-08-07 08:57:27 +0530

    new

--- application/views/fontend/jobseeker/dashboard_new.php
+++ application/views/fontend/jobseeker/dashboard_new.php
@@ -572,9 +572,9 @@ z-index: 1000;
                 <div class="panel-body msg_container_base" >
                   <input type="search" name="search_connection" placeholder="search new connection" id="search_connection" style="display: none; border-radius: 0;margin-top: 43px;max-width: 88%;margin-left: 2px; color: black;">
                   <button class="btn btn-primary btn-sm" id="connection_btn" style="display: none;float: right;margin-right: -9px;margin-top: 1px;height: 36px;background-color: #18c5bd;border: none;"><i class="fa fa-plus fa-1x" onclick="add_connection();" aria-hidden="true"></i></button>
-
-                   <div class="connections" style="margin-top: 50px;">
                     <input type="hidden" name="job_seeker_id" value="" id="auto-value">
+                  
+                   <div class="connections" style="margin-top: 50px;">
                     <?php foreach ($chatbox as $row) {?>
 
                     <div class="row msg_container base_receive">
