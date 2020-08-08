0c449f73080edf440d90f6ce4817458b7f8f4380
unknown <santu@edunetsol.com>
2020-08-08 15:00:36 +0530

    new

--- application/views/fontend/employer/change_password.php
+++ application/views/fontend/employer/change_password.php
@@ -1,130 +1,59 @@
-<style>
-.btn-pass{background-color:#18c5bd;
-border-radius:30px;
-padding:0px 25px;border:none;}
-.btn-pass:hover{background-color:#16a9a2;
-}
-@media (min-width: 768px){
-.modal-dialog {
-    width: 600px;
-    margin: 165px auto;
-}    
- .modal-dialog{width:250px;}
-
-}
- .modal-footer{border-top:none;text-align:center;padding: 20px 15px 20px 15px;}
-.modal-header{border-bottom:none;padding:20px 15px 0px 15px}
-.modal-title{padding-top:15px;}
-.btn-default {
-    color: #fff;
-    background-color:#18c5bd;
-	border:none;
-}
-.btn-default1{
-    color: #fff;
-    background-color:#18c5bd;
-	border:none;
-}
-.btn-default:hover{
-	 background-color:#118c86;
-	  color: #fff;}
-.btn-default1:hover{
-	 background-color:#118c86;
-	  color: #fff;
-}
-.modal-title {
-    padding-top: 0px;
-    font-size: 15px;
-    color: #403e3e;
-    font-weight: 700;
-	text-align:center;
-}
-
+<?php $this->load->view('fontend/layout/employer_new_header.php');?> 
+<!---header-->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/employer/post_new_job.css">
+<style type="text/css">
+   .required
+   {
+   color: red;
+   }
+   label.error {
+   color: red;
+   }
+   input.select2-search__field {
+   display: inline;
+   border-radius: 0px;
+   margin-top: 0px;
+   }
+   ul.select2-results__options {
+   margin-top: 30px;
+   }
+   div#errorbox {
+   margin-top: 25px;
+   /*font-weight: bold;*/
+   color: red;
+   }
 </style>
-
-
-
-<?php 
-    $this->load->view('fontend/layout/employer_header.php');
-?>                
-<!-- Page Title start -->
-<div class="pageTitle">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6 col-sm-6">
-        <h1 class="page-heading">Change Password</h1>
-      </div>
-      <div class="col-md-6 col-sm-6">
-        <div class="breadCrumb"><a href="#.">Home</a> / <span>Change Password</span></div>
-      </div>
-    </div>
-  </div>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/employer/questionbank.css">
+<div class="container">
+   <div class="col-md-12">
+      <?php $this->load->view('fontend/layout/employer_menu.php'); ?>
+      <?php echo $this->session->flashdata('change_password'); ?>
+      <form id="submit" class="submit-form" action="<?php echo base_url(); ?>employer/change_password" method="post">
+         <div class="col-md-9 add-question">
+            <div class="header-bookbank">
+               Change Password
+            </div>
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="form-group lineitem_id ">
+                     <label for="exampleInputEmail1">Current Password<span class="required">*</span></label>
+                     <input type="password" name="oldpassword" class="form-control" placeholder="Type your current password">
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="form-group ques_type">
+                     <label for="exampleInputEmail1">New Password<span class="required">*</span></label>
+                     <input type="password" name="newpassword" class="form-control" placeholder="Type your new password">
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4" style="text-align:right;">
+               <button id="submit" type="Submit" class="save_question">Save question</button>
+            </div>
+         </div>
+      </form>
+   </div>
 </div>
-<!-- Page Title End -->              
-
-          <div class="section lb">
-                <div class="container">
-                   <div class="row">
-                     <?php $this->load->view('fontend/layout/employer_left.php'); ?>
-
-                        <div class="content col-md-9">
-                        <div class="userccount">
-                            <div class="formpanel">
-                           <?php echo $this->session->flashdata('change_password'); ?>
-                               <form id="submit" class="submit-form" action="<?php echo base_url(); ?>employer/change_password" method="post">
-                                    <div class="row">
-                                        <div class="col-md-6 col-sm-12">
-                                            <label class="control-label">Current Password </label>
-                                            <input type="password" name="oldpassword" class="form-control" placeholder="Type your current password">
-                                            <br>
-                                            <label class="control-label">New Password</label>
-                                            <input type="password" name="newpassword" class="form-control" placeholder="Type your new password">
-                                            <br>
-                                            <div class="change-pass_btn" style="text-align:end;">
-                                               <button type="submit" class="btn-pass" data-toggle="modal" data-target="#modal_pass">Update Password</button>
-                                          	 </div>
-                                             
-                                             
-                                             
-                                       
-                            
-                      <!-- Modal -->
-                      <div class="modal fade" id="modal_pass" role="dialog">
-                        <div class="modal-dialog">
-                            
-                          <!-- Modal content-->
-                          <div class="modal-content">
-                            <div class="modal-header">
-                             
-                              <h4 class="modal-title">Your password has been changed successfully .</h4>
-                            </div>
-                           
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-default" data-dismiss="modal">Ok</button>
-                            </div>
-                          </div>
-                          
-                        </div>
-                      </div>
-                      
-
-                                             
-                                             
-                                             
-                                             
-                                             
-                                        </div>
-
-                                    </div>
-                                </form>
-                            </div><!-- end post-padding -->    
-                        </div>
-                        </div>
-
-                </div> <!--end row -->
-                </div><!-- end container -->
-            </div><!-- end section -->
-
-
-  
- <?php $this->load->view("fontend/layout/footer.php"); ?>
