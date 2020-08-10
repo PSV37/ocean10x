20a4bd043bcd737b5dfe790217a9d5cb27d16a92
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-10 21:13:03 +0530

    active_cv_notice_period_30

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1270,7 +1270,7 @@
                <p style="font-size:18px;margin-top:15px;">Notice Period</p>
                <div class="range-wrap">
                   <input type="range" class="range" id="notice_period" onchange="get_data();" min="0" max="7">
-                  <output class="bubble notice_period" id="notice_period_val"></output>
+                  <output class="bubble notice_period"></output>
                </div>
             </div>
             <div class="filter1">
@@ -1623,7 +1623,7 @@
    function get_data()
    {
       var value = $('#exp').val();
-      var notice_period_value = $('#notice_period_val').val();
+      var notice_period_value = $('#notice_period').val();
       var education_value = $('#education_id').val();
       var current_ctc_value = $('#current_ctc_id').val();
       var sta_value = $('#stability_id').val();
