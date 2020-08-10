2bcfc6c4e0813751203be17561e2d9afb57a8a16
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-10 21:52:21 +0530

    active_cv_notice_period_36

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1630,7 +1630,7 @@
        $.ajax({
                url: "<?php echo base_url();?>employer/get_active_cvs",
                type: "POST",
-               data:{exp:value, notice_period:notice_period_value, education:education_value, current_ctc:current_ctc_value},
+               data:{exp:value},
                  success: function(data)
                  {
                      var getarray = jQuery.parseJSON(data);
