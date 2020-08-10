882249bf2b1282a40cd77722ba02666639dc3d1e
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-10 18:07:30 +0530

    active_cv_notice_period_20

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1269,7 +1269,7 @@
             <div class="filter1">
                <p style="font-size:18px;margin-top:15px;">Notice Period</p>
                <div class="range-wrap">
-                  <input type="range" class="range" id="notice_period_id" onchange="get_data();" min="0" max="7">
+                  <input type="range" class="range" id="notice_period" onchange="get_data();" min="0" max="7">
                   <output class="bubble notice_period" id="notice_period_val"></output>
                </div>
             </div>
@@ -1623,14 +1623,14 @@
    function get_data()
    {
       var value = $('#exp').val();
-      var notice_period_value = $('#notice_period_id').text();
+      var notice_period_value = $('#notice_period').text();
       var education_value = $('#education_id').val();
       var current_ctc_value = $('#current_ctc_id').val();
       var stability_value = $('#stability_id').val();
        $.ajax({
                url: "<?php echo base_url();?>employer/get_active_cvs",
                type: "POST",
-               data:{exp:value, notice_period_id:notice_period_value, education_id:education_value, current_ctc_id:current_ctc_value  },
+               data:{exp:value, notice_period:notice_period_value, education_id:education_value, current_ctc_id:current_ctc_value  },
                  success: function(data)
                  {
                      var getarray = jQuery.parseJSON(data);
