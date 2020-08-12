2c8cc507b7b6a2e62186384f08c4b0ac21c1cdaa
unknown <santu@edunetsol.com>
2020-08-12 13:59:54 +0530

    new

--- application/views/fontend/layout/employer_new_header.php
+++ application/views/fontend/layout/employer_new_header.php
@@ -121,6 +121,7 @@ a.dropdown-toggle {
     </a>
     <ul class="dropdown-menu">
       <li><a href="<?php echo base_url() ?>employer/profile-setting"><i class="fas fa-user"></i>My Profile</a> </li>
+      <li><a href="<?php echo base_url() ?>employer/addemployee"><i class="fas fa-user"></i>Employee Management</a> </li>
       <li><a href="<?php echo base_url(); ?>employer/change-password"><i class="fas fa-lock"></i>Change Password</a></li>
       <li ><a href="#" class="btn-logoff" data-toggle="modal" data-target="#modal_logoff"><i class="fas fa-power-off"></i>Logout</a></li>
     </ul>
