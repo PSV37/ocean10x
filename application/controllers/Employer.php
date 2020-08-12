685dd673097531b1292167ba5c655ec6b31fa1ca
unknown <santu@edunetsol.com>
2020-08-12 18:35:45 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3210,6 +3210,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
         //load model
         $this->load->model('Questionbank_employer_model');
         // Check form submit or not
+        print_r($_POST);die();
         if ($_POST['upload']) {
             $data = array();
             print_r($_FILES);
