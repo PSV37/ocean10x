a92d5445591628ba6a8321e1d3b64f3c118ed539
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-12 11:36:25 +0530

    cv_bank_12

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -4352,8 +4352,6 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
         $active_cv = $this->Master_model->getMaster('corporate_cv_bank', $where = $where_active, $join = $join_cond, $order = false, $field = false, $select = false, $limit = false, $start = false, $search = false);
 
 
-        print_r $exp_var;
- 
         echo $this->db->last_query();
 
 
