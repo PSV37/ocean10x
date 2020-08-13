47235d5d629f1f776a51a13b70e8e68c4e77964e
ravitejaram <raviteja.mailaram@gmail.com>
2020-08-13 17:48:09 +0530

    cv_bank_time_period_02

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1337,21 +1337,21 @@
             <div class="filter1">
                <p style="font-size:18px;margin-top:15px;">Experience</p>
                <div class="range-wrap">
-                  <input type="range" class="range" id="exp_id" onchange="get_data();" min="0" max="20">
+                  <input type="range" class="range" id="exp_id" onchange="get_data();" min="0" max="20" value="0">
                   <output class="bubble"></output>
                </div>
             </div>
             <div class="filter1">
                <p style="font-size:18px;margin-top:15px;">Notice Period</p>
                <div class="range-wrap">
-                  <input type="range" class="range" id="notice_period_id" onchange="get_data();" min="0" max="90" step="5">
+                  <input type="range" class="range" id="notice_period_id" onchange="get_data();" min="0" max="90" step="5" value="0">
                   <output class="bubble notice_period" ></output>
                </div>
             </div>
             <div class="filter1">
                <p style="font-size:18px;margin-top:15px;">Current CTC</p>
                <div class="range-wrap">
-               <input type="range" class="range" id='current_ctc_id' onchange="get_data();" min="1" max="99">
+               <input type="range" class="range" id='current_ctc_id' onchange="get_data();" min="1" max="99" value="0">
                   <output class="bubble "></output>
                </div>
                <!--<select class="selectpicker"  multiple="" data-live-search="true" data-live-search-placeholder="Search" id='current_ctc_id' onchange="get_data();" tabindex="-98">
