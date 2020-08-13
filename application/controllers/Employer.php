ec45051aa88ecf542cebdee473a5d16d2fb5fca9
unknown <santu@edunetsol.com>
2020-08-13 11:26:37 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3290,14 +3290,14 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
 
     public function bulk_upload_folder()
     {
-       $uploads_dir = '/uploads/cv_folder';
-        foreach ($_FILES["pictures"]["error"] as $key => $error) {
-            if ($error == UPLOAD_ERR_OK) {
-                $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-                // basename() may prevent filesystem traversal attacks;
-                // further validation/sanitation of the filename may be appropriate
-                $name = basename($_FILES["pictures"]["name"][$key]);
-                move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        $count = 0;
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            foreach ($_FILES['files']['name'] as $i => $name) {
+                if (strlen($_FILES['files']['name'][$i]) > 1) {
+                    if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'cv_folder/'.$name)) {
+                        $count++;
+                    }
+                }
             }
         }
         if (isset($_POST['upload'])) {
