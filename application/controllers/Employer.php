987fb7e9b2058e87b2ef5d55ec45e19f1b0d72e4
unknown <santu@edunetsol.com>
2020-08-13 16:03:35 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3374,9 +3374,9 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
                                 foreach ($cv as $cvs) {
                                     $where = "corporate_cv_bank.cv_id = '$cvs'";
                                   $cv_name = $this->Master_model->get_master_row('corporate_cv_bank', $select = 'js_name', $where, $join = FALSE);
-                                  if (str_contains($name,  $cv_name)) { 
-                                            echo 'true';die;
-                                        }
+                                 if (strpos($name,$cv_name ) !== false) {
+                                        echo 'true';die;
+                                    }
                                   // print_r($this->db->last_query());die;
                                 }
                             }
