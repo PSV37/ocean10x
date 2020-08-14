861b3de4a864db730af1407e2223f993f19bd603
unknown <santu@edunetsol.com>
2020-08-14 18:37:48 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3395,18 +3395,18 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
                                             print_r($folders[$n]);
                                         }
                                         
-                                        if (!file_exists('cv_folder/'.$folders[$j].'/'.$folder_name)) {
-                                            mkdir('cv_folder/'.$folders[$j].'/'.$folder_name, 0777, true);
-                                        }
+                                        // if (!file_exists('cv_folder/'.$folders[$j].'/'.$folder_name)) {
+                                        //     mkdir('cv_folder/'.$folders[$j].'/'.$folder_name, 0777, true);
+                                        // }
 
                                         $folder_path_final= 'cv_folder/'.$folders[$j].'/'.$folder_name;
                                   
                                         }
                                         else
                                         {
-                                            if (!file_exists('cv_folder/'.$folder_name)) {
-                                                mkdir('cv_folder/'.$folder_name, 0777, true);
-                                            }
+                                            // if (!file_exists('cv_folder/'.$folder_name)) {
+                                            //     mkdir('cv_folder/'.$folder_name, 0777, true);
+                                            // }
                                              $folder_path_final= 'cv_folder/'.$folder_name;
                                         }
                                      }
