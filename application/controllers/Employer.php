d61986cae7f5d39ee71821d56b6610f11c59826a
unknown <santu@edunetsol.com>
2020-08-14 18:47:27 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3394,7 +3394,6 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
                                         if (!file_exists('cv_folder/'.$folders[$j].'/'.$folder_name)) {
                                             mkdir('cv_folder/'.$folders[$j].'/'.$folder_name, 0777, true);
                                         }
-                                        
                                         $folder_path_final= 'cv_folder/'.$folders[$j].'/'.$folder_name;
                                   
                                         }
