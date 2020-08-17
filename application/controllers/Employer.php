ad687c291e7856474cb1d6f2ebf70e3ff8cff767
unknown <santu@edunetsol.com>
2020-08-17 13:38:11 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3424,7 +3424,6 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
 
                                             $where_curr_folder = "cv_folder.folder_name = '$folder_name' and company_id = '$employer_id'";
                                             $curr_foldr = $this->Master_model->get_master_row('cv_folder', $select = 'id', $where =  $where_curr_folder, $join = FALSE);
-                                            
                                             $previous_folder = $folders[$j];
                                             $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id'";
                                             $parent = $this->Master_model->get_master_row('cv_folder', $select = 'id', $where =  $where_folder, $join = FALSE);
@@ -3434,7 +3433,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
                                                // print_r($parent);
                                                // print_r($folder_name); die;
 
-                                               if ($parent && empty($curr_foldr)) {
+                                               if ($parent) {
                                                   $insert_folder_data['folder_name'] = $folder_name;
                                                     $insert_folder_data['company_id'] = $employer_id;
                                                     $insert_folder_data['parent_id'] = $parent['id'];
