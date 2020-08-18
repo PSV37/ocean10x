17cfb1760042f924f2f7dc3aa27f33f47094f8ac
unknown <santu@edunetsol.com>
2020-08-18 16:44:36 +0530

    new

--- application/views/fontend/employer/list_tests.php
+++ application/views/fontend/employer/list_tests.php
@@ -1,5 +1,5 @@
 <?php $this->load->view('fontend/layout/employer_new_header.php');?>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/employer/oceanchamp_exp.css">
+<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/employer/oceanchamp_exp.css"> -->
 <style>
    .employ_tab {
    margin-top: 55px;
