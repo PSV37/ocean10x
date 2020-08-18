5785454493d7c6886362a12b94f649ca66a25d65
unknown <santu@edunetsol.com>
2020-08-18 15:13:42 +0530

    new

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1522,13 +1522,15 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
         $test_name = $this->input->post('test_name');
         $test_duration = $this->input->post('test_duration');
         $technical_id = $this->input->post('technical_id');
-        $topic_id = $this->input->post('topic_id_ocean');
+        $topic_id = $this->input->post('topic_id');
         $subtopic_id = $this->input->post('subtopic_id');
         $level = $this->input->post('level');
         $ques_type = $this->input->post('ques_type');
 
         $where = "technical_id = '$technical_id' and topic_id ='$topic_id' and subtopic_id ='$subtopic_id' and level ='$level' and ques_type ='$ques_type' and sum(time_for_question) = '$test_duration' ";
-        $questions = $this->Master_model->getMaster('questionbank', $where , $join = FALSE, $order = 'RANDOM', $field = 'ques_id', $select = false,$limit=20,$start=false, $search=false);
+        $where .= "HAVING time_duration = '$test_duration' ";
+        $questions = $this->Master_model->getMaster('questionbank', $where , $join = FALSE, $order = 'RANDOM', $field = 'ques_id', $select = 'questions,sum(time_for_question) as time_duration',$limit=20,$start=false, $search=false);
+        print_r($questions);
         print_r($this->db->last_query());die;
     }
     public function update_test() {
