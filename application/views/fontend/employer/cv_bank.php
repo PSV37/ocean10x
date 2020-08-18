e9dd2069bcc957979d21f63be1650a3d9dd1312d
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-18 18:02:44 +0530

    cv_bank

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1393,7 +1393,7 @@
                <p style="font-size:18px;margin-top:15px;">Time Period in Current Job</p>
                   <div class="range-wrap">
                   <input type="range" class="range" id="stability_id" onchange="get_data();" min="0" max="3" value="0">
-                  <output class="bubble rngOutput" ></output>
+                  <span id="bubble rngOutput" ></span>
 
                <!--<select class="selectpicker"  multiple="" data-live-search="true" data-live-search-placeholder="Search" id='stability_id' tabindex="-98">
                   <optgroup label="Driver Groups">
