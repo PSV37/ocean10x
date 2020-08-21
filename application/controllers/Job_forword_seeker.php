f60cae1f863a5624bcda837242b800f61b4d62ea
unknown <santu@edunetsol.com>
2020-08-21 16:58:03 +0530

    new

--- application/controllers/Job_forword_seeker.php
+++ application/controllers/Job_forword_seeker.php
@@ -86,7 +86,7 @@ class Job_forword_seeker extends CI_Controller {
                                 {
                                     $seeker_id = $this->session->userdata('job_seeker_id');
                                     $wherejob = "job_posting.job_post_id='$job_post_id' AND company_profile_id='$company_id' and job_apply.job_seeker_id = '$seeker_id";
-                                    $join = array('job_apply','job_apply.job_post_id = job_posting.job_post_id')
+                                    $join = array('job_apply','job_apply.job_post_id = job_posting.job_post_id');
                                     $select_test = "is_test_required,job_post_id,company_profile_id,test_for_job,job_apply.*";
                                   
                                     $data1['job_test'] = $this->Master_model->getMaster('job_posting',$wherejob,$join , $order = false, $field = false, $select_test,$limit=false,$start=false, $search=false);
