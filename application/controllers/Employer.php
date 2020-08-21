dceb2c983dce00cb48fed02045f370f01057a1ec
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-21 19:18:13 +0530

    active_cv

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -4931,7 +4931,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
 
 
 
-         $where_applied = "job_apply.apply_date <= apply_date +7 and job_posting.created_at <= created_at +7  job_apply.job_post_id='$job_id'";
+         $where_applied = "job_apply.apply_date <=','apply_date(NOW(),INTERVAL 7 DAYS )') and job_posting.created_at <=','created_at(NOW(),INTERVAL 7 DAYS )') job_apply.job_post_id='$job_id'";
          join_test = array('job_posting' => 'job_posting.job_post_id=job_apply.job_post_id',
         'seeker_test_result' => 'seeker_test_result.test_id=job_posting.test_for_job');
         $data['Total_count_early_applied'] = $this->Master_model->getMaster('job_apply', $where = $where_applied, $join = FALSE, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
