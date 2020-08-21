bf8e29b5c676b34c5d708e2c0e0c6906e1b28376
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-21 12:57:53 +0530

    cv_bank

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -4845,7 +4845,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
         'external_tracker' => 'external_tracker.job_post_id=job_apply.job_post_id | Left ');
         $data['Total_offer_accepted'] = $this->Master_model->getMaster('job_apply', $where = $where_offer, $join = $join_test_passed, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
 
-        // echo $this->db->last_query();
+         echo $this->db->last_query();
 
         echo json_encode($data);
 
