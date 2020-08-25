d5ed50b97b4cc0fc614fc720bf8b23382ddbc464
unknown <santu@edunetsol.com>
2020-08-25 10:28:43 +0530

    new

--- application/views/fontend/employer/employer_login.php
+++ application/views/fontend/employer/employer_login.php
@@ -283,12 +283,12 @@ input:-internal-autofill-selected{background-color:#fff !important;}
         <label for="checkbox_qu_01"><small class="remember" style="color:#7b7a7a;font-weight:100;">Remember me</small></label>
       </div>
     </div>           
-    <div class="row" style="text-align:center;"  tabindex="3">
-       <button class="sign_in" type="submit">Sign in</button>
-       <a href="<?php echo base_url() . 'employer_login/forgot_pass' ?>"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;">Forgot Password ?</p></a>
+    <div class="row" style="text-align:center;" >
+       <button class="sign_in" type="submit"  tabindex="3">Sign in</button>
+       <a href="<?php echo base_url() . 'employer_login/forgot_pass' ?>"  tabindex="4"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;">Forgot Password ?</p></a>
     </div>
   </form>
-  <div class="row" style="text-align:center;"  tabindex="4">
+  <div class="row" style="text-align:center;"  tabindex="5">
   <div class="newuser"><i class="fa fa-user" aria-hidden="true"></i> Not on Ocean ? <a href="<?php echo base_url(); ?>employer_register">Register Now</a></div>
   </div>
 </div>
