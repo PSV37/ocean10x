71f78e9c92e3223011851aa846c0f3997fee4e63
unknown <santu@edunetsol.com>
2020-08-25 10:40:17 +0530

    new

--- application/views/fontend/employer/employer_login.php
+++ application/views/fontend/employer/employer_login.php
@@ -277,18 +277,18 @@ input:-internal-autofill-selected{background-color:#fff !important;}
       <span toggle="#password-field" class="fa fa-eye-slash field-icon toggle-password"></span>
      
     </div>    
-    <div class="row"  tabindex="2">
+    <div class="row" >
       <div class="col-md-6 remb">
         <input id="checkbox_qu_01" type="checkbox" class="styled" checked="">
         <label for="checkbox_qu_01"><small class="remember" style="color:#7b7a7a;font-weight:100;">Remember me</small></label>
       </div>
     </div>           
     <div class="row" style="text-align:center;" >
-       <button class="sign_in" type="submit"  tabindex="3">Sign in</button>
-       <a href="<?php echo base_url() . 'employer_login/forgot_pass' ?>"  tabindex="4"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;">Forgot Password ?</p></a>
+       <button class="sign_in" type="submit"  tabindex="2">Sign in</button>
+       <a href="<?php echo base_url() . 'employer_login/forgot_pass' ?>" tabindex="3"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;">Forgot Password ?</p></a>
     </div>
   </form>
-  <div class="row" style="text-align:center;"  tabindex="5">
+  <div class="row" style="text-align:center;"  tabindex="4">
   <div class="newuser"><i class="fa fa-user" aria-hidden="true"></i> Not on Ocean ? <a href="<?php echo base_url(); ?>employer_register">Register Now</a></div>
   </div>
 </div>
