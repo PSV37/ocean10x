1044de9b2de9d87b2dc0fb2d7e83f7251e01fbd9
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-25 13:26:39 +0530

    Dashboard

--- application/views/fontend/employer/employer_dashboard.php
+++ application/views/fontend/employer/employer_dashboard.php
@@ -279,12 +279,13 @@
                               <div class="card-body-icon">
                                  <i class="fas fa-fw fa-download"></i>
                               </div>
-                              <span><?php echo sizeof($company_active_jobs); ?></span>
+                              <div >Active Job Posts</div>
+                              
 
                               <!--<span>Active Job Posts </span>-->
                            </div>
                            <a class="card-footer text-white clearfix small z-1" href="#">
-                           <span class="float-left" style="font-size:2px;">10</span>
+                           <span class="float-left" style="font-size:2px;"><?php echo sizeof($company_active_jobs); ?></span>
                            </a>
                         </div>
                      </div>
