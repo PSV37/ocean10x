b80d6f9c3fb2cfd60a5a8f5dc19f11624911cd35
unknown <santu@edunetsol.com>
2020-08-25 17:05:09 +0530

    new

--- application/views/fontend/employer/profile.php
+++ application/views/fontend/employer/profile.php
@@ -688,7 +688,8 @@ input.capital {
 <script>
   $(document).on('focus', '.select2-selection.select2-selection--single', function (e) {
   $(this).closest(".select2-container").siblings('select:enabled').select2('open');
-});.
+});
+
 
 </script>
 <script>
