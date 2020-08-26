6bb519665d099b3902009610b8ace5fd08dc73d8
unknown <santu@edunetsol.com>
2020-08-26 11:02:21 +0530

    new

--- application/views/fontend/employer/employer_login.php
+++ application/views/fontend/employer/employer_login.php
@@ -245,7 +245,13 @@ input:-internal-autofill-selected{background-color:#fff !important;}
   position: relative;
   z-index: 2;
   cursor:pointer;    
-}   
+}  
+a#forgot_pass:focus{
+  background-color: yellow;
+} 
+a#register:focus{
+  background-color: yellow;
+} 
 /*.user-box input:focus{background-color:#fff;}
 :focus {
     outline: none !important;
@@ -285,11 +291,11 @@ input:-internal-autofill-selected{background-color:#fff !important;}
     </div>           
     <div class="row" style="text-align:center;">
        <button class="sign_in" type="submit">Sign in</button>
-       <a href="<?php echo base_url() . 'employer_login/forgot_pass' ?>"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;" tabindex="4">Forgot Password ?</p></a>
+       <a id="forgot_pass" href="<?php echo base_url() . 'employer_login/forgot_pass' ?>"><p class="forgot" style="color:#7b7a7a;font-size: 12px;margin-top: 9px;">Forgot Password ?</p></a>
     </div>
   </form>
   <div class="row" style="text-align:center;">
-  <div class="newuser"><i class="fa fa-user" aria-hidden="true"></i> Not on Ocean ? <a href="<?php echo base_url(); ?>employer_register" tabindex="5">Register Now</a></div>
+  <div class="newuser"><i class="fa fa-user" aria-hidden="true"></i> Not on Ocean ? <a id="register" href="<?php echo base_url(); ?>employer_register">Register Now</a></div>
   </div>
 </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
