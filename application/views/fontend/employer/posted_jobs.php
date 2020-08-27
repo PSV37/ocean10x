1ae520429a8b3cf7127b22e58bd7eb3c1970133e
unknown <santu@edunetsol.com>
2020-08-27 10:11:43 +0530

    new

--- application/views/fontend/employer/posted_jobs.php
+++ application/views/fontend/employer/posted_jobs.php
@@ -950,9 +950,10 @@
 <div class="col-md-12">
 <?php $this->load->view('fontend/layout/employer_menu.php'); ?>
 <!-- <div class="panel-body"></div> -->
-         <?php echo $this->session->flashdata('success'); ?>
+        
 
 <div class="col-md-6 active-job">
+    <?php echo $this->session->flashdata('success'); ?>
    <?php if (!empty($company_active_jobs)): foreach ($company_active_jobs as $v_companyjobs) : ?>
    <label>
       <div class="border-top1"></div>
