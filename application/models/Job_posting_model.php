33c94a0e9bc9ac3b24339299ddc55aad15c4b31e
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-27 14:59:00 +0530

    Dashboard

--- application/models/Job_posting_model.php
+++ application/models/Job_posting_model.php
@@ -224,7 +224,7 @@ order by created_date asc limit 10");
 
     public function open_positions_active_jobs($company_id)
     {
-        $sub_query_from = '((SELECT SUM(no_jobs) FROM job_posting) as no_jobs)';
+        //$sub_query_from = '((SELECT SUM(no_jobs) FROM job_posting) as no_jobs)';
         $this->db->select('SUM(no_jobs)');
         $this->db->from('job_posting');
         //$this->db->from('job_posting');
