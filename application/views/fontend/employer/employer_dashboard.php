fd06a353af9ef782beaf08130f65137cea0d1c92
raju <raviteja.mailaram@tele-kinetics.com>
2020-08-28 11:06:59 +0530

    Dashboard

--- application/views/fontend/employer/employer_dashboard.php
+++ application/views/fontend/employer/employer_dashboard.php
@@ -321,7 +321,7 @@
                               <span>Open Positions</span>
                            </div>
                            <a class="card-footer text-white clearfix small z-1" href="#">
-                           <span class="float-left" style="font-size:22px;"><span><?php echo array_values($open_positions)[0]; ?></span>
+                           <span class="float-left" style="font-size:22px;"><span><?php echo count_all_results($open_positions[0]); ?></span>
                            </a>
                         </div>
                      </div>
