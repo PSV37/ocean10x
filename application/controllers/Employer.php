8a4ac01028c5ef19c5c9798690345099ca18e278
unknown <santu@edunetsol.com>
2020-08-29 19:26:49 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1483,9 +1483,12 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
                         <div class="clear"></div>
                       </div>
                       <!-- <div id="skills"> -->
-                      <span>Skill sets</span>:';
+                      ';
                       $sk=$require['skills_required'];
                       if (isset($sk) && !empty($sk)) {
+                        $message.='
+                      <br>
+                      <span>Skills</span>:';
                       $where_sk= "id IN (".$sk.") AND status=1";
                       $select_sk = "skill_name ,id";
                       $skills = $this->Master_model->getMaster('skill_master',$where_sk,$join = FALSE, $order = false, $field = false, $select_sk,$limit=10,$start=false, $search=false);
@@ -1496,8 +1499,28 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
                       ';
                       }
                       } }   
+                      $benefits=explode(',', $require['benefits']);
+                      if(!empty($benefits)){ 
+                        $message.='
+                      <br>
+                      <span>Benefits</span>:';
+                      $i=0;
+                      foreach($benefits as $benefit){ 
+                      $message.='
+                      <br>
+                      <span>Benefits</span>:<lable class=""><button id="sklbtn">'. $benefits[$i].'</button></lable>
+                      ';
+                      $i++; }
+                      } if ($v_companyjobs['job_deadline'] > date('Y-m-d')){
+                      // echo '<button class="btn btn-success btn-xs">Live <i class="fa fa-check-circle" aria-hidden="true"></i></button>';
+                      $message.='<span class="active-span">Active</span>';
+                      }
+                      else {
+                      // echo'<button class="btn btn-danger btn-xs">Expired <i class="fa fa-times" aria-hidden="true"></i></button> ';
+                      $message.= '<span class="pasive-span">Expired</span>';
+                      } 
                       
-                   $message.='</div>
+                   $message.='<span>Skill sets</span>:</div>
                   </label<br><br>   <a href="'.base_url().'job/show/'. $v_companyjobs['job_slugs'].'">Link</a>
                   <br><br>Thanks,<br><br>Team TheOcean<br><br>
                 </td>
