48c73acfd3519f1d4ada49b4c826b2277ae6e3d5
unknown <santu@edunetsol.com>
2020-08-31 15:53:21 +0530

    try

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -5,9 +5,10 @@ if (!defined('BASEPATH')) {
 class Cron extends CI_controller {
 	public function index()
 	{
-		$where =  "company_profile.create_at BETWEEN NOW() AND DATE_ADD(company_profile.create_at, INTERVAL 3 DAY) and verify = 'N'";
+		$where =  "company_profile.create_at BETWEEN NOW() AND DATE_ADD(company_profile.create_at, INTERVAL 3 DAY) and company_status = '0'";
 		$all_mails = $this->Master_model->getMaster('company_profile', $where , $join = FALSE, $order = false, $field = false, $select = false,$limit=false,$start=false, $search=false);
 		print_r($all_mails);
+		print_r($this->db->last_query());
 
 	}
 
