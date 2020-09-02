233737f4089b0c23298771de2b5884b6d76d5c31
unknown <santu@edunetsol.com>
2020-09-02 19:04:02 +0530

    try

--- application/models/Employer_login_model.php
+++ application/models/Employer_login_model.php
@@ -17,7 +17,7 @@ class Employer_Login_model extends MY_Model
             $this->db->from('company_profile');
             $this->db->where('company_email',$email);
             $this->db->where('company_password',$password);
-            $this->db->join('country','country.country_id = company_profile.country_id','left')
+            $this->db->join('country','country.country_id = company_profile.country_id','left');
             // $this->db->where('company_status','1');
             //$this->db->cache_off();
             $query = $this->db->get();
