2f376400ffac9c7e5a000a452a2a265e2f9f8bf8
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-04 11:35:18 +0530

    seekerinfo

--- application/views/fontend/jobseeker/update_experience.php
+++ application/views/fontend/jobseeker/update_experience.php
@@ -106,20 +106,7 @@ $experinece = $this->Job_seeker_experience_model->get($v_experience->js_experien
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Experince Information</h4>
       </div>
-       <script>
-      $(document).on("click", ".modal-body", function () {
-       $(".datepicker").datepicker({
-         // dateFormat: 'dd-mm-yy'     
-          //changeMonth: true,
-
-          //changeYear: true,
-
-          dateFormat: 'dd-mm-yy', minDate: '0' 
-
-          //yearRange: '2000:2019',                               
-         });
-       });  
-    </script> 
+       
       <div class="modal-body">
          <form id="UpdateExperience-info" class="form-horizontal" action="<?php echo base_url('job_seeker/update_experience');?>" method="post" style="padding: 30px;">
          <input type="hidden" name="js_experience_id" value="<?php echo $v_experience->js_experience_id; ?>">
@@ -378,6 +365,21 @@ if (!empty($experinece->start_date)) {
 
   </div>
 </div>
+
+<script>
+      $(document).on("click", ".modal-body", function () {
+       $(".datepicker").datepicker({
+         // dateFormat: 'dd-mm-yy'     
+          //changeMonth: true,
+
+          //changeYear: true,
+
+          dateFormat: 'dd-mm-yy', minDate: '0' 
+
+          //yearRange: '2000:2019',                               
+         });
+       });  
+    </script> 
 
 <script type="text/javascript">
   // $(document).ready(function() {
