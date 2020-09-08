366a78b7839074a2188971bff32df3510c772788
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-08 16:20:20 +0530

    header

--- application/views/fontend/layout/employer_new_header.php
+++ application/views/fontend/layout/employer_new_header.php
@@ -164,4 +164,11 @@ a.dropdown-toggle {
      }
     
   }
+
+  document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        alert('Esc key pressed.');
+    }
+};
 </script>
