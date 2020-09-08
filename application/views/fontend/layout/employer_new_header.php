28a7d4dc8a02edf6c69040f85e22b48b45625a10
unknown <santu@edunetsol.com>
2020-09-08 17:50:01 +0530

    try

--- application/views/fontend/layout/employer_new_header.php
+++ application/views/fontend/layout/employer_new_header.php
@@ -165,9 +165,9 @@ a.dropdown-toggle {
     
   }
 
-$(document).keyup(function(e) {
-     if (e.keyCode == 27) { // escape key maps to keycode `27`
-      alert('dd');
-    }
-});
+// $(document).keyup(function(e) {
+//      if (e.keyCode == 27) { // escape key maps to keycode `27`
+//       alert('dd');
+//     }
+// });
 </script>
