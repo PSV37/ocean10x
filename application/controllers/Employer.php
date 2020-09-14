860b59fce8db38ad6aa6830662193b6fc872ab42
unknown <santu@edunetsol.com>
2020-09-14 11:25:04 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -5551,6 +5551,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
              $join = array('company_profile'=>'company_profile.company_profile_id = tracker_consultant_mapping.consultant_id ');
             $where ="tracker_consultant_mapping.tracking_id IN ('".$array."')  group by tracker_consultant_mapping.consultant_id";
            $shared_list = $this->Master_model->get_master_row('tracker_consultant_mapping', $select = FALSE, $where, $join );
+           print_r($this->db->last_query());
         // print_r($shared_list);
         //    if (!empty($shared_list)) {
         //       array_push($shared_people, $shared_list->company_name);
