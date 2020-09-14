5747712b008a5894a4b38b0fdf7a3ec28856712e
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-14 11:09:17 +0530

    corp q-bank

--- application/views/fontend/employer/list_questions.php
+++ application/views/fontend/employer/list_questions.php
@@ -1617,7 +1617,7 @@
               <div class="panel-heading">
                 <h3 class="panel-title">Location</h3>
                 <div class="pull-right">
-                  <span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+                  <span class="clickable filter" data-toggle="tooltip"  data-container="body">
                   <i class="glyphicon glyphicon-filter"></i>
                   </span>
                 </div>
