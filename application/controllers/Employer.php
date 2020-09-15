a349aa97a2b3d1390f157cc75b663fd7dce6a1ef
unknown <santu@edunetsol.com>
2020-09-15 18:54:15 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3463,8 +3463,9 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
         } elseif (isset($_POST['sort'])) {
             $sort_val = $this->input->post('sort_val');
             if (isset($sort_val) && !empty($sort_val)) {
-                $where_c['company_id'] = $company_id;
-                $where_c['js_status'] = '0';
+                // $where_c['company_id'] = $company_id;
+                // $where_c['js_status'] = '0';
+                $where_c = "company_id = '$company_id' and js_status = '0'";
                 $data['cv_bank_data'] = $this->Master_model->getMaster('corporate_cv_bank', $where_c, $join = false, $order = 'desc', $field = $sort_val, $select = false, $limit = false, $start = false, $search = false);
                 print_r($this->db->last_query());die;
                  $data['company_active_jobs'] = $this->job_posting_model->get_company_activedeasline_jobs($company_id);
