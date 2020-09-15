f71cc9de18bc5cb25d9851abd240c0c81adc0c3e
unknown <santu@edunetsol.com>
2020-09-15 16:23:27 +0530

    try

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1093,7 +1093,7 @@
                 <div class="btn-group">
                   <a title="view Details" href="<?php echo base_url(); ?>employer/preview_cv/<?php echo base64_encode($cv_row['cv_id']); ?>"><i class="fa fa-info-circle icon_backg"></i></a>
                   <a  title="Edit" href=" <?php echo base_url(); ?>employer/edit_cv/<?php echo base64_encode($cv_row['cv_id']); ?>"><i class="far fa-edit icon_backg"></i></a>
-                  <a title="Delete" onclick="confirm_delete(<?php echo $ct_row['ques_id']; ?>);" href="<?php echo base_url(); ?>employer/delete_cv/<?php echo base64_encode($cv_row['cv_id']); ?>"><i class="fas fa-trash-alt icon_backg"></i></a>
+                  <a title="Delete" onclick="confirm_delete(<?php echo base64_encode($cv_row['cv_id']); ?>);" href="#"><i class="fas fa-trash-alt icon_backg"></i></a>
                 </div>
                 <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-ellipsis-h" aria-hidden="true"></i>
