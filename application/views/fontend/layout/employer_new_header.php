65eceb34a76302478057583c659956e2ec2256a8
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-16 15:07:32 +0530

    register

--- application/views/fontend/layout/employer_new_header.php
+++ application/views/fontend/layout/employer_new_header.php
@@ -130,7 +130,8 @@ a.dropdown-toggle {
     <ul class="dropdown-menu">
       <li> <p>Last Login : <?php echo date("d-m-Y h:i:s ", strtotime($last_login)); ?> </p></li>
       <li><a href="<?php echo base_url() ?>employer/profile-setting"><i class="fas fa-user"></i>Profile</a> </li>
-      <li><a href="<?php echo base_url() ?>employer/addemployee"><i class="fas fa-user"></i>Employee Management</a> </li>
+      <li><a href="<?php echo base_url() ?>employer/addemployee"><i class="fas fa-user"></i>Team Members</a> </li>
+      <!--<li><a href="<?php echo base_url() ?>employer/addemployee"><i class="fas fa-user"></i>Employee Management</a> </li>-->
       <li><a href="<?php echo base_url(); ?>employer/change-password"><i class="fas fa-lock"></i>Change Password</a></li>
       <li ><a href="#" class="btn-logoff" data-toggle="modal" data-target="#modal_logoff"><i class="fas fa-power-off"></i>Logout</a></li>
     </ul>
