955870ac8502c7573f4a0bb4bc17ead387e30f3d
unknown <santu@edunetsol.com>
2020-09-18 12:20:53 +0530

    try

--- application/models/Pincode_model.php
+++ application/models/Pincode_model.php
@@ -15,7 +15,7 @@ class Pincode_model extends MY_Model{
     }
 
     public function gettotal_cvs($company_id){
-    $sql = $this->db->query("SELECT *  FROM corporate_cv_bank WHERE corporate_cv_bank.company_id='$company_id' GROUP by cv_id and js_status= '0'");
+    $sql = $this->db->query("SELECT *  FROM corporate_cv_bank WHERE corporate_cv_bank.company_id='$company_id'  and js_status= '0'");
     return $sql->result();
     }
 
