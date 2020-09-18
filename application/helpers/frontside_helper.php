a1bf9b552941ab8a79e5d00ef19f495b26c8eaae
unknown <santu@edunetsol.com>
2020-09-18 10:09:30 +0530

    try

--- application/helpers/frontside_helper.php
+++ application/helpers/frontside_helper.php
@@ -164,10 +164,10 @@ $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
 // Additional headers
-$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
-$headers[] = 'From: Birthday Reminder <birthday@example.com>';
-$headers[] = 'Cc: birthdayarchive@example.com';
-$headers[] = 'Bcc: birthdaycheck@example.com';
+$headers[] = 'To: Mary <kmugdha410@gmail.com>';
+$headers[] = 'From: Birthday Reminder <kmugdha410@gmail.com>';
+// $headers[] = 'Cc: birthdayarchive@example.com';
+// $headers[] = 'Bcc: birthdaycheck@example.com';
 
 // Mail it
 $success = mail($to, $subject, $message, implode("\r\n", $headers));
