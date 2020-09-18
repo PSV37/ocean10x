e8aaffac6532baf4a02890709e4a3461e5fa5358
unknown <santu@edunetsol.com>
2020-09-18 10:36:44 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3591,11 +3591,11 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             $join = array('education_level' => 'education_level.education_level_id = corporate_cv_bank.js_top_education | left outer',
                 'forwarded_jobs_cv' => 'forwarded_jobs_cv.cv_id = corporate_cv_bank.cv_id | LEFT OUTER',
                 'job_posting'=>'job_posting.job_post_id = forwarded_jobs_cv.job_post_id | LEFT OUTER');
-            $data['cv_bank_data'] = $this->Master_model->getMaster('corporate_cv_bank', $where_c, $join, $order = 'desc', $field = 'cv_id', $select = false, $config["per_page"], $page, $search = false);
+            $data['cv_bank_data'] = $this->Master_model->getMaster('corporate_cv_bank', $where_c, $join, $order = 'desc', $field = 'corporate_cv_bank.cv_id', $select = false, $config["per_page"], $page, $search = false);
             $where_trash = "company_id ='$company_id' and js_status = '1'";
             // $where_c['company_id'] = $company_id;
             $join = array('education_level' => 'education_level.education_level_id = corporate_cv_bank.js_top_education | left outer');
-            $data['cv_trash_data'] = $this->Master_model->getMaster('corporate_cv_bank', $where_trash, $join, $order = 'desc', $field = 'corporate_cv_bank.cv_id', $select = false, $limit = false, $start = false, $search = false);
+            $data['cv_trash_data'] = $this->Master_model->getMaster('corporate_cv_bank', $where_trash, $join, $order = 'desc', $field = 'cv_id', $select = false, $limit = false, $start = false, $search = false);
        
             $this->load->view('fontend/employer/cv_bank', $data);
         }
