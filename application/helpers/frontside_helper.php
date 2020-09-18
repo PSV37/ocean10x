84a978ebb5239529fcc533c5e4595dfdb5b7361e
unknown <santu@edunetsol.com>
2020-09-18 10:14:54 +0530

    try

--- application/helpers/frontside_helper.php
+++ application/helpers/frontside_helper.php
@@ -110,73 +110,30 @@ function get_metas() {
         // $ci->email->send();
         
         // return true;
-        // $ci = get_instance();
-        // $ci->load->library('email');
-        //     /*$config['protocol'] = "smtp";
-        //     $config['smtp_host'] = "ssl://smtp.gmail.com";
-        //     $config['smtp_port'] = "465";
-        //     $config['smtp_user'] = "notification@yourdomain.com"; 
-        //     $config['smtp_pass'] = "Romesh-shil1995";*/
+        $ci = get_instance();
+        $ci->load->library('email');
+            /*$config['protocol'] = "smtp";
+            $config['smtp_host'] = "ssl://smtp.gmail.com";
+            $config['smtp_port'] = "465";
+            $config['smtp_user'] = "notification@yourdomain.com"; 
+            $config['smtp_pass'] = "Romesh-shil1995";*/
             
-        //     $config['protocol'] = "mail";
-        //     $config['charset'] = "utf-8";
-        //     $config['mailtype'] = "html";
-        //     $config['newline'] = "\r\n";
+            $config['protocol'] = "mail";
+            $config['charset'] = "utf-8";
+            $config['mailtype'] = "html";
+            $config['newline'] = "\r\n";
 
 
-        //     $ci->email->initialize($config);
-        //     $ci->email->from('info@consultnhire.com', 'ConsultnHire');
-        //     $ci->email->to($to_email);
-        //     $ci->email->reply_to('info@consultnhire.com', 'ConsultnHire');
-        //     $ci->email->subject($subject);
-        //     $ci->email->message($message);
-        //     $ci->email->send(FALSE);
-        $to = 'kmugdha410@gmail.com'; // note the comma
-
-// Subject
-$subject = 'Birthday Reminders for August';
-
-// Message
-$message = '
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
-
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-
-// Additional headers
-// $headers[] = 'To: Mary <kmugdha410@gmail.com>';
-// $headers[] = 'From: Birthday Reminder <kmugdha410@gmail.com>';
-// $headers[] = 'Cc: birthdayarchive@example.com';
-// $headers[] = 'Bcc: birthdaycheck@example.com';
-
-// Mail it
-$success = mail($to, $subject, $message, implode("\r\n", $headers));
-if (!$success) {
-    $errorMessage = error_get_last()['message'];
-}
-         return  $success;
-           // exit;
-            // return true;
+            $ci->email->initialize($config);
+            $ci->email->from('info@consultnhire.com', 'ConsultnHire');
+            $ci->email->to($to_email);
+            $ci->email->reply_to('info@consultnhire.com', 'ConsultnHire');
+            $ci->email->subject($subject);
+            $ci->email->message($message);
+            $ci->email->send(FALSE);
+          //  $ci->email->print_debugger(array('headers'));
+          //  exit;
+            return true;
     }
 
     // to fetch exam result by job seeker ids
