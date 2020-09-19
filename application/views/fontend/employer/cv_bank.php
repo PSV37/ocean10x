f438574c86397eb4d874d308779acd79c541ae12
unknown <santu@edunetsol.com>
2020-09-19 15:37:42 +0530

    try

--- application/views/fontend/employer/cv_bank.php
+++ application/views/fontend/employer/cv_bank.php
@@ -1185,7 +1185,7 @@ button.folder_popup {
         </div>
       </div>
       <div class="col-md-3 right_side">
-        <div class="pai_chart">
+        <div class="pai_chart" id="pie-chart">
           <main>
             <section>
               <ul class="pieID legend">
@@ -2328,7 +2328,7 @@ Phone : <?php echo $this->session->userdata('phone'); ?>
   
   
 </script>
-<script>
+<!-- <script>
   function sliceSize(dataNum, dataTotal) {
     return (dataNum / dataTotal) * 360;
   }
@@ -2388,6 +2388,59 @@ Phone : <?php echo $this->session->userdata('phone'); ?>
   }
   createPie(".pieID.legend", ".pieID.pie");
   
+</script> -->
+<script>
+  function createPie()
+  {
+    google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawCharts);
+function drawCharts() {
+  
+  // BEGIN PIE CHART
+  
+  // pie chart data
+  var pieData = google.visualization.arrayToDataTable([
+    ['Country', 'Page Hits'],
+    ['USA',      7242],
+    ['Canada',   4563],
+    ['Mexico',   1345],
+    ['Sweden',    946],
+    ['Germany',  2150]
+  ]);
+  // pie chart options
+  var pieOptions = {
+    backgroundColor: 'transparent',
+    pieHole: 0.4,
+    colors: [ "cornflowerblue", 
+              "olivedrab", 
+              "orange", 
+              "tomato", 
+              "crimson", 
+              "purple", 
+              "turquoise", 
+              "forestgreen", 
+              "navy", 
+              "gray"],
+    pieSliceText: 'value',
+    tooltip: {
+      text: 'percentage'
+    },
+    fontName: 'Open Sans',
+    chartArea: {
+      width: '100%',
+      height: '94%'
+    },
+    legend: {
+      textStyle: {
+        fontSize: 13
+      }
+    }
+  };
+  // draw pie chart
+  var pieChart = new google.visualization.PieChart(document.getElementById('pie-chart'));
+  pieChart.draw(pieData, pieOptions);
+}
+  }
 </script>
 <script>
   /**
