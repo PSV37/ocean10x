39b0666d36fec08b862a50b7c2b9acf998306c53
unknown <santu@edunetsol.com>
2020-09-22 18:36:34 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3496,8 +3496,8 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             $join_cond = array('corporate_cv_bank' => 'corporate_cv_bank.cv_id = cv_folder_relation.cv_id|Left outer',
                 'cv_folder' => 'cv_folder.id=cv_folder_relation.cv_folder_id');
             $data['cv_bank_data'] = $this->Master_model->getMaster('cv_folder_relation', $where_c, $join_cond, $order = 'desc', $field = 'relation_id', $select = false, $limit = false, $start = false, $search = false);
-                $data['total_cvs']=sizeof($cv_bank_data);
-            
+            $data['total_cvs']=sizeof($cv_bank_data);
+            $data['active_cv'] = $this->Pincode_model->getactive_folder_cvs($company_id,$fid);
              // print_r($this->db->last_query());die;
             $data['fid'] = $fid;
             $data['fname'] = $data['cv_bank_data'][0]['folder_name'];
@@ -3529,6 +3529,8 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             $cv_bank_data = $this->Master_model->getMaster('corporate_cv_bank', $where_c, $join_cond , $order = 'desc', $field = $sort_val, $select = false, $limit = false, $start = false, $search = false);
                 // print_r($this->db->last_query());die;
             // $config['base_url'] = base_url() . 'employer/corporate_cv_bank';
+             $data['total_cvs']=sizeof($cv_bank_data);
+            $data['active_cv'] = $this->Pincode_model->getactive_folder_cvs($company_id,$fid);
                 $data['total_cvs']=sizeof($cv_bank_data);
 
             $config['total_rows'] = sizeof($cv_bank_data);
