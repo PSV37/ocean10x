fb42054ba181ff22be1cf594129c2006f23879ff
unknown <santu@edunetsol.com>
2020-09-22 18:12:56 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -4380,13 +4380,13 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             $where11['js_email'] = $email_id;
             $this->Master_model->master_update($update_profile, 'corporate_cv_bank', $where11);
              $this->session->set_flashdata('success', '<div class="alert alert-success text-center">Profile Updated successfully with the latest ocean profile...!</div>');
-                redirect('employer/corporate_cv_bank'.$fid);
+                redirect('employer/corporate_cv_bank/'.$fid);
               }
               else
               {
 
                  $this->session->set_flashdata('success', '<div class="alert alert-success text-center"> CV is already updated with latest data from Ocean.</div>');
-                redirect('employer/corporate_cv_bank'.$fid);
+                redirect('employer/corporate_cv_bank/'.$fid);
               }
             // echo $this->db->last_query();die;
             
