e9471dfcec7f00abd06b69c0d745c64c02dfbcfc
unknown <santu@edunetsol.com>
2020-09-22 18:37:45 +0530

    try

--- application/models/Pincode_model.php
+++ application/models/Pincode_model.php
@@ -15,7 +15,7 @@ class Pincode_model extends MY_Model{
     }
 
     public function getactive_folder_cvs($company_id,$fid){
-    $sql = $this->db->query("SELECT * FROM corporate_cv_bank LEFT  OUTER JOIN js_info on js_info.email = corporate_cv_bank.js_email LEFT OUTER  JOIN js_login_logs on js_info.job_seeker_id = js_login_logs.job_seeker_id left OUTER join cv_folder_relation on cv_folder_relation.cv_id = corporate_cv_bank.cv_id WHERE login BETWEEN DATE_SUB(NOW(), INTERVAL 90 DAY) AND NOW() and corporate_cv_bank.company_id='$company_id' and cv_folder_relation.cv_folder_id ='$fid' GROUP by cv_id");
+    $sql = $this->db->query("SELECT * FROM corporate_cv_bank LEFT  OUTER JOIN js_info on js_info.email = corporate_cv_bank.js_email LEFT OUTER  JOIN js_login_logs on js_info.job_seeker_id = js_login_logs.job_seeker_id left OUTER join cv_folder_relation on cv_folder_relation.cv_id = corporate_cv_bank.cv_id WHERE login BETWEEN DATE_SUB(NOW(), INTERVAL 90 DAY) AND NOW() and corporate_cv_bank.company_id='$company_id' and cv_folder_relation.cv_folder_id ='$fid' GROUP by corporate_cv_bank.cv_id");
     return $sql->result();
     }
 
