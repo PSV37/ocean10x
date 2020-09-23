eeca0f3b334f9d5d175f7145d63ec7c3397f9427
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-23 10:45:47 +0530

    job_seeker_profile

--- application/views/fontend/employer/internal_tracker.php
+++ application/views/fontend/employer/internal_tracker.php
@@ -584,7 +584,7 @@ hr {
               <table class="table table-borderless" id="myTable">
                 <thead>
                   <tr>
-                    <th scope="col">Name</th>
+                    <th scope="col">Name <span class="required">*</span></th>
                     <th scope="col">Email</th>
                     <th scope="col">Mobile</th>
                     <th scope="col">Sal</th>
