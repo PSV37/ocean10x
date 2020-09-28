a79936f6bc28e7d1f0fb1bd83d15e82fc8f8d60c
unknown <santu@edunetsol.com>
2020-09-28 19:34:26 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -3151,7 +3151,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             if (!empty($_FILES['file']['name'])) {
                 // Set preference
                 $config['upload_path'] = 'question_excel/files/';
-                $config['allowed_types'] = 'csv';
+                $config['allowed_types'] = '*';
                 $config['max_size'] = '1000'; // max_size in kb
                 $config['file_name'] = $_FILES['file']['name'];
                 // Load upload library
