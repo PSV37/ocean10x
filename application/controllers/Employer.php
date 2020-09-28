d1847ea8dd5ffe38876741685f7f70573cb0f0f9
raju <raviteja.mailaram@tele-kinetics.com>
2020-09-28 16:59:10 +0530

    corp_q_bank

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -1796,10 +1796,10 @@ Team ConsultnHire!<br>Enjoy personalized job searching experience<br>Goa a Quest
         
 
         
-        $sort_val = $this->input->post('sort_val');
-        if (empty($sort_val)) {
-            $sort_val = $this->input->get('sort');
-        }
+        //$sort_val = $this->input->post('sort_val');
+        //if (empty($sort_val)) {
+          //  $sort_val = $this->input->get('sort');
+        //}
         // print_r($_GET);
         // echo $sort_val;die;
 
