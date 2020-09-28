4189b1355aed4ba9812bd03cab29b80722ae3d62
unknown <santu@edunetsol.com>
2020-09-28 15:00:01 +0530

    try

--- application/views/fontend/employer/list_questions.php
+++ application/views/fontend/employer/list_questions.php
@@ -995,6 +995,14 @@
 div#trash_append {
     margin-top: 40px;
 }
+.btn.btn-primary {
+    /* float: right; */
+    background-color: #18c5bd;
+    border: none;
+    border-radius: 35px;
+    margin-top: 13px;
+    font-size: 12px;
+}
 </style>
 <!---header-->
 <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>fontend/css/employer/questionbank.css">
@@ -1128,7 +1136,7 @@ div#trash_append {
                     <div class="col-md-6"></div>
                     <div class="col-md-6">
                       <div class="col-md-3">
-                       <input type="reset"  class="btn btn-primary">Discard
+                       <input type="reset"  class="btn btn-primary">
                       </div>
                       <div class="col-md-3" style="margin-left: 20;">
                         <button class="btn btn-primary" id="frwd_btn" type="submit">Create</button>
