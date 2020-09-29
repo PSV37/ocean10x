8c6ad76f46e840d737cf3743382b6f954bdd65dc
unknown <santu@edunetsol.com>
2020-09-29 15:44:28 +0530

    try

--- application/views/fontend/employer/create_test.php
+++ application/views/fontend/employer/create_test.php
@@ -155,6 +155,18 @@ input.btn.btn-primary {
                         <?php echo form_error('level'); ?>   
                       </div>
                     </div>
+                    <div class="col-md-4">
+                      <div class="form-group ques_type">
+                        <label for="exampleInputEmail1">Test Deadline <span class="required">*</span></label>
+                         <?php
+                        $old_date = date('d-m-Y');
+                        $next_due_date = date('d-m-Y', strtotime($old_date. ' +30 days'));
+                        
+                        ?>
+                         <input type="text" id="deadline_picker" name="test_deadline" style="display: inline-block;" class="form-control datepicker "   value="<?php echo $next_due_date; ?>">
+                        <?php echo form_error('ques_type'); ?>   
+                      </div>
+                    </div>
                   </div>
                   <div class="box-body">
                     <div class="box" >
@@ -556,6 +568,7 @@ function sum(input){
      
      $("#my_date_picker").datepicker({ dateFormat: 'yy-mm-dd',maxDate: '0' });
      $("#last_salary_hike").datepicker({ dateFormat: 'yy-mm-dd',maxDate: '0' });
+      $("#test_deadline").datepicker({ dateFormat: 'yy-mm-dd',minDate: '0' });
      });
 // $('#btn').on('click', function() {
 $("#js").validate (  
