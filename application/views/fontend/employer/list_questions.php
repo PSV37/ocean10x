d75bc214fdc46eab0518e71c8de59c8da5737152
unknown <santu@edunetsol.com>
2020-09-29 14:04:40 +0530

    try

--- application/views/fontend/employer/list_questions.php
+++ application/views/fontend/employer/list_questions.php
@@ -1054,15 +1054,15 @@ input.btn.btn-primary {
             <div class="row" style="float: right;">
               <!--  <a style="float: right;margin-right: 200px;"  href="<?php echo base_url(); ?>employer/create-test"><button type="button" id="question_add" class="btn btn-primary"><i class="fa fa-plus"> Create Test</i></button></a> -->
                <a  href="<?php echo base_url(); ?>employer/choose_questions"><i class="fa fa-plus"> </i> I will select the Questions</a>
-              <div class="btn-group-toggle" data-toggle="buttons" >
+             <!--  <div class="btn-group-toggle" data-toggle="buttons" > -->
                 <!-- <a href="#add_test" data-toggle="tab"><label class="btn btn-secondary   <?php $submenu=$this->session->userdata('submenu');  if(isset($submenu) && !empty($submenu)){if($submenu == '1' ) {echo 'active'; } }else{ echo 'active'; }?>"> -->
 
                <!--  <input type="radio" name="options" id="option1" autocomplete="off"  style="display: none;"><i class="fa fa-plus"> </i> I will select the Questions -->
                 <!-- </label></a><br> -->
-                <a href="#create_test" data-toggle="tab"><label class="btn btn-secondary  <?php $submenu=$this->session->userdata('submenu');  if(isset($submenu) && !empty($submenu)){if( $submenu == '2'   ) echo 'active'; } ?>">
+                <a href="<?php echo base_url(); ?>employer/choose_questions" data-toggle="tab">
                 <input type="radio" name="options" id="option2" autocomplete="off" style="display: none;"><i class="fa fa-plus"> </i>  Ocean can select Questions
-                </label></a>
-              </div>
+               </a>
+              <!-- </div> -->
             </div>
             <div class="row">
               <!-- <div class="btn-group-toggle" data-toggle="buttons" >
