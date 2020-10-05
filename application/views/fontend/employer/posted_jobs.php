361a7c54f94cbda16a65f35fc8756a083e382a28
unknown <santu@edunetsol.com>
2020-10-05 11:37:37 +0530

    try

--- application/views/fontend/employer/posted_jobs.php
+++ application/views/fontend/employer/posted_jobs.php
@@ -1155,7 +1155,7 @@ button.btn.btn-primary.trash {
    <label class="checkbox_label">
       <div class="border-top1"></div>
       <input type="checkbox" id='posted_job' class="posted_job" onchange="get_report_data(<?php echo $v_companyjobs['job_post_id'] ?>)" />
-      <div class="card">
+      <div class="card content">
          <div class="front">
 
 
@@ -1215,7 +1215,7 @@ button.btn.btn-primary.trash {
                 if ($v_companyjobs['is_test_required'] == 'Yes' && empty($v_companyjobs['test_for_job'])) { echo $v_companyjobs['is_test_required'];  ?>
                   <sup><span title="Marked yes but test is not attached" class="required">*</span></sup>
              <?php  } elseif ($v_companyjobs['is_test_required'] == 'Yes' && !empty($v_companyjobs['test_for_job'])) { ?>
-               <a style="margin-left: 15px" title="start test" href="<?php echo base_url() ?>employer/show_test_details/<?php echo base64_encode($v_companyjobs['test_for_job']); ?>/" >Yes</i></a>
+               <a style="margin-left: 15px" title="start test" href="<?php echo base_url() ?>employer/show_test_details/<?php echo base64_encode($v_companyjobs['test_for_job']); ?>/" >Yes</a>
             <?php }else{echo $v_companyjobs['is_test_required']; } ?> </li>
              <li class="left-title">JD attached&nbsp;<i class="fas fa-link"></i></li>
 
