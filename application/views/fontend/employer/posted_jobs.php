bc92075b48a120765f269107ba5de8fb3cf62acb
unknown <santu@edunetsol.com>
2020-10-05 10:25:34 +0530

    try

--- application/views/fontend/employer/posted_jobs.php
+++ application/views/fontend/employer/posted_jobs.php
@@ -1113,7 +1113,7 @@ button.btn.btn-primary.trash {
                
                <li class="left-title">Ocean Test</li>
                <li class="right-title">&nbsp;:<?php 
-               <?php if ($v_companyjobs['is_test_required'] == 'Yes' && empty($v_companyjobs['test_for_job'])) { echo $v_companyjobs['is_test_required'];  ?>
+                if ($v_companyjobs['is_test_required'] == 'Yes' && empty($v_companyjobs['test_for_job'])) { echo $v_companyjobs['is_test_required'];  ?>
                   <sup><span title="Marked yes but test is not attached" class="required">*</span></sup>
              <?php  } elseif ($v_companyjobs['is_test_required'] == 'Yes' && !empty($v_companyjobs['test_for_job'])) { ?>
                <a style="margin-left: 15px" title="start test" href="<?php echo base_url() ?>employer/show_test_details/<?php echo base64_encode($v_companyjobs['test_for_job']); ?>/" >Yes</i></a>
