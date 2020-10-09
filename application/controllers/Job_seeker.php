e2554e7979b293bfed4e2a23696f4146b7af1b82
unknown <santu@edunetsol.com>
2020-10-09 10:34:20 +0530

    try

--- application/controllers/Job_seeker.php
+++ application/controllers/Job_seeker.php
@@ -1842,6 +1842,7 @@ public function user_profile()
             $ques_type = 'MCQ';
             $where = "technical_id IN('".$technical_id."')  and level ='$level' and ques_type ='$ques_type' ";
         $questions = $this->Master_model->getMaster('questionbank', $where, $join = FALSE, $order = 'RANDOM', $field = 'ques_id', $select = false, $limit = 10, $start = false, $search = false);
+        print_r($this->db->last_query());die;
             $all_questions = array();
            // foreach ($oceanchamp_tests as $question) {
                 # code...
