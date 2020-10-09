1ffb2a7aac09e240ff6aa647619dd1ad89dd904c
unknown <santu@edunetsol.com>
2020-10-09 14:23:37 +0530

    try

--- application/views/fontend/jobseeker/list_skills.php
+++ application/views/fontend/jobseeker/list_skills.php
@@ -933,7 +933,7 @@
     padding: 1px 10px;
     margin-bottom: 7px;
     font-size: 11px;
-    float: right;
+    /*float: right;*/
 }
 </style>
 <div class="container-fluid main-d">
@@ -1052,8 +1052,10 @@
                     <li class="right-title">&nbsp;:<?php echo $tests['negative_marks']; ?></li>
                     <div class="clear"></div>
                   </div>
+                  <div>
                    <?php $test_id = $tests['test_id'];  ?>
                     <a style="float: right;" title="start test" href="<?php echo base_url() ?>job_seeker/ocean_test_instructions/<?php echo base64_encode($test_id) ?>/<?php echo base64_encode($apply_id); ?>/<?php echo $singlejob->job_post_id ?>" ><lable class=""><button id="sklbtn">Start Test</button></lable></a>
+                  </div><br>
                    <div class="btn-group"></div>
                  <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-ellipsis-h" aria-hidden="true"></i>
