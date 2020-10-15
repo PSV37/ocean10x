4948b0cc8ab56adff9e400e412f4e870512386ae
unknown <santu@edunetsol.com>
2020-10-15 09:57:52 +0530

    try

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1519,7 +1519,8 @@ function set_timer(n,total_slides)
                     var timeinterval = setInterval(updateClock, 1000);
                   }
                   
-                  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+                  // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+                  var deadline = <?php $oceanchamp_tests['test_duration'] ?>
                   initializeClock('clockdiv', deadline);
                </script>
               <!--  <script>
