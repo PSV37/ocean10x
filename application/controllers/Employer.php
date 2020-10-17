1a928e67d1a6705dd386fde4de222d55734c3785
unknown <santu@edunetsol.com>
2020-10-17 13:36:17 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -5917,7 +5917,7 @@ Team ConsultnHire!<br>Thank You for choosing us!<br>Goa a Question? Check out ho
             $cenvertedTime = date('Y-m-d H:i:s', strtotime('+5 hour +30 minutes', strtotime($created_on)));
                 $avg_time = array();
              $answers_selected = $this->input->post('answers_selected');
-             $answers_selected = explode(',', $answers_selected)
+             $answers_selected = explode(',', $answers_selected);
              $ans=array();
              foreach ($answers_selected as $row) {
              
