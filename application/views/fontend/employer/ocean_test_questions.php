4757b42b3670cbc348f8a8a7244437d616bee9fb
unknown <santu@edunetsol.com>
2020-10-17 12:11:37 +0530

    try

--- application/views/fontend/employer/ocean_test_questions.php
+++ application/views/fontend/employer/ocean_test_questions.php
@@ -1280,7 +1280,7 @@ var ans_selected = [];
           // })();
          
             var nval =[];
-            var ans_selected =new Array();
+            var ans_selected =[];
           function get_checked(n,option)
           {
             if(ans_selected.length==0)
