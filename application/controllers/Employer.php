d0bf3d3d85440a7ca0d61f68fe55d74804b0aeec
unknown <santu@edunetsol.com>
2020-10-23 11:19:36 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6788,7 +6788,12 @@ public  function upload_folder()
             // $string = preg_replace('/[^A-Za-z0-9\-]/', '', $name);
              $string = preg_replace('/\\.[^.\\s]{3,4}$/', '', $name);
              // echo "string";
-            print_r($folder_path_final.'/'.$name);
+
+            print_r($folder_path_final
+                .$name);
+            echo "<br>";
+            print_r(file_get_contents($folder_path_final
+                .$name));
             die;
              // echo $string;
             $fileName = 'data-' . $today . '.xlsx';
