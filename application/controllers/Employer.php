1a9da0e19b52c41419f1752f02156ff42a7b8608
unknown <santu@edunetsol.com>
2020-10-26 17:11:12 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6734,7 +6734,7 @@ public  function upload_folder()
                 }
                  $names = implode('/', $folder_struct);
                  
-                if(!file_exists('cv_folder/' . $names . '/' . $folder_name)) 
+                if(file_exists('cv_folder/' . $names . '/' . $folder_name) == '1') 
                 {
                  mkdir('cv_folder/' . $names . '/' . $folder_name, 0777, true);
                 }
