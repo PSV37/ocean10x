1b00ce7b79bf0051558ea0340327b31f6dc4b242
unknown <santu@edunetsol.com>
2020-10-27 17:55:25 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6862,6 +6862,7 @@ $outtext  = $pdf->getText();
         header('Cache-Control: max-age=0');
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'CSV');
       $name = 'cv_bank_excel/files/'.$filename;
+      print_r($name);die;
         $objWriter->save($name);
         // $objWriter->save('php://output');
         // $ext = strtolower(end(explode('.', $filename)));
