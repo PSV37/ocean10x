302eb1b9ae5cf2261372eb09f7d974c9ef0dabb1
unknown <santu@edunetsol.com>
2020-10-27 17:46:28 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6885,33 +6885,33 @@ $outtext  = $pdf->getText();
         // {
         //   $uploadData = $this->upload->data();
         //   $filename = $uploadData['file_name'];
-          $file = fopen($filename, "r");
-          $i = 0;
-          $importData_arr = array();
-          while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) 
-          {
-            $num = count($filedata);
-            for ($c = 0;$c < $num;$c++) 
-            {
-             $importData_arr[$i][] = $filedata[$c];
-            }
-            $i++;
-           }
-           fclose($file);
-           $skip = 0;
-           $cv = array();
-           foreach ($importData_arr as $userdata) 
-           {
-             if ($skip != 0) 
-             {
-               $cv_id = $this->Questionbank_employer_model->InsertCVData($userdata);
-               $company_name = $this->session->userdata('company_name');
-               $data = array('company' => $company_name, 'action_taken_for' => $this->session->userdata('company_name'), 'field_changed' => 'Imported CVs', 'Action' => 'Imported Multiple CVs', 'datetime' => date('Y-m-d H:i:s'), 'updated_by' => $company_name);
-               $result = $this->Master_model->master_insert($data, 'employer_audit_record');
-                  array_push($cv, $cv_id);
-               }
+        //   $file = fopen("cv_bank_excel/files/" . $filename, "r");
+        //   $i = 0;
+        //   $importData_arr = array();
+        //   while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) 
+        //   {
+        //     $num = count($filedata);
+        //     for ($c = 0;$c < $num;$c++) 
+        //     {
+        //      $importData_arr[$i][] = $filedata[$c];
+        //     }
+        //     $i++;
+        //    }
+        //    fclose($file);
+        //    $skip = 0;
+        //    $cv = array();
+        //    foreach ($importData_arr as $userdata) 
+        //    {
+        //      if ($skip != 0) 
+        //      {
+        //        $cv_id = $this->Questionbank_employer_model->InsertCVData($userdata);
+        //        $company_name = $this->session->userdata('company_name');
+        //        $data = array('company' => $company_name, 'action_taken_for' => $this->session->userdata('company_name'), 'field_changed' => 'Imported CVs', 'Action' => 'Imported Multiple CVs', 'datetime' => date('Y-m-d H:i:s'), 'updated_by' => $company_name);
+        //        $result = $this->Master_model->master_insert($data, 'employer_audit_record');
+        //           array_push($cv, $cv_id);
+        //        }
         //         $skip++;
-           }
+        //    }
         //   } 
         //   else
         //   {
