17098c4c7e40fd864b536f7479258371d9a2adff
unknown <santu@edunetsol.com>
2020-10-29 11:15:58 +0530

    try

--- application/controllers/Employer.php
+++ application/controllers/Employer.php
@@ -6959,20 +6959,20 @@ $outtext  = $pdf->getText();
                print_r($previous_folder);
                // print_r($doc_path);
 
-         //       // $join =array("cv_folder a"=>
-         //       //      "a.parent_id = cv_folder.id");
-         //       // if ($parent == $previous_folder) {
-         //       //     $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id'";
-         //       // }
-         //       // else
-         //       // {
+               $join =array("cv_folder a"=>
+                    "a.parent_id = cv_folder.id");
+               if ($parent == $previous_folder) {
+                   $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id'";
+               }
+               else
+               {
 
-         //       //  $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id' and a.folder_name = ' $parent'
-         //       //  ";
-         //       // }
+                $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id' and a.folder_name = ' $parent'
+                ";
+               }
                
-         //       // $parent = $this->Master_model->get_master_row('cv_folder', $select = 'id', $where = $where_folder, $join);
-         //       // print_r($parent);
+               $parent = $this->Master_model->get_master_row('cv_folder', $select = 'id', $where = $where_folder, $join);
+               print_r($this->db->last_query());
          //       // $previous_folder = $folders[$q];
          //       $where_folder = "cv_folder.folder_name = '$previous_folder' and company_id = '$employer_id'";
          //       $parent = $this->Master_model->get_master_row('cv_folder', $select = 'id', $where = $where_folder, $join = FALSE);
